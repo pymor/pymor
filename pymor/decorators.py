b00a64b80bf9431aaa42775f3c4f43c4b8508fa7
Rene Milk <rene.milk@uni-muenster.de>
2012-11-02 14:49:17 +0100

    adds a tweaked implementation of pycontract's @contract wrapper

--- pymor/decorators.py
+++ pymor/decorators.py
@@ -65,3 +65,115 @@ class Deprecated(DecoratorBase):
 			return func(*args, **kwargs)
 		return new_func
 
+def contract(*arg, **kwargs):
+    '''
+        Decorator for adding contracts to functions.
+
+        It is smart enough to support functions with variable number of
+        arguments and keyword arguments.
+
+        There are three ways to specify the contracts. In order of precedence:
+
+        - As arguments to this decorator. For example: ::
+
+              @contract(a='int,>0',b='list[N],N>0',returns='list[N]')
+              def my_function(a, b):
+                  # ...
+                  pass
+
+        - As annotations (supported only in Python 3): ::
+
+              @contract
+              def my_function(a:'int,>0', b:'list[N],N>0') -> 'list[N]':
+                  # ...
+                  pass
+
+        - Using ``:type:`` and ``:rtype:`` tags in the function's docstring: ::
+
+              @contract
+              def my_function(a, b):
+                  """ Function description.
+                      :type a: int,>0
+                      :type b: list[N],N>0
+                      :rtype: list[N]
+                  """
+                  pass
+
+        **Signature and docstrings**: The signature of the decorated
+        function is conserved. By default, the docstring is modified
+        by adding ``:type:`` and ``:rtype:`` definitions. To avoid that,
+        pass ``modify_docstring=False`` as a parameter.
+
+
+        **Contracts evaluation**: Note that all contracts for the arguments
+        and the return values
+        are evaluated in the same context. This make it possible to use
+        common variables in the contract expression. For example, in the
+        example above, the return value is constrained to be a list of the same
+        length (``N``) as the parameter ``b``.
+
+        **Using docstrings** Note that, by convention, those annotations must
+        be parseable as RestructuredText. This is relevant if you are using
+        Sphinx.
+        If the contract string has special RST characters in it, like ``*``,
+        you can include it in double ticks. |pycontracts| will remove
+        the double ticks before interpreting the string.
+
+        For example, the two annotations in this docstring are equivalent
+        for |pycontracts|, but the latter is better for Sphinx: ::
+
+              """ My function
+
+                  :param a: First parameter
+                  :type a: list(tuple(str,*))
+
+                  :param b: First parameter
+                  :type b: ``list(tuple(str,*))``
+              """
+
+        :raise: ContractException, if arguments are not coherent
+        :raise: ContractSyntaxError
+    '''
+
+    #this bit tags function as decorated
+    def tag_and_decorate(function, **kwargs):
+        cargs = copy.deepcopy(kwargs)
+        function.contract_kwargs = cargs or dict()
+        function.decorated = True
+        @functools.wraps(function)
+        def __functools_wrap(function, **kwargs):
+            return contracts.main.contracts_decorate(function, **kwargs)
+        return __functools_wrap(function, **kwargs)
+
+    # OK, this is black magic. You are not expected to understand this.
+    if arg:
+        if isinstance(arg[0], types.FunctionType):
+            # We were called without parameters
+            function = arg[0]
+            if contracts.all_disabled():
+                return function
+            try:
+                return tag_and_decorate(function, **kwargs)
+            except contracts.ContractSyntaxError as e:
+                # Erase the stack
+                raise contracts.ContractSyntaxError(e.error, e.where)
+        else:
+            msg = ('I expect that  contracts() is called with '
+                    'only keyword arguments (passed: %r)' % arg)
+            raise contracts.ContractException(msg)
+    else:
+        # We were called *with* parameters.
+        if contracts.all_disabled():
+            def tmp_wrap(function):
+                return function
+        else:
+            def tmp_wrap(function):
+                try:
+                    return tag_and_decorate(function, **kwargs)
+                except contracts.ContractSyntaxError as e:
+                    # Erase the stack
+                    raise contracts.ContractSyntaxError(e.error, e.where)
+        return tmp_wrap
+
+#alias this so we need no contracts import outside this module
+contracts_decorate = contracts.main.contracts_decorate
