2d9e3bfa0272ffc36fd07b2a7bdf0ca15c399858
Felix Albrecht <felix.albrecht@uni-muenster.de>
2012-11-12 21:48:07 +0100

    [common.function.constant] added name()

--- src/pymor/common/function/constant.py
+++ src/pymor/common/function/constant.py
@@ -2,7 +2,13 @@
 from .interfaces import Interface
 
 class Constant(Interface):
-    def __init__(self, a):
-        self._a = a
+
+    def __init__(self, value, name='common.function.constant'):
+        self._value = value
+        self._name = name
+
     def evaluate(self, x):
-        return self._a
+        return self._value
+
+    def name(self, x):
+        return self._name
