292c905a50eaf41d5211c7786558592e2461950f
Felix Albrecht <felix.albrecht@uni-muenster.de>
2012-11-15 01:14:29 +0100

    [problem...elliptic.analytical] minor change

--- src/pymor/problem/stationary/linear/elliptic/analytical.py
+++ src/pymor/problem/stationary/linear/elliptic/analytical.py
@@ -1,13 +1,12 @@
+#!/usr/bin/env python
 
 from __future__ import print_function
-
 import abc
-
-from pymor import core
+import pymor.core
 from pymor.common import function
 
 
-class Interface(core.BaseInterface):
+class Interface(pymor.core.BaseInterface):
 
     def name(self):
         return 'problem.stationary.linear.elliptic'
@@ -33,10 +32,10 @@ class Default(Interface):
 
     def __init__(self, name='problem.stationary.linear.elliptic.default'):
         self._name = name
-        self._diffusion = function.Constant(1, 'diffusion')
-        self._force = function.Constant(1, 'force')
-        self._dirichlet = function.Constant(0, 'dirichlet')
-        self._neumann = function.Constant(0, 'neumann')
+        self._diffusion = function.nonparametric.Constant(1, 'diffusion')
+        self._force = function.nonparametric.Constant(1, 'force')
+        self._dirichlet = function.nonparametric.Constant(0, 'dirichlet')
+        self._neumann = function.nonparametric.Constant(0, 'neumann')
 
     def name(self):
         return self._name
