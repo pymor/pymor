a366e9e22b52b40a074516105f6cbaad57cd37d0
Felix Albrecht <felix.albrecht@uni-muenster.de>
2012-11-15 01:15:08 +0100

    [grid.oned] added simple 1d grid

---
+++ src/pymor/grid/oned.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+from __future__ import division
+import numpy as np
+from pymor import core
+
+
+class Oned(core.BaseInterface):
+
+    dim = 1
+
+    def __init__(self, interval=np.array((0, 1)), size=4):
+        self._interval = interval
+        self._size = size
+        self._width = np.abs(self._interval[1] - self._interval[0]) / self._size
+        self._centers = self._interval[0] + self._width * (np.arange(self._size) + 0.5)
+        self._vertices = self._interval[0] + self._width * np.arange(self._size + 1)
+        self._volumes_codim_0 = self._width * np.ones((1, self._size))
+        self._volumes_codim_1 = np.ones((1, self._size))
+
+    def name(self):
+        return 'grid.oned'
+
+    def size(self, codim=0):
+        if codim == 0:
+            return self._size
+        elif codim == 1:
+            return self._size + 1
+        else:
+            raise ValueError('in pymor.' + self.name() + ': codim has to be between 0 and ' + self.dim + '!')
+
+    def centers(self, codim=0):
+        '''
+        Since vectors in pymor are column vectors,
+        centers(codim) returns a matrix of size (dim - codim) x size(codim)
+        '''
+        if codim == 0:
+            return self._centers
+        elif codim == 1:
+            return self._vertices
+        else:
+            raise ValueError('in pymor.' + self.name() + ': codim has to be between 0 and ' + self.dim + '!')
+
+    def volumes(self, codim=0):
+        if codim == 0:
+            return self._volumes_codim_0
+        elif codim == 1:
+            return self._volumes_codim_1
+        else:
+            raise ValueError('in pymor.' + self.name() + ': codim has to be between 0 and ' + self.dim + '!')
+
+
+if __name__ == '__main__':
+    g = Oned()
+    print(g.centers(0))
+    print(g.centers(1))
+    print(g.volumes(0))
+    print(g.volumes(1))
