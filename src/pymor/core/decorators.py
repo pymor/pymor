543ef02579ef5d25186066965aeebd8e6bd95b94
Rene Milk <rene.milk@uni-muenster.de>
2012-11-16 16:45:56 +0100

    [core] cleanup whitespace in decorators

--- src/pymor/core/decorators.py
+++ src/pymor/core/decorators.py
@@ -15,55 +15,54 @@ import contracts
 import copy
 
 def _is_decorated(func):
-	return 'decorated' in dir(func)
+    return 'decorated' in dir(func)
 
 
 class DecoratorBase(object):
-	"""A base for all decorators that does the common automagic"""
-	def __init__(self, func):
-		functools.wraps(func)(self)
-		func.decorated = self
-		self.func = func
-		assert _is_decorated(func)
+    """A base for all decorators that does the common automagic"""
+    def __init__(self, func):
+        functools.wraps(func)(self)
+        func.decorated = self
+        self.func = func
+        assert _is_decorated(func)
 
-	def __get__(self, obj, ownerClass=None):
-		# Return a wrapper that binds self as a method of obj (!)
-		self.obj = obj
-		return types.MethodType(self, obj)
-
+    def __get__(self, obj, ownerClass=None):
+        # Return a wrapper that binds self as a method of obj (!)
+        self.obj = obj
+        return types.MethodType(self, obj)
 
 class DecoratorWithArgsBase(object):
-	"""A base for all decorators with args that sadly can do little common automagic"""
-	def mark(self, func):
-		functools.wraps(func)
-		func.decorated = self
+    """A base for all decorators with args that sadly can do little common automagic"""
+    def mark(self, func):
+        functools.wraps(func)
+        func.decorated = self
 
-	def __get__(self, obj, ownerClass=None):
-		# Return a wrapper that binds self as a method of obj (!)
-		self.obj = obj
-		return types.MethodType(self, obj)
+    def __get__(self, obj, ownerClass=None):
+        # Return a wrapper that binds self as a method of obj (!)
+        self.obj = obj
+        return types.MethodType(self, obj)
 
 
 class Deprecated(DecoratorBase):
-	"""This is a decorator which can be used to mark functions
-	as deprecated. It will result in a warning being emitted
-	when the function is used.
-	"""
+    """This is a decorator which can be used to mark functions
+    as deprecated. It will result in a warning being emitted
+    when the function is used.
+    """
 
-	def __init__(self,alt='no alternative given'):
-		self._alt = alt
+    def __init__(self, alt='no alternative given'):
+        self._alt = alt
 
-	def __call__(self,func):
-		func.decorated = self
-		@functools.wraps(func)
-		def new_func(*args, **kwargs):
-			frame = inspect.currentframe().f_back
-			msg = "DeprecationWarning. Call to deprecated function %s in %s:%s\nUse %s instead" % (
-							func.__name__,frame.f_code.co_filename,
-							frame.f_code.co_firstlineno,self._alt)
-			logging.warning(msg)
-			return func(*args, **kwargs)
-		return new_func
+    def __call__(self, func):
+        func.decorated = self
+        @functools.wraps(func)
+        def new_func(*args, **kwargs):
+            frame = inspect.currentframe().f_back
+            msg = "DeprecationWarning. Call to deprecated function %s in %s:%s\nUse %s instead" % (
+                            func.__name__, frame.f_code.co_filename,
+                            frame.f_code.co_firstlineno, self._alt)
+            logging.warning(msg)
+            return func(*args, **kwargs)
+        return new_func
 
 def contract(*arg, **kwargs):
     '''
@@ -135,7 +134,7 @@ def contract(*arg, **kwargs):
         :raise: ContractSyntaxError
     '''
 
-    #this bit tags function as decorated
+    # this bit tags function as decorated
     def tag_and_decorate(function, **kwargs):
         @functools.wraps(function)
         def __functools_wrap(function, **kwargs):
@@ -176,7 +175,7 @@ def contract(*arg, **kwargs):
                     raise contracts.ContractSyntaxError(e.error, e.where)
         return tmp_wrap
 
-#alias this so we need no contracts import outside this module
+# alias this so we need no contracts import outside this module
 contracts_decorate = contracts.main.contracts_decorate
 
 def contains_contract(string):
