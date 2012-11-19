cdd6efeb10e9329a3ad1f5425d4584a3f6b80938
Felix Albrecht <mail@felixalbrecht.de>
2012-11-19 17:01:21 +0100

    [core.warnings] merged into exceptions

--- src/pymor/core/exceptions.py
+++ src/pymor/core/exceptions.py
@@ -6,14 +6,25 @@ Created on Fri Nov  9 17:28:14 2012
 """
 
 import contracts
+import warnings
 
+warn = warnings.warn
 ContractNotRespected = contracts.ContractNotRespected
+
 
 class ConstError(Exception):
     '''I get thrown when you try to add a new member to
     a locked class instance'''
     pass
 
+
 class CodimError(Exception):
     '''Is raised if an invalid codimension index is unsed.'''
+    pass
+
+
+class CallOrderWarning(UserWarning):
+    '''I am raised when there's a preferred call order, but the user didn't follow it.
+    For an Example see pymor.discretizer.stationary.elliptic.cg
+    '''
     pass
