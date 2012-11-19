afb3f92feffc827daa2b06779d776d747ce4d457
Felix Albrecht <mail@felixalbrecht.de>
2012-11-19 23:02:02 +0100

    [common.boundaryinfo.oned] some cleanup

--- src/pymor/common/boundaryinfo/oned.py
+++ src/pymor/common/boundaryinfo/oned.py
@@ -1,27 +1,55 @@
 #!/usr/bin/env python
 
-from pymor import core
+# only for name == main
+from __future__ import print_function
 
-class Interface(core.BasicInterface):
-    
-    id = 'common.boundaryinfo'
-    
+# pymor
+from pymor.core import interfaces
+
+
+class Interface(interfaces.BasicInterface):
+
+    id = 'common.boundaryinfo.oned'
+
     def __init__(self, left='dirichlet', right='dirichlet'):
         assert (left == 'dirichlet' or left == 'neumann')
         assert (right == 'dirichlet' or right == 'neumann')
         self._left = left
         self._right = right
-    
+
     def __str__(self):
-        return 'implement me'
-    
+        return id
+
     def left(self):
         return self._left
-    
+
     def right(self):
         return self._right
 
 
 class AllDirichlet(Interface):
-    
+
     id = Interface.id + '.alldirichlet'
+
+
+class AllNeumann(Interface):
+
+    id = Interface.id + '.allneumann'
+
+    def __init__(self):
+        Interface.__init__(self, 'neumann', 'neumann')
+
+
+if __name__ == '__main__':
+    print('testing ', end='')
+    dirichlet = AllDirichlet()
+    print('{id}... '.format(id=dirichlet.id), end='')
+    assert dirichlet.left() == 'dirichlet'
+    assert dirichlet.right() == 'dirichlet'
+    print('done')
+    print('testing ', end='')
+    neumann = AllNeumann()
+    print('{id}... '.format(id=neumann.id), end='')
+    assert neumann.left() == 'neumann'
+    assert neumann.right() == 'neumann'
+    print('done')
