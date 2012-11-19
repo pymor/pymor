ece4ef441f7d34e7214b1b68ba9d3e803abdc102
Felix Albrecht <mail@felixalbrecht.de>
2012-11-19 15:21:39 +0100

    [common.discreteoperator.stationary.linear] some cleanup

--- src/pymor/common/discreteoperator/stationary/linear.py
+++ src/pymor/common/discreteoperator/stationary/linear.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python
 
-from __future__ import print_function, division
-import pymor.core
-import scipy.sparse
+from __future__ import print_function
+from pymor.core import interfaces
 
 
-class Interface(pymor.core.BasicInterface):
-    
+class Interface(interfaces.BasicInterface):
+
     id = 'common.discreteoperator.stationary.linear'
-    
+    size = (0, 0)
+
     def apply(self):
         pass
-    
+
     def __str__(self):
-        return self.id
+        return '{id}, size {rows}x{cols}'.format(id=self.id,
+                                                 rows=self.size[0],
+                                                 cols=self.size[1])
+
 
 class ScipySparse(Interface):
-    
+
     id = Interface.id + '.scipysparse'
-    
+
     def __init__(self, matrix):
         '''
         here should be a contract to check if matrix is a scipy.sparse
