b54c81d1a9e09d2b32e04326d25ecb8262bf556e
Stephan Rave <stephanrave@uni-muenster.de>
2012-11-20 03:45:12 +0100

    [grid.oned] made an ISimpleAffineGrid

--- src/pymor/grid/oned.py
+++ src/pymor/grid/oned.py
@@ -11,23 +11,23 @@ import numpy as np
 
 # pymor
 from pymor.core.exceptions import CodimError
-from pymor.grid.interfaces import IConformalTopologicalGrid
+from pymor.grid.interfaces import ISimpleAffineGrid
+from .referenceelements import line
 
 
-class Oned(IConformalTopologicalGrid):
+class Oned(ISimpleAffineGrid):
 
     dim = 1
     id = 'grid.oned'
 
     def __init__(self, interval=np.array((0, 1)), size=4):
+        self.reference_element = line
         self._interval = interval
         self._size = size
         self._width = np.abs(self._interval[1] - self._interval[0]) / self._size
-        self._centers = self._interval[0] + self._width * (np.arange(self._size) + 0.5)
-        self._vertices = self._interval[0] + self._width * np.arange(self._size + 1)
-        self._volumes_codim_0 = self._width * np.ones((1, self._size))
-        self._volumes_codim_1 = np.ones((1, self._size))
         self._subentities = np.vstack((np.arange(self._size), np.arange(self._size) + 1))
+        self._A = np.ones(self._size)[:, np.newaxis, np.newaxis] * self._width
+        self._B = (self._interval[0] + self._width * (np.arange(self._size)))[:, np.newaxis]
 
     def __str__(self):
         return (self.id + ', domain [{xmin},{xmax}]'
@@ -51,25 +51,11 @@ class Oned(IConformalTopologicalGrid):
         assert subentity_codim is None or subentity_codim == 1, CodimError('Invalid subentity codimension')
         return self._subentities.T
 
-    def centers(self, codim=0):
-        '''
-        Since vectors in pymor are column vectors,
-        centers(codim) returns a matrix of size (dim - codim) x size(codim)
-        '''
+    def embeddings(self, codim=0):
         if codim == 0:
-            return self._centers
-        elif codim == 1:
-            return self._vertices
+            return self._A, self._B
         else:
-            raise CodimError('in pymor.' + self.name() + ': codim has to be between 0 and ' + self.dim + '!')
-
-    def volumes(self, codim=0):
-        if codim == 0:
-            return self._volumes_codim_0
-        elif codim == 1:
-            return self._volumes_codim_1
-        else:
-            raise CodimError('in pymor.' + self.name() + ': codim has to be between 0 and ' + self.dim + '!')
+            return super(Oned, self).embeddings(codim)
 
 
 if __name__ == '__main__':
