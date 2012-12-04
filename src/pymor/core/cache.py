804fb82d436bec7f1f637890203d424eff38dd16
Stephan Rave <stephanrave@uni-muenster.de>
2012-12-04 19:28:29 +0100

    [core.cache] a little timing and logging of cached function calls

--- src/pymor/core/cache.py
+++ src/pymor/core/cache.py
@@ -1,5 +1,10 @@
-from functools import wraps
+import logging
+import functools
 
+from .timing import Timer
+
+cache_miss_nesting = 0
+cache_dt_nested = []
 
 def cached(user_function):
     '''Our cache decorator. The current implementation is copied from
@@ -11,8 +16,10 @@ def cached(user_function):
 
     cache = dict()              # simple cache without ordering or size limit
 
-    @wraps(user_function)
+    @functools.wraps(user_function)
     def wrapper(*args, **kwds):
+        global cache_miss_nesting
+        global cache_dt_nested
         key = args
         if kwds:
             key += kwd_mark + tuple(sorted(kwds.items()))
@@ -22,7 +29,30 @@ def cached(user_function):
             return result
         except KeyError:
             pass
+        if len(cache_dt_nested) <= cache_miss_nesting:
+            cache_dt_nested.append(0)
+        msg = '|  ' * cache_miss_nesting + '/ CACHE MISS calling {}({},{})'.format(user_function.__name__, str(args), str(kwds))
+        logging.info(msg)
+
+        cache_miss_nesting += 1
+        timer = Timer('')
+        timer.start()
         result = user_function(*args, **kwds)
+        timer.stop()
+        cache_miss_nesting -= 1
+
+        if len(cache_dt_nested) > cache_miss_nesting + 1:
+            dt_nested = cache_dt_nested[cache_miss_nesting + 1]
+            cache_dt_nested[cache_miss_nesting + 1] = 0
+        else:
+            dt_nested = 0
+        cache_dt_nested[cache_miss_nesting] += timer.dt
+
+        msg = '|  ' * cache_miss_nesting + '\ call took {}s (own code:{}s, nested cache misses:{}s)'.format(timer.dt,
+                timer.dt - dt_nested, dt_nested)
+        logging.info(msg)
+        if cache_miss_nesting == 0:
+            logging.info('')
         cache[key] = result
         misses[0] += 1
         return result
