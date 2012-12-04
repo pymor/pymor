20bf263d82069d2ddfa14c39cb65017f1ca3c68e
Stephan Rave <stephanrave@uni-muenster.de>
2012-12-04 15:55:51 +0100

    [discreteoperator.cg.diffusion] added non-constant diffusion coeff

--- src/pymor/common/discreteoperator/cg/diffusion.py
+++ src/pymor/common/discreteoperator/cg/diffusion.py
@@ -14,11 +14,15 @@ class DiffusionOperatorP1D2(ILinearDiscreteOperator):
     constant diffusion coefficient of value 1. Add more functionality later ...
     '''
 
-    def __init__(self, grid, boundary_info, clear_columns=True):
+    def __init__(self, grid, boundary_info, diffusion_function=None, diffusion_constant=None,
+                 dirichlet_clear_columns=True, dirichlet_clear_diag=False):
         assert grid.reference_element(0) == triangle, ValueError('A triangular grid is expected!')
         self.grid = grid
         self.boundary_info = boundary_info
-        self.clear_columns = clear_columns
+        self.diffusion_constant = diffusion_constant
+        self.diffusion_function = diffusion_function
+        self.dirichlet_clear_columns = dirichlet_clear_columns
+        self.dirichlet_clear_diag = dirichlet_clear_diag
 
     def assemble(self, mu=np.array([])):
         assert mu.size == self.parameter_dim,\
@@ -36,7 +40,14 @@ class DiffusionOperatorP1D2(ILinearDiscreteOperator):
         SF_GRADS = np.einsum('eij,pj->epi', g.jacobian_inverse_transposed(0), SF_GRAD)
 
         print('Calculate all local scalar products beween gradients ...')
-        SF_INTS = np.einsum('epi,eqi,e->epq', SF_GRADS, SF_GRADS, g.volumes(0)).ravel()
+        if self.diffusion_function is not None:
+            D = self.diffusion_function(self.grid.centers(0))
+            SF_INTS = np.einsum('epi,eqi,e,e->epq', SF_GRADS, SF_GRADS, g.volumes(0), D).ravel()
+        else:
+            SF_INTS = np.einsum('epi,eqi,e->epq', SF_GRADS, SF_GRADS, g.volumes(0)).ravel()
+
+        if self.diffusion_constant is not None:
+            SF_INTS *= self.diffusion_constant
 
         print('Determine global dofs ...')
         SF_I0 = np.repeat(g.subentities(0, 2), 3, axis=1).ravel()
@@ -45,12 +56,14 @@ class DiffusionOperatorP1D2(ILinearDiscreteOperator):
         print('Boundary treatment ...')
         if bi.has_dirichlet:
             SF_INTS = np.where(bi.dirichlet_mask(2)[SF_I0], 0, SF_INTS)
-            if self.clear_columns:
+            if self.dirichlet_clear_columns:
                 SF_INTS = np.where(bi.dirichlet_mask(2)[SF_I1], 0, SF_INTS)
 
-        SF_INTS = np.hstack((SF_INTS, np.ones(bi.dirichlet_boundaries(2).size)))
-        SF_I0 = np.hstack((SF_I0, bi.dirichlet_boundaries(2)))
-        SF_I1 = np.hstack((SF_I1, bi.dirichlet_boundaries(2)))
+        if not self.dirichlet_clear_diag:
+            SF_INTS = np.hstack((SF_INTS, np.ones(bi.dirichlet_boundaries(2).size)))
+            SF_I0 = np.hstack((SF_I0, bi.dirichlet_boundaries(2)))
+            SF_I1 = np.hstack((SF_I1, bi.dirichlet_boundaries(2)))
+
 
         print('Assemble system matrix ...')
         A = coo_matrix((SF_INTS, (SF_I0, SF_I1)), shape=(g.size(2), g.size(2)))
