06327928fdc6a2427c5d83b8dab7aedb50a489da
Stephan Rave <stephanrave@uni-muenster.de>
2012-12-05 17:21:01 +0100

    [demos] updated cg and cg2

--- src/pymor/demos/cg.py
+++ src/pymor/demos/cg.py
@@ -8,62 +8,44 @@ import sys
 
 import numpy as np
 import matplotlib.pyplot as pl
-from scipy.sparse import coo_matrix, csr_matrix
 from scipy.sparse.linalg import bicg
 
 from pymor.grid.tria import Tria
-from pymor.common.boundaryinfo import AllDirichletZero
-from pymor.common.discreteoperator.cg import DiffusionOperatorP1D2
+from pymor.common.boundaryinfo import AllDirichlet
+from pymor.common.discreteoperator.cg import DiffusionOperatorP1D2, L2ProductFunctionalP1D2
 
-if len(sys.argv) < 2:
-    sys.exit('Usage: %s PROBLEM-NUMBER'.format(sys.argv[0]))
+if len(sys.argv) < 3:
+    sys.exit('Usage: %s RHS-NUMBER BOUNDARY-NUMBER'.format(sys.argv[0]))
+
+rhs0 = lambda X: np.ones(X.shape[0]) * 10
+rhs1 = lambda X: (X[:, 0] - 0.5) ** 2 * 1000
+dirichlet0 = lambda X: np.zeros(X.shape[0])
+dirichlet1 = lambda X: np.ones(X.shape[0])
+dirichlet2 = lambda X: X[:, 0]
 
 nrhs = int(sys.argv[1])
+assert 0 <= nrhs <= 1, ValueError('Invalid rhs number.')
+rhs = eval('rhs{}'.format(nrhs))
 
-
-def rhs0(x):
-    return np.ones(x.shape[0]) * 10
-
-
-def rhs1(x):
-    return (x[:, 0] - 0.5) ** 2 * 1000
-
-if nrhs == 0:
-    rhs = rhs0
-elif nrhs == 1:
-    rhs = rhs1
-else:
-    raise ValueError('Invalid problem number!')
+ndirichlet = int(sys.argv[2])
+assert 0 <= ndirichlet <= 2, ValueError('Invalid boundary number.')
+dirichlet = eval('dirichlet{}'.format(ndirichlet))
 
 for n in [32, 64, 128, 256]:
     print('Solving on Tria(({0},{0}))'.format(n))
 
-    print('Generate grid ...')
+    print('Setup grid ...')
     g = Tria((n, n))
+    bi = AllDirichlet(g, dirichlet)
 
-    # the following calls are only here to seperate all
-    # grid calculations (which are currentily terribly slow)
-    # from the rest of the work
-    print('Calculate grid data ...')
-    g.subentities(0, 2)
-    g.centers(2)
-    g.jacobian_inverse_transposed(0)
-    g.volumes(0)
-    g.boundaries(2)
-    g.boundary_mask(2)
-
-    bi = AllDirichletZero(g)
-
-    print('Assemble right hand side ...')
-    F = rhs(g.centers(2)) * g.integration_element(0)[0]
-    F[bi.dirichlet_boundaries(2)] = 0
-
-    print('Assemble system matrix ...')
+    print('Assemble operators ...')
+    F = L2ProductFunctionalP1D2(g, bi, rhs)
     L = DiffusionOperatorP1D2(g, bi)
+    RHS = F.matrix()
     A = L.matrix()
 
     print('Solve ...')
-    U, info = bicg(A, F)
+    U, info = bicg(A, RHS)
 
     print('Plot ...')
     pl.tripcolor(g.centers(2)[:, 0], g.centers(2)[:, 1], g.subentities(0, 2), U)
