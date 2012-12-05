c05ef0a3880d44bcacc20f8039a3b00b4e63fc14
Stephan Rave <stephanrave@uni-muenster.de>
2012-12-05 01:43:50 +0100

    [la.algorithms.inverse] added vectorized 2-by-2 matrix inverse

---
+++ src/pymor/la/algorithms/inverse.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+
+def inv_two_by_two(A):
+    '''retval[i1,...,ik,m,n] = numpy.linalg.inv(A[i1,...,ik,:,:])'''
+
+    assert A.shape[-1] == A.shape[-2] == 2, ValueError('Wrong shape of argmument.')
+
+    D = A[..., 0, 0] * A[..., 1, 1] - A[..., 1, 0] * A[..., 0, 1]
+    D = 1 / D
+
+    INV = np.empty_like(A)
+    INV[..., 0, 0] = A[..., 1, 1]
+    INV[..., 1, 1] = A[..., 0, 0]
+    INV[..., 1, 0] = - A[..., 1, 0]
+    INV[..., 0, 1] = - A[..., 0, 1]
+    INV *= D[..., np.newaxis, np.newaxis]
+
+    return INV
+
+def inv_transposed_two_by_two(A):
+    '''retval[i1,...,ik,m,n] = numpy.linalg.inv(A[i1,...,ik,:,:])'''
+
+    assert A.shape[-1] == A.shape[-2] == 2, ValueError('Wrong shape of argmument.')
+
+    D = A[..., 0, 0] * A[..., 1, 1] - A[..., 1, 0] * A[..., 0, 1]
+    D = 1 / D
+
+    INV = np.empty_like(A)
+    INV[..., 0, 0] = A[..., 1, 1]
+    INV[..., 1, 1] = A[..., 0, 0]
+    INV[..., 1, 0] = - A[..., 0, 1]
+    INV[..., 0, 1] = - A[..., 1, 0]
+    INV *= D[..., np.newaxis, np.newaxis]
+
+    return INV
