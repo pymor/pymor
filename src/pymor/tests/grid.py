7d1203af7651cffc78f27cf8d50c6a4b5b6ef8e0
Stephan Rave <stephanrave@uni-muenster.de>
2012-12-06 18:24:17 +0100

    [tests.grid] a few more tests

--- src/pymor/tests/grid.py
+++ src/pymor/tests/grid.py
@@ -42,18 +42,112 @@ class ConformalTopologicalGridTest(unittest.TestCase):
             with self.assertRaises(AssertionError):
                 g.size(g.dim + 1)
 
-    def test_subentitites(self):
+    def test_subentities_wrong_arguments(self):
+        for g in self.grids:
+            for e in xrange(g.dim + 1):
+                with self.assertRaises(AssertionError):
+                    g.subentities(e, g.dim + 1)
+                with self.assertRaises(AssertionError):
+                    g.subentities(e, e - 1)
+            with self.assertRaises(AssertionError):
+                g.subentities(g.dim + 1, 0)
+            with self.assertRaises(AssertionError):
+                g.subentities(-1, 0)
+
+    def test_subentities_shape(self):
         for g in self.grids:
             for e in xrange(g.dim + 1):
                 for s in xrange(e, g.dim + 1):
+                    self.assertEqual(g.subentities(e, s).ndim, 2)
                     self.assertEqual(g.subentities(e, s).shape[0], g.size(e))
+
+    def test_subentities_entry_value_range(self):
+        for g in self.grids:
+            for e in xrange(g.dim + 1):
+                for s in xrange(e, g.dim + 1):
+                    np.testing.assert_array_less(g.subentities(e, s), g.size(s))
+                    np.testing.assert_array_less(-2, g.subentities(e, s))
+
+    def test_subentities_entry_values_unique(self):
+        for g in self.grids:
+            for e in xrange(g.dim + 1):
+                for s in xrange(e, g.dim + 1):
+                    for S in g.subentities(e, s):
+                        S = S[S >= 0]
+                        self.assertEqual(S.size, np.unique(S).size)
+
+    def test_subentities_codim_d_codim_d(self):
+        for g in self.grids:
+            for d in xrange(g.dim + 1):
+                self.assertEqual(g.subentities(d, d).shape, (g.size(d), 1))
+                np.testing.assert_array_equal(g.subentities(d, d).ravel(), np.arange(g.size(d)))
+
+    def test_subentities_transitivity(self):
+        for g in self.grids:
+            for e in xrange(g.dim + 1):
+                for s in xrange(e + 1, g.dim + 1):
+                    for ss in xrange(s + 1, g.dim + 1):
+                        SE = g.subentities(e, s)
+                        SSE = g.subentities(e, ss)
+                        SESE = g.subentities(s, ss)
+                        for i in xrange(SE.shape[0]):
+                            for j in xrange(SE.shape[1]):
+                                if SE[i, j] != -1:
+                                    self.assertTrue(set(SESE[SE[i, j]]) <= set(SSE[i]).union((-1,)))
+
+    def test_superentities_wrong_arguments(self):
+        for g in self.grids:
+            for e in xrange(g.dim + 1):
                 with self.assertRaises(AssertionError):
-                    g.subentities(e, g.dim + 1)
-                    g.subentities(e, e - 1)
-                    g.subentities(e, 0)
+                    g.superentities(e, -1)
+                with self.assertRaises(AssertionError):
+                    g.superentities(e, e + 1)
             with self.assertRaises(AssertionError):
-                g.subentities(g.dim + 1, 0)
-                g.subentities(-1, 0)
+                g.superentities(g.dim + 1, 0)
+            with self.assertRaises(AssertionError):
+                g.superentities(-1, 0)
+            with self.assertRaises(AssertionError):
+                g.superentities(-1, -2)
+
+    def test_superentities_shape(self):
+        for g in self.grids:
+            for e in xrange(g.dim + 1):
+                for s in xrange(e):
+                    self.assertEqual(g.superentities(e, s).ndim, 2)
+                    self.assertEqual(g.superentities(e, s).shape[0], g.size(e))
+
+    def test_superentities_entry_value_range(self):
+        for g in self.grids:
+            for e in xrange(g.dim + 1):
+                for s in xrange(e):
+                    np.testing.assert_array_less(g.superentities(e, s), g.size(s))
+                    np.testing.assert_array_less(-2, g.superentities(e, s))
+
+    def test_superentities_entry_values_unique(self):
+        for g in self.grids:
+            for e in xrange(g.dim + 1):
+                for s in xrange(e):
+                    for S in g.superentities(e, s):
+                        S = S[S >= 0]
+                        self.assertEqual(S.size, np.unique(S).size)
+
+    def test_superentities_codim_d_codim_d(self):
+        for g in self.grids:
+            for d in xrange(g.dim + 1):
+                self.assertEqual(g.superentities(d, d).shape, (g.size(d), 1))
+                np.testing.assert_array_equal(g.superentities(d, d).ravel(), np.arange(g.size(d)))
+
+    def test_superentities_each_entry_superentity(self):
+        for g in self.grids:
+            for e in xrange(g.dim + 1):
+                for s in xrange(e):
+                    SE = g.superentities(e, s)
+                    SUBE = g.subentities(s, e)
+                    for i in xrange(SE.shape[0]):
+                        for se in SE[i]:
+                            if se != -1:
+                                self.assertTrue(i in SUBE[se])
+
 
 
 class SimpleAffineGridTest(unittest.TestCase):
