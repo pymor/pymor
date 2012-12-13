b58a874bc0349814514f08838a180cee7f5e98f8
Stephan Rave <stephanrave@uni-muenster.de>
2012-12-13 16:47:03 +0100

    [analyticalproblem] added poisson problem

---
+++ src/pymor/analyticalproblem/__init__.py
@@ -0,0 +1,1 @@
+from poisson import Poisson
---
+++ src/pymor/analyticalproblem/poisson.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+
+import pymor.core as core
+from pymor.common import BoundaryType
+from common.domaindescription import Rect
+from common.function.nonparametric import Constant as ConstantFunc
+
+
+class Poisson(object):
+
+    def __init__(self, domain=Rect(), rhs=ConstantFunc(dim_domain=2), diffusion_functions=(ConstantFunc(dim_domain=2),),
+                 dirichlet_data=ConstantFunc(value=0, dim_domain=2), parameter_dependent=None):
+        self.domain = domain
+        self.rhs = rhs
+        self.diffusion_functions = diffusion_functions
+        self.dirichlet_data = dirichlet_data
+        if parameter_dependent is None:
+            self.parameter_dependent = len(diffusion_functions) > 1
+        else:
+            self.parameter_dependent = parameter_independent
