d6c397d05ca9d0f1a7cefa56874221a29fa5af63
Stephan Rave <stephanrave@uni-muenster.de>
2013-01-28 19:10:10 +0100

    [*] make use of tools.Named

--- src/pymor/discreteoperators/interfaces.py
+++ src/pymor/discreteoperators/interfaces.py
@@ -3,24 +3,10 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import numpy as np
 
 import pymor.core as core
+from pymor.tools import Named
 from pymor.parameters import Parametric
 
-class DiscreteOperatorInterface(core.BasicInterface, Parametric):
-
-    __name = None
-    @property
-    def name(self):
-        if self.__name is None:
-            import uuid
-            self.__name = str(uuid.uuid4())
-        return self.__name
-
-    @name.setter
-    def name(self, n):
-        if self.__name is not None:
-            raise AttributeError('Name has already been set and cannot be changed')
-        else:
-            self.__name = n
+class DiscreteOperatorInterface(core.BasicInterface, Parametric, Named):
 
     source_dim = 0
     range_dim = 0
--- src/pymor/functions/interfaces.py
+++ src/pymor/functions/interfaces.py
@@ -1,27 +1,13 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import pymor.core as core
+from pymor.tools import Named
 from pymor.parameters import Parametric
 
-class FunctionInterface(core.BasicInterface, Parametric):
+class FunctionInterface(core.BasicInterface, Parametric, Named):
 
     dim_domain = 0
     dim_range = 0
-
-    __name = None
-    @property
-    def name(self):
-        if self.__name is None:
-            import uuid
-            self.__name = str(uuid.uuid4())
-        return self.__name
-
-    @name.setter
-    def name(self, n):
-        if self.__name is not None:
-            raise AttributeError('Name has already been set and cannot be changed')
-        else:
-            self.__name = n
 
     @core.interfaces.abstractmethod
     def evaluate(self, x, mu={}):
