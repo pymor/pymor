37b68ff85c475e11174b6e24687567ce103f7b40
Stephan Rave <stephanrave@uni-muenster.de>
2013-01-29 16:55:58 +0100

    [parameters] once again __init__.py ...

--- src/pymor/parameters/__init__.py
+++ src/pymor/parameters/__init__.py
@@ -1,4 +1,4 @@
-from .interfaces import ParameterFunctionalInterface
+from .interfaces import ParameterSpaceInterface, ParameterFunctionalInterface
 from .base import Parameter, Parametric
 from .functionals import ProjectionParameterFunctional, GenericParameterFunctional
 from .spaces import CubicParameterSpace
