3c4b19270b814ab187613519b00ea440331b64f3
Stephan Rave <stephanrave@uni-muenster.de>
2013-01-30 18:03:20 +0100

    [grids.oned] renamed arguments of __init__

--- src/pymor/grids/oned.py
+++ src/pymor/grids/oned.py
@@ -16,29 +16,29 @@ class OnedGrid(AffineGridInterface):
     reference_element = line
     id = 'grid.oned'
 
-    def __init__(self, interval=np.array((0, 1)), size=4):
+    def __init__(self, domain=np.array((0, 1)), num_intervals=4):
         self.reference_element = line
-        self._interval = interval
-        self._size = size
-        self._width = np.abs(self._interval[1] - self._interval[0]) / self._size
-        self.__subentities = np.vstack((np.arange(self._size, dtype=np.int32),
-                                        np.arange(self._size, dtype=np.int32) + 1))
-        self.__A = np.ones(self._size, dtype=np.int32)[:, np.newaxis, np.newaxis] * self._width
-        self.__B = (self._interval[0] + self._width * (np.arange(self._size, dtype=np.int32)))[:, np.newaxis]
+        self._domain = domain
+        self._num_intervals = num_intervals
+        self._width = np.abs(self._domain[1] - self._domain[0]) / self._num_intervals
+        self.__subentities = np.vstack((np.arange(self._num_intervals, dtype=np.int32),
+                                        np.arange(self._num_intervals, dtype=np.int32) + 1))
+        self.__A = np.ones(self._num_intervals, dtype=np.int32)[:, np.newaxis, np.newaxis] * self._width
+        self.__B = (self._domain[0] + self._width * (np.arange(self._num_intervals, dtype=np.int32)))[:, np.newaxis]
 
     def __str__(self):
         return (self.id + ', domain [{xmin},{xmax}]'
                 + ', {elements} elements'
                 + ', {vertices} vertices'
-                ).format(xmin=self._interval[0],
-                         xmax=self._interval[1],
+                ).format(xmin=self._domain[0],
+                         xmax=self._domain[1],
                          elements=self.size(0),
                          vertices=self.size(1))
 
     def size(self, codim=0):
         assert 0 <= codim <= 1, 'codim has to be between 0 and {}!'.format(self.dim)
         if 0 <= codim <= 1:
-            return self._size + codim
+            return self._num_intervals + codim
 
     def subentities(self, codim=0, subentity_codim=None):
         assert 0 <= codim <= 1, CodimError('Invalid codimension')
@@ -60,7 +60,7 @@ class OnedGrid(AffineGridInterface):
             return super(OnedGrid, self).embeddings(codim)
 
     def test_instances():
-        return [OnedGrid(interval=np.array((-2, 2)), size=10),
-                OnedGrid(interval=np.array((-2, -4)), size=100),
-                OnedGrid(interval=np.array((3, 2)), size=10),
-                OnedGrid(interval=np.array((1, 2)), size=10000)]
+        return [OnedGrid(domain=np.array((-2, 2)), num_intervals=10),
+                OnedGrid(domain=np.array((-2, -4)), num_intervals=100),
+                OnedGrid(domain=np.array((3, 2)), num_intervals=10),
+                OnedGrid(domain=np.array((1, 2)), num_intervals=10000)]
