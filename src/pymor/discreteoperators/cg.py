afb3d800791725f4ece5d6b313cb00c109a4d0a6
Stephan Rave <stephanrave@uni-muenster.de>
2013-01-30 20:31:39 +0100

    [discreteoperators.cg] added DiffusionOperatorP1D1

--- src/pymor/discreteoperators/cg.py
+++ src/pymor/discreteoperators/cg.py
@@ -98,8 +98,8 @@ class L2ProductFunctionalP1D1(LinearDiscreteOperatorInterface):
 
 
 class DiffusionOperatorP1D2(LinearDiscreteOperatorInterface):
-    '''Simple Diffusion Operator for linear finite elements in two dimensions on an triangular grid and
-    constant diffusion coefficient of value 1. Add more functionality later ...
+    '''Simple Diffusion Operator for linear finite elements in two dimensions on an triangular grid. 
+    Add more functionality later ...
     '''
 
     def __init__(self, grid, boundary_info, diffusion_function=None, diffusion_constant=None,
@@ -157,6 +157,69 @@ class DiffusionOperatorP1D2(LinearDiscreteOperatorInterface):
 
         print('Assemble system matrix ...')
         A = coo_matrix((SF_INTS, (SF_I0, SF_I1)), shape=(g.size(2), g.size(2)))
+        A = csr_matrix(A)
+
+        return A
+
+
+# TODO make a DiffusionOperatorP1 ...
+class DiffusionOperatorP1D1(LinearDiscreteOperatorInterface):
+    '''Simple Diffusion Operator for linear finite elements in one dimension.'''
+
+    def __init__(self, grid, boundary_info, diffusion_function=None, diffusion_constant=None,
+                 dirichlet_clear_columns=False, dirichlet_clear_diag=False, name=None):
+        assert grid.reference_element(0) == line
+        self.source_dim = self.range_dim = grid.size(1)
+        self.grid = grid
+        self.boundary_info = boundary_info
+        self.diffusion_constant = diffusion_constant
+        self.diffusion_function = diffusion_function
+        self.dirichlet_clear_columns = dirichlet_clear_columns
+        self.dirichlet_clear_diag = dirichlet_clear_diag
+        self.name = name
+        if diffusion_function is not None:
+            self.set_parameter_type(inherits={'diffusion':diffusion_function})
+
+    def assemble(self, mu={}):
+        self.map_parameter(mu)
+        g = self.grid
+        bi = self.boundary_info
+
+        # gradients of shape functions
+        SF_GRAD = np.array(([-1.],
+                            [1.,]))
+
+        print('Calulate gradients of shape functions transformed by reference map ...')
+        SF_GRADS = np.einsum('eij,pj->epi', g.jacobian_inverse_transposed(0), SF_GRAD)
+
+        print('Calculate all local scalar products beween gradients ...')
+        if self.diffusion_function is not None:
+            D = self.diffusion_function(self.grid.centers(0), mu=self.map_parameter(mu, 'diffusion')).ravel()
+            SF_INTS = np.einsum('epi,eqi,e,e->epq', SF_GRADS, SF_GRADS, g.volumes(0), D).ravel()
+        else:
+            SF_INTS = np.einsum('epi,eqi,e->epq', SF_GRADS, SF_GRADS, g.volumes(0)).ravel()
+
+        if self.diffusion_constant is not None:
+            SF_INTS *= self.diffusion_constant
+
+        print('Determine global dofs ...')
+        SF_I0 = np.repeat(g.subentities(0, 1), 2, axis=1).ravel()
+        SF_I1 = np.tile(g.subentities(0, 1), [1, 2]).ravel()
+
+        print('Boundary treatment ...')
+        if bi.has_dirichlet:
+            SF_INTS = np.where(bi.dirichlet_mask(1)[SF_I0], 0, SF_INTS)
+            if self.dirichlet_clear_columns:
+                SF_INTS = np.where(bi.dirichlet_mask(1)[SF_I1], 0, SF_INTS)
+
+        if not self.dirichlet_clear_diag:
+            SF_INTS = np.hstack((SF_INTS, np.ones(bi.dirichlet_boundaries(1).size)))
+            SF_I0 = np.hstack((SF_I0, bi.dirichlet_boundaries(1)))
+            SF_I1 = np.hstack((SF_I1, bi.dirichlet_boundaries(1)))
+
+
+        print('Assemble system matrix ...')
+        A = coo_matrix((SF_INTS, (SF_I0, SF_I1)), shape=(g.size(1), g.size(1)))
         A = csr_matrix(A)
 
         return A
