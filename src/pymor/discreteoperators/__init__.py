399cb19319e7955f80bb62d67ccc601fce625d42
Stephan Rave <stephanrave@uni-muenster.de>
2013-01-31 15:49:19 +0100

    [discreteoperators.basic] added GenericOperator and GenericLinearOperator

--- src/pymor/discreteoperators/__init__.py
+++ src/pymor/discreteoperators/__init__.py
@@ -1,3 +1,4 @@
 from .interfaces import DiscreteOperatorInterface, LinearDiscreteOperatorInterface
+from .basic import GenericOperator, GenericLinearOperator
 from .affine import LinearAffinelyDecomposedOperator
 from . import cg
---
+++ src/pymor/discreteoperators/basic.py
@@ -0,0 +1,40 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+
+from .interfaces import DiscreteOperatorInterface, LinearDiscreteOperatorInterface
+
+
+class GenericOperator(DiscreteOperatorInterface):
+
+    def __init__(self, mapping, dim_domain=1, dim_range=1, parameter_type=None, name=None):
+        self.dim_domain = dim_domain
+        self.dim_range = dim_range
+        self.name = name
+        self._mapping = mapping
+        if parameter_type is not None:
+            self.set_parameter_type(parameter_type)
+            self._with_mu = True
+        else:
+            self._with_mu = False
+
+    def evaluate(self, U, mu={}):
+        mu = self.map_parameter(mu)
+        assert U.shape[-1] == self.dim_domain
+        if self._with_mu:
+            return self._mapping((U, mu))
+        else:
+            return self._mapping(U)
+
+
+class GenericLinearOperator(LinearDiscreteOperatorInterface):
+
+    def __init__(self, matrix, name=None):
+        self.dim_domain = matrix.shape[1]
+        self.dim_range = matrix.shape[0]
+        self.name = name
+        self._matrix = matrix
+
+    def assemble(self, mu={}):
+        mu = self.parse_parameter(mu)
+        return self._matrix
