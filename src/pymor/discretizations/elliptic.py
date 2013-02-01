680f5b8406cd56497664180348de3c94845ff9dd
Stephan Rave <stephanrave@uni-muenster.de>
2013-02-01 19:17:03 +0100

    [discretizations.elliptic] ensure that changing operators[] changes the operators everywhere

--- src/pymor/discretizations/elliptic.py
+++ src/pymor/discretizations/elliptic.py
@@ -1,20 +1,24 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import copy
+
 import numpy as np
 from scipy.sparse.linalg import bicg
 from scipy.sparse import issparse
 
 import pymor.core as core
+from pymor.tools import dict_property
 from pymor.domaindescriptions import BoundaryType
 from pymor.parameters import Parametric
 
 
 class EllipticDiscretization(Parametric):
 
+    operator = dict_property('operators', 'operator')
+    rhs = dict_property('operators', 'rhs')
+
     def __init__(self, operator, rhs, solver=None, visualizer=None):
-        self.operator = operator
-        self.operators = {operator.name: operator}
-        self.rhs = rhs
+        self.operators = {'operator': operator, 'rhs':rhs}
         self.build_parameter_type(inherits={'operator':operator, 'rhs':rhs})
 
         def default_solver(A, RHS):
@@ -28,6 +32,10 @@ class EllipticDiscretization(Parametric):
         if visualizer is not None:
             self.visualize = visualizer
 
+    def copy(self):
+        c = copy.copy(self)
+        c.operators = c.operators.copy()
+        return c
 
     def solve(self, mu={}):
         A = self.operator.matrix(self.map_parameter(mu, 'operator'))
