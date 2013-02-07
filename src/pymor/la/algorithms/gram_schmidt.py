3589f62e28203289466f1e6b25befecb87285a0b
Stephan Rave <stephanrave@uni-muenster.de>
2013-02-07 13:48:06 +0100

    [la.algorithms.gram_schmidt] rearranged code and added row_offset

--- src/pymor/la/algorithms/gram_schmidt.py
+++ src/pymor/la/algorithms/gram_schmidt.py
@@ -7,7 +7,7 @@ from pymor.la import float_cmp_all
 from pymor.discreteoperators import DiscreteOperatorInterface, GenericLinearOperator
 
 
-def gram_schmidt(A, product=None, tol=None, find_row_duplicates=True, find_col_duplicates=False):
+def gram_schmidt(A, product=None, tol=None, row_offset=0, find_row_duplicates=True, find_col_duplicates=False):
 
     A = A.copy()
     if product is not None and not isinstance(product, DiscreteOperatorInterface):
@@ -30,28 +30,24 @@ def gram_schmidt(A, product=None, tol=None, find_row_duplicates=True, find_col_d
                     A[:, j] = 0
 
     # main loop
-    if product is None:
-        for i in xrange(A.shape[0]):
+    for i in xrange(row_offset, A.shape[0]):
+
+        if product is None:
             norm = np.sqrt(np.sum(A[i] ** 2))
-
-            if norm < tol:
-                A[i] = 0
-            else:
-                A[i] = A[i] / norm
-
-            p = np.sum(A[i + 1:] * A[i], axis=-1)
-            A[i + 1:] -= p[..., np.newaxis] * A[i]
-    else:
-        for i in xrange(A.shape[0]):
+        else:
             norm = np.sqrt(product.apply2(A[i], A[i]))
 
-            if norm < tol:
-                A[i] = 0
-            else:
-                A[i] = A[i] / norm
+        if norm < tol:
+            A[i] = 0
+        else:
+            A[i] = A[i] / norm
 
+        if product is None:
+            p = np.sum(A[i + 1:] * A[i], axis=-1)
+        else:
             p = product.apply2(A[i + 1:], A[i], pairwise=False)
-            A[i + 1:] -= p[..., np.newaxis] * A[i]
+
+        A[i + 1:] -= p[..., np.newaxis] * A[i]
 
     rows = np.logical_not(np.all(A == 0, axis=-1))
     return A[rows]
