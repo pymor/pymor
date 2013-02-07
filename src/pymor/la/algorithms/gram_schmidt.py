c53d86047faa201452597f09679e9c9c2c48d4e8
Stephan Rave <stephanrave@uni-muenster.de>
2013-02-07 16:35:34 +0100

    [la.algorithms.gram_schmidt] bugfixes

--- src/pymor/la/algorithms/gram_schmidt.py
+++ src/pymor/la/algorithms/gram_schmidt.py
@@ -21,43 +21,46 @@ def gram_schmidt(A, product=None, tol=None, row_offset=0, find_row_duplicates=Tr
     # find duplicate rows since in some circumstances these cannot be detected in the main loop
     # (is this really needed or is in this cases the tolerance poorly chosen anyhow)
     if find_row_duplicates:
-        for i in xrange(row_offset, A.shape[0]):
-            for j in xrange(i + 1, A.shape[0]):
+        for i in xrange(A.shape[0]):
+            for j in xrange(max(row_offset, i + 1), A.shape[0]):
                 if float_cmp_all(A[i], A[j]):
                     A[j] = 0
 
     # find duplicate columns
     if find_col_duplicates:
-        for i in xrange(row_offest, A.shape[1]):
+        for i in xrange(A.shape[1]):
             for j in xrange(i, A.shape[1]):
                 if float_cmp_all(A[:, i], A[:, j]):
                     A[:, j] = 0
 
     # main loop
-    for i in xrange(row_offset, A.shape[0]):
+    for i in xrange(A.shape[0]):
 
+        if i >= row_offset:
+            if product is None:
+                norm = np.sqrt(np.sum(A[i] ** 2))
+            else:
+                norm = np.sqrt(product.apply2(A[i], A[i]))
+
+            if norm < tol:
+                A[i] = 0
+            else:
+                A[i] = A[i] / norm
+
+        j = max(row_offset, i + 1)
         if product is None:
-            norm = np.sqrt(np.sum(A[i] ** 2))
+            p = np.sum(A[j:] * A[i], axis=-1)
         else:
-            norm = np.sqrt(product.apply2(A[i], A[i]))
+            p = product.apply2(A[j:], A[i], pairwise=False)
 
-        if norm < tol:
-            A[i] = 0
-        else:
-            A[i] = A[i] / norm
-
-        if product is None:
-            p = np.sum(A[i + 1:] * A[i], axis=-1)
-        else:
-            p = product.apply2(A[i + 1:], A[i], pairwise=False)
-
-        A[i + 1:] -= p[..., np.newaxis] * A[i]
+        A[j:] -= p[..., np.newaxis] * A[i]
 
     rows = np.logical_not(np.all(A == 0, axis=-1))
+    A = A[rows]
 
     if check:
         if not float_cmp_all(A.dot(A.T), np.eye(A.shape[0]), check_tol):
             err = np.max(np.abs(A.dot(A.T) - np.eye(A.shape[0])))
             raise AccuracyError('result not orthogonal (max err={})'.format(err))
 
-    return A[rows]
+    return A
