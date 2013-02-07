b3b34a9461d855f46eb42f3c8369f30ceeba560b
Stephan Rave <stephanrave@uni-muenster.de>
2013-02-07 11:52:07 +0100

    [la.basic] dont multiply by eps in float_cmp
    I guess in  most cases when you want to specify a tolerance,
    multiples of eps is not a natural unit and it simplifies
    the code.

--- src/pymor/la/basic.py
+++ src/pymor/la/basic.py
@@ -1,7 +1,5 @@
 from __future__ import absolute_import, division, print_function
 
-import numbers
-
 import numpy as np
 from pymor.core import defaults
 
@@ -13,19 +11,12 @@ def float_cmp(x, y, rtol=None, atol=None):
 
     We define almost equality as
 
-       float_cmp(x,y) <=> |x - y| <= atol*eps + |y|*rtol*eps
+       float_cmp(x,y) <=> |x - y| <= atol + |y|*rtol
     '''
 
     rtol = rtol or defaults.float_cmp_tol
     atol = atol or rtol
-    if isinstance(x, (numbers.Rational, numbers.Integral)):
-        x = float(x)
-    if isinstance(y, (numbers.Rational, numbers.Integral)):
-        y = float(y)
-    dtx = x.dtype if isinstance(x, np.ndarray) else np.dtype(type(x))
-    dty = y.dtype if isinstance(y, np.ndarray) else np.dtype(type(y))
-    eps = min(np.finfo(dtx).eps, np.finfo(dty).eps)
-    return np.abs(x - y) <= atol * eps + np.abs(y) * rtol * eps
+    return np.abs(x - y) <= atol + np.abs(y) * rtol
 
 def float_cmp_all(x, y, rtol=None, atol=None):
     return np.all(float_cmp(x, y, rtol, atol))
