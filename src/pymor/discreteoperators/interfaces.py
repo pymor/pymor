91dd222e611e867563e736c22cad55d5b5881efa
Stephan Rave <stephanrave@uni-muenster.de>
2013-02-07 12:56:37 +0100

    [discreteoperators.interfaces] apply2: operands do not need to have same shape
    if pairwise is False

--- src/pymor/discreteoperators/interfaces.py
+++ src/pymor/discreteoperators/interfaces.py
@@ -22,7 +22,7 @@ class DiscreteOperatorInterface(core.BasicInterface, Parametric, Named):
             A.apply2(V, U) = V^T*B*U
         '''
         AU = self.apply(U, mu)
-        assert AU.shape == V.shape
+        assert not pairwise or AU.shape == V.shape
         if product is None:
             if pairwise:
                 return np.sum(V * AU, axis=-1)
