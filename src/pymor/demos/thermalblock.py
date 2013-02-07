596026e2798c035e42da547b479538d0ea522ad1
Stephan Rave <stephanrave@uni-muenster.de>
2013-02-07 16:38:07 +0100

    [demos.thermalblock] some updates

--- src/pymor/demos/thermalblock.py
+++ src/pymor/demos/thermalblock.py
@@ -18,15 +18,16 @@ from pymor.algorithms import GreedyRB
 from pymor.core import getLogger; getLogger('pymor.algorithms').setLevel('INFO')
 from pymor.core import getLogger; getLogger('pymor.discretizations').setLevel('INFO')
 
-if len(sys.argv) < 6:
-    sys.exit('Usage: {} X Y N SNAP RB PLOT'.format(sys.argv[0]))
+if len(sys.argv) < 8:
+    sys.exit('Usage: {} X Y N SNAP RB EXT_ALG PLOT'.format(sys.argv[0]))
 
 nx = int(sys.argv[1])
 ny = int(sys.argv[2])
 n = int(sys.argv[3])
 snap_size = int(sys.argv[4])
 rb_size = int(sys.argv[5])
-plot = bool(int(sys.argv[6]))
+ext_alg = sys.argv[6]
+plot = bool(int(sys.argv[7]))
 
 print('Solving on TriaGrid(({0},{0}))'.format(n))
 
@@ -39,7 +40,7 @@ discretization = discretizer.discretize(diameter=m.sqrt(2) / n)
 
 print(discretization.parameter_info())
 
-if print:
+if plot > 1:
     print('Showing some solutions')
     for mu in discretization.parameter_space.sample_randomly(2):
         print('Solving for diffusion = \n{} ... '.format(mu['diffusion']))
@@ -51,26 +52,35 @@ if print:
 print('RB generation ...')
 
 reductor = GenericRBReductor(discretization)
-greedy = GreedyRB(discretization, reductor)
-greedy.run(discretization.parameter_space.sample_uniformly(snap_size), Nmax=rb_size)
+greedy = GreedyRB(discretization, reductor, extension_algorithm=ext_alg)
+RB = greedy.run(discretization.parameter_space.sample_uniformly(snap_size), Nmax=rb_size)
 rb_discretization, reconstructor = greedy.rb_discretization, greedy.reconstructor
 
 print('\nSearching for maximum error on random snapshots ...')
 l2_err_max = -1
-for mu in discretization.parameter_space.sample_randomly(10):
+cond_max = -1
+for mu in discretization.parameter_space.sample_randomly(20):
     print('Solving RB-Scheme for mu = {} ... '.format(mu), end='')
     URB = reconstructor.reconstruct(rb_discretization.solve(mu))
     U = discretization.solve(mu)
-    l2_err = np.sqrt(np.sum((U-URB)**2)) / np.sqrt(np.sum(U**2))
+    l2_err = np.sqrt(np.sum((U-URB)**2))
+    cond = np.linalg.cond(rb_discretization.operator.matrix(mu))
     if l2_err > l2_err_max:
         l2_err_max = l2_err
         Umax = U
         URBmax = URB
         mumax = mu
-    print('rel L2-error = {}'.format(l2_err))
+    if cond > cond_max:
+        cond_max = cond
+        cond_max_mu = mu
+    print('L2-error = {}, condition = {}'.format(l2_err, cond))
 
 print('')
-print('Maximal relative L2-error: {} for mu = {}'.format(l2_err_max, mu))
+print('Basis size: {}'.format(RB.shape[0]))
+print('')
+print('Maximal L2-error: {} for mu = {}'.format(l2_err_max, mu))
+print('')
+print('Maximal condition of system matrix: {} for mu = {}'.format(cond_max, cond_max_mu))
 sys.stdout.flush()
 if plot:
     discretization.visualize(U-URB)
