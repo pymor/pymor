a9d7757e3ae8342d6f3521c078103b6d8b9f5867
Stephan Rave <stephanrave@uni-muenster.de>
2013-02-24 02:42:42 +0100

    [demos.thermalblock] use docopt for option parsing

--- src/pymor/demos/thermalblock.py
+++ src/pymor/demos/thermalblock.py
@@ -1,5 +1,42 @@
 #!/usr/bin/env python
 # vim: set filetype=python:
+'''Thermalblock demo.
+
+Usage:
+  thermalblock.py [options] XBLOCKS YBLOCKS SNAPSHOTS RBSIZE
+
+
+Arguments:
+  XBLOCKS    Number of blocks in x direction.
+
+  YBLOCKS    Number of blocks in y direction.
+
+  SNAPSHOTS  Number of snapshots for basis generation per component.
+             In total SNAPSHOTS^(XBLOCKS * YBLOCKS).
+
+  RBSIZE     Size of the reduced basis
+
+
+Options:
+  -e, --with-estimator   Use error estimator.
+
+  --estimator-norm=NORM  Norm (trivial, h1) in which to calculate the residual
+                         [default: trivial].
+
+  --extension-alg=ALG    Basis extension algorithm (trivial, gram_schmidt) to be used
+                         [default: gram_schmidt].
+
+  -g NI --grid=NI        Use grid with 2*NI*NI elements [default: 100].
+
+  -h --help              Show this message.
+
+  -p --plot-err          Plot error.
+
+  --plot-solutions       Plot some example solutions.
+
+  --test=COUNT           Use COUNT snapshots for stochastic error estimation
+                         [default: 20].
+'''
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
@@ -8,6 +45,7 @@ import math as m
 import time
 
 import numpy as np
+from docopt import docopt
 
 from pymor.analyticalproblems import ThermalBlockProblem
 from pymor.discretizers import PoissonCGDiscretizer
@@ -21,37 +59,32 @@ from pymor.core import getLogger; getLogger('pymor.algorithms').setLevel('INFO')
 from pymor.core import getLogger; getLogger('pymor.discretizations').setLevel('INFO')
 
 # parse arguments
-if len(sys.argv) < 11:
-    sys.exit('Usage: {} X Y N SNAP RB_SIZE USE_ESTIMATOR ESTIMATOR_NORM EXTENSION_ALG TEST_SIZE PLOT'.format(sys.argv[0]))
+args = docopt(__doc__)
 
-nx = int(sys.argv[1])
-ny = int(sys.argv[2])
-n = int(sys.argv[3])
-snap_size = int(sys.argv[4])
-rb_size = int(sys.argv[5])
-use_estimator = bool(int(sys.argv[6]))
-estimator_norm = sys.argv[7].lower()
-assert estimator_norm in {'trivial', 'h1'}
-ext_alg = sys.argv[8]
-assert ext_alg in {'trivial', 'gram_schmidt'}
-extension_algorithm = gram_schmidt_basis_extension if ext_alg == 'gram_schmidt' else trivial_basis_extension
-test_size = int(sys.argv[9])
-plot = int(sys.argv[10])
+args['XBLOCKS']          = int(args['XBLOCKS'])
+args['YBLOCKS']          = int(args['YBLOCKS'])
+args['--grid']           = int(args['--grid'])
+args['SNAPSHOTS']        = int(args['SNAPSHOTS'])
+args['RBSIZE']           = int(args['RBSIZE'])
+args['--test']           = int(args['--test'])
+args['--estimator-norm'] = args['--estimator-norm'].lower()
+assert args['--estimator-norm'] in {'trivial', 'h1'}
+args['--extension-alg']  = args['--extension-alg'].lower()
+assert args['--extension-alg'] in {'trivial', 'gram_schmidt'}
 
 
-
-print('Solving on TriaGrid(({0},{0}))'.format(n))
+print('Solving on TriaGrid(({0},{0}))'.format(args['--grid']))
 
 print('Setup Problem ...')
-problem = ThermalBlockProblem(num_blocks=(nx, ny))
+problem = ThermalBlockProblem(num_blocks=(args['XBLOCKS'], args['YBLOCKS']))
 
 print('Discretize ...')
 discretizer = PoissonCGDiscretizer(problem)
-discretization = discretizer.discretize(diameter=m.sqrt(2) / n)
+discretization = discretizer.discretize(diameter=m.sqrt(2) / args['--grid'])
 
 print(discretization.parameter_info())
 
-if plot > 1:
+if args['--plot-solutions']:
     print('Showing some solutions')
     for mu in discretization.parameter_space.sample_randomly(2):
         print('Solving for diffusion = \n{} ... '.format(mu['diffusion']))
@@ -62,19 +95,21 @@ if plot > 1:
 
 print('RB generation ...')
 
-error_product = discretization.h1_product if estimator_norm == 'h1' else None
+error_product = discretization.h1_product if args['--estimator-norm'] == 'h1' else None
 reductor = StationaryAffineLinearReductor(discretization, error_product=error_product)
-greedy_data = greedy(discretization, reductor, discretization.parameter_space.sample_uniformly(snap_size),
-                     use_estimator=use_estimator, error_norm=discretization.h1_norm,
-                     extension_algorithm=extension_algorithm, max_extensions=rb_size)
+extension_algorithm = gram_schmidt_basis_extension if args['--extension-alg'] == 'gram_schmidt' else trivial_basis_extension
+greedy_data = greedy(discretization, reductor, discretization.parameter_space.sample_uniformly(args['SNAPSHOTS']),
+                     use_estimator=args['--with-estimator'], error_norm=discretization.h1_norm,
+                     extension_algorithm=extension_algorithm, max_extensions=args['RBSIZE'])
 rb_discretization, reconstructor = greedy_data['reduced_discretization'], greedy_data['reconstructor']
+
 
 print('\nSearching for maximum error on random snapshots ...')
 
 tic = time.time()
 h1_err_max = -1
 cond_max = -1
-for mu in discretization.parameter_space.sample_randomly(test_size):
+for mu in discretization.parameter_space.sample_randomly(args['--test']):
     print('Solving RB-Scheme for mu = {} ... '.format(mu), end='')
     URB = reconstructor.reconstruct(rb_discretization.solve(mu))
     U = discretization.solve(mu)
@@ -96,25 +131,25 @@ print('''
 *** RESULTS ***
 
 Problem:
-   number of blocks:                   {nx}x{ny}
-   h:                                  sqrt(2)/{n}
+   number of blocks:                   {args[XBLOCKS]}x{args[YBLOCKS]}
+   h:                                  sqrt(2)/{args[--grid]}
 
 Greedy basis generation:
-   number of snapshots:                {snap_size}^({nx}x{ny})
-   used estimator:                     {use_estimator}
-   estimator norm:                     {estimator_norm}
-   extension method:                   {ext_alg}
-   prescribed basis size:              {rb_size}
+   number of snapshots:                {args[SNAPSHOTS]}^({args[XBLOCKS]}x{args[YBLOCKS]})
+   used estimator:                     {args[--with-estimator]}
+   estimator norm:                     {args[--estimator-norm]}
+   extension method:                   {args[--extension-alg]}
+   prescribed basis size:              {args[RBSIZE]}
    actual basis size:                  {greedy_data[data].shape[0]}
    elapsed time:                       {greedy_data[time]}
 
 Stochastic error estimation:
-   number of samples:                  {test_size}
+   number of samples:                  {args[--test]}
    maximal H1-error:                   {h1_err_max}  (mu = {mumax})
    maximal condition of system matrix: {cond_max}  (mu = {cond_max_mu})
    elapsed time:                       {t_est}
 '''.format(**locals()))
 
 sys.stdout.flush()
-if plot:
+if args['--plot-err']:
     discretization.visualize(U-URB)
