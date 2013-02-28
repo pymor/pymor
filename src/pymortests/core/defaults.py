dc71cf56044966a7e4c931aaeec675d438d99c8f
Rene Milk <rene.milk@uni-muenster.de>
2013-02-28 14:18:18 +0100

    [playground] move dysfunctional code to where it doesn't bother anyone

---
+++ src/pymortests/core/defaults.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, division, print_function
+
+from pymor.core import defaults
+from pymortests.base import TestBase, runmodule
+        
+class DefaultsTest(TestBase):
+
+           
+    def testStr(self):
+        rep = str(defaults)
+        self.assertGreater(len(rep), 0)
+        
+if __name__ == "__main__":
+    runmodule(name='pymortests.core.defaults')
+       
+        
