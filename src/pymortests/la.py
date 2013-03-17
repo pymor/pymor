38038038ed5d10be18c378387e876a62d4c28105
Rene Milk <rene.milk@uni-muenster.de>
2013-03-17 23:35:50 +0100

    [tests] prototype norm test

---
+++ src/pymortests/la.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+from mock import Mock
+
+from pymor import la
+from pymor import discretizations
+from pymor.discreteoperators.cg import L2ProductP1
+from pymortests.base import TestBase, runmodule
+from pymor.grids.rect import RectGrid
+from pymor.grids.tria import TriaGrid
+
+
+class TestBasicParameterspace(TestBase):
+
+    def test_induced(self):
+        grid = TriaGrid(num_intervals=(10, 10))
+        product = L2ProductP1(grid)
+        zero = np.zeros(10 * 10 * 3 * 2)
+        norm = la.induced_norm(product)
+        value = norm(zero)
+        self.assertAlmostEqual(value, 0.0)
+
+
+if __name__ == "__main__":
+    runmodule(name='pymortests.la')
