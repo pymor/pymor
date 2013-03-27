b2e3422b8ee146f04403802fb9cded5d89d2725c
Rene Milk <rene.milk@uni-muenster.de>
2013-03-27 16:42:20 +0100

    refactor setup.py, fixes #242

--- setup.py
+++ setup.py
@@ -5,46 +5,61 @@ from distribute_setup import use_setuptools
 use_setuptools()
 
 import sys
-from setuptools import setup
+import os
+import subprocess
+from setuptools import setup, find_packages
 from distutils.extension import Extension
 
-tests_requires = ['mock', 'nose-cov', 'nose', 'nosehtmloutput', 'nose-progressive', 'tissue']
-install_requires = ['distribute', 'scipy', 'matplotlib', 'numpy', 'pycontracts', 'sympy', 'docopt' ] + tests_requires
+def write_version():
+    revstring = subprocess.check_output(['git', 'describe', '--tags', '--candidates', '20', '--match', '*.*.*']).strip()
+    filename = os.path.join(os.path.dirname(__file__), 'src', 'pymor', 'version.py')
+    with open(filename, 'w') as out:
+        out.write('revstring = \'{}\''.format(revstring))
 
-cmdclass = {}
-ext_modules = []
-try:
-    import Cython.Distutils
-    cmdclass['build_ext'] = Cython.Distutils.build_ext
-    ext_modules.append(Extension("pymor.tools.relations", ["src/pymor/tools/relations.pyx"]))
-except ImportError:
-    sys.stderr.write('*' * 79 + '\n')
-    sys.stderr.write('Failed to import build_ext from cython, no extension modules will be build.\n')
-    sys.stderr.write('*' * 79 + '\n')
+def setup_package():
+    write_version()
+
+    tests_requires = ['mock', 'nose-cov', 'nose', 'nosehtmloutput', 'nose-progressive', 'tissue']
+    install_requires = ['distribute', 'scipy', 'matplotlib', 'numpy', 'pycontracts',
+                        'sympy', 'docopt' ] + tests_requires
     
-setup(
-    name = 'pyMor',
-    version = '0.0.1',
-    author = 'pyMor developers',
-    author_email = 'pymor-dev@listserv.uni-muenster.de',
-    maintainer = 'Rene Milk',
-    maintainer_email = 'rene.milk@wwu.de',
-    package_dir = {'': 'src'},
-    packages = ['pymor', 'pymortests'],
-    scripts = ['bin/%s'%n for n in [] ] + ['run_tests.py'],
-    url = 'http://dune-project.uni-muenster.de/git/projects/pymor',
-    description = ' ',
-    long_description = open('README.txt').read(),
-    tests_require = tests_requires,
-    install_requires = install_requires,
-    classifiers = ['Development Status :: 2 - Pre-Alpha',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Scientific/Engineering :: Visualization'],
-    license = 'LICENSE.txt',
-    #cmdclass = cmdclass,
-    ext_modules = ext_modules,
-)
+    cmdclass = {}
+    ext_modules = []
+    try:
+        import Cython.Distutils
+        cmdclass['build_ext'] = Cython.Distutils.build_ext
+        ext_modules.append(Extension("pymor.tools.relations", ["src/pymor/tools/relations.pyx"]))
+    except ImportError:
+        sys.stderr.write('*' * 79 + '\n')
+        sys.stderr.write('Failed to import build_ext from cython, no extension modules will be build.\n')
+        sys.stderr.write('*' * 79 + '\n')
+        
+    setup(
+        name='pyMor',
+        version='0.0.1',
+        author='pyMor developers',
+        author_email='pymor-dev@listserv.uni-muenster.de',
+        maintainer='Rene Milk',
+        maintainer_email='rene.milk@wwu.de',
+        package_dir={'': 'src'},
+        packages=find_packages('src'),
+        scripts=['bin/%s' % n for n in [] ] + ['run_tests.py'],
+        url='http://dune-project.uni-muenster.de/git/projects/pymor',
+        description=' ' ,
+        long_description=open('README.txt').read(),
+        tests_require=tests_requires,
+        install_requires=install_requires,
+        classifiers=['Development Status :: 2 - Pre-Alpha',
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Intended Audience :: Science/Research',
+            'Topic :: Scientific/Engineering :: Mathematics',
+            'Topic :: Scientific/Engineering :: Visualization'],
+        license = 'LICENSE.txt',
+        cmdclass=cmdclass,
+        ext_modules=ext_modules,
+    )
+
+if __name__ == '__main__':
+    setup_package()
--- src/pymor/__init__.py
+++ src/pymor/__init__.py
@@ -1,20 +1,22 @@
-NO_VERSION = (0, 0, 0, 'NOVERSION')
+NO_VERSIONSTRING = '0.0.0-0'
 
 try:
-    import pymorversion
-    version = pymorversion.version
+    import pymor.version
+    revstring = pymor.version.revstring
 except ImportError:
+    import subprocess
     try:
-        import subprocess
         revstring = subprocess.check_output(['git', 'describe', '--tags', '--candidates', '20', '--match', '*.*.*'])
-        pos = revstring.find('-')
-        version = tuple(int(x) for x in revstring[:pos].split('.'))
-        if pos > -1:
-            git = revstring.strip().split('-')
-            distance = int(git[1])
-            shorthash = git[2]
-            version = version + (distance, shorthash)
     except:
-        version = NO_VERSION
+        revstring = NO_VERSIONSTRING
 finally:
-    print('Loading pymor version {}'.format(version))
+    pos = revstring.find('-')
+    version = tuple(int(x) for x in revstring[:pos].split('.'))
+    if pos > -1:
+        git = revstring.strip().split('-')
+        distance = int(git[1])
+        shorthash = git[2]
+        version = version + (distance, shorthash)
+
+VERSION = version
+print('Loading pymor version {}'.format(VERSION))
