8647d51146a6935ca4add280f3392a03a0ff86c5
Rene Milk <rene.milk@uni-muenster.de>
2013-03-28 16:50:53 +0100

    [demos] thermalblock gui woth red/det sim

--- src/pymor/demos/thermalblock_gui.py
+++ src/pymor/demos/thermalblock_gui.py
@@ -34,10 +34,13 @@ Options:
   --test=COUNT           Use COUNT snapshots for stochastic error estimation
                          [default: 10].
 '''
-
+from __future__ import absolute_import, division, print_function
 import sys
 from docopt import docopt
+import time
+from functools import partial
 import math as m
+import numpy as np
 import matplotlib
 matplotlib.use('Qt4Agg')
 matplotlib.rcParams['backend.qt4'] = 'PySide'
@@ -48,31 +51,41 @@ from matplotlib.figure import Figure
 from PySide import QtCore, QtGui
 
 PARAM_STEPS = 10
+PARAM_MIN = 0.01
+PARAM_MAX = 1
 
 from pymor.analyticalproblems import ThermalBlockProblem
 from pymor.discretizers import discretize_elliptic_cg
 from pymor.reductors.linear import reduce_stationary_affine_linear
 from pymor.algorithms import greedy, trivial_basis_extension, gram_schmidt_basis_extension
+from pymor.parameters.base import Parameter
 
 # set log level
 from pymor.core import getLogger
-getLogger('pymor.algorithms').setLevel('INFO')
-getLogger('pymor.discretizations').setLevel('INFO')
+getLogger('pymor.algorithms').setLevel('DEBUG')
+getLogger('pymor.discretizations').setLevel('DEBUG')
 
 class ParamRuler(QtGui.QWidget):
-    def __init__(self, parent):
+    def __init__(self, parent, sim):
         super(ParamRuler, self).__init__(parent)
+        self.sim = sim
         self.setMinimumSize(200, 100)
-        box = QtGui.QVBoxLayout()
-        self.slider = QtGui.QSlider(QtCore.Qt.Horizontal)
-        self.slider.setMinimum(0)
-        self.slider.setMaximum(PARAM_STEPS)
-        self.slider.setFocusPolicy(QtCore.Qt.StrongFocus)
-        # self.slider.setTickInterval()
-        self.slider.setSingleStep(1)
-        box.addWidget(self.slider)
+        box = QtGui.QGridLayout()
+        self.spins = []
+        for i in xrange(args['XBLOCKS']):
+            for j in xrange(args['YBLOCKS']):
+                spin = QtGui.QDoubleSpinBox()
+                spin.setRange(PARAM_MIN, PARAM_MAX)
+                spin.setSingleStep((PARAM_MAX - PARAM_MIN) / PARAM_STEPS)
+                spin.setValue(PARAM_MIN)
+                self.spins.append(spin)
+                box.addWidget(spin, i, j)
+                spin.valueChanged.connect(parent.solve_update)
         self.setLayout(box)
-        self.slider.valueChanged.connect(parent.solve_update)
+
+    def enable(self, enable=True):
+        for spin in self.spins:
+            spin.isEnabled = enable
 
 class SimPanel(QtGui.QWidget):
     def __init__(self, parent, sim):
@@ -81,21 +94,31 @@ class SimPanel(QtGui.QWidget):
         box = QtGui.QHBoxLayout()
         self.fig = Figure(figsize=(300, 300), dpi=72, facecolor=(1, 1, 1), edgecolor=(0, 0, 0))
         self.ax = self.fig.add_subplot(111)
-        self.ax.plot([0, 1])
         # generate the canvas to display the plot
         self.canvas = FigureCanvas(self.fig)
         box.addWidget(self.canvas)
         box.addStretch()
-        self.param_panel = ParamRuler(self)
+        self.param_panel = ParamRuler(self, sim)
         box.addWidget(self.param_panel)
         self.setLayout(box)
 
     def solve_update(self):
-        U = self.sim.solve(self.sim.params[self.param_panel.slider.value()])
+        tic = time.time()
+        self.param_panel.enable(False)
+        args = self.sim.args
+        shape = (args['XBLOCKS'], args['YBLOCKS'])
+        mu = Parameter({'diffusion': np.array([s.value() for s in self.param_panel.spins]).reshape(shape)})
+        print(mu)
+        U = self.sim.solve(mu)
+        print('Simtime {}'.format(time.time() - tic))
+        tic = time.time()
         grid = self.sim.grid
+        self.ax.clear()
         self.ax.tripcolor(grid.centers(2)[:, 0], grid.centers(2)[:, 1], U)
-        # self.ax.colorbar()
+        self.fig.colorbar()
         self.canvas.draw()
+        self.param_panel.enable(True)
+        print('Drawtime {}'.format(time.time() - tic))
 
 
 class AllPanel(QtGui.QWidget):
@@ -120,30 +143,46 @@ class RBGui(QtGui.QMainWindow):
         assert args['--estimator-norm'] in {'trivial', 'h1'}
         args['--extension-alg'] = args['--extension-alg'].lower()
         assert args['--extension-alg'] in {'trivial', 'gram_schmidt'}
-        # reduced = ReducedSim(args)
-
+        reduced = ReducedSim(args)
         detailed = DetailedSim(args)
-        reduced = DetailedSim(args)
         panel = AllPanel(self, reduced, detailed)
         self.setCentralWidget(panel)
 
-class ReducedSim(object):
-    
+class SimBase(object):
     def __init__(self, args):
         self.args = args
-
-    def solve(self, mu):
-        pass
-
-class DetailedSim(object):
-
-    def __init__(self, args):
-        self.args = args
-        self.problem = ThermalBlockProblem(num_blocks=(args['XBLOCKS'], args['YBLOCKS']))
+        self.first = True
+        self.problem = ThermalBlockProblem(num_blocks=(args['XBLOCKS'], args['YBLOCKS']),
+                                           parameter_range=(PARAM_MIN, PARAM_MAX))
         self.discretization, pack = discretize_elliptic_cg(self.problem, diameter=m.sqrt(2) / args['--grid'])
         self.grid = pack['grid']
-        self.params = list(self.problem.parameter_space.sample_uniformly(PARAM_STEPS))
 
+class ReducedSim(SimBase):
+
+    def __init__(self, args):
+        super(ReducedSim, self).__init__(args)
+
+    def _first(self):
+        args = self.args
+        error_product = self.discretization.h1_product if args['--estimator-norm'] == 'h1' else None
+        reductor = partial(reduce_stationary_affine_linear, error_product=error_product)
+        extension_algorithm = (gram_schmidt_basis_extension if args['--extension-alg'] == 'gram_schmidt'
+                       else trivial_basis_extension)
+        greedy_data = greedy(self.discretization, reductor, self.discretization.parameter_space.sample_uniformly(args['SNAPSHOTS']),
+                            use_estimator=args['--with-estimator'], error_norm=self.discretization.h1_norm,
+                            extension_algorithm=extension_algorithm, max_extensions=args['RBSIZE'])
+        self.rb_discretization, self.reconstructor = greedy_data['reduced_discretization'], greedy_data['reconstructor']
+        self.first = False
+
+    def solve(self, mu):
+        if self.first:
+            self._first()
+        return self.reconstructor.reconstruct(self.rb_discretization.solve(mu))
+
+class DetailedSim(SimBase):
+
+    def __init__(self, args):
+        super(DetailedSim, self).__init__(args)
 
     def solve(self, mu):
         return self.discretization.solve(mu)
