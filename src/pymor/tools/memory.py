8f2839c71c981c3b8913ecdeb855b9346a6d69c9
Stephan Rave <stephanrave@uni-muenster.de>
2013-04-03 19:23:11 +0200

    [tools.memory] add memory_usage() and print_memory_usage()

--- src/pymor/tools/memory.py
+++ src/pymor/tools/memory.py
@@ -3,10 +3,41 @@ from __future__ import absolute_import, division, print_function
 from sys import getsizeof, stderr
 from itertools import chain
 from collections import deque
+import resource
 try:
     from reprlib import repr
 except ImportError:
     pass
+
+
+last_memory_usage = 0
+def memory_usage(unit='mb'):
+    '''Returns the memory usage of the current process in bytes
+
+    Returns
+    -------
+    usage
+        Current memory usage.
+    change
+        Change of memory usage since last call.
+    '''
+    unit = unit.lower()
+    assert unit in ('b', 'kb', 'mb', 'gb')
+    factors = {'b': 1,
+               'kb': 1024,
+               'mb': 1024**2,
+               'gb': 1024**3}
+    global last_memory_usage
+    x = last_memory_usage
+    last_memory_usage = resource.getrusage(resource.RUSAGE_SELF)[2] * 1024
+    return (last_memory_usage / factors[unit], (last_memory_usage - x) / factors[unit])
+
+def print_memory_usage(msg=None, unit='mb'):
+    u = memory_usage(unit)
+    if msg is None:
+        print('Memory usage {0:5.1f} {1} - delta: {2:5.1f} {1}'.format(u[0], unit.upper(), u[1]))
+    else:
+        print('Memory usage {0:5.1f} {1} - delta: {2:5.1f} {1} - {3}'.format(u[0], unit.upper(), u[1], msg))
 
 
 def total_size(o, handlers={}, verbose=False):
