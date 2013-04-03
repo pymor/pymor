0d89daae3795ac3b4f911528ebc3c1d89c336b8e
Felix Albrecht <felix.albrecht@uni-muenster.de>
2013-04-03 17:44:49 +0200

    [distribute_setup] removed header

--- distribute_setup.py
+++ distribute_setup.py
@@ -1,7 +1,4 @@
 #!python
-# pymor (http://www.pymor.org)
-# Copyright Holders: Felix Albrecht, Rene Milk, Stephan Rave
-# License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
 """Bootstrap distribute installation
 
