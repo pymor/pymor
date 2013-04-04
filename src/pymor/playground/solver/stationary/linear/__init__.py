1988958b72a460a5fa940f44edfca4b130c3355c
Rene Milk <rene.milk@uni-muenster.de>
2013-04-04 16:41:09 +0200

    clear out some obsolete playground modules

--- src/pymor/playground/solver/stationary/linear/__init__.py
+++
@@ -1,5 +0,0 @@
-# pymor (http://www.pymor.org)
-# Copyright Holders: Felix Albrecht, Rene Milk, Stephan Rave
-# License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
-
-import scalar
