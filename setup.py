03e4469b7ed93a51a807a6cf58646f1964fef541
Felix Albrecht <mail@felixalbrecht.de>
2013-04-10 00:21:46 +0200

    [setup] added missing numpydoc

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def setup_package():
 
     tests_requires = ['mock', 'nose-cov', 'nose', 'nosehtmloutput', 'nose-progressive', 'tissue>=0.8']
     install_requires = ['distribute', 'scipy', 'matplotlib', 'numpy', 'PyContracts',
-                        'docopt', 'dogpile.cache' ] + tests_requires
+                        'docopt', 'dogpile.cache' , 'numpydoc'] + tests_requires
     
     cmdclass = {}
     ext_modules = []
