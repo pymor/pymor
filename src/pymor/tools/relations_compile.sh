085e9a9e69391f82009d0c09b9fa863101fb0cc1
Rene Milk <rene.milk@uni-muenster.de>
2013-04-22 14:50:13 +0200

    rely on distutils to compile cython extension module, fixes #26

--- src/pymor/tools/relations_compile.sh
+++ src/pymor/tools/relations_compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cython -a relations.pyx
-gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I/usr/include/python2.7 -I$HOME/.virtualenvs/pyMor27/lib/python2.7/site-packages/numpy/core/include -o relations.so relations.c
+
+cd $(dirname ${0})/../../../
+python setup.py build_ext --inplace
