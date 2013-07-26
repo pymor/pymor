01064882a2100edd8068e058f07e0dc44ac1de92
Stephan Rave <stephanrave@uni-muenster.de>
2013-07-26 20:08:17 +0200

    [discretizations] move StationaryLinearDiscretization to basic module

--- src/pymor/discretizations/__init__.py
+++ src/pymor/discretizations/__init__.py
@@ -3,5 +3,5 @@
 # License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
 from pymor.discretizations.interfaces import DiscretizationInterface
-from pymor.discretizations.linear import StationaryLinearDiscretization
+from pymor.discretizations.basic import StationaryLinearDiscretization
 from pymor.discretizations.nonlinear import InstationaryNonlinearDiscretization
