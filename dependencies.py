19182fbd852429e70df461ff979586047d84371e
Stephan Rave <stephanrave@uni-muenster.de>
2013-07-31 18:47:48 +0200

    [tests] update dependencies

--- dependencies.py
+++ dependencies.py
@@ -1,12 +1,11 @@
-tests_require = ['mock', 'nose-cov', 'nose', 'nosehtmloutput', 'nose-progressive', 'pep8', 'tissue']
+tests_require = ['pytest', 'pytest-cache', 'pytest-capturelog', 'pytest-instafail']
 install_requires = ['Cython', 'numpy', 'scipy', 'PyContracts', 'PyOpenGL', 'PySide', 'Sphinx',
                     'docopt', 'dogpile.cache' , 'glumpy', 'numpydoc']
 pre_setup_requires = ['cython', 'numpy']
 setup_requires = pre_setup_requires + ['nose']
 install_suggests = ['ipython', 'ipdb', 'matplotlib', 'sympy'] + tests_require
 
-import_names = {'nose-progressive': 'noseprogressive', 'nose-cov': 'nose_cov',
-                'nosehtmloutput': 'htmloutput', 'ipython': 'IPython'}
+import_names = {'ipython': 'IPython'}
 
 if __name__ == '__main__':
     print(' '.join([i for i in install_requires + install_suggests]))
