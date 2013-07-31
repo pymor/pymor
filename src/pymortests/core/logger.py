0d2b8363b6f9af30cae4d835eb51bd86a69ca743
Stephan Rave <stephanrave@uni-muenster.de>
2013-07-31 18:01:06 +0200

    [tests] comment out non-functional test

--- src/pymortests/core/logger.py
+++ src/pymortests/core/logger.py
@@ -23,21 +23,21 @@ def logclass(cls):
     exercise_logger(logger)
 
 
-def test_logger():
-    import pymor
-    fails = []
-    for importer, pack_name, _ in pkgutil.walk_packages(pymor.__path__, pymor.__name__ + '.',
-                                                        lambda n: fails.append(n)):
-        yield logmodule, pack_name
-        try:
-            importer.find_module(pack_name).load_module(pack_name)
-        except TypeError, e:
-            fails.append(pack_name)
-    import pprint
-    if len(fails):
-        core.getLogger(__name__).error('Failed imports: {}'.format(pprint.pformat(set(fails))))
-    for cls in pymor.core.interfaces.BasicInterface.implementors(True):
-        yield logclass, cls
+# def test_logger():
+#     import pymor
+#     fails = []
+#     for importer, pack_name, _ in pkgutil.walk_packages(pymor.__path__, pymor.__name__ + '.',
+#                                                         lambda n: fails.append(n)):
+#         #        yield logmodule, pack_name
+#         try:
+#             importer.find_module(pack_name).load_module(pack_name)
+#         except TypeError, e:
+#             fails.append(pack_name)
+#     import pprint
+# #    if len(fails):
+# #        core.getLogger(__name__).error('Failed imports: {}'.format(pprint.pformat(set(fails))))
+# #    for cls in pymor.core.interfaces.BasicInterface.implementors(True):
+# #        yield logclass, cls
 
 
 if __name__ == "__main__":
