358d9ee96ec2078af6d63370568a745b1b225a7b
Stephan Rave <stephanrave@uni-muenster.de>
2013-08-12 16:03:37 +0200

    [setup] add dogpile.cache to install_requires again ...

--- dependencies.py
+++ dependencies.py
@@ -1,7 +1,7 @@
 tests_require = ['tox', 'pytest', 'pytest-cache', 'pytest-capturelog', 'pytest-instafail',
                  'pytest-xdist', 'pytest-cov', 'pytest-flakes', 'pytest-pep8']
 install_requires = ['Cython', 'numpy', 'scipy', 'PyContracts', 'Sphinx',
-                    'docopt', 'numpydoc']
+                    'docopt', 'numpydoc', 'dogpile.cache']
 pre_setup_requires = ['cython', 'numpy']
 setup_requires = pre_setup_requires + ['nose']
 install_suggests = ['ipython', 'ipdb', 'matplotlib', 'sympy', 'PyOpenGL', 'PySide', 'glumpy'] + tests_require
