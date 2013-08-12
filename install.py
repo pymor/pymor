7b17bc7d0da78e90135f68678044b94cb2fccac6
Stephan Rave <stephanrave@uni-muenster.de>
2013-08-12 14:37:40 +0200

    [travis] run apt-get update before installation

--- install.py
+++ install.py
@@ -24,7 +24,8 @@ UBUNTU_12_04_RECIPE = {'name': 'Ubuntu 12.04',
 
 _travis_local = [i for i in deps.install_requires + deps.tests_require if i not in ['PySide', 'matplotlib']]
 TRAVIS_RECIPE = {'name': 'travis',
-                       'system': [  'sudo apt-get install -qq python-virtualenv python-numpy python-scipy python-sympy '
+                       'system': [  'sudo apt-get update',
+                                    'sudo apt-get install -qq python-virtualenv python-numpy python-scipy python-sympy '
                                   + 'python3-numpy python3-scipy python3-pyside python-pyside' ],
                        'local': _travis_local,
                        'venv_cmd': []}
