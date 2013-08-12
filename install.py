b70db970508ae48ffe3a102237cacc7a0dac1e7e
Stephan Rave <stephanrave@uni-muenster.de>
2013-08-12 22:37:07 +0200

    [travis] make apt-get install call more verbose
    The travis build often fails at this point.

--- install.py
+++ install.py
@@ -24,7 +24,7 @@ UBUNTU_12_04_RECIPE = {'name': 'Ubuntu 12.04',
 
 TRAVIS_RECIPE = {'name': 'travis',
                        'system': [  'sudo apt-get update',
-                                    'sudo apt-get install -qq python-virtualenv python-numpy python-scipy python-sympy '
+                                    'sudo apt-get install -y python-virtualenv python-numpy python-scipy python-sympy '
                                   + 'python3-numpy python3-scipy python3-pyside python-pyside' ],
                        'local': deps.install_requires + deps.tests_require,
                        'venv_cmd': []}
