0aa312c238647ca73f06e6d564619d0cea20488a
Stephan Rave <stephanrave@uni-muenster.de>
2013-08-12 15:48:43 +0200

    [setup] remove dogpile.cache from install_requires

--- dependencies.py
+++ dependencies.py
@@ -1,7 +1,7 @@
 tests_require = ['tox', 'pytest', 'pytest-cache', 'pytest-capturelog', 'pytest-instafail',
                  'pytest-xdist', 'pytest-cov', 'pytest-flakes', 'pytest-pep8']
 install_requires = ['Cython', 'numpy', 'scipy', 'PyContracts', 'PyOpenGL', 'PySide', 'Sphinx',
-                    'docopt', 'dogpile.cache' , 'glumpy', 'numpydoc']
+                    'docopt', 'glumpy', 'numpydoc']
 pre_setup_requires = ['cython', 'numpy']
 setup_requires = pre_setup_requires + ['nose']
 install_suggests = ['ipython', 'ipdb', 'matplotlib', 'sympy'] + tests_require
