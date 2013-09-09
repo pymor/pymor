fb413f2bd3492f39801705a85e409d50e3894717
Stephan Rave <stephanrave@uni-muenster.de>
2013-09-09 11:49:09 +0200

    [core.interfaces] use _with_via_init by default

--- src/pymor/core/interfaces.py
+++ src/pymor/core/interfaces.py
@@ -154,18 +154,7 @@ class BasicInterface(object):
         -------
         Copy of `self` with changed attributes.
         '''
-        if not set(kwargs.keys()) <= self.with_arguments:
-            raise ConstError('Changing "{}" using with() is not allowed in {} (only "{}")'.format(
-                kwargs.keys(), self.__class__, self.with_arguments))
-        c = copy.copy(self)
-        locked = c._locked
-        self._locked = False
-        for k, v in kwargs.iteritems():
-            setattr(c, k, v)
-        if c._added_attributes is not None:
-            c._added_attributes = list(c._added_attributes)
-        c._locked = locked
-        return c
+        return self._with_via_init(kwargs)
 
     def _with_via_init(self, kwargs, new_class=None):
         '''Default implementation for with_ by calling __init__.
