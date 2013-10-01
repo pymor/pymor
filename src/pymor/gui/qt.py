3120d098df2957b985402fd23ff77a7720b0a04d
Stephan Rave <stephanrave@uni-muenster.de>
2013-10-01 16:53:11 +0200

    [gui.qt] refactor visualize_glumpy_patch

--- src/pymor/gui/qt.py
+++ src/pymor/gui/qt.py
@@ -17,143 +17,155 @@ from pymor.grids import RectGrid, TriaGrid
 from pymor.gui.glumpy import GlumpyPatchWidget, ColorBarWidget
 
 
-def visualize_glumpy_patch(grid, U, bounding_box=[[0, 0], [1, 1]], codim=2):
+class PlotMainWindow(QWidget):
+    def __init__(self, U, plot):
+        super(PlotMainWindow, self).__init__()
 
-    class MainWindow(QWidget):
-        def __init__(self, grid, U):
-            assert isinstance(U, Communicable)
-            super(MainWindow, self).__init__()
+        layout = QVBoxLayout()
+        layout.addWidget(plot)
 
-            U = U.data
+        if len(U) == 1:
+            plot.set(U.ravel())
+        else:
+            plot.set(U[0])
 
-            layout = QVBoxLayout()
+            hlayout = QHBoxLayout()
 
-            plotBox = QHBoxLayout()
-            plot = GlumpyPatchWidget(self, grid, vmin=np.min(U), vmax=np.max(U), bounding_box=bounding_box, codim=codim)
-            bar = ColorBarWidget(self, vmin=np.min(U), vmax=np.max(U))
-            plotBox.addWidget(plot)
-            plotBox.addWidget(bar)
-            layout.addLayout(plotBox)
+            self.slider = QSlider(Qt.Horizontal)
+            self.slider.setMinimum(0)
+            self.slider.setMaximum(len(U) - 1)
+            self.slider.setTickPosition(QSlider.TicksBelow)
+            hlayout.addWidget(self.slider)
 
-            if len(U) == 1:
-                plot.set(U.ravel())
+            lcd = QLCDNumber(m.ceil(m.log10(len(U))))
+            lcd.setDecMode()
+            lcd.setSegmentStyle(QLCDNumber.Flat)
+            hlayout.addWidget(lcd)
+
+            layout.addLayout(hlayout)
+
+            hlayout = QHBoxLayout()
+
+            toolbar = QToolBar()
+            self.a_play = QAction(self.style().standardIcon(QStyle.SP_MediaPlay), 'Play', self)
+            self.a_play.setCheckable(True)
+            self.a_rewind = QAction(self.style().standardIcon(QStyle.SP_MediaSeekBackward), 'Rewind', self)
+            self.a_toend = QAction(self.style().standardIcon(QStyle.SP_MediaSeekForward), 'End', self)
+            self.a_step_backward = QAction(self.style().standardIcon(QStyle.SP_MediaSkipBackward), 'Step Back', self)
+            self.a_step_forward = QAction(self.style().standardIcon(QStyle.SP_MediaSkipForward), 'Step', self)
+            self.a_loop = QAction(self.style().standardIcon(QStyle.SP_BrowserReload), 'Loop', self)
+            self.a_loop.setCheckable(True)
+            toolbar.addAction(self.a_play)
+            toolbar.addAction(self.a_rewind)
+            toolbar.addAction(self.a_toend)
+            toolbar.addAction(self.a_step_backward)
+            toolbar.addAction(self.a_step_forward)
+            toolbar.addAction(self.a_loop)
+            hlayout.addWidget(toolbar)
+
+            self.speed = QSlider(Qt.Horizontal)
+            self.speed.setMinimum(0)
+            self.speed.setMaximum(100)
+            hlayout.addWidget(QLabel('Speed:'))
+            hlayout.addWidget(self.speed)
+
+            layout.addLayout(hlayout)
+
+            self.timer = QTimer()
+            self.timer.timeout.connect(self.update_solution)
+
+            self.slider.valueChanged.connect(self.slider_changed)
+            self.slider.valueChanged.connect(lcd.display)
+            self.speed.valueChanged.connect(self.speed_changed)
+            self.a_play.toggled.connect(self.toggle_play)
+            self.a_rewind.triggered.connect(self.rewind)
+            self.a_toend.triggered.connect(self.to_end)
+            self.a_step_forward.triggered.connect(self.step_forward)
+            self.a_step_backward.triggered.connect(self.step_backward)
+
+            self.speed.setValue(50)
+
+        self.setLayout(layout)
+        self.plot = plot
+        self.U = U
+
+    def slider_changed(self, ind):
+        self.plot.set(self.U[ind])
+
+    def speed_changed(self, val):
+        self.timer.setInterval(val * 20)
+
+    def update_solution(self):
+        ind = self.slider.value() + 1
+        if ind >= len(self.U):
+            if self.a_loop.isChecked():
+                ind = 0
             else:
-                plot.set(U[0])
+                self.a_play.setChecked(False)
+                return
+        self.slider.setValue(ind)
 
-                hlayout = QHBoxLayout()
+    def toggle_play(self, checked):
+        if checked:
+            if self.slider.value() + 1 == len(self.U):
+                self.slider.setValue(0)
+            self.timer.start()
+        else:
+            self.timer.stop()
 
-                self.slider = QSlider(Qt.Horizontal)
-                self.slider.setMinimum(0)
-                self.slider.setMaximum(len(U) - 1)
-                self.slider.setTickPosition(QSlider.TicksBelow)
-                hlayout.addWidget(self.slider)
+    def rewind(self):
+        self.slider.setValue(0)
 
-                lcd = QLCDNumber(m.ceil(m.log10(len(U))))
-                lcd.setDecMode()
-                lcd.setSegmentStyle(QLCDNumber.Flat)
-                hlayout.addWidget(lcd)
+    def to_end(self):
+        self.a_play.setChecked(False)
+        self.slider.setValue(len(self.U) - 1)
 
-                layout.addLayout(hlayout)
-
-                hlayout = QHBoxLayout()
-
-                toolbar = QToolBar()
-                self.a_play = QAction(self.style().standardIcon(QStyle.SP_MediaPlay), 'Play', self)
-                self.a_play.setCheckable(True)
-                self.a_rewind = QAction(self.style().standardIcon(QStyle.SP_MediaSeekBackward), 'Rewind', self)
-                self.a_toend = QAction(self.style().standardIcon(QStyle.SP_MediaSeekForward), 'End', self)
-                self.a_step_backward = QAction(self.style().standardIcon(QStyle.SP_MediaSkipBackward), 'Step Back', self)
-                self.a_step_forward = QAction(self.style().standardIcon(QStyle.SP_MediaSkipForward), 'Step', self)
-                self.a_loop = QAction(self.style().standardIcon(QStyle.SP_BrowserReload), 'Loop', self)
-                self.a_loop.setCheckable(True)
-                toolbar.addAction(self.a_play)
-                toolbar.addAction(self.a_rewind)
-                toolbar.addAction(self.a_toend)
-                toolbar.addAction(self.a_step_backward)
-                toolbar.addAction(self.a_step_forward)
-                toolbar.addAction(self.a_loop)
-                hlayout.addWidget(toolbar)
-
-                self.speed = QSlider(Qt.Horizontal)
-                self.speed.setMinimum(0)
-                self.speed.setMaximum(100)
-                hlayout.addWidget(QLabel('Speed:'))
-                hlayout.addWidget(self.speed)
-
-                layout.addLayout(hlayout)
-
-                self.timer = QTimer()
-                self.timer.timeout.connect(self.update_solution)
-
-                self.slider.valueChanged.connect(self.slider_changed)
-                self.slider.valueChanged.connect(lcd.display)
-                self.speed.valueChanged.connect(self.speed_changed)
-                self.a_play.toggled.connect(self.toggle_play)
-                self.a_rewind.triggered.connect(self.rewind)
-                self.a_toend.triggered.connect(self.to_end)
-                self.a_step_forward.triggered.connect(self.step_forward)
-                self.a_step_backward.triggered.connect(self.step_backward)
-
-                self.speed.setValue(50)
-
-            self.setLayout(layout)
-            self.plot = plot
-            self.U = U
-
-        def slider_changed(self, ind):
-            self.plot.set(self.U[ind])
-
-        def speed_changed(self, val):
-            self.timer.setInterval(val * 20)
-
-        def update_solution(self):
-            ind = self.slider.value() + 1
-            if ind >= len(self.U):
-                if self.a_loop.isChecked():
-                    ind = 0
-                else:
-                    self.a_play.setChecked(False)
-                    return
+    def step_forward(self):
+        self.a_play.setChecked(False)
+        ind = self.slider.value() + 1
+        if ind == len(self.U) and self.a_loop.isChecked():
+            ind = 0
+        if ind < len(self.U):
             self.slider.setValue(ind)
 
-        def toggle_play(self, checked):
-            if checked:
-                if self.slider.value() + 1 == len(self.U):
-                    self.slider.setValue(0)
-                self.timer.start()
-            else:
-                self.timer.stop()
+    def step_backward(self):
+        self.a_play.setChecked(False)
+        ind = self.slider.value() - 1
+        if ind == -1 and self.a_loop.isChecked():
+            ind = len(self.U) - 1
+        if ind >= 0:
+            self.slider.setValue(ind)
 
-        def rewind(self):
-            self.slider.setValue(0)
 
-        def to_end(self):
-            self.a_play.setChecked(False)
-            self.slider.setValue(len(self.U) - 1)
+def visualize_glumpy_patch(grid, U, bounding_box=[[0, 0], [1, 1]], codim=2):
 
-        def step_forward(self):
-            self.a_play.setChecked(False)
-            ind = self.slider.value() + 1
-            if ind == len(self.U) and self.a_loop.isChecked():
-                ind = 0
-            if ind < len(self.U):
-                self.slider.setValue(ind)
+    class MainWindow(PlotMainWindow):
+        def __init__(self, grid, U, bounding_box, codim):
+            assert isinstance(U, Communicable)
+            U = U.data
 
-        def step_backward(self):
-            self.a_play.setChecked(False)
-            ind = self.slider.value() - 1
-            if ind == -1 and self.a_loop.isChecked():
-                ind = len(self.U) - 1
-            if ind >= 0:
-                self.slider.setValue(ind)
+            class PlotWidget(QWidget):
+                def __init__(self):
+                    super(PlotWidget, self).__init__()
+                    layout = QHBoxLayout()
+                    plot = GlumpyPatchWidget(self, grid, vmin=np.min(U), vmax=np.max(U), bounding_box=bounding_box, codim=codim)
+                    bar = ColorBarWidget(self, vmin=np.min(U), vmax=np.max(U))
+                    layout.addWidget(plot)
+                    layout.addWidget(bar)
+                    self.setLayout(layout)
+                    self.plot = plot
 
+                def set(self, U):
+                    self.plot.set(U)
+
+            super(MainWindow, self).__init__(U, PlotWidget())
 
     try:
         app = QApplication([])
     except RuntimeError:
         app = QCoreApplication.instance()
 
-    win = MainWindow(grid, U)
+    win = MainWindow(grid, U, bounding_box, codim)
     win.show()
     app.exec_()
 
