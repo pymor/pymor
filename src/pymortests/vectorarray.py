33347b4476f134422368aebe047dbefa2b6fca6a
Stephan Rave <stephanrave@uni-muenster.de>
2013-10-13 00:43:07 +0200

    [tests] improve VectorArray tests

--- src/pymortests/vectorarray.py
+++ src/pymortests/vectorarray.py
@@ -1,11 +1,12 @@
 from functools import partial
 from itertools import product
+from numbers import Number
 
 import pytest
 import numpy as np
 
 from pymor.la import NumpyVectorArray
-from pymor.la.listvectorarray import NumpyListVectorArray
+from pymor.la.listvectorarray import NumpyListVectorArray, NumpyVector
 
 
 def random_integers(count, seed):
@@ -20,34 +21,65 @@ def numpy_vector_array_factory(length, dim, seed):
 
 def numpy_list_vector_array_factory(length, dim, seed):
     np.random.seed(seed)
-    return NumpyListVectorArray(list(np.random.random((length, dim))), copy=False)
+    return NumpyListVectorArray([NumpyVector(v, copy=False) for v in np.random.random((length, dim))], copy=False)
 
+numpy_vector_array_factory_arguments = \
+    zip([0, 0, 1, 43, 1024],
+        [0, 10, 34, 32, 0],
+        random_integers(4, 123))
+
+numpy_vector_array_factory_arguments_pairs_with_same_dim = \
+    zip([0, 0, 1, 43, 1024],
+        [0, 1, 37, 9, 104],
+        [0, 10, 34, 32, 3],
+        random_integers(4, 1234),
+        random_integers(4, 1235))
+
+numpy_vector_array_factory_arguments_pairs_with_different_dim = \
+    zip([0, 0, 1, 43, 1024],
+        [0, 1, 1, 9, 104],
+        [0, 10, 34, 32, 3],
+        [1, 11, 0, 33, 2],
+        random_integers(4, 1234),
+        random_integers(4, 1235))
 
 numpy_vector_array_generators = \
-    [lambda: numpy_vector_array_factory(l, d, s) for l, d, s in zip([0, 0, 1, 43, 1024],
-                                                                    [0, 10, 34, 32, 0],
-                                                                    random_integers(4, 123))]
+    [lambda: numpy_vector_array_factory(*args) for args in numpy_vector_array_factory_arguments]
 
 numpy_list_vector_array_generators = \
-    [lambda: numpy_vector_array_factory(l, d, s) for l, d, s in zip([0, 0, 1, 43, 1024],
-                                                                    [0, 10, 34, 32, 0],
-                                                                    random_integers(4, 123))]
+    [lambda: numpy_list_vector_array_factory(*args) for args in numpy_vector_array_factory_arguments]
 
 numpy_vector_array_pair_with_same_dim_generators = \
     [lambda: (numpy_vector_array_factory(l, d, s1), numpy_vector_array_factory(l2, d, s2))
-     for l, l2, d, s1, s2 in zip([0, 0, 1, 43, 1024],
-                                 [0, 1, 37, 9, 104],
-                                 [0, 10, 34, 32, 3],
-                                 random_integers(4, 1234),
-                                 random_integers(4, 1235))]
+     for l, l2, d, s1, s2 in numpy_vector_array_factory_arguments_pairs_with_same_dim]
 
 numpy_list_vector_array_pair_with_same_dim_generators = \
-    [lambda: (numpy_vector_array_factory(l, d, s1), numpy_vector_array_factory(l2, d, s2))
-     for l, l2, d, s1, s2 in zip([0, 0, 1, 43, 1024],
-                                 [0, 1, 37, 9, 104],
-                                 [0, 10, 34, 32, 3],
-                                 random_integers(4, 1234),
-                                 random_integers(4, 1235))]
+    [lambda: (numpy_list_vector_array_factory(l, d, s1), numpy_list_vector_array_factory(l2, d, s2))
+     for l, l2, d, s1, s2 in numpy_vector_array_factory_arguments_pairs_with_same_dim]
+
+numpy_vector_array_pair_with_different_dim_generators = \
+    [lambda: (numpy_vector_array_factory(l, d1, s1), numpy_vector_array_factory(l2, d2, s2))
+     for l, l2, d1, d2, s1, s2 in numpy_vector_array_factory_arguments_pairs_with_different_dim]
+
+numpy_list_vector_array_pair_with_different_dim_generators = \
+    [lambda: (numpy_list_vector_array_factory(l, d1, s1), numpy_list_vector_array_factory(l2, d2, s2))
+     for l, l2, d1, d2, s1, s2 in numpy_vector_array_factory_arguments_pairs_with_different_dim]
+
+
+def invalid_inds(v):
+    yield len(v)
+    yield [len(v)]
+    yield -len(v) - 1
+    yield [-len(v) - 1]
+    yield [0, len(v)]
+
+
+def valid_inds(v):
+    for ind in [None, range(len(v)), range(int(len(v)/2)), range(len(v)) * 2]:
+        yield ind
+    if len(v) > 0:
+        for ind in [0, len(v) - 1, -len(v), range(-len(v) + 1, 0)]:
+            yield ind
 
 
 @pytest.fixture(params = numpy_vector_array_generators + numpy_list_vector_array_generators)
@@ -58,6 +90,12 @@ def vector_array(request):
 @pytest.fixture(params = numpy_vector_array_pair_with_same_dim_generators +
                          numpy_list_vector_array_pair_with_same_dim_generators)
 def vector_array_pair_with_same_dim(request):
+    return request.param()
+
+
+@pytest.fixture(params = numpy_vector_array_pair_with_different_dim_generators +
+                         numpy_list_vector_array_pair_with_different_dim_generators)
+def vector_array_pair_with_different_dim(request):
     return request.param()
 
 
@@ -97,15 +135,20 @@ def test_shape(vector_array):
 
 
 def test_copy(vector_array):
-    with pytest.raises(Exception):
-        vector_array.copy(len(vector_array))
-    with pytest.raises(Exception):
-        vector_array.copy([len(vector_array)])
-    for ind in (None, range(len(vector_array)), range(int(len(vector_array)/2)), range(len(vector_array)) * 2):
-        c = vector_array.copy(ind=ind)
-        assert len(c) == len(vector_array) if ind is None else len(c) == len(ind)
-        assert c.dim == vector_array.dim
-        assert np.all(c.almost_equal(vector_array, o_ind=ind))
+    v = vector_array
+    for ind in invalid_inds(v):
+        with pytest.raises(Exception):
+            v.copy(ind)
+    for ind in valid_inds(v):
+        c = v.copy(ind=ind)
+        if ind is None:
+            assert len(c) == len(v)
+        elif isinstance(ind, Number):
+            assert len(c) == 1
+        else:
+            assert len(c) == len(ind)
+        assert c.dim == v.dim
+        assert np.all(c.almost_equal(v, o_ind=ind))
 
 
 def test_copy_repeated_index(vector_array):
@@ -122,35 +165,25 @@ def test_copy_repeated_index(vector_array):
 
 def test_append(vector_array_pair_with_same_dim):
     v1, v2 = vector_array_pair_with_same_dim
-    with pytest.raises(Exception):
-        v1.append(v2, o_ind=len(v2))
-    with pytest.raises(Exception):
-        v1.append(v2, o_ind=[len(v2)])
+    for ind in invalid_inds(v2):
+        with pytest.raises(Exception):
+            v1.append(v2, o_ind=ind)
     len_v1 = len(v1)
     len_v2 = len(v2)
-    v1.append(v2)
-    assert len(v1) == len_v1 + len_v2
-    assert np.all(v1.almost_equal(v2, ind=range(len_v1, len(v1))))
-    v1.append(v2, remove_from_other=True)
-    assert len(v2) == 0
-    assert v2.dim == v1.dim
-    assert len(v1) == len_v1 + 2 * len_v2
-    assert np.all(v1.almost_equal(v1, ind=range(len_v1, len_v1 + len_v2), o_ind=range(len_v1 + len_v2, len(v1))))
-
-
-def test_append_2(vector_array_pair_with_same_dim):
-    v1, v2 = vector_array_pair_with_same_dim
-    ind = range(int(len(v2) * 3 / 4)) * 2
-    len_v1 = len(v1)
-    len_v2 = len(v2)
-    v1.append(v2, o_ind=ind)
-    assert len(v1) == len_v1 + len(ind)
-    assert len(v2) == len_v2
-    assert np.all(v1.almost_equal(v2, ind=range(len_v1, len(v1)), o_ind=ind))
-    v1.append(v2, o_ind=ind, remove_from_other=True)
-    assert len(v1) == len_v1 + 2*len(ind)
-    assert len(v2) == len_v2 - len(ind) / 2
-    assert np.all(v1.almost_equal(v1, ind=range(len_v1, len_v1 + len(ind)), o_ind=range(len_v1 + len(ind), len(v1))))
+    for ind in valid_inds(v2):
+        c1 = v1.copy()
+        c1.append(v2, o_ind=ind)
+        len_ind = len_v2 if ind is None else 1 if isinstance(ind, Number) else len(ind)
+        len_ind_unique = len_v2 if ind is None else 1 if isinstance(ind, Number) \
+                              else len(set(i % len_v2 for i in ind))
+        assert len(c1) == len_v1 + len_ind
+        assert np.all(c1.almost_equal(v2, ind=range(len_v1, len(c1)), o_ind=ind))
+        c2 = v2.copy()
+        c1.append(c2, o_ind=ind, remove_from_other=True)
+        assert len(c2) == len_v2 - len_ind_unique
+        assert c2.dim == c1.dim
+        assert len(c1) == len_v1 + 2 * len_ind
+        assert np.all(c1.almost_equal(c1, ind=range(len_v1, len_v1 + len_ind), o_ind=range(len_v1 + len_ind, len(c1))))
 
 
 def test_append_self(vector_array):
@@ -164,9 +197,22 @@ def test_append_self(vector_array):
         v.append(v, remove_from_other=True)
 
 
+def test_append_wrong_dim(vector_array_pair_with_different_dim):
+    v1, v2 = vector_array_pair_with_different_dim
+    with pytest.raises(Exception):
+        v1.append(v2, remove_from_other=False)
+    with pytest.raises(Exception):
+        v1.append(v2, remove_from_other=True)
+    with pytest.raises(Exception):
+        v1.append(v2, ind=0)
+
+
 def test_remove(vector_array):
     v = vector_array
     c = v.copy()
+    for ind in invalid_inds(v):
+        with pytest.raises(Exception):
+            v.remove(ind)
     ind = range(int(len(v) * 3 / 4)) * 2
     v.remove(ind)
     assert v.dim == c.dim
