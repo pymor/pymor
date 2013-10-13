fcc1d712a61301a57deb451970b3eb656c40c6e6
Stephan Rave <stephanrave@uni-muenster.de>
2013-10-13 23:37:02 +0200

    [la.interfaces] bugfix

--- src/pymor/la/interfaces.py
+++ src/pymor/la/interfaces.py
@@ -455,4 +455,5 @@ class VectorArrayInterface(BasicInterface):
         return (ind is None or
                 isinstance(ind, Number) and 0 <= ind < len(self) or
                 isinstance(ind, list) and (len(ind) == 0 or 0 <= min(ind) and max(ind) < len(self)) or
-                isinstance(ind, np.ndarray) and ind.ndim == 1 and 0 <= np.min(ind) and np.max(ind) < len(self))
+                isinstance(ind, np.ndarray) and ind.ndim == 1
+                                            and (len(ind) == 0 or 0 <= np.min(ind) and np.max(ind) < len(self)))
