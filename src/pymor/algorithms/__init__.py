5d2ab0fe94bcb8ae10459351a2036e8b553185f2
Felix Albrecht <mail@felixalbrecht.de>
2013-11-14 23:37:57 +0100

    [algorithms] add pod_basis_extension on import

--- src/pymor/algorithms/__init__.py
+++ src/pymor/algorithms/__init__.py
@@ -2,5 +2,5 @@
 # Copyright Holders: Felix Albrecht, Rene Milk, Stephan Rave
 # License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
-from pymor.algorithms.basisextension import trivial_basis_extension, gram_schmidt_basis_extension
+from pymor.algorithms.basisextension import trivial_basis_extension, gram_schmidt_basis_extension, pod_basis_extension
 from pymor.algorithms.greedy import greedy
