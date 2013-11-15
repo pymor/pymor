d5ea95fdf379f1099ae09b12941047a940e28da5
Stephan Rave <stephanrave@uni-muenster.de>
2013-11-15 14:50:50 +0100

    [tests] more VectorArray tests

--- src/pymortests/vectorarray.py
+++ src/pymortests/vectorarray.py
@@ -5,7 +5,7 @@ from numbers import Number
 import pytest
 import numpy as np
 
-from pymor.la import NumpyVectorArray
+from pymor.la import NumpyVectorArray, Communicable
 from pymor.la.listvectorarray import NumpyListVectorArray, NumpyVector
 
 
@@ -44,6 +44,7 @@ numpy_vector_array_factory_arguments = \
         random_integers(5, 123))   # seed
 
 numpy_vector_array_factory_arguments_pairs_with_same_dim = \
+    [[2, 2, 13, 3, 3]] + \
     zip([0,  0,  1, 43, 102],      # len1
         [0,  1, 37,  9, 104],      # len2
         [0, 10, 34, 32,   3],      # dim
@@ -87,6 +88,14 @@ def ind_complement(v, ind):
     if isinstance(ind, Number):
         ind = [ind]
     return sorted(set(xrange(len(v))) - set(ind))
+
+def indexed(v, ind):
+    if ind is None:
+        return v
+    elif isinstance(ind, Number):
+        return v[[ind]]
+    else:
+        return v[ind]
 
 
 def invalid_inds(v, length=None):
@@ -207,10 +216,13 @@ def test_empty(VectorArray):
         VectorArray.empty(-1)
     with pytest.raises(Exception):
         VectorArray.empty(1, reserve=-1)
-    for d, r in product((0, 1, 2, 10, 100), (0, 1, 100)):
-        v = VectorArray.empty(d, reserve=r)
-        assert v.dim == d
+    for dim, r in product((0, 1, 2, 10, 100), (0, 1, 100)):
+        v = VectorArray.empty(dim, reserve=r)
+        assert v.dim == dim
         assert len(v) == 0
+        if isinstance(v, Communicable):
+            d = v.data
+            assert d.shape == (0, dim)
 
 
 def test_zeros(VectorArray):
@@ -218,18 +230,26 @@ def test_zeros(VectorArray):
         VectorArray.zeros(-1)
     with pytest.raises(Exception):
         VectorArray.zeros(1, count=-1)
-    for d, c in product((0, 1, 10, 100), (0, 1, 2, 30)):
-        v = VectorArray.zeros(d, count=c)
-        assert v.dim == d
+    for dim, c in product((0, 1, 10, 100), (0, 1, 2, 30)):
+        v = VectorArray.zeros(dim, count=c)
+        assert v.dim == dim
         assert len(v) == c
-        if min(d, c) > 0:
+        if min(dim, c) > 0:
             assert max(v.sup_norm()) == 0
             assert max(v.l2_norm()) == 0
+        if isinstance(v, Communicable):
+            d = v.data
+            assert d.shape == (c, dim)
+            assert np.allclose(d, np.zeros((c, dim)))
 
 
 def test_shape(vector_array):
+    v = vector_array
     assert len(vector_array) >= 0
     assert vector_array.dim >= 0
+    if isinstance(v, Communicable):
+        d = v.data
+        assert d.shape == (len(v), v.dim)
 
 
 def test_copy(vector_array):
@@ -239,6 +259,10 @@ def test_copy(vector_array):
         assert len(c) == v.len_ind(ind)
         assert c.dim == v.dim
         assert np.all(c.almost_equal(v, o_ind=ind))
+        if isinstance(v, Communicable):
+            dv = v.data
+            dc = c.data
+            assert np.allclose(dc, indexed(dv, ind))
 
 
 def test_copy_repeated_index(vector_array):
@@ -249,13 +273,24 @@ def test_copy_repeated_index(vector_array):
     c = v.copy(ind)
     assert c.almost_equal(v, ind=0, o_ind=ind[0])
     assert c.almost_equal(v, ind=1, o_ind=ind[0])
+    if isinstance(v, Communicable):
+        dv = indexed(v.data, ind)
+        dc = c.data
+        assert dv.shape == dc.shape
     c.scal(2., ind=0)
     assert c.almost_equal(v, ind=1, o_ind=ind[0])
     assert c.l2_norm(ind=0) == 2 * v.l2_norm(ind=ind[0])
+    if isinstance(v, Communicable):
+        dv = indexed(v.data, ind)
+        dc = c.data
+        assert dv.shape == dc.shape
 
 
 def test_append(vector_array_pair_with_same_dim):
     v1, v2 = vector_array_pair_with_same_dim
+    if isinstance(v1, Communicable):
+        dv1 = v1.data
+        dv2 = v2.data
     len_v1, len_v2 = len(v1), len(v2)
     for ind in valid_inds(v2):
         c1, c2 = v1.copy(), v2.copy()
@@ -264,20 +299,27 @@ def test_append(vector_array_pair_with_same_dim):
         ind_complement_ = ind_complement(v2, ind)
         assert len(c1) == len_v1 + len_ind
         assert np.all(c1.almost_equal(c2, ind=range(len_v1, len(c1)), o_ind=ind))
+        if isinstance(v1, Communicable):
+            assert np.allclose(c1.data, np.vstack((dv1, indexed(dv2, ind))))
         c1.append(c2, o_ind=ind, remove_from_other=True)
         assert len(c2) == len(ind_complement_)
         assert c2.dim == c1.dim
         assert len(c1) == len_v1 + 2 * len_ind
         assert np.all(c1.almost_equal(c1, ind=range(len_v1, len_v1 + len_ind), o_ind=range(len_v1 + len_ind, len(c1))))
         assert np.all(c2.almost_equal(v2, o_ind=ind_complement_))
+        if isinstance(v1, Communicable):
+            assert np.allclose(c2.data, indexed(dv2, ind_complement_))
 
 
 def test_append_self(vector_array):
     v = vector_array
+    c = v.copy()
     len_v = len(v)
-    v.append(v)
-    assert len(v) == 2 * len_v
-    assert np.all(v.almost_equal(v, ind=range(len_v), o_ind=range(len_v, len(v))))
+    c.append(c)
+    assert len(c) == 2 * len_v
+    assert np.all(c.almost_equal(c, ind=range(len_v), o_ind=range(len_v, len(c))))
+    if isinstance(v, Communicable):
+        assert np.allclose(c.data, np.vstack((v.data, v.data)))
     c = v.copy()
     with pytest.raises(Exception):
         v.append(v, remove_from_other=True)
@@ -285,6 +327,8 @@ def test_append_self(vector_array):
 
 def test_remove(vector_array):
     v = vector_array
+    if isinstance(v, Communicable):
+        dv = v.data
     for ind in valid_inds(v):
         ind_complement_ = ind_complement(v, ind)
         c = v.copy()
@@ -292,6 +336,8 @@ def test_remove(vector_array):
         assert c.dim == v.dim
         assert len(c) == len(ind_complement_)
         assert np.all(v.almost_equal(c, ind=ind_complement_))
+        if isinstance(v, Communicable):
+            assert np.allclose(c.data, indexed(dv, ind_complement_))
         c.remove()
         assert len(c) == 0
 
@@ -299,6 +345,9 @@ def test_remove(vector_array):
 def test_replace(vector_array_pair_with_same_dim):
     v1, v2 = vector_array_pair_with_same_dim
     len_v1, len_v2 = len(v1), len(v2)
+    if isinstance(v1, Communicable):
+        dv1 = v1.data
+        dv2 = v2.data
     for ind1, ind2 in valid_inds_of_same_length(v1, v2):
         c1, c2 = v1.copy(), v2.copy()
         c1.replace(c2, ind=ind1, o_ind=ind2, remove_from_other=False)
@@ -314,7 +363,11 @@ def test_replace(vector_array_pair_with_same_dim):
             ind2 = [last_inds[i] for i in ind1]
         assert np.all(c1.almost_equal(v2, ind=ind1, o_ind=ind2))
         assert np.all(c2.almost_equal(v2))
-    for ind1, ind2 in valid_inds_of_same_length(v1, v2):
+        if isinstance(v1, Communicable):
+            x = dv1.copy()
+            x[ind1] = indexed(dv2, ind2)
+            assert np.allclose(c1.data, x)
+
         c1, c2 = v1.copy(), v2.copy()
         c1.replace(c2, ind=ind1, o_ind=ind2, remove_from_other=True)
         assert len(c1) == len(v1)
@@ -331,10 +384,17 @@ def test_replace(vector_array_pair_with_same_dim):
         assert np.all(c1.almost_equal(v2, ind=ind1, o_ind=ind2))
         assert len(c2) == len(ind2_complement)
         assert np.all(c2.almost_equal(v2, o_ind=ind2_complement))
+        if isinstance(v1, Communicable):
+            x = dv1.copy()
+            x[ind1] = indexed(dv2, ind2)
+            assert np.allclose(c1.data, x)
+            assert np.allclose(c2.data, indexed(dv2, ind2_complement))
 
 
 def test_replace_self(vector_array):
     v = vector_array
+    if isinstance(v, Communicable):
+        dv = v.data
     for ind1, ind2 in valid_inds_of_same_length(v, v):
         c = v.copy()
         with pytest.raises(Exception):
@@ -355,6 +415,62 @@ def test_replace_self(vector_array):
         else:
             last_ind2 = ind2
         assert np.all(c.almost_equal(v, ind=ind1, o_ind=last_ind2))
+        if isinstance(v, Communicable):
+            x = dv.copy()
+            x[ind1] = indexed(dv, ind2)
+            assert np.allclose(c.data, x)
+
+
+def test_almost_equal(vector_array_pair_with_same_dim):
+    v1, v2 = vector_array_pair_with_same_dim
+    if isinstance(v1, Communicable):
+        dv1 = v1.data
+        dv2 = v2.data
+    for ind1, ind2 in valid_inds_of_same_length(v1, v2):
+        for rtol, atol in ((1e-5, 1e-8), (1e-10, 1e-12), (0., 1e-8), (1e-5, 1e-8)):
+            r = v1.almost_equal(v2, ind=ind1, o_ind=ind2)
+            assert isinstance(r, np.ndarray)
+            assert r.shape == (v1.len_ind(ind1),)
+            if isinstance(v1, Communicable):
+                assert np.all(r == np.all(np.abs(indexed(dv1, ind1) - indexed(dv2, ind2))
+                                          <= atol + rtol * np.abs(indexed(dv2, ind2)), axis=1))
+
+
+def test_almost_equal2(vector_array):
+    v = vector_array
+    for ind in valid_inds(v):
+        for rtol, atol in ((1e-5, 1e-8), (1e-10, 1e-12), (0., 1e-8), (1e-5, 1e-8)):
+            r = v.almost_equal(v, ind=ind, o_ind=ind)
+            assert isinstance(r, np.ndarray)
+            assert r.shape == (v.len_ind(ind),)
+            assert np.all(r)
+            if v.dim == 0:
+                continue
+
+            c = v.copy()
+            c.scal(1. / (np.max(v.sup_norm(ind)) * atol))
+            assert np.all(c.almost_equal(c.zeros(v.dim, v.len_ind(ind)), ind=ind))
+
+            c = v.copy()
+            c.scal(2. / (np.max(v.sup_norm(ind)) * atol))
+            assert not np.all(c.almost_equal(c.zeros(v.dim, v.len_ind(ind)), ind=ind))
+
+            c = v.copy()
+            c.scal(1. + atol / np.max(v.sup_norm(ind)))
+            assert np.all(c.almost_equal(v, ind=ind, o_ind=ind))
+
+            c = v.copy()
+            c.scal(2. + atol / np.max(v.sup_norm(ind)))
+            assert not np.all(c.almost_equal(v, ind=ind, o_ind=ind))
+
+            c = v.copy()
+            c.scal(1. + rtol)
+            assert np.all(c.almost_equal(v, ind=ind, o_ind=ind))
+
+            c = v.copy()
+            c.scal(2. + rtol)
+            assert not np.all(c.almost_equal(v, ind=ind, o_ind=ind))
+
 
 
 ########################################################################################################################
