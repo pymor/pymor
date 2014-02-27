238b877944807e49e5c046c0c092246ff40e3239
Stephan Rave <stephanrave@uni-muenster.de>
2014-02-27 19:54:24 +0100

    [tests] move VectorArray fixtures to pymortests.fixtures.vectorarray

--- src/pymortests/vectorarray.py
+++ src/pymortests/vectorarray.py
@@ -4,85 +4,9 @@ from numbers import Number
 import pytest
 import numpy as np
 
-from pymor.la import NumpyVectorArray
-from pymor.la.listvectorarray import NumpyListVectorArray, NumpyVector
-
-
-def random_integers(count, seed):
-    np.random.seed(seed)
-    return list(np.random.randint(0, 3200, count))
-
-
-def numpy_vector_array_factory(length, dim, seed):
-    np.random.seed(seed)
-    return NumpyVectorArray(np.random.random((length, dim)), copy=False)
-
-
-def numpy_list_vector_array_factory(length, dim, seed):
-    np.random.seed(seed)
-    return NumpyListVectorArray([NumpyVector(v, copy=False) for v in np.random.random((length, dim))],
-                                dim=dim, copy=False)
-
-
-def vector_array_from_empty_reserve(v, reserve):
-    if reserve == 0:
-        return v
-    if reserve == 1:
-        r = 0
-    elif reserve == 2:
-        r = len(v) + 10
-    elif reserve == 3:
-        r = int(len(v) / 2)
-    c = type(v).empty(v.dim, reserve=r)
-    c.append(v)
-    return c
-
-
-numpy_vector_array_factory_arguments = \
-    zip([0,  0,  1, 43, 102],      # len
-        [0, 10, 34, 32,   0],      # dim
-        random_integers(5, 123))   # seed
-
-numpy_vector_array_factory_arguments_pairs_with_same_dim = \
-    zip([0,  0,  1, 43, 102,  2],         # len1
-        [0,  1, 37,  9, 104,  2],         # len2
-        [0, 10, 34, 32,   3, 13],         # dim
-        random_integers(5, 1234) + [42],  # seed1
-        random_integers(5, 1235) + [42])  # seed2
-
-numpy_vector_array_factory_arguments_pairs_with_different_dim = \
-    zip([0,  0,  1, 43, 102],      # len1
-        [0,  1,  1,  9,  10],      # len2
-        [0, 10, 34, 32,   3],      # dim1
-        [1, 11,  0, 33,   2],      # dim2
-        random_integers(5, 1234),  # seed1
-        random_integers(5, 1235))  # seed2
-
-numpy_vector_array_generators = \
-    [lambda args=args: numpy_vector_array_factory(*args) for args in numpy_vector_array_factory_arguments]
-
-numpy_list_vector_array_generators = \
-    [lambda args=args: numpy_list_vector_array_factory(*args) for args in numpy_vector_array_factory_arguments]
-
-numpy_vector_array_pair_with_same_dim_generators = \
-    [lambda l=l, l2=l2, d=d, s1=s1, s2=s2: (numpy_vector_array_factory(l, d, s1),
-                                            numpy_vector_array_factory(l2, d, s2))
-     for l, l2, d, s1, s2 in numpy_vector_array_factory_arguments_pairs_with_same_dim]
-
-numpy_list_vector_array_pair_with_same_dim_generators = \
-    [lambda l=l, l2=l2, d=d, s1=s1, s2=s2: (numpy_list_vector_array_factory(l, d, s1),
-                                            numpy_list_vector_array_factory(l2, d, s2))
-     for l, l2, d, s1, s2 in numpy_vector_array_factory_arguments_pairs_with_same_dim]
-
-numpy_vector_array_pair_with_different_dim_generators = \
-    [lambda l=l, l2=l2, d1=d1, d2=d2, s1=s1, s2=s2: (numpy_vector_array_factory(l, d1, s1),
-                                                     numpy_vector_array_factory(l2, d2, s2))
-     for l, l2, d1, d2, s1, s2 in numpy_vector_array_factory_arguments_pairs_with_different_dim]
-
-numpy_list_vector_array_pair_with_different_dim_generators = \
-    [lambda l=l, l2=l2, d1=d1, d2=d2, s1=s1, s2=s2: (numpy_list_vector_array_factory(l, d1, s1),
-                                                     numpy_list_vector_array_factory(l2, d2, s2))
-     for l, l2, d1, d2, s1, s2 in numpy_vector_array_factory_arguments_pairs_with_different_dim]
+from pymortests.fixtures.vectorarray import \
+    (vector_array_without_reserve, vector_array, vector_array_pair_with_same_dim_without_reserve,
+     vector_array_pair_with_same_dim, vector_array_pair_with_different_dim, VectorArray)
 
 
 def ind_complement(v, ind):
@@ -186,39 +110,6 @@ def invalid_ind_pairs(v1, v2):
     for ind2 in valid_inds(v2):
         for ind1 in invalid_inds(v1, length=v2.len_ind(ind2)):
             yield ind1, ind2
-
-
-@pytest.fixture(params=numpy_vector_array_generators + numpy_list_vector_array_generators)
-def vector_array_without_reserve(request):
-    return request.param()
-
-
-@pytest.fixture(params=range(3))
-def vector_array(vector_array_without_reserve, request):
-    return vector_array_from_empty_reserve(vector_array_without_reserve, request.param)
-
-
-@pytest.fixture(params=(numpy_vector_array_pair_with_same_dim_generators +
-                        numpy_list_vector_array_pair_with_same_dim_generators))
-def vector_array_pair_with_same_dim_without_reserve(request):
-    return request.param()
-
-
-@pytest.fixture(params=list(product(range(3), range(3))))
-def vector_array_pair_with_same_dim(vector_array_pair_with_same_dim_without_reserve, request):
-    v1, v2 = vector_array_pair_with_same_dim_without_reserve
-    return vector_array_from_empty_reserve(v1, request.param[0]), vector_array_from_empty_reserve(v2, request.param[1])
-
-
-@pytest.fixture(params=(numpy_vector_array_pair_with_different_dim_generators +
-                        numpy_list_vector_array_pair_with_different_dim_generators))
-def vector_array_pair_with_different_dim(request):
-    return request.param()
-
-
-@pytest.fixture(params=[NumpyVectorArray, NumpyListVectorArray])
-def VectorArray(request):
-    return request.param
 
 
 def test_empty(VectorArray):
