69b87b16a7ed516d6835d1f5f6671768b1ddba1c
Rene Milk <rene.milk@uni-muenster.de>
2014-03-14 14:59:45 +0100

    [debian] deletes example files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(pymor-base):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="pymor-base" command="/usr/bin/pymor-base"
--- debian/pymor-base.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the pymor-base package
-#
-0 4	* * *	root	[ -x /usr/bin/pymor-base_maintenance ] && /usr/bin/pymor-base_maintenance
