5faeadc0fc1e8d28c16f037ad71beba6c4e5e14f
Stephan Rave <stephanrave@uni-muenster.de>
2014-03-17 00:39:22 +0100

    [functions.basic] make GenericFunction in more cases picklable

--- src/pymor/functions/basic.py
+++ src/pymor/functions/basic.py
@@ -11,6 +11,7 @@ from numbers import Number
 from itertools import izip
 import numpy as np
 
+from pymor.core.pickle import dumps, loads, dumps_function, loads_function, PicklingError
 from pymor.functions.interfaces import FunctionInterface
 from pymor.parameters import ParameterFunctionalInterface
 
@@ -143,6 +144,26 @@ class GenericFunction(FunctionBase):
         assert v.shape == x.shape[:-1] + self.shape_range
 
         return v
+
+    def __getstate__(self):
+        s = self.__dict__.copy()
+        try:
+            pickled_mapping = dumps(self._mapping)
+            picklable = True
+        except PicklingError:
+            self.logger.warn('Mapping not picklable, trying pymor.core.pickle.dumps_function.')
+            pickled_mapping = dumps_function(self._mapping)
+            picklable = False
+        s['_mapping'] = pickled_mapping
+        s['_picklable'] = picklable
+        return s
+
+    def __setstate__(self, state):
+        if state.pop('_picklable'):
+            state['_mapping'] = loads(state['_mapping'])
+        else:
+            state['_mapping'] = loads_function(state['_mapping'])
+        self.__dict__.update(state)
 
 
 class LincombFunction(FunctionBase):
