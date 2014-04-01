7b3db3cbeaac632046cad8d6455abef0b9083911
Felix Schindler <felix@schindlerfamily.de>
2014-04-01 15:42:19 +0200

    Albrecht -> Schindler

--- src/pymor/playground/progressbar.py
+++ src/pymor/playground/progressbar.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # This file is part of the pyMOR project (http://www.pymor.org).
-# Copyright Holders: Felix Albrecht, Rene Milk, Stephan Rave
+# Copyright Holders: Rene Milk, Stephan Rave, Felix Schindler
 # License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
 import sys
