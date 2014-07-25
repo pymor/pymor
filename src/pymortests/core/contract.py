0ba3505003d87d6898fc30a53e6c2d957429f22c
Rene Milk <rene.milk@uni-muenster.de>
2014-07-25 14:35:56 +0200

    [tests] fixes contract test module
    test had actually never run..

--- src/pymortests/core/contract.py
+++ src/pymortests/core/contract.py
@@ -3,84 +3,77 @@
 # License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
 from __future__ import absolute_import, division, print_function
+
 import pytest
+import contracts
+import mock
 
 from pymortests.base import TestInterface, runmodule
-from pymortests.core.dummies import (AllDirichletBoundaryInfo, AverageImplementer, BoringTestClass)
 import pymor.grids.boundaryinfos
+from pymortests.core.dummies import (AllDirichletBoundaryInfo, AverageImplementer, BoringTestClass)
 from pymor.core import exceptions
 from pymor.core.interfaces import (contract,)
 from pymor.grids import AllDirichletBoundaryInfo as ADIA
+from pymor.core.exceptions import ContractNotRespected
 
 
-try:
-    import contracts
-    import mock
+class TestContract(TestInterface):
 
-    from pymor.core.exceptions import ContractNotRespected
+    def test_contract_fail(self):
+        with pytest.raises(ContractNotRespected):
+            AverageImplementer().whisper('Wheee\n', -2)
 
-    # noinspection PyShadowingNames,PyShadowingNames
-    class TestContract(TestInterface):
+    def test_contract_success(self):
+        AverageImplementer().shout('Wheee\n', 6)
 
-        def testContractFail(self):
+    def test_naming(self):
+        imp = BoringTestClass()
+
+        def _combo(dirichletA, dirichletB):
+            assert imp.dirichletTest(dirichletA, dirichletB)
             with pytest.raises(ContractNotRespected):
-                AverageImplementer().whisper('Wheee\n', -2)
+                imp.dirichletTest(dirichletA, dirichletA)
+            with pytest.raises(ContractNotRespected):
+                imp.dirichletTest(dirichletB, dirichletA)
+            with pytest.raises(ContractNotRespected):
+                imp.dirichletTest(dirichletA, 1)
+        grid = mock.Mock()
+        dirichletB = AllDirichletBoundaryInfo()
+        dirichletA = pymor.grids.boundaryinfos.AllDirichletBoundaryInfo(grid)
+        _combo(dirichletA, dirichletB)
+        dirichletA = ADIA(grid)
+        _combo(dirichletA, dirichletB)
 
-        def testContractSuccess(self):
-            AverageImplementer().shout('Wheee\n', 6)
-
-        def testNaming(self):
-            imp = BoringTestClass()
-
-            def _combo(dirichletA, dirichletB):
-                self.assertTrue(imp.dirichletTest(dirichletA, dirichletB))
-                with pytest.raises(ContractNotRespected):
-                    imp.dirichletTest(dirichletA, dirichletA)
-                with pytest.raises(ContractNotRespected):
-                    imp.dirichletTest(dirichletB, dirichletA)
-                with pytest.raises(ContractNotRespected):
-                    imp.dirichletTest(dirichletA, 1)
+    def test_custom_contract_types(self):
+        inst = BoringTestClass()
+        with pytest.raises(exceptions.ContractNotRespected):
             grid = mock.Mock()
-            dirichletB = AllDirichletBoundaryInfo()
-            dirichletA = pymor.grids.boundaryinfos.AllDirichletBoundaryInfo(grid)
-            _combo(dirichletA, dirichletB)
-            dirichletA = ADIA(grid)
-            _combo(dirichletA, dirichletB)
+            inst.validate_interface(object(), pymor.grids.boundaryinfos.AllDirichletBoundaryInfo(grid))
 
-        def test_custom_contract_types(self):
-            inst = BoringTestClass()
-            with pytest.raises(exceptions.ContractNotRespected):
-                grid = mock.Mock()
-                inst.validate_interface(object(), pymor.grids.boundaryinfos.AllDirichletBoundaryInfo(grid))
+    def test_disabled_contracts(self):
+        contracts.disable_all()
 
-        def test_disabled_contracts(self):
-            contracts.disable_all()
+        @contract
+        def disabled(phrase):
+            """
+            :type phrase: str
+            """
+            return phrase
+        # this should not throw w/ contracts disabled
+        disabled(int(8))
+        contracts.enable_all()
+        # this will still not throw because the disabled value is checked at decoration time only
+        disabled(int(8))
 
-            @contract
-            def disabled(phrase):
-                """
-                :type phrase: str
-                """
-                return phrase
-            # this should not throw w/ contracts disabled
-            disabled(int(8))
-            contracts.enable_all()
-            # this will still not throw because the disabled value is checked at decoration time only
-            disabled(int(8))
-
-            @contract
-            def enabled(phrase):
-                """
-                :type phrase: str
-                """
-                return phrase
-            # a newly decorated function will throw
-            with pytest.raises(exceptions.ContractNotRespected):
-                enabled(int(8))
-
-except ImportError:
-    pass
-
+        @contract
+        def enabled(phrase):
+            """
+            :type phrase: str
+            """
+            return phrase
+        # a newly decorated function will throw
+        with pytest.raises(exceptions.ContractNotRespected):
+            enabled(int(8))
 
 if __name__ == "__main__":
     runmodule(filename=__file__)
