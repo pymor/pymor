a7f45b5ed02fef883fffc196d9653539c8ae1b9a
Stephan Rave <stephanrave@uni-muenster.de>
2014-12-04 00:11:49 +0100

    [grids] let TriaGrid subdivide each rectangle into 4 triangles

--- src/pymor/grids/tria.py
+++ src/pymor/grids/tria.py
@@ -15,15 +15,23 @@ class TriaGrid(AffineGridInterface):
 
     The global face, edge and vertex indices are given as follows ::
 
-                 6---10----7---11----8
-                 | \     6 | \     7 |
-                 3   14    4   15    5
-                 | 2     \ | 3     \ |
-                 3----8----4----9----5
-                 | \     4 | \     5 |
-                 0   12    1   13    2
-                 | 0    \  | 1     \ |
-                 0----6----1----7----2
+                 6---------10----------7---------11----------8
+                 | \                 / | \                 / |
+                 |    22   10    18    |    23   11    19    |
+                 |       \     /       |       \     /       |
+                 3    14   11     6    4    15   12     7    5
+                 |       /     \       |       /     \       |
+                 |    14    2    26    |    15    3    27    |
+                 | /                 \ | /                 \ |
+                 3----------8----------4----------9----------5
+                 | \                 / | \                 / |
+                 |    20    8    16    |    21    9    17    |
+                 |       \     /       |       \     /       |
+                 0    12    9     4    1    13   10     5    2
+                 |       /     \       |       /     \       |
+                 |    12    0    24    |    13    1    25    |
+                 | /                 \ | /                 \ |
+                 0----------6----------1----------7----------2
 
     Parameters
     ----------
@@ -57,14 +65,15 @@ class TriaGrid(AffineGridInterface):
         self.x1_width = self.x1_range[1] - self.x1_range[0]
         self.x0_diameter = self.x0_width / x0_num_intervals
         self.x1_diameter = self.x1_width / x1_num_intervals
-        n_elements = x0_num_intervals * x1_num_intervals * 2
+        n_elements = x0_num_intervals * x1_num_intervals * 4
 
         # TOPOLOGY
+        n_outer_vertices = (x0_num_intervals + 1 - identify_left_right) * (x1_num_intervals + 1 - identify_bottom_top)
         self.__sizes = (n_elements,
                         ((x0_num_intervals + 1 - identify_left_right) * x1_num_intervals +
                          (x1_num_intervals + 1 - identify_bottom_top) * x0_num_intervals +
-                         int(n_elements / 2)),
-                        (x0_num_intervals + 1 - identify_left_right) * (x1_num_intervals + 1 - identify_bottom_top))
+                         n_elements),
+                        n_outer_vertices + int(n_elements / 4))
 
         # calculate subentities -- codim-1
         V_EDGE_H_INDICES = np.arange(x0_num_intervals + 1, dtype=np.int32)
@@ -82,16 +91,25 @@ class TriaGrid(AffineGridInterface):
         H_EDGE_INDICES = H_EDGE_V_INDICES[:, np.newaxis] + H_EDGE_H_INDICES + num_v_edges
         num_h_edges = x0_num_intervals * (x1_num_intervals + 1 - identify_bottom_top)
 
-        D_EDGE_INDICES = np.arange(x0_num_intervals * x1_num_intervals, dtype=np.int32) + (num_v_edges + num_h_edges)
+        D_EDGE_LL_INDICES = np.arange(n_elements / 4, dtype=np.int32) + (num_v_edges + num_h_edges)
+        D_EDGE_UR_INDICES = D_EDGE_LL_INDICES + int(n_elements / 4)
+        D_EDGE_UL_INDICES = D_EDGE_UR_INDICES + int(n_elements / 4)
+        D_EDGE_LR_INDICES = D_EDGE_UL_INDICES + int(n_elements / 4)
 
-        E0 = np.array([D_EDGE_INDICES,
-                       V_EDGE_INDICES[:, :-1].ravel(),
-                       H_EDGE_INDICES[:-1, :].ravel()]).T
-        E1 = np.array([D_EDGE_INDICES,
-                       V_EDGE_INDICES[:, 1:].ravel(),
-                       H_EDGE_INDICES[1:, :].ravel()]).T
+        E0 = np.array([H_EDGE_INDICES[:-1, :].ravel(),
+                       D_EDGE_LR_INDICES,
+                       D_EDGE_LL_INDICES]).T
+        E1 = np.array([V_EDGE_INDICES[:, 1:].ravel(),
+                       D_EDGE_UR_INDICES,
+                       D_EDGE_LR_INDICES]).T
+        E2 = np.array([H_EDGE_INDICES[1:, :].ravel(),
+                       D_EDGE_UL_INDICES,
+                       D_EDGE_UR_INDICES]).T
+        E3 = np.array([V_EDGE_INDICES[:, :-1].ravel(),
+                       D_EDGE_LL_INDICES,
+                       D_EDGE_UL_INDICES]).T
 
-        codim1_subentities = np.vstack((E0, E1))
+        codim1_subentities = np.vstack((E0, E1, E2, E3))
 
         # calculate subentities -- codim-2
         VERTEX_H_INDICES = np.arange(x0_num_intervals + 1, dtype=np.int32)
@@ -102,29 +120,46 @@ class TriaGrid(AffineGridInterface):
             VERTEX_V_INDICES[-1] = 0
         VERTEX_V_INDICES *= x0_num_intervals + 1 - identify_left_right
         VERTEX_NUMERS = VERTEX_V_INDICES[:, np.newaxis] + VERTEX_H_INDICES
+        VERTEX_CENTER_NUMBERS = np.arange(x0_num_intervals * x1_num_intervals, dtype=np.int32) + n_outer_vertices
 
-        V0 = np.array([VERTEX_NUMERS[:-1, :-1].ravel(),
-                       VERTEX_NUMERS[:-1, 1:].ravel(),
-                       VERTEX_NUMERS[1:, :-1].ravel()]).T
-        V1 = np.array([VERTEX_NUMERS[1:, 1:].ravel(),
-                       VERTEX_NUMERS[1:, :-1].ravel(),
+        V0 = np.array([VERTEX_CENTER_NUMBERS,
+                       VERTEX_NUMERS[:-1, :-1].ravel(),
                        VERTEX_NUMERS[:-1, 1:].ravel()]).T
+        V1 = np.array([VERTEX_CENTER_NUMBERS,
+                       VERTEX_NUMERS[:-1, 1:].ravel(),
+                       VERTEX_NUMERS[1:, 1:].ravel()]).T
+        V2 = np.array([VERTEX_CENTER_NUMBERS,
+                       VERTEX_NUMERS[1:, 1:].ravel(),
+                       VERTEX_NUMERS[1:, :-1].ravel()]).T
+        V3 = np.array([VERTEX_CENTER_NUMBERS,
+                       VERTEX_NUMERS[1:, :-1].ravel(),
+                       VERTEX_NUMERS[:-1, :-1].ravel()]).T
 
-        codim2_subentities = np.vstack((V0, V1))
+        codim2_subentities = np.vstack((V0, V1, V2, V3))
         self.__subentities = (codim1_subentities, codim2_subentities)
 
         # GEOMETRY
 
         # embeddings
-        x0_shifts0 = np.arange(x0_num_intervals) * self.x0_diameter + self.x0_range[0]
-        x1_shifts0 = np.arange(x1_num_intervals) * self.x1_diameter + self.x1_range[0]
-        x0_shifts1 = x0_shifts0 + self.x0_diameter
-        x1_shifts1 = x1_shifts0 + self.x1_diameter
-        B = np.vstack((np.array(np.meshgrid(x0_shifts0, x1_shifts0)).reshape((2, -1)).T,
-                       np.array(np.meshgrid(x0_shifts1, x1_shifts1)).reshape((2, -1)).T))
-        A0 = np.tile(np.diag([self.x0_diameter, self.x1_diameter]), (n_elements / 2, 1, 1))
-        A1 = - A0
-        A = np.vstack((A0, A1))
+        x0_shifts = np.arange(x0_num_intervals) * self.x0_diameter + (self.x0_range[0] + 0.5 * self.x0_diameter)
+        x1_shifts = np.arange(x1_num_intervals) * self.x1_diameter + (self.x1_range[0] + 0.5 * self.x1_diameter)
+        B = np.tile(np.array(np.meshgrid(x0_shifts, x1_shifts)).reshape((2, -1)).T,
+                    (4, 1))
+
+        ROT45  = np.array([[1./np.sqrt(2.),   -1./np.sqrt(2.)],
+                           [1./np.sqrt(2.),    1./np.sqrt(2.)]])
+        ROT135 = np.array([[-1./np.sqrt(2.),  -1./np.sqrt(2.)],
+                           [1./np.sqrt(2.),   -1./np.sqrt(2.)]])
+        ROT225 = np.array([[-1./np.sqrt(2.),   1./np.sqrt(2.)],
+                           [-1./np.sqrt(2.),  -1./np.sqrt(2.)]])
+        ROT315 = np.array([[1./np.sqrt(2.),    1./np.sqrt(2.)],
+                           [-1./np.sqrt(2.),   1./np.sqrt(2.)]])
+        SCAL = np.diag([self.x0_diameter / np.sqrt(2), self.x1_diameter / np.sqrt(2)])
+        A0 = np.tile(SCAL.dot(ROT225), (n_elements / 4, 1, 1))
+        A1 = np.tile(SCAL.dot(ROT315), (n_elements / 4, 1, 1))
+        A2 = np.tile(SCAL.dot(ROT45), (n_elements / 4, 1, 1))
+        A3 = np.tile(SCAL.dot(ROT135), (n_elements / 4, 1, 1))
+        A = np.vstack((A0, A1, A2, A3))
         self.__embeddings = (A, B)
 
     def __reduce__(self):
