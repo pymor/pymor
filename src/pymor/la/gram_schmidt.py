9c8528c5dc0debf36a3e035ab5bcbedd1f9d8fff
Stephan Rave <stephanrave@uni-muenster.de>
2015-02-09 11:40:42 +0100

    [la.gram_schmidt] do not fail in orthogonality check if all vectors are thrown away

--- src/pymor/la/gram_schmidt.py
+++ src/pymor/la/gram_schmidt.py
@@ -134,8 +134,7 @@ def gram_schmidt(A, product=None, atol=1e-13, rtol=1e-13, offset=0, find_duplica
         else:
             error_matrix = A.dot(A, ind=range(offset, len(A)), pairwise=False)
         error_matrix[:len(A) - offset, offset:len(A)] -= np.eye(len(A) - offset)
-        err = np.max(np.abs(error_matrix))
-        if err >= check_tol:
-            raise AccuracyError('result not orthogonal (max err={})'.format(err))
-
-    return A
+        if error_matrix.size > 0:
+            err = np.max(np.abs(error_matrix))
+            if err >= check_tol:
+                raise AccuracyError('result not orthogonal (max err={})'.format(err))
