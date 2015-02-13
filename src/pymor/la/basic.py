a4c03ac87fa4dce5dfc41362daa49cef964831f9
Stephan Rave <stephanrave@uni-muenster.de>
2015-02-13 18:15:37 +0100

    [la] fix cat_arrays

--- src/pymor/la/basic.py
+++ src/pymor/la/basic.py
@@ -69,7 +69,7 @@ def cat_arrays(vector_arrays):
     """Return a new |VectorArray| which a concatenation of the arrays in `vector_arrays`."""
     vector_arrays = list(vector_arrays)
     total_length = sum(map(len, vector_arrays))
-    cated_arrays = vector_arrays[0].empty(vector_arrays[0].dim, reserve=total_length)
+    cated_arrays = vector_arrays[0].empty(reserve=total_length)
     for a in vector_arrays:
         cated_arrays.append(a)
     return cated_arrays
