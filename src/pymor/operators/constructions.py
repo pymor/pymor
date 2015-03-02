f91072ff2b1f2c92b0c3affc47e291f519858628
Stephan Rave <stephanrave@uni-muenster.de>
2015-03-02 16:50:11 +0100

    [operators.constructions.SelectionOperator] PEP8 cleanup

--- src/pymor/operators/constructions.py
+++ src/pymor/operators/constructions.py
@@ -627,22 +627,19 @@ class SelectionOperator(OperatorBase):
     operators
         List of |Operators| from which one |Operator| is
         selected based on a parameter.
-    parameterfunctional
+    parameter_functional
         A |ParameterFunctional| used for the selection of one |Operator|..
     name
         Name of the operator.
 
     """
-    def __init__(self, operators, parameterfunctional, boundaries, name=None):
+    def __init__(self, operators, parameter_functional, boundaries, name=None):
         assert len(operators) > 0
-
         assert len(boundaries) == len(operators) - 1
         # check that boundaries are ascending:
         for i in range(len(boundaries)-1):
             assert boundaries[i] <= boundaries[i+1]
-
         assert all(isinstance(op, OperatorInterface) for op in operators)
-
         assert all(op.source == operators[0].source for op in operators[1:])
         assert all(op.range == operators[0].range for op in operators[1:])
         self.source = operators[0].source
@@ -651,14 +648,14 @@ class SelectionOperator(OperatorBase):
         self.linear = all(op.linear for op in operators)
 
         self.name = name
-        self.build_parameter_type(inherits=list(operators) + [parameterfunctional])
+        self.build_parameter_type(inherits=list(operators) + [parameter_functional])
         self._try_assemble = not self.parametric
 
         self.boundaries = boundaries
-        self.parameterfunctional = parameterfunctional
+        self.parameter_functional = parameter_functional
 
     def _get_operator_number(self, mu):
-        value = self.parameterfunctional.evaluate(mu)
+        value = self.parameter_functional.evaluate(mu)
         for i in range(len(self.boundaries)):
             if self.boundaries[i] >= value:
                 return i
@@ -667,7 +664,7 @@ class SelectionOperator(OperatorBase):
     def apply(self, U, ind=None, mu=None):
         mu = self.parse_parameter(mu)
         operator_number = self._get_operator_number(mu)
-        return self.operators[operator_number].apply(U,ind=ind, mu=mu)
+        return self.operators[operator_number].apply(U, ind=ind, mu=mu)
 
     def as_vector(self, mu=None):
         mu = self.parse_parameter(mu)
