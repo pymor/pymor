c49799f76224a753df35b4629ac8290b629b01ec
falk <falk.meyer@uni-muenster.de>
2015-07-28 10:30:27 +0200

    Added tests in pymor.playground and test data in testdata

---
+++ src/pymor/playground/disk/stationary_disk_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# This file is part of the pyMOR project (http://www.pymor.org).
+# Copyright Holders: Rene Milk, Stephan Rave, Felix Schindler
+# License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+#
+# Contributors: Falk Meyer <falk.meyer@wwu.de>
+
+from __future__ import absolute_import, division, print_function
+
+from pymor.discretizers.disk import discretize_stationary_from_disk
+
+
+# Test a simple stationary discretization for pymor.discretizers.disk
+def discretize_stationary_test(parameter_file="parameter_stationary.ini"):
+
+    # load discretization
+    dis = discretize_stationary_from_disk(parameter_file)
+
+    # compute solutions
+    for parameter in dis.parameter_space.sample_uniformly(4):
+        print(dis.solve(parameter))
+
+if __name__ == '__main__':
+    discretize_stationary_test()
