cee9da23b1fbcccb2277bd98619d2f8e042a5763
Felix Schindler <felix.schindler@wwu.de>
2015-08-04 11:38:56 +0200

    [pymortests] properly integrate disk-based discretizations

--- src/pymortests/fixtures/discretization.py
+++ src/pymortests/fixtures/discretization.py
@@ -8,8 +8,10 @@ from __future__ import absolute_import, division, print_function
 from itertools import product
 
 import pytest
+import os
 
 from pymor.discretizers.advection import discretize_nonlinear_instationary_advection_fv
+from pymor.discretizers.disk import discretize_stationary_from_disk, discretize_instationary_from_disk
 from pymor.discretizers.elliptic import discretize_elliptic_cg
 from pymortests.fixtures.analyticalproblem import (picklable_thermalblock_problems, non_picklable_thermalblock_problems,
                                                    burgers_problems)
@@ -19,7 +21,11 @@ picklable_discretizaion_generators = \
         [lambda p=p,d=d: discretize_elliptic_cg(p, diameter=d)[0]
          for p, d in product(picklable_thermalblock_problems, [1./50., 1./100.])] + \
         [lambda p=p,d=d: discretize_nonlinear_instationary_advection_fv(p, diameter=d)[0]
-         for p, d in product(burgers_problems, [1./10., 1./15.])]
+         for p, d in product(burgers_problems, [1./10., 1./15.])] + \
+        [lambda p=p: discretize_stationary_from_disk(parameter_file=p)
+         for p in (os.path.join(os.path.dirname(__file__), '../../../testdata/parameter_stationary.ini'),)] + \
+        [lambda p=p: discretize_instationary_from_disk(parameter_file=p)
+         for p in (os.path.join(os.path.dirname(__file__), '../../../testdata/parameter_instationary.ini'),)]
 
 
 non_picklable_discretization_generators = \
