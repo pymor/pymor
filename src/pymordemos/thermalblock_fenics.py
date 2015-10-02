2f617209fffec5a3f01e0a80e9bf9e43039e5315
Felix Schindler <felix.schindler@wwu.de>
2015-10-02 16:46:40 +0200

    [fenics] correctly interpret data function as piecewise constant

--- src/pymordemos/thermalblock_fenics.py
+++ src/pymordemos/thermalblock_fenics.py
@@ -87,7 +87,8 @@ def discretize(args):
     diffusion = df.Expression(  '(lower0 <= x[0]) * (open0 ? (x[0] < upper0) : (x[0] <= upper0)) *'
                               + '(lower1 <= x[1]) * (open1 ? (x[1] < upper1) : (x[1] <= upper1))',
                               lower0=0., upper0=0., open0=0,
-                              lower1=0., upper1=0., open1=0)
+                              lower1=0., upper1=0., open1=0,
+                              element=df.FunctionSpace(mesh, 'DG', 0).ufl_element())
 
     def assemble_matrix(x, y, nx, ny):
         diffusion.user_parameters['lower0'] = x/nx
