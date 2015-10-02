d190aa16ceebfccd1923c41479f4a462b46c1e0b
Stephan Rave <stephanrave@uni-muenster.de>
2015-10-02 15:47:06 +0200

    [parallel] fix signature of WorkerPoolInterface.map

--- src/pymor/parallel/interfaces.py
+++ src/pymor/parallel/interfaces.py
@@ -32,7 +32,34 @@ class WorkerPoolInterface(BasicInterface):
         pass
 
     @abstractmethod
-    def map(self, function, zip_return_values=True, *args, **kwargs):
+    def map(self, function, *args, **kwargs):
+        """Parallel version of the builtin map function.
+
+        Each positional argument (after `function`) must be a sequence
+        of same length n. `map` calls `function` in parallel on each of these n
+        positional argument combinations, always passing `kwargs` as keyword
+        arguments.  Keyword arguments which are |RemoteObjects| are automatically
+        mapped to the respective object on the worker. Moreover, keyword arguments
+        which are |immutable| objects that have already been pushed to the workers
+        will not be transmitted again. (|Immutable| objects which have not
+        been pushed before will be transmitted and the remote copy will be
+        destroyed after function execution.)
+
+        Parameters
+        ----------
+        function
+            The function to execute on each worker.
+        args
+            The sequences of positional arguments for `function`.
+        kwargs
+            The keyword arguments for `function`.
+
+        Returns
+        -------
+        List of return values of the function executions, ordered by
+        the sequence of positional arguments.  If `function` returns
+        a (tuple) of k values, k lists of return values are returned.
+        """
         pass
 
 
