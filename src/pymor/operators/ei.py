[operators.ei] small fix in ProjectedEmpiciralInterpolatedOperator.jacobian

src/pymor/operators/ei.py
@@ -215,7 +215,7 @@ class ProjectedEmpiciralInterpolatedOperator(OperatorBase):
             else:
                 interpolation_coefficients = np.linalg.solve(self.interpolation_matrix, J.data.T).T
         except ValueError:  # this exception occurs when J contains NaNs ...
-            interpolation_coefficients = (np.empty((len(self.projected_collateral_basis), len(self.source_basis_dofs)))
+            interpolation_coefficients = (np.empty((len(self.source_basis_dofs), len(self.projected_collateral_basis)))
                                           + np.nan)
         M = self.projected_collateral_basis.lincomb(interpolation_coefficients)
         if isinstance(M, NumpyVectorArray):