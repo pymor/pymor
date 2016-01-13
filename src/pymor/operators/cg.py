[analyticalproblems][operators] correct sign in advection in EllipticProblem

src/pymor/operators/cg.py
@@ -771,7 +771,7 @@ class AdvectionOperatorP1(NumpyMatrixBasedOperator):
 
         self.logger.info('Calculate all local scalar products beween gradients ...')
         D = self.advection_function(self.grid.quadrature_points(0, order=2), mu=mu)
-        SF_INTS = np.einsum('pc,eqi,c,e,eci->epq', SFQ, SF_GRADS, w, g.integration_elements(0), D).ravel()
+        SF_INTS = - np.einsum('pc,eqi,c,e,eci->eqp', SFQ, SF_GRADS, w, g.integration_elements(0), D).ravel()
         del D
         del SF_GRADS
 