88b3d9f1962fc3079f7dc8a45d1274469cbcca87
Petar Mlinarić <mlinaric@mpi-magdeburg.mpg.de>
2016-01-15 15:05:19 +0100

    [vectorarrays.numpy] correction in axpy

--- src/pymor/vectorarrays/numpy.py
+++ src/pymor/vectorarrays/numpy.py
@@ -273,8 +273,10 @@ class NumpyVectorArray(VectorArrayInterface):
 
         alpha_type = type(alpha)
         alpha_dtype = alpha.dtype if alpha_type is np.ndarray else alpha_type
-        if self._array.dtype != alpha_dtype:
-            self._array = self._array.astype(np.promote_types(self._array.dtype, alpha_dtype))
+        if self._array.dtype != alpha_dtype or self._array.dtype != B.dtype:
+            dtype = np.promote_types(self._array.dtype, alpha_dtype)
+            dtype = np.promote_types(dtype, B.dtype)
+            self._array = self._array.astype(dtype)
 
         if np.all(alpha == 1):
             if ind is None:
