742bd18bb5f6104456da14bee98a169b6ebefc10
Stephan Rave <stephanrave@uni-muenster.de>
2016-02-19 09:56:53 +0100

    add license header to pymortests.complex_values

--- src/pymortests/complex_values.py
+++ src/pymortests/complex_values.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# This file is part of the pyMOR project (http://www.pymor.org).
+# Copyright 2013-2016 pyMOR developers and contributors. All rights reserved.
+# License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
 from __future__ import print_function, unicode_literals, absolute_import, division
 
 import numpy as np
