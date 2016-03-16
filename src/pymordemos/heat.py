50655303ef9dac62faad779f958a1afd4d717aa3
Petar Mlinarić <mlinaric@mpi-magdeburg.mpg.de>
2016-03-16 07:17:42 +0100

    update file headers

--- src/pymordemos/heat.py
+++ src/pymordemos/heat.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 # This file is part of the pyMOR project (http://www.pymor.org).
-# Copyright Holders: Rene Milk, Stephan Rave, Felix Schindler
+# Copyright 2013-2016 pyMOR developers and contributors. All rights reserved.
 # License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
 """1D heat equation demo
