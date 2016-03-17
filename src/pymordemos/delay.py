7cb58ff76b234eb4f11213de5fab9f04c97a7170
Petar Mlinarić <mlinaric@mpi-magdeburg.mpg.de>
2016-03-17 10:57:44 +0100

    [tests.delay] add more computations

--- src/pymordemos/delay.py
+++ src/pymordemos/delay.py
@@ -2,7 +2,10 @@
 # Copyright 2013-2016 pyMOR developers and contributors. All rights reserved.
 # License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
+from __future__ import print_function, division, absolute_import
+
 import numpy as np
+import scipy.linalg as spla
 import matplotlib.pyplot as plt
 
 import pymor.discretizations.iosys as iosys
@@ -16,7 +19,37 @@ def dH(s):
 
 tf = iosys.TF(1, 1, H, dH)
 
-w = np.logspace(-1, 1, 100)
+w = np.logspace(-3, 3, 1000)
 tfw = tf.bode(w)
-plt.loglog(w, np.abs(tfw[0, 0, :]), '.-')
+
+r = 10
+sigma = np.logspace(-1, 0, r)
+b = np.ones((1, r))
+c = np.ones((1, r))
+tol = 1e-4
+maxit = 100
+rom = tf.tf_irka(sigma, b, c, tol, maxit, verbose=True)
+
+tfw_rom = rom.bode(w)
+
+plt.loglog(w, np.abs(tfw[0, 0, :] - tfw_rom[0, 0, :]))
+plt.show()
+
+# step response
+E = rom.E._matrix
+A = rom.A._matrix
+B = rom.B._matrix
+C = rom.C._matrix
+
+nt = 100
+t = np.linspace(0, 5, nt)
+x_old = np.zeros(rom.n, dtype=complex)
+x_new = np.zeros(rom.n, dtype=complex)
+y = np.zeros(nt, dtype=complex)
+for i in xrange(1, nt):
+    h = t[i] - t[i - 1]
+    x_new = spla.solve(E - h * A, E.dot(x_old) + h * B[:, 0])
+    y[i] = C.dot(x_new)[0]
+
+plt.plot(t, y.real)
 plt.show()
