ec64e9e8b6891b5800b0e4f390c907e0dde51dc4
Petar Mlinarić <mlinaric@mpi-magdeburg.mpg.de>
2016-03-18 10:47:14 +0100

    [algorithms.lyapunov] add choosing a default solver

--- src/pymor/algorithms/lyapunov.py
+++ src/pymor/algorithms/lyapunov.py
@@ -12,136 +12,138 @@ from pymor.operators.constructions import IdentityOperator, LincombOperator
 from pymor.operators.numpy import NumpyMatrixOperator
 from pymor.vectorarrays.numpy import NumpyVectorArray
 
-import pymess
+try:
+    import pymess
 
+    class LyapunovEquation(pymess.equation):
+        """Lyapunov equation class for pymess
 
-class LyapunovEquation(pymess.equation):
-    """Lyapunov equation class for pymess
+        Represents a Lyapunov equation::
 
-    Represents a Lyapunov equation::
+            A * X + X * A^T + RHS * RHS^T = 0
 
-        A * X + X * A^T + RHS * RHS^T = 0
+        if E is None, otherwise a generalized Lyapunov equation::
 
-    if E is None, otherwise a generalized Lyapunov equation::
+            A * X * E^T + E * X * A^T + RHS * RHS^T = 0.
 
-        A * X * E^T + E * X * A^T + RHS * RHS^T = 0.
+        For the dual Lyapunov equation::
 
-    For the dual Lyapunov equation::
+            A^T * X + X * A + RHS^T * RHS = 0,
+            A^T * X * E + E^T * X * A^T + RHS^T * RHS = 0,
 
-        A^T * X + X * A + RHS^T * RHS = 0,
-        A^T * X * E + E^T * X * A^T + RHS^T * RHS = 0,
+        opt.type needs to be pymess.MESS_OP_TRANSPOSE.
 
-    opt.type needs to be pymess.MESS_OP_TRANSPOSE.
+        Parameters
+        ----------
+        opt
+            pymess options structure
+        A
+            The |Operator| A.
+        E
+            The |Operator| E or None.
+        RHS
+            The |Operator| RHS.
+        """
+        def __init__(self, opt, A, E, RHS):
+            dim = A.source.dim
 
-    Parameters
-    ----------
-    opt
-        pymess options structure
-    A
-        The |Operator| A.
-    E
-        The |Operator| E or None.
-    RHS
-        The |Operator| RHS.
-    """
-    def __init__(self, opt, A, E, RHS):
-        dim = A.source.dim
+            super(LyapunovEquation, self).__init__(name='lyap_eqn', opt=opt, dim=dim)
 
-        super(LyapunovEquation, self).__init__(name='lyap_eqn', opt=opt, dim=dim)
-
-        if isinstance(RHS, NumpyMatrixOperator):
-            if RHS.sparse:
-                self.RHS = RHS._matrix.toarray()
+            if isinstance(RHS, NumpyMatrixOperator):
+                if RHS.sparse:
+                    self.RHS = RHS._matrix.toarray()
+                else:
+                    self.RHS = RHS._matrix
+                if opt.type == pymess.MESS_OP_TRANSPOSE:
+                    self.RHS = self.RHS.T
             else:
-                self.RHS = RHS._matrix
-            if opt.type == pymess.MESS_OP_TRANSPOSE:
-                self.RHS = self.RHS.T
-        else:
-            if opt.type == pymess.MESS_OP_NONE:
-                eye = NumpyVectorArray(sps.eye(RHS.source.dim))
-                self.RHS = np.array(RHS.apply(eye).data)
+                if opt.type == pymess.MESS_OP_NONE:
+                    eye = NumpyVectorArray(sps.eye(RHS.source.dim))
+                    self.RHS = np.array(RHS.apply(eye).data)
+                else:
+                    eye = NumpyVectorArray(sps.eye(RHS.range.dim))
+                    self.RHS = np.array(RHS.apply_adjoint(eye).data.T)
+
+            self.A = A
+            self.E = E
+            self.p = []
+
+        def A_apply(self, op, y):
+            y = NumpyVectorArray(np.array(y).T)
+            if op == pymess.MESS_OP_NONE:
+                x = self.A.apply(y)
             else:
-                eye = NumpyVectorArray(sps.eye(RHS.range.dim))
-                self.RHS = np.array(RHS.apply_adjoint(eye).data.T)
+                x = self.A.apply_adjoint(y)
+            return np.matrix(x.data).T
 
-        self.A = A
-        self.E = E
-        self.p = []
-
-    def A_apply(self, op, y):
-        y = NumpyVectorArray(np.array(y).T)
-        if op == pymess.MESS_OP_NONE:
-            x = self.A.apply(y)
-        else:
-            x = self.A.apply_adjoint(y)
-        return np.matrix(x.data).T
-
-    def E_apply(self, op, y):
-        if self.E is None:
-            return y
-
-        y = NumpyVectorArray(np.array(y).T)
-        if op == pymess.MESS_OP_NONE:
-            x = self.E.apply(y)
-        else:
-            x = self.E.apply_adjoint(y)
-        return np.matrix(x.data).T
-
-    def As_apply(self, op, y):
-        y = NumpyVectorArray(np.array(y).T)
-        if op == pymess.MESS_OP_NONE:
-            x = self.A.apply_inverse(y)
-        else:
-            x = self.A.apply_inverse_adjoint(y)
-        return np.matrix(x.data).T
-
-    def Es_apply(self, op, y):
-        if self.E is None:
-            return y
-
-        y = NumpyVectorArray(np.array(y).T)
-        if op == pymess.MESS_OP_NONE:
-            x = self.E.apply_inverse(y)
-        else:
-            x = self.E.apply_inverse_adjoint(y)
-        return np.matrix(x.data).T
-
-    def ApE_apply(self, op, p, idx_p, y):
-        y = NumpyVectorArray(np.array(y).T)
-        if op == pymess.MESS_OP_NONE:
-            x = self.A.apply(y)
+        def E_apply(self, op, y):
             if self.E is None:
-                x += p * y
+                return y
+
+            y = NumpyVectorArray(np.array(y).T)
+            if op == pymess.MESS_OP_NONE:
+                x = self.E.apply(y)
             else:
-                x += p * self.E.apply(y)
-        else:
-            x = self.A.apply_adjoint(y)
+                x = self.E.apply_adjoint(y)
+            return np.matrix(x.data).T
+
+        def As_apply(self, op, y):
+            y = NumpyVectorArray(np.array(y).T)
+            if op == pymess.MESS_OP_NONE:
+                x = self.A.apply_inverse(y)
+            else:
+                x = self.A.apply_inverse_adjoint(y)
+            return np.matrix(x.data).T
+
+        def Es_apply(self, op, y):
             if self.E is None:
-                x += p.conjugate() * y
+                return y
+
+            y = NumpyVectorArray(np.array(y).T)
+            if op == pymess.MESS_OP_NONE:
+                x = self.E.apply_inverse(y)
             else:
-                x += p.conjugate() * self.E.apply_adjoint(y)
-        return np.matrix(x.data).T
+                x = self.E.apply_inverse_adjoint(y)
+            return np.matrix(x.data).T
 
-    def ApEs_apply(self, op, p, idx_p, y):
-        y = NumpyVectorArray(np.array(y).T)
-        if self.E is None:
-            E = IdentityOperator(self.A.source)
-        else:
-            E = self.E
+        def ApE_apply(self, op, p, idx_p, y):
+            y = NumpyVectorArray(np.array(y).T)
+            if op == pymess.MESS_OP_NONE:
+                x = self.A.apply(y)
+                if self.E is None:
+                    x += p * y
+                else:
+                    x += p * self.E.apply(y)
+            else:
+                x = self.A.apply_adjoint(y)
+                if self.E is None:
+                    x += p.conjugate() * y
+                else:
+                    x += p.conjugate() * self.E.apply_adjoint(y)
+            return np.matrix(x.data).T
 
-        if p.imag == 0.0:
-            ApE = LincombOperator((self.A, E), (1., p.real))
-        else:
-            ApE = LincombOperator((self.A, E), (1., p))
+        def ApEs_apply(self, op, p, idx_p, y):
+            y = NumpyVectorArray(np.array(y).T)
+            if self.E is None:
+                E = IdentityOperator(self.A.source)
+            else:
+                E = self.E
 
-        if op == pymess.MESS_OP_NONE:
-            x = ApE.apply_inverse(y)
-        else:
-            x = ApE.apply_inverse_adjoint(y)
-        return np.matrix(x.data).T
+            if p.imag == 0.0:
+                ApE = LincombOperator((self.A, E), (1., p.real))
+            else:
+                ApE = LincombOperator((self.A, E), (1., p))
+
+            if op == pymess.MESS_OP_NONE:
+                x = ApE.apply_inverse(y)
+            else:
+                x = ApE.apply_inverse_adjoint(y)
+            return np.matrix(x.data).T
+except ImportError:
+    pass
 
 
-def solve_lyap(A, E, B, trans=False, meth='scipy', tol=None):
+def solve_lyap(A, E, B, trans=False, meth=None, tol=None):
     """Find a factor of the solution of a Lyapunov equation
 
     Returns factor Z such that Z * Z^T is approximately the solution X of a Lyapunov equation (if E is None)::
@@ -169,9 +171,12 @@ def solve_lyap(A, E, B, trans=False, meth='scipy', tol=None):
     B
         The |Operator| B.
     trans
-        If A, E, and B need to be transposed.
+        If the dual equation needs to be solved.
     meth
-        Method to use {'scipy', 'pymess_lyap', 'pymess_lradi', 'slycot'}.
+        Method to use ('scipy', 'slycot', 'pymess_lyap', 'pymess_lradi').
+        If meth is None, a method is chosen according to availability and priority::
+
+            'pymess_lradi' (for bigger problems) > 'pymess_lyap' (for smaller problems) > 'slycot' > 'scipy'.
     tol
         Tolerance parameter.
     """
@@ -180,7 +185,21 @@ def solve_lyap(A, E, B, trans=False, meth='scipy', tol=None):
     assert isinstance(B, OperatorInterface) and B.linear
     assert not trans and B.range == A.source or trans and B.source == A.source
     assert E is None or isinstance(E, OperatorInterface) and E.linear and E.source == E.range == A.source
-    assert meth in {'scipy', 'pymess_lyap', 'pymess_lradi', 'slycot'}
+    assert meth is None or meth in {'scipy', 'slycot', 'pymess_lyap', 'pymess_lradi'}
+
+    if meth is None:
+        try:
+            import pymess
+            if A.source.dim >= 1000:
+                meth = 'pymess_lradi'
+            else:
+                meth = 'pymess_lyap'
+        except ImportError:
+            try:
+                import slycot
+                meth = 'slycot'
+            except ImportError:
+                meth = 'scipy'
 
     if meth == 'scipy':
         if E is not None:
@@ -199,7 +218,6 @@ def solve_lyap(A, E, B, trans=False, meth='scipy', tol=None):
         from pymor.algorithms.cholp import cholp
         Z = cholp(X, copy=False)
     elif meth == 'slycot':
-        import slycot
         A_matrix = A._matrix
         if A.sparse:
             A_matrix = A_matrix.toarray()
