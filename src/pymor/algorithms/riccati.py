cae13e55d1bebafe2edc37f137dd5872a0939576
Petar Mlinarić <mlinaric@mpi-magdeburg.mpg.de>
2016-04-15 19:25:26 +0200

    [tests] add riccati test, fix some bugs and improve code

--- src/pymor/algorithms/riccati.py
+++ src/pymor/algorithms/riccati.py
@@ -18,11 +18,16 @@ def solve_ricc(A, B=None, E=None, Q=None, C=None, R=None, D=None, G=None, L=None
         A^T * X * E + E^T * X * A - (L + E^T * X * B) * R^{-1} * (L + E^T * X * B)^T + Q = 0.
 
     If E in None, it is taken to be the identity matrix.
+    B * R^-1 B^T can instead be given by G. In this case, B, R, D, L need to be None, and G not a None.
     Q can instead be given as C^T * C. In this case, Q needs to be None, and C not a None.
     R can instead be given as D^T * D. In this case, R needs to be None, and D not a None.
-    B * R^-1 B^T can instead be given by G. In this case, B, R, D need to be None, and G not a None.
-    If L in None, it is taken to be the zero matrix.
-    If trans is True, then A and E are transposed.
+    If R, D, G are None, then R is taken to be the identity matrix.
+    If L in None, then it is taken to be the zero matrix.
+    If trans is True, then the dual Riccati equation is solved::
+
+        A * X * E^T + E * X * A^T - (L + E * X * C^T) * R^{-1} * (L + E * X * C^T)^T + Q = 0,
+
+    where Q can be replaced by B * B^T and C^T * R^{-1} * C by G.
 
     Parameters
     ----------
@@ -53,40 +58,71 @@ def solve_ricc(A, B=None, E=None, Q=None, C=None, R=None, D=None, G=None, L=None
     """
     assert isinstance(A, OperatorInterface) and A.linear
     assert A.source == A.range
-    assert isinstance(B, OperatorInterface) and B.linear
-    assert not trans and B.range == A.source or trans and B.source == A.source
     assert E is None or isinstance(E, OperatorInterface) and E.linear and E.source == E.range == A.source
-    if Q is not None:
-        assert isinstance(Q, OperatorInterface) and Q.linear
-        assert Q.source == Q.range == A.source
-        assert C is None
-    else:
-        assert isinstance(C, OperatorInterface) and C.linear
-        assert not trans and C.source == A.source or trans and C.range == A.source
-    if B is not None:
-        if R is not None:
-            assert isinstance(R, OperatorInterface) and R.linear
-            assert not trans and R.source == R.range == B.source or trans and R.source == R.range == B.range
-            assert D is None
+    if not trans:
+        if G is not None:
+            assert B is None and R is None and D is None and L is None
+            assert isinstance(G, OperatorInterface) and G.linear
+            assert G.source == G.range == A.source
         else:
+            assert B is not None
+            assert isinstance(B, OperatorInterface) and B.linear
+            assert B.range == A.source
+        if C is not None:
+            assert Q is None
+            assert isinstance(C, OperatorInterface) and C.linear
+            assert C.source == A.source
+        else:
+            assert isinstance(Q, OperatorInterface) and Q.linear
+            assert Q.source == Q.range == A.source
+        if D is not None:
+            assert R is None
             assert isinstance(D, OperatorInterface) and D.linear
-            assert not trans and D.source == B.source or trans and D.range == B.range
+            assert D.source == B.source
+        else:
+            if R is not None:
+                assert isinstance(R, OperatorInterface) and R.linear
+                assert R.source == R.range == B.source
+        if L is not None:
+            assert isinstance(L, OperatorInterface) and L.linear
+            assert L.source == B.source and L.range == B.range
     else:
-        assert isinstance(G, OperatorInterface) and G.linear
-        assert G.source == G.range == A.source
-        assert L is None
-    if L is not None:
-        assert isinstance(L, OperatorInterface) and L.linear
-        assert L.source == B.source and L.range == B.range
+        if G is not None:
+            assert C is None and R is None and D is None and L is None
+            assert isinstance(G, OperatorInterface) and G.linear
+            assert G.source == G.range == A.source
+        else:
+            assert C is not None
+            assert isinstance(C, OperatorInterface) and C.linear
+            assert C.source == A.source
+        if B is not None:
+            assert Q is None
+            assert isinstance(B, OperatorInterface) and B.linear
+            assert B.range == A.source
+        else:
+            assert isinstance(Q, OperatorInterface) and Q.linear
+            assert Q.source == Q.range == A.source
+        if D is not None:
+            assert R is None
+            assert isinstance(D, OperatorInterface) and D.linear
+            assert D.source == C.range
+        else:
+            if R is not None:
+                assert isinstance(R, OperatorInterface) and R.linear
+                assert R.source == R.range == C.range
+        if L is not None:
+            assert isinstance(L, OperatorInterface) and L.linear
+            assert L.source == C.range and L.range == C.source
     assert meth is None or meth in {'scipy', 'slycot', 'pymess_care'}
 
     if meth is None:
+        import imp
         try:
-            import slycot
+            imp.find_module('slycot')
             meth = 'slycot'
         except ImportError:
             try:
-                import pymess
+                imp.find_module('pymess')
                 meth = 'pymess_care'
             except ImportError:
                 meth = 'scipy'
@@ -95,106 +131,133 @@ def solve_ricc(A, B=None, E=None, Q=None, C=None, R=None, D=None, G=None, L=None
         if E is not None or G is not None or L is not None:
             raise NotImplementedError()
         import scipy.linalg as spla
-        A_matrix = A._matrix
-        if trans:
-            A_matrix = A_matrix.T
+        A_mat = A._matrix
         if A.sparse:
-            A_matrix = A_matrix.toarray()
-        B_matrix = B._matrix
-        if trans:
-            B_matrix = B_matrix.T
-        if B.sparse:
-            B_matrix = B_matrix.toarray()
-        if Q is not None:
-            Q_matrix = Q._matrix
-            if Q.sparse:
-                Q_matrix = Q_matrix.toarray()
+            A_mat = A_mat.toarray()
+        if R is not None:
+            R_mat = R._matrix
+            if R.sparse:
+                R_mat = R_mat.toarray()
+        elif D is not None:
+            D_mat = D._matrix
+            if D.sparse:
+                D_mat = D_mat.toarray()
+            R_mat = D_mat.T.dot(D_mat)
         else:
-            C_matrix = C._matrix
-            if C.sparse:
-                C_matrix = C_matrix.toarray()
             if not trans:
-                Q_matrix = C_matrix.T.dot(C_matrix)
+                R_mat = np.eye(B.source.dim)
             else:
-                Q_matrix = C_matrix.dot(C_matrix.T)
-        R_matrix = R._matrix
-        if R.sparse:
-            R_matrix = R_matrix.toarray()
-        X = spla.solve_continuous_are(A_matrix, B_matrix, Q_matrix, R_matrix)
+                R_mat = np.eye(C.range.dim)
+        if not trans:
+            B_mat = B._matrix
+            if B.sparse:
+                B_mat = B_mat.toarray()
+            if Q is not None:
+                Q_mat = Q._matrix
+                if Q.sparse:
+                    Q_mat = Q_mat.toarray()
+            else:
+                C_mat = C._matrix
+                if C.sparse:
+                    C_mat = C_mat.toarray()
+                Q_mat = C_mat.T.dot(C_mat)
+            X = spla.solve_continuous_are(A_mat, B_mat, Q_mat, R_mat)
+        else:
+            C_mat = C._matrix
+            if C.sparse:
+                C_mat = C_mat.toarray()
+            if Q is not None:
+                Q_mat = Q._matrix
+                if Q.sparse:
+                    Q_mat = Q_mat.toarray()
+            else:
+                B_mat = B._matrix
+                if B.sparse:
+                    B_mat = B_mat.toarray()
+                Q_mat = B_mat.dot(B_mat.T)
+            X = spla.solve_continuous_are(A_mat.T, C_mat.T, Q_mat, R_mat)
         from pymor.algorithms.cholp import cholp
         Z = cholp(X, copy=False)
     elif meth == 'slycot':
-        A_matrix = A._matrix
+        if G is not None:
+            raise NotImplementedError()
+        import slycot
+        A_mat = A._matrix
         if trans:
-            A_matrix = A_matrix.T
+            A_mat = A_mat.T
         if A.sparse:
-            A_matrix = A_matrix.toarray()
+            A_mat = A_mat.toarray()
         if E is not None:
-            E_matrix = E._matrix
+            E_mat = E._matrix
             if trans:
-                E_matrix = E_matrix.T
+                E_mat = E_mat.T
             if E.sparse:
-                E_matrix = E_matrix.toarray()
-        B_matrix = B._matrix
+                E_mat = E_mat.toarray()
+        B_mat = B._matrix
         if trans:
-            B_matrix = B_matrix.T
+            B_mat = B_mat.T
         if B.sparse:
-            B_matrix = B_matrix.toarray()
+            B_mat = B_mat.toarray()
 
-        n = A_matrix.shape[0]
-        m = B_matrix.shape[1] if not trans else B_matrix.shape[0]
+        n = A_mat.shape[0]
+        m = B_mat.shape[1] if not trans else B_mat.shape[0]
         dico = 'C'
         p = None
         fact = 'N'
         if C is not None:
-            Q_matrix = Q._matrix
+            Q_mat = C._matrix
             if trans:
-                Q_matrix = Q_matrix.T
+                Q_mat = Q_mat.T
             if C.sparse:
-                Q_matrix = Q_matrix.toarray()
-            p = Q_matrix.shape[1]
+                Q_mat = Q_mat.toarray()
+            p = Q_mat.shape[0]
             fact = 'C'
         else:
-            Q_matrix = Q._matrix
+            Q_mat = Q._matrix
             if Q.sparse:
-                Q_matrix = Q_matrix.toarray()
+                Q_mat = Q_mat.toarray()
         if D is not None:
-            R_matrix = R._matrix
+            R_mat = D._matrix
             if trans:
-                R_matrix = R_matrix.T
+                R_mat = R_mat.T
             if D.sparse:
-                R_matrix = R_matrix.toarray()
-            p = R_matrix.shape[1]
+                R_mat = R_mat.toarray()
+            p = R_mat.shape[0]
             if fact == 'N':
                 fact = 'D'
             else:
                 fact = 'B'
         else:
-            R_matrix = R._matrix
-            if R.sparse:
-                R_matrix = R_matrix.toarray()
+            if R is None:
+                R_mat = np.eye(m)
+            else:
+                R_mat = R._matrix
+                if R.sparse:
+                    R_mat = R_mat.toarray()
         if L is not None:
             jobl = 'N'
-            L_matrix = L._matrix
+            L_mat = L._matrix
             if trans:
-                L_matrix = L_matrix.T
+                L_mat = L_mat.T
             if L.sparse:
-                L_matrix = L_matrix.toarray()
+                L_mat = L_mat.toarray()
         else:
             jobl = 'Z'
-            L_matrix = None
+            L_mat = None
 
         if E is None:
-            X, _, _, _, _ = slycot.sb02od(n, m, A_matrix, B_matrix, Q_matrix, R_matrix, dico, p=p, L=L_matrix,
+            X, _, _, _, _ = slycot.sb02od(n, m, A_mat, B_mat, Q_mat, R_mat, dico, p=p, L=L_mat,
                                           fact=fact)
         else:
             jobb = 'B'
             uplo = 'L'
-            scal = 'G'
+            scal = 'N'
             sort = 'S'
             acc = 'R'
-            _, X, _, _, _, _, _, _, iwarn, _ = slycot.sg02ad(dico, jobb, fact, uplo, jobl, scal, sort, acc, n, m, p,
-                                                             A_matrix, E_matrix, B_matrix, Q_matrix, R_matrix, L_matrix)
+            if L is None:
+                L_mat = np.zeros((n, m))
+            _, X, _, _, _, _, _, _, iwarn = slycot.sg02ad(dico, jobb, fact, uplo, jobl, scal, sort, acc, n, m, p,
+                                                          A_mat, E_mat, B_mat, Q_mat, R_mat, L_mat)
             if iwarn == 1:
                 print('slycot.sg02ad warning: solution may be inaccurate.')
 
@@ -203,30 +266,30 @@ def solve_ricc(A, B=None, E=None, Q=None, C=None, R=None, D=None, G=None, L=None
     elif meth == 'pymess_care':
         if Q is not None or R is not None or D is not None or G is not None or L is not None:
             raise NotImplementedError()
-        A_matrix = A._matrix
-        if trans:
-            A_matrix = A_matrix.T
+        import pymess
+        A_mat = A._matrix
         if A.sparse:
-            A_matrix = A_matrix.toarray()
+            A_mat = A_mat.toarray()
         if E is not None:
-            E_matrix = E._matrix
-            if trans:
-                E_matrix = E_matrix.T
+            E_mat = E._matrix
             if E.sparse:
-                E_matrix = E_matrix.toarray()
-        else:
-            E_matrix = None
-        B_matrix = B._matrix
-        if trans:
-            B_matrix = B_matrix.T
+                E_mat = E_mat.toarray()
+        B_mat = B._matrix
         if B.sparse:
-            B_matrix = B_matrix.toarray()
-        C_matrix = C._matrix
-        if trans:
-            C_matrix = C_matrix.T
+            B_mat = B_mat.toarray()
+        C_mat = C._matrix
         if C.sparse:
-            C_matrix = C_matrix.toarray()
-        Z = pymess.care(A_matrix, E_matrix, B_matrix, C_matrix)
+            C_mat = C_mat.toarray()
+        if not trans:
+            if E is None:
+                Z = pymess.care(A_mat, None, B_mat, C_mat)
+            else:
+                Z = pymess.care(A_mat, E_mat, B_mat, C_mat)
+        else:
+            if E is None:
+                Z = pymess.care(A_mat.T, None, C_mat.T, B_mat.T)
+            else:
+                Z = pymess.care(A_mat.T, E_mat.T, C_mat.T, B_mat.T)
 
     Z = NumpyVectorArray(np.array(Z).T)
 
---
+++ src/pymortests/riccati.py
@@ -0,0 +1,236 @@
+# This file is part of the pyMOR project (http://www.pymor.org).
+# Copyright 2013-2016 pyMOR developers and contributors. All rights reserved.
+# License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+import scipy as sp
+import scipy.linalg as spla
+import scipy.sparse as sps
+
+from pymor.operators.numpy import NumpyMatrixOperator
+from pymor.algorithms.riccati import solve_ricc
+
+import pytest
+
+
+n_list = [10, 100]
+m_list = [1, 2]
+p_list = [1, 2]
+
+
+def fro_norm(A):
+    if not sps.issparse(A):
+        return sp.linalg.norm(A)
+    else:
+        return sps.linalg.norm(A)
+
+
+@pytest.mark.parametrize('n', n_list)
+@pytest.mark.parametrize('m', m_list)
+@pytest.mark.parametrize('p', p_list)
+def test_scipy(n, m, p):
+    np.random.seed(0)
+    A = np.random.randn(n, n) - n * np.eye(n)
+    B = np.random.randn(n, m)
+    C = np.random.randn(p, n)
+    R = np.random.randn(m, m)
+    R = (R + R.T) / 2
+
+    Aop = NumpyMatrixOperator(A)
+    Bop = NumpyMatrixOperator(B)
+    Cop = NumpyMatrixOperator(C)
+    Rop = NumpyMatrixOperator(R)
+
+    Z = solve_ricc(Aop, B=Bop, C=Cop, R=Rop, meth='scipy')
+
+    assert len(Z) <= n
+
+    ATX = A.T.dot(Z.data.T).dot(Z.data)
+    ZTB = Z.data.dot(B)
+    XB = Z.data.T.dot(ZTB)
+    RinvBTXT = spla.solve(R, XB.T)
+    CTC = C.T.dot(C)
+    assert fro_norm(ATX + ATX.T - XB.dot(RinvBTXT) + CTC) / fro_norm(CTC) < 1e-10
+
+
+@pytest.mark.parametrize('n', n_list)
+@pytest.mark.parametrize('m', m_list)
+@pytest.mark.parametrize('p', p_list)
+def test_scipy_trans(n, m, p):
+    np.random.seed(0)
+    A = np.random.randn(n, n) - n * np.eye(n)
+    B = np.random.randn(n, m)
+    C = np.random.randn(p, n)
+    R = np.random.randn(p, p)
+    R = (R + R.T) / 2
+
+    Aop = NumpyMatrixOperator(A)
+    Bop = NumpyMatrixOperator(B)
+    Cop = NumpyMatrixOperator(C)
+    Rop = NumpyMatrixOperator(R)
+
+    Z = solve_ricc(Aop, B=Bop, C=Cop, R=Rop, trans=True, meth='scipy')
+
+    assert len(Z) <= n
+
+    AX = A.dot(Z.data.T).dot(Z.data)
+    ZTCT = Z.data.dot(C.T)
+    XCT = Z.data.T.dot(ZTCT)
+    RinvCXT = spla.solve(R, XCT.T)
+    BBT = B.dot(B.T)
+    assert fro_norm(AX + AX.T - XCT.dot(RinvCXT) + BBT) / fro_norm(BBT) < 1e-10
+
+
+@pytest.mark.parametrize('n', n_list)
+@pytest.mark.parametrize('m', m_list)
+@pytest.mark.parametrize('p', p_list)
+def test_pymess(n, m, p):
+    np.random.seed(0)
+    A = np.random.randn(n, n) - n * np.eye(n)
+    B = np.random.randn(n, m)
+    C = np.random.randn(p, n)
+
+    Aop = NumpyMatrixOperator(A)
+    Bop = NumpyMatrixOperator(B)
+    Cop = NumpyMatrixOperator(C)
+
+    Z = solve_ricc(Aop, B=Bop, C=Cop, meth='pymess_care')
+
+    assert len(Z) <= n
+
+    ATX = A.T.dot(Z.data.T).dot(Z.data)
+    XB = Z.data.T.dot(Z.data.dot(B))
+    CTC = C.T.dot(C)
+    assert fro_norm(ATX + ATX.T - XB.dot(XB.T) + CTC) / fro_norm(CTC) < 1e-10
+
+
+@pytest.mark.parametrize('n', n_list)
+@pytest.mark.parametrize('m', m_list)
+@pytest.mark.parametrize('p', p_list)
+def test_pymess_trans(n, m, p):
+    np.random.seed(0)
+    A = np.random.randn(n, n) - n * np.eye(n)
+    B = np.random.randn(n, m)
+    C = np.random.randn(p, n)
+
+    Aop = NumpyMatrixOperator(A)
+    Bop = NumpyMatrixOperator(B)
+    Cop = NumpyMatrixOperator(C)
+
+    Z = solve_ricc(Aop, B=Bop, C=Cop, trans=True, meth='pymess_care')
+
+    assert len(Z) <= n
+
+    AX = A.dot(Z.data.T).dot(Z.data)
+    XCT = Z.data.T.dot(Z.data.dot(C.T))
+    BBT = B.dot(B.T)
+    assert fro_norm(AX + AX.T - XCT.dot(XCT.T) + BBT) / fro_norm(BBT) < 1e-10
+
+
+@pytest.mark.parametrize('n', n_list)
+@pytest.mark.parametrize('m', m_list)
+@pytest.mark.parametrize('p', p_list)
+def test_pymess_E(n, m, p):
+    np.random.seed(0)
+    A = np.random.randn(n, n) - n * np.eye(n)
+    B = np.random.randn(n, m)
+    C = np.random.randn(p, n)
+    E = np.random.randn(n, n) + n * np.eye(n)
+
+    Aop = NumpyMatrixOperator(A)
+    Bop = NumpyMatrixOperator(B)
+    Cop = NumpyMatrixOperator(C)
+    Eop = NumpyMatrixOperator(E)
+
+    Z = solve_ricc(Aop, B=Bop, C=Cop, E=Eop, meth='pymess_care')
+
+    assert len(Z) <= n
+
+    ATZ = A.T.dot(Z.data.T)
+    ZTE = Z.data.dot(E)
+    ATXE = ATZ.dot(ZTE)
+    ZTB = Z.data.dot(B)
+    ETXB = E.T.dot(Z.data.T).dot(ZTB)
+    CTC = C.T.dot(C)
+    assert fro_norm(ATXE + ATXE.T - ETXB.dot(ETXB.T) + CTC) / fro_norm(CTC) < 1e-10
+
+
+@pytest.mark.parametrize('n', n_list)
+@pytest.mark.parametrize('m', m_list)
+@pytest.mark.parametrize('p', p_list)
+def test_pymess_E_trans(n, m, p):
+    np.random.seed(0)
+    A = np.random.randn(n, n) - n * np.eye(n)
+    B = np.random.randn(n, m)
+    C = np.random.randn(p, n)
+    E = np.random.randn(n, n) + n * np.eye(n)
+
+    Aop = NumpyMatrixOperator(A)
+    Bop = NumpyMatrixOperator(B)
+    Cop = NumpyMatrixOperator(C)
+    Eop = NumpyMatrixOperator(E)
+
+    Z = solve_ricc(Aop, B=Bop, C=Cop, E=Eop, trans=True, meth='pymess_care')
+
+    assert len(Z) <= n
+
+    AZ = A.dot(Z.data.T)
+    ZTET = Z.data.dot(E.T)
+    AXET = AZ.dot(ZTET)
+    ZTCT = Z.data.dot(C.T)
+    EXCT = E.dot(Z.data.T).dot(ZTCT)
+    BBT = B.dot(B.T)
+    assert fro_norm(AXET + AXET.T - EXCT.dot(EXCT.T) + BBT) / fro_norm(BBT) < 1e-10
+
+
+@pytest.mark.parametrize('n', n_list)
+@pytest.mark.parametrize('m', m_list)
+@pytest.mark.parametrize('p', p_list)
+def test_slycot(n, m, p):
+    np.random.seed(0)
+    A = np.random.randn(n, n) - n * np.eye(n)
+    B = np.random.randn(n, m)
+    C = np.random.randn(p, n)
+
+    A = NumpyMatrixOperator(A)
+    B = NumpyMatrixOperator(B)
+    C = NumpyMatrixOperator(C)
+
+    Z = solve_ricc(A, B=B, C=C, meth='slycot')
+
+    assert len(Z) <= n
+
+    ATX = A._matrix.T.dot(Z.data.T).dot(Z.data)
+    XB = Z.data.T.dot(Z.data.dot(B._matrix))
+    CTC = C._matrix.T.dot(C._matrix)
+    assert fro_norm(ATX + ATX.T - XB.dot(XB.T) + CTC) / fro_norm(CTC) < 1e-10
+
+
+@pytest.mark.parametrize('n', n_list)
+@pytest.mark.parametrize('m', m_list)
+@pytest.mark.parametrize('p', p_list)
+def test_slycot_E(n, m, p):
+    np.random.seed(0)
+    A = np.random.randn(n, n) - n * np.eye(n)
+    B = np.random.randn(n, m)
+    C = np.random.randn(p, n)
+    E = np.random.randn(n, n) + n * np.eye(n)
+
+    A = NumpyMatrixOperator(A)
+    B = NumpyMatrixOperator(B)
+    C = NumpyMatrixOperator(C)
+    E = NumpyMatrixOperator(E)
+
+    Z = solve_ricc(A, B=B, C=C, E=E, meth='slycot')
+
+    assert len(Z) <= n
+
+    ATZ = A._matrix.T.dot(Z.data.T)
+    ZTE = Z.data.dot(E._matrix)
+    ATXE = ATZ.dot(ZTE)
+    ZTB = Z.data.dot(B._matrix)
+    ETXB = E._matrix.T.dot(Z.data.T).dot(ZTB)
+    CTC = C._matrix.T.dot(C._matrix)
+    assert fro_norm(ATXE + ATXE.T - ETXB.dot(ETXB.T) + CTC) / fro_norm(CTC) < 1e-10
