150f081db17d205b0d6a8a69ab1e5607c8d957f7
Petar Mlinarić <mlinaric@mpi-magdeburg.mpg.de>
2016-05-10 23:11:09 +0200

    [tests.to_matrix] update test

--- src/pymortests/to_matrix.py
+++ src/pymortests/to_matrix.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import, division, print_function
 import numpy as np
 import scipy.sparse as sps
 
-from pymor.algorithms.numpy import to_numpy_operator
+from pymor.algorithms.to_matrix import to_matrix
 from pymor.operators.block import BlockDiagonalOperator
 from pymor.operators.constructions import (AdjointOperator, Concatenation, IdentityOperator, LincombOperator,
                                            VectorArrayOperator)
@@ -15,7 +15,7 @@ from pymor.operators.numpy import NumpyMatrixOperator
 from pymor.vectorarrays.numpy import NumpyVectorArray, NumpyVectorSpace
 
 
-def test_to_numpy_operator():
+def test_to_matrix():
     np.random.seed(0)
     A = np.random.randn(2, 2)
     B = np.random.randn(3, 3)
@@ -32,12 +32,12 @@ def test_to_numpy_operator():
     Xop = BlockDiagonalOperator([LincombOperator([IdentityOperator(NumpyVectorSpace(2)), Aop],
                                                  [1, 1]), Concatenation(Bop, AdjointOperator(Cop))])
 
-    assert np.allclose(X, to_numpy_operator(Xop)._matrix)
+    assert np.allclose(X, to_matrix(Xop))
 
     np.random.seed(0)
     V = np.random.randn(10, 2)
     Vva = NumpyVectorArray(V.T)
     Vop = VectorArrayOperator(Vva)
-    assert np.allclose(V, to_numpy_operator(Vop)._matrix)
+    assert np.allclose(V, to_matrix(Vop))
     Vop = VectorArrayOperator(Vva, transposed=True)
-    assert np.allclose(V, to_numpy_operator(Vop)._matrix.T)
+    assert np.allclose(V, to_matrix(Vop).T)
