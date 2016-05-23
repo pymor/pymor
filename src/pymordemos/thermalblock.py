d5f3fa995eca6247ba47daada182d3d7b159362b
Rene Milk <rene.milk@wwu.de>
2016-05-23 11:00:12 +0200

    move petsc/MPI workaround into main package's init
    this is still somewhat hacky, but I've no better idea atm.
    @sdrave maybe?

--- src/pymordemos/thermalblock.py
+++ src/pymordemos/thermalblock.py
@@ -292,13 +292,6 @@ def _discretize_fenics(xblocks, yblocks, grid_num_intervals, element_order):
     ########################################
 
     import dolfin as df
-    # this solves sporadic mpi calls happening after finalize
-    try:
-        import petsc4py
-        petsc4py.init()
-    except ImportError:
-        pass
-
     mesh = df.UnitSquareMesh(grid_num_intervals, grid_num_intervals, 'crossed')
     V = df.FunctionSpace(mesh, 'Lagrange', element_order)
     u = df.TrialFunction(V)
