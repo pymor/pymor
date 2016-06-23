c42027e450234a63ef3b71458743bda209a88b7c
Stephan Rave <stephanrave@uni-muenster.de>
2016-06-23 13:04:27 +0200

    [ngsolve] make ngsolve_visualize importable

--- src/pymor/gui/ngsolve_visualize.py
+++ src/pymor/gui/ngsolve_visualize.py
@@ -1,8 +1,9 @@
-import os
+if __name__ == '__main__':
+    import os
 
-from pymor.core.pickle import load
+    from pymor.core.pickle import load
 
-with open(os.environ['NGSOLVE_VISUALIZE_FILE'], 'rb') as f:
-    u = load(f)
+    with open(os.environ['NGSOLVE_VISUALIZE_FILE'], 'rb') as f:
+        u = load(f)
 
-Draw(u)
+    Draw(u)
