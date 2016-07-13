08ee6e68fa8dca74f0e2100e9cd3993fb35c61b0
Stephan Rave <stephanrave@uni-muenster.de>
2016-07-13 18:25:10 +0200

    add stub .py files for Cython extension modules
    Importing one of these modules will lead to an ImportError
    with information on building pyMOR's Cython modules.

---
+++ src/pymor/tools/inplace.py
@@ -0,0 +1,4 @@
+raise ImportError('''
+Cython extension module 'pymor.tools.inplace' has not been built.
+Please run 'python setup.py build_ext --inplace' in the root
+directory of the pyMOR repository.''')
