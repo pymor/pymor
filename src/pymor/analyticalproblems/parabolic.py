59bb765b3f295df872cfffa30146090ee85864cc
Stephan Rave <stephanrave@uni-muenster.de>
2016-08-25 09:26:02 +0200

    [docs] improve analyticalproblems.parabolic

--- src/pymor/analyticalproblems/parabolic.py
+++ src/pymor/analyticalproblems/parabolic.py
@@ -36,15 +36,15 @@ class ParabolicProblem(ImmutableInterface):
         `len(diffusion_functions) == 1`, `diffusion_functionals` is allowed
         to be `None`, in which case no parameter dependence is assumed.
     dirichlet_data
-        |Function| providing the Dirichlet boundary values in global coordinates.
+        |Function| providing the Dirichlet boundary values.
     neumann_data
-        |Function| providing the Neumann boundary values in global coordinates.
+        |Function| providing the Neumann boundary values.
+    initial_data
+        |Function| providing the initial values.
+    T
+        The final time T.
     parameter_space
         |ParameterSpace| for the problem.
-    initial_data
-        |Function| providing the initial values in global coordinates.
-    T
-        The end time T.
     name
         Name of the problem.
 
