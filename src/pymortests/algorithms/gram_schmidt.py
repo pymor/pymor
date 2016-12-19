12045162153749713abc3d7e2e0a72ce5d66c12e
Stephan Rave <stephanrave@uni-muenster.de>
2016-12-19 17:07:01 +0100

    [test] enhance gram_schmidt tests

---
+++ src/pymortests/algorithms/gram_schmidt.py
@@ -0,0 +1,38 @@
+# This file is part of the pyMOR project (http://www.pymor.org).
+# Copyright 2013-2016 pyMOR developers and contributors. All rights reserved.
+# License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+import numpy as np
+
+from pymor.algorithms.basic import almost_equal
+from pymor.algorithms.gram_schmidt import gram_schmidt
+from pymortests.fixtures.operator import operator_with_arrays_and_products
+from pymortests.fixtures.vectorarray import vector_array, vector_array_without_reserve
+
+
+def test_gram_schmidt(vector_array):
+    U = vector_array
+
+    V = U.copy()
+    onb = gram_schmidt(U, copy=True)
+    assert np.all(almost_equal(U, V))
+    assert np.allclose(onb.dot(onb), np.eye(len(onb)))
+    assert np.all(almost_equal(U, onb.lincomb(U.dot(onb)), rtol=1e-13))
+
+    onb2 = gram_schmidt(U, copy=False)
+    assert np.all(almost_equal(onb, onb2))
+    assert np.all(almost_equal(onb, U))
+
+
+def test_gram_schmidt_with_product(operator_with_arrays_and_products):
+    _, _, U, _, p, _ = operator_with_arrays_and_products
+
+    V = U.copy()
+    onb = gram_schmidt(U, product=p, copy=True)
+    assert np.all(almost_equal(U, V))
+    assert np.allclose(p.apply2(onb, onb), np.eye(len(onb)))
+    assert np.all(almost_equal(U, onb.lincomb(p.apply2(U, onb)), rtol=1e-13))
+
+    onb2 = gram_schmidt(U, product=p, copy=False)
+    assert np.all(almost_equal(onb, onb2))
+    assert np.all(almost_equal(onb, U))
