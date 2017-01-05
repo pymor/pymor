2833af4b7ad1a6bd47722ede5cc35539aaea6ff0
Stephan Rave <stephanrave@uni-muenster.de>
2017-01-05 15:53:59 +0100

    [fenics] move fenics bindings to bindings.fenics

--- src/pymordemos/thermalblock.py
+++ src/pymordemos/thermalblock.py
@@ -335,9 +335,7 @@ def _discretize_fenics(xblocks, yblocks, grid_num_intervals, element_order):
     ###########################################
 
     # FEniCS wrappers
-    from pymor.gui.fenics import FenicsVisualizer
-    from pymor.operators.fenics import FenicsMatrixOperator
-    from pymor.vectorarrays.fenics import FenicsVectorSpace
+    from pymor.bindings.fenics import FenicsVectorSpace, FenicsMatrixOperator, FenicsVisualizer
 
     # generic pyMOR classes
     from pymor.discretizations.basic import StationaryDiscretization
