ba7ba39132482866ca7a4224ec549ca0d1bc63ad
Rene Milk <rene.milk@wwu.de>
2017-01-10 15:20:25 +0100

    [test] adds a little py.test plugin to putput test timings
    it will leave a csv file at $CWD/test_timings.csv

---
+++ src/pymortests/conftest.py
@@ -0,0 +1,46 @@
+# This file is part of the pyMOR project (http://www.pymor.org).
+# Copyright 2013-2016 pyMOR developers and contributors. All rights reserved.
+# License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+import pytest
+import time
+from csv import DictWriter
+
+
+class ExecutionTimeCSV:
+
+    def __init__(self):
+        import pymor
+        self._elapsed_times = {'version': pymor.__version__}
+
+    @pytest.hookimpl()
+    def pytest_addoption(self, parser):
+        # this doesn't actually work atm
+        parser.addoption('--timings-file', type=str, dest='timings-file', default='test_timings.csv')
+
+    @pytest.hookimpl()
+    def pytest_configure(self, config):
+        self._filename = config.getoption('timings-file')
+
+    @pytest.hookimpl(hookwrapper=True)
+    def pytest_runtest_setup(self, item):
+        started = time.process_time()
+        yield
+        self._elapsed_times[item.name] = started
+
+    @pytest.hookimpl(hookwrapper=True)
+    def pytest_runtest_teardown(self, item):
+        stopped = time.process_time()
+        yield
+        self._elapsed_times[item.name] = str(stopped - self._elapsed_times[item.name])
+
+    @pytest.hookimpl()
+    def pytest_terminal_summary(self, terminalreporter, exitstatus):
+        with open(self._filename, 'w') as csvfile:
+            writer = DictWriter(csvfile, fieldnames=self._elapsed_times.keys())
+            writer.writeheader()
+            writer.writerow(self._elapsed_times)
+
+
+def pytest_configure(config):
+    config.pluginmanager.register(ExecutionTimeCSV(), 'ExecutionTimeCSV')
