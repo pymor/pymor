30985c671b159e19ce8925c234f2896c43a451dc
Stephan Rave <stephanrave@uni-muenster.de>
2017-05-09 16:13:48 +0200

    [BitmapFunction] accept non-grayscale images
    and do not use rawdata to convert to numpy array

--- src/pymor/functions/bitmap.py
+++ src/pymor/functions/bitmap.py
@@ -31,10 +31,10 @@ class BitmapFunction(FunctionBase):
         except ImportError:
             raise ImportError("PIL is needed for loading images. Try 'pip install pillow'")
         img = Image.open(filename)
-        assert img.mode == "L", "Image " + filename + " not in grayscale mode"
-        rawdata = np.array(img.getdata())
-        assert rawdata.shape[0] == img.size[0]*img.size[1]
-        self.bitmap = rawdata.reshape(img.size[0], img.size[1]).T[:, ::-1]
+        if not img.mode == "L":
+            self.logger.warn("Image " + filename + " not in grayscale mode. Convertig to grayscale.")
+            img = img.convert('L')
+        self.bitmap = np.array(img).T[:, ::-1]
         self.bounding_box = bounding_box
         self.lower_left = np.array(bounding_box[0])
         self.size = np.array(bounding_box[1] - self.lower_left)
