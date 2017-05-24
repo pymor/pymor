6e5f8cc100bb58af8e1bc6cc8d9541ca1bbfe3a8
Stephan Rave <stephanrave@uni-muenster.de>
2017-05-24 11:10:01 +0200

    [doc] fix mocking of Qt modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,7 @@ if os.environ.get('READTHEDOCS', None) == 'True':
                     'docopt',
                     'dogpile', 'dogpile.cache', 'dogpile.cache.backends', 'dogpile.cache.backends.file',
                     'dogpile.cache.compat',
-                    'PySide', 'PySide.QtGui', 'PySide.QtCore', 'PySide.QtOpenGL',
+                    'Qt', 'Qt.QtGui', 'Qt.QtCore', 'Qt.QtOpenGL', 'Qt.QtWidgets',
                     'OpenGL', 'OpenGL.GL',
                     'matplotlib', 'matplotlib.backends', 'matplotlib.backends.backend_qt4agg', 'matplotlib.figure',
                     'matplotlib.pyplot',
