74743c11e6a4e06c113c36e2a59ec028d6b91fee
Rene Milk <rene.milk@wwu.de>
2017-07-21 10:37:54 +0200

    [deps] add some magic to get technically unfitting suggests into extras

--- dependencies.py
+++ dependencies.py
@@ -71,7 +71,26 @@ def extras():
             name = import_names[name]
         return name
 
-    full = [_ex(f) for f in set(itertools.chain(doc_requires, tests_require, install_suggests.keys()))]
+    def _candidates():
+        # skip those which aren't needed in our current environment (py ver, platform)
+        for pkg in set(itertools.chain(doc_requires, tests_require, install_suggests.keys())):
+            try:
+                marker = next(pkg_resources.parse_requirements(pkg)).marker
+                if marker is None or marker.evaluate():
+                    yield pkg
+            except pkg_resources.RequirementParseError:
+                # try to fake a package to get the marker parsed
+                clean = _ex(pkg)
+                stripped = strip_markers(pkg)
+                fake_pkg = 'pip ' + pkg.replace(stripped, '')
+                try:
+                    marker = next(pkg_resources.parse_requirements(fake_pkg)).marker
+                    if marker is None or marker.evaluate():
+                        yield pkg
+                except pkg_resources.RequirementParseError:
+                    continue
+
+    full = [_ex(f) for f in _candidates()]
     return {
         'full':  full,
         'travis':  travis_requires,
