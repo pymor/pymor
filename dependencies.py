5a55d0959db58f1942f6e17dbfd3ac1d9e2ca947
Rene Milk <rene.milk@wwu.de>
2017-08-02 10:54:54 +0200

    [test/setup] add pytest-xdist, execute tests with "-n auto"

--- dependencies.py
+++ dependencies.py
@@ -13,7 +13,7 @@ def _pyside(rev, marker=True):
     return '{}'.format(_PYSIDE[rev])
 
 _QT_COMMENT = 'solution visualization for builtin discretizations'
-tests_require = ['pytest>=3.1', 'pytest-cov']
+tests_require = ['pytest>=3.2', 'pytest-cov', 'pytest-xdist']
 install_requires = ['cython>=0.20.1', 'numpy>=1.8.1', 'scipy>=0.13.3', 'Sphinx>=1.4.0', 'docopt', 'Qt.py>=1.0.0b3']
 setup_requires = ['pytest-runner>=2.9', 'cython>=0.20.1', 'numpy>=1.8.1']
 install_suggests = {'ipython>=3.0': 'an enhanced interactive python shell',
@@ -32,7 +32,7 @@ install_suggests = {'ipython>=3.0': 'an enhanced interactive python shell',
                     'pillow': 'image library used for bitmap data functions',
                     'psutil': 'Process management abstractions used for gui'}
 doc_requires = ['sphinx>=1.5', 'cython', 'numpy']
-travis_requires = ['pytest-cov', 'check-manifest', 'python-coveralls', 'pytest-travis-fold']
+travis_requires = ['pytest-cov', 'pytest-xdist', 'check-manifest', 'python-coveralls', 'pytest-travis-fold']
 import_names = {'ipython': 'IPython',
                 'pytest-cache': 'pytest_cache',
                 'pytest-capturelog': 'pytest_capturelog',
