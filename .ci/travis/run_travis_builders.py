98272c42f3efd790f72f720b4becc2d76f29f8c3
René Milk <rene.milk@wwu.de>
2017-10-26 14:51:36 +0200

    [travis] revamp local run script

--- .ci/travis/run_travis_builders.py
+++ .ci/travis/run_travis_builders.py
@@ -22,30 +22,38 @@ def _run_config(tm, clone_dir, commit):
 
     with TemporaryDirectory() as tmp_dir:
         os.chdir(tmp_dir)
-        subprocess.check_call(['git', 'clone', clone_dir])
+        subprocess.check_call(['git', 'clone', clone_dir], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         os.chdir('pymor')
         volumes = { os.getcwd(): { 'bind': '/src',  'mode': 'rw'}}
-        subprocess.check_call(['git', 'checkout', commit])
+        subprocess.check_call(['git', 'checkout', commit], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         try:
-            cont = client.containers.run(image, environment=env,
+            container = client.containers.run(image, environment=env,
                                             stderr=True, detach=True,
                                             volumes=volumes,
                                             command='./.ci/travis/script.bash')
         except docker.errors.ContainerError as err:
-            logging.error('Failed config: {} - {}'.format(tag, marker))
-            logging.error(err)
-            return
-        for lg in cont.logs(stream=True, stderr=True, stdout=True):
-            print(lg.decode())
+            return False, err
+        exit = container.wait()
+        if exit:
+            return False, container.logs(stream=False, stderr=True, stdout=True)
+        return True, ''
 
 
-docker_tags = env.list('PYMOR_DOCKER_TAG', default=['2.7', '3.4', '3.5', '3.6'])
+docker_tags = env.list('PYMOR_DOCKER_TAG', default=['2.7', '3.5', '3.6'])
 pytest_marker = env.list('PYMOR_PYTEST_MARKER', default=["None", 'PIP_ONLY', 'MPI'])
 commit = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode().strip()
-variations = list(product(docker_tags, pytest_marker)) + [('3.6', 'NUMPY')]
+variations = list(product(docker_tags, pytest_marker))
+if '3.6' in docker_tags and len(pytest_marker) > 1:
+    variations += [('3.6', 'NUMPY')]
 with TemporaryDirectory() as clone_tmp:
     clone_dir = os.path.join(clone_tmp, 'pymor')
-    subprocess.check_call(['git', 'clone', os.getcwd(), clone_dir])
+    subprocess.check_call(['git', 'clone', os.getcwd(), clone_dir], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     run_configs = partial(_run_config, clone_dir=clone_dir, commit=commit)
+    results = dict()
     for tm in variations:
-        run_configs(tm)
+        results[tm] = run_configs(tm)
+for tm, (success, msg) in results.items():
+    print('Variation {0[0]} - {0[1]}: {1}'.format(tm, 'success' if success else 'failed'))
+for tm, (success, msg) in results.items():
+    if not success:
+        print('\nFaillogs {0[0]} - {0[1]}: \n{1}'.format(tm, msg))
