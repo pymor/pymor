cf6713a8c5dc5b4b7360f78e37c78f10a1222a99
René Milk <rene.milk@wwu.de>
2017-11-08 12:31:19 +0100

    [deps] update pyside url

--- dependencies.py
+++ dependencies.py
@@ -3,7 +3,7 @@
 # Copyright 2013-2017 pyMOR developers and contributors. All rights reserved.
 # License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
-_PYSIDE = {'2.7': 'https://pymor.github.io/wheels/PySide-1.2.2-cp27-none-linux_x86_64.whl',
+_PYSIDE = {'2.7': 'https://pymor.github.io/wheels/PySide-1.2.4-cp27-none-linux_x86_64.whl',
            '3.3': 'https://pymor.github.io/wheels/PySide-1.2.2-cp33-cp33m-linux_x86_64.whl',
            '3.4': 'https://pymor.github.io/wheels/PySide-1.2.4-cp34-cp34m-linux_x86_64.whl'}
 
