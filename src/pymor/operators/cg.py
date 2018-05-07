8d17527ac897507e77e243d2fb62ad11388e35b1
deneick <d.eickhorn@mail.de>
2018-05-07 16:06:46 +0200

    added missing robin_data parameters in operators.cg

--- src/pymor/operators/cg.py
+++ src/pymor/operators/cg.py
@@ -71,7 +71,7 @@ class L2ProductFunctionalP1(NumpyMatrixBasedOperator):
         self.order = order
         self.solver_options = solver_options
         self.name = name
-        self.build_parameter_type(function, dirichlet_data, neumann_data)
+        self.build_parameter_type(function, dirichlet_data, neumann_data, robin_data[0], robin_data[1])
 
     def _assemble(self, mu=None):
         g = self.grid
