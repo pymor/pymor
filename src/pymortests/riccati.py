70e6a215aa1ffc853f00ab633184364bb0a558f3
Stephan Rave <stephanrave@uni-muenster.de>
2018-08-06 17:34:24 +0200

    DOCKER_NGSOLVE -> DOCKER_PYMOR

--- src/pymortests/riccati.py
+++ src/pymortests/riccati.py
@@ -107,7 +107,7 @@ def test_scipy_trans(n, m, p):
     assert fro_norm(AX + AX.T - XCT.dot(RinvCXT) + BBT) / fro_norm(BBT) < 1e-10
 
 
-@pytest.mark.skipif(not os.environ.get('DOCKER_NGSOLVE', False) and not config.HAVE_PYMESS, reason='pymess not available')
+@pytest.mark.skipif(not os.environ.get('DOCKER_PYMOR', False) and not config.HAVE_PYMESS, reason='pymess not available')
 @pytest.mark.parametrize('n', n_list_big)
 @pytest.mark.parametrize('m', m_list)
 @pytest.mark.parametrize('p', p_list)
@@ -133,7 +133,7 @@ def test_pymess(n, m, p, me_solver):
     assert fro_norm(ATX + ATX.T - XB.dot(XB.T) + CTC) / fro_norm(CTC) < 1e-10
 
 
-@pytest.mark.skipif(not os.environ.get('DOCKER_NGSOLVE', False) and not config.HAVE_PYMESS, reason='pymess not available')
+@pytest.mark.skipif(not os.environ.get('DOCKER_PYMOR', False) and not config.HAVE_PYMESS, reason='pymess not available')
 @pytest.mark.parametrize('n', n_list_big)
 @pytest.mark.parametrize('m', m_list)
 @pytest.mark.parametrize('p', p_list)
@@ -159,7 +159,7 @@ def test_pymess_trans(n, m, p, me_solver):
     assert fro_norm(AX + AX.T - XCT.dot(XCT.T) + BBT) / fro_norm(BBT) < 1e-10
 
 
-@pytest.mark.skipif(not os.environ.get('DOCKER_NGSOLVE', False) and not config.HAVE_PYMESS, reason='pymess not available')
+@pytest.mark.skipif(not os.environ.get('DOCKER_PYMOR', False) and not config.HAVE_PYMESS, reason='pymess not available')
 @pytest.mark.parametrize('n', n_list_big)
 @pytest.mark.parametrize('m', m_list)
 @pytest.mark.parametrize('p', p_list)
@@ -189,7 +189,7 @@ def test_pymess_E(n, m, p, me_solver):
     assert fro_norm(ATXE + ATXE.T - ETXB.dot(ETXB.T) + CTC) / fro_norm(CTC) < 1e-10
 
 
-@pytest.mark.skipif(not os.environ.get('DOCKER_NGSOLVE', False) and not config.HAVE_PYMESS, reason='pymess not available')
+@pytest.mark.skipif(not os.environ.get('DOCKER_PYMOR', False) and not config.HAVE_PYMESS, reason='pymess not available')
 @pytest.mark.parametrize('n', n_list_big)
 @pytest.mark.parametrize('m', m_list)
 @pytest.mark.parametrize('p', p_list)
@@ -219,7 +219,7 @@ def test_pymess_E_trans(n, m, p, me_solver):
     assert fro_norm(AXET + AXET.T - EXCT.dot(EXCT.T) + BBT) / fro_norm(BBT) < 1e-10
 
 
-@pytest.mark.skipif(not os.environ.get('DOCKER_NGSOLVE', False) and not config.HAVE_SLYCOT, reason='slycot not available')
+@pytest.mark.skipif(not os.environ.get('DOCKER_PYMOR', False) and not config.HAVE_SLYCOT, reason='slycot not available')
 @pytest.mark.parametrize('n', n_list_small)
 @pytest.mark.parametrize('m', m_list)
 @pytest.mark.parametrize('p', p_list)
@@ -244,7 +244,7 @@ def test_slycot(n, m, p):
     assert fro_norm(ATX + ATX.T - XB.dot(XB.T) + CTC) / fro_norm(CTC) < 1e-8
 
 
-@pytest.mark.skipif(not os.environ.get('DOCKER_NGSOLVE', False) and not config.HAVE_SLYCOT, reason='slycot not available')
+@pytest.mark.skipif(not os.environ.get('DOCKER_PYMOR', False) and not config.HAVE_SLYCOT, reason='slycot not available')
 @pytest.mark.parametrize('n', n_list_small)
 @pytest.mark.parametrize('m', m_list)
 @pytest.mark.parametrize('p', p_list)
@@ -269,7 +269,7 @@ def test_slycot_trans(n, m, p):
     assert fro_norm(AX + AX.T - XCT.dot(XCT.T) + BBT) / fro_norm(BBT) < 1e-8
 
 
-@pytest.mark.skipif(not os.environ.get('DOCKER_NGSOLVE', False) and not config.HAVE_SLYCOT, reason='slycot not available')
+@pytest.mark.skipif(not os.environ.get('DOCKER_PYMOR', False) and not config.HAVE_SLYCOT, reason='slycot not available')
 @pytest.mark.parametrize('n', n_list_small)
 @pytest.mark.parametrize('m', m_list)
 @pytest.mark.parametrize('p', p_list)
@@ -298,7 +298,7 @@ def test_slycot_E(n, m, p):
     assert fro_norm(ATXE + ATXE.T - ETXB.dot(ETXB.T) + CTC) / fro_norm(CTC) < 1e-8
 
 
-@pytest.mark.skipif(not os.environ.get('DOCKER_NGSOLVE', False) and not config.HAVE_SLYCOT, reason='slycot not available')
+@pytest.mark.skipif(not os.environ.get('DOCKER_PYMOR', False) and not config.HAVE_SLYCOT, reason='slycot not available')
 @pytest.mark.parametrize('n', n_list_small)
 @pytest.mark.parametrize('m', m_list)
 @pytest.mark.parametrize('p', p_list)
