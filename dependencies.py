f454f62b68d613bcfde18acda91e26eadeb49459
René Milk <rene.milk@wwu.de>
2018-08-13 17:11:49 +0200

    [requirements] temp switch pymess location for possible fortran + 3.7 fix

--- dependencies.py
+++ dependencies.py
@@ -7,6 +7,8 @@ _PYTEST = 'pytest>=3.3'
 
 def _pymess(rev, major, minor, marker=True):
     url = 'https://www.mpi-magdeburg.mpg.de/mpcsc/software/cmess/{rev}/pymess-{rev}-cp{major}{minor}-cp{major}{minor}m-manylinux1_x86_64.whl'.format(rev=rev, major=major, minor=minor)
+    # tmp workaround till next release
+    url = 'https://pymor.github.io/wheels/pymess-{rev}-cp{major}{minor}-cp{major}{minor}m-manylinux1_x86_64.whl'.format(rev=rev, major=major, minor=minor)
     if marker:
         return '{url} ; python_version == "{major}.{minor}" and "linux" in sys_platform'.format(url=url, major=major, minor=minor)
     return url
@@ -24,6 +26,7 @@ install_suggests = {'ipython>=3.0': 'an enhanced interactive python shell',
                     _PYTEST: 'testing framework required to execute unit tests',
                     _pymess('1.0.0', 3, 5): 'Python bindings for M.E.S.S. (Matrix Equation Sparse Solver)',
                     _pymess('1.0.0', 3, 6): 'Python bindings for M.E.S.S. (Matrix Equation Sparse Solver)',
+                    _pymess('1.0.0', 3, 7): 'Python bindings for M.E.S.S. (Matrix Equation Sparse Solver)',
                     'PyQt5': 'solution visualization for builtin discretizations',
                     'pillow': 'image library used for bitmap data functions',
                     'psutil': 'Process management abstractions used for gui',
@@ -39,6 +42,7 @@ import_names = {'ipython': 'IPython',
                 'pytest-pep8': 'pytest_pep8',
                 _pymess('1.0.0', 3, 5, False): 'pymess',
                 _pymess('1.0.0', 3, 6, False): 'pymess',
+                _pymess('1.0.0', 3, 7, False): 'pymess',
                 'pyopengl': 'OpenGL'}
 needs_extra_compile_setup = ['mpi4py']
 
