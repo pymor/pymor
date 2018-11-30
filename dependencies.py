20dbdaadd692e3c69a904a6cf3a0d8138a32866a
René Milk <rene.milk@wwu.de>
2018-11-30 20:25:31 +0100

    [deps] try using pep 508 resource urls for pymess wheel
    see also https://www.python.org/dev/peps/pep-0508/#examples
    Note however that the upcoming pip 18.2 will fail to
    install any package that uses those form installing
    from PyPi

--- dependencies.py
+++ dependencies.py
@@ -6,9 +6,9 @@
 _PYTEST = 'pytest>=3.3'
 
 def _pymess(rev, major, minor, marker=True):
-    url = 'https://www.mpi-magdeburg.mpg.de/mpcsc/software/cmess/{rev}/pymess-{rev}-cp{major}{minor}-cp{major}{minor}m-manylinux1_x86_64.whl'.format(rev=rev, major=major, minor=minor)
+    url = 'pymess@https://www.mpi-magdeburg.mpg.de/mpcsc/software/cmess/{rev}/pymess-{rev}-cp{major}{minor}-cp{major}{minor}m-manylinux1_x86_64.whl'.format(rev=rev, major=major, minor=minor)
     # tmp workaround till next release
-    url = 'https://pymor.github.io/wheels/pymess-{rev}-cp{major}{minor}-cp{major}{minor}m-manylinux1_x86_64.whl'.format(rev=rev, major=major, minor=minor)
+    url = 'pymess@https://pymor.github.io/wheels/pymess-{rev}-cp{major}{minor}-cp{major}{minor}m-manylinux1_x86_64.whl'.format(rev=rev, major=major, minor=minor)
     if marker:
         return '{url} ; python_version == "{major}.{minor}" and "linux" in sys_platform'.format(url=url, major=major, minor=minor)
     return url
