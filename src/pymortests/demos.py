10a24861f7b620b2dd6a4656f42f0c1083010b70
Stephan Rave <stephanrave@uni-muenster.de>
2018-12-04 14:28:17 +0100

    [demos] add hapod demo

--- src/pymortests/demos.py
+++ src/pymortests/demos.py
@@ -86,9 +86,13 @@ SYS_MOR_ARGS = (
     ('string_equation', [])
 )
 
+HAPOD_ARGS = (
+    ('hapod', [1e-2, 10, 100]),
+)
+
 DEMO_ARGS = (DISCRETIZATION_ARGS +
              THERMALBLOCK_ARGS + THERMALBLOCK_ADAPTIVE_ARGS + THERMALBLOCK_SIMPLE_ARGS + THERMALBLOCK_GUI_ARGS +
-             BURGERS_EI_ARGS + PARABOLIC_MOR_ARGS + SYS_MOR_ARGS)
+             BURGERS_EI_ARGS + PARABOLIC_MOR_ARGS + SYS_MOR_ARGS + HAPOD_ARGS)
 DEMO_ARGS = [('pymordemos.{}'.format(a), b) for (a, b) in DEMO_ARGS]
 
 
