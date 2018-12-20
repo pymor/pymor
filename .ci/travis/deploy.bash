339112aa0800a058b569a3a4e7fe138cc68ee980
René Fritze <rene.fritze@wwu.de>
2018-12-20 10:13:48 +0100

    [ci] check install for to-be-deployed wheels

--- .ci/travis/deploy.bash
+++ .ci/travis/deploy.bash
@@ -23,7 +23,15 @@ for py in 3.5 3.6 ; do
     docker pull ${BUILDER_IMAGE} 1> /dev/null
     docker run --rm  -t -e LOCAL_USER_ID=$(id -u)  \
         -v ${BUILDER_WHEELHOUSE}:/io/wheelhouse \
-        -v ${PYMOR_ROOT}:/io/pymor ${BUILDER_IMAGE} /usr/local/bin/build-wheels.sh 1> /dev/null
+        -v ${PYMOR_ROOT}:/io/pymor ${BUILDER_IMAGE} /usr/local/bin/build-wheels.sh
+done
+
+# for os in debian_stable debian_testing centos_7 ; do
+for os in centos_7 ; do
+    docker build --build-arg tag=${os} -f .ci/docker/deploy_checks/Dockerfile ${BUILDER_WHEELHOUSE}
+done
+
+for py in 3.5 3.6 3.7 ; do
     ${REPODIR}/add_wheels.py ${TRAVIS_BRANCH} ${BUILDER_WHEELHOUSE}/pymor*manylinux*.whl
 done
 
