e4d69f53e672150b0557e2f7567c1c6bcf84891a
René Fritze <rene.fritze@wwu.de>
2019-01-24 14:08:35 +0100

    fix a couple more f-string conversions

--- src/pymor/domaindescriptions/basic.py
+++ src/pymor/domaindescriptions/basic.py
@@ -196,7 +196,8 @@ class TorusDomain(DomainDescriptionInterface):
         return np.sqrt(self.width ** 2 + self.height ** 2)
 
     def __repr__(self):
-        return f'TorusDomain({str(self.domain).replace("\n", ",")})'
+        domain = str(self.domain).replace("\n", ",")
+        return f'TorusDomain({domain})'
 
 
 class LineDomain(DomainDescriptionInterface):
