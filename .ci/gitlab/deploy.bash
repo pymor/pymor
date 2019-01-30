a1398e5ddae97ef28f5d6a1393a5c811b2ad806f
René Fritze <rene.fritze@wwu.de>
2019-01-30 14:49:08 +0100

    [ci] rewire deploy step for gitlab

--- .ci/gitlab/deploy.bash
+++ .ci/gitlab/deploy.bash
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-set -eu
+set -e
 
-BUILDER_WHEELHOUSE=/tmp/wheelhouse
+if [[ "x${CI_COMMIT_TAG}" == "x" ]] ; then
+    sed -i -e 's;style\ \=\ pep440;style\ \=\ ci_wheel_builder;g' setup.cfg
+fi
+
+set -u
+
+# since we're in a d-in-d setup this needs to a be a path shared from the real host
+BUILDER_WHEELHOUSE=${SHARED_PATH}
 REPODIR=${HOME}/wheels
 PYMOR_ROOT="$(cd "$(dirname ${BASH_SOURCE[0]})" ; cd ../../ ; pwd -P )"
 cd "${PYMOR_ROOT}"
 
-TAG=${DRONE_TAG:-${TRAVIS_TAG}}
-if [[ "x${TAG}" == "x" ]] ; then
-    sed -i -e 's;style\ \=\ pep440;style\ \=\ ci_wheel_builder;g' setup.cfg
-fi
-rm -rf ~/.ssh
-
-./.ci/travis/init_sshkey.bash "${encrypted_a599472c800f_key}" "${encrypted_a599472c800f_iv}" \
-    ${PYMOR_ROOT}/.ci/travis/wheels.deploy.key
+source ./.ci/gitlab/init_sshkey.bash
+init_ssh
 
 set -x
 mkdir -p ${BUILDER_WHEELHOUSE}
@@ -25,7 +26,7 @@ for py in 3.6 3.7 ; do
     docker pull ${BUILDER_IMAGE} 1> /dev/null
     docker run --rm  -t -e LOCAL_USER_ID=$(id -u)  \
         -v ${BUILDER_WHEELHOUSE}:/io/wheelhouse \
-        -v ${PYMOR_ROOT}:/io/pymor ${BUILDER_IMAGE} /usr/local/bin/build-wheels.sh 1> /dev/null
+        -v ${PYMOR_ROOT}:/io/pymor ${BUILDER_IMAGE} /usr/local/bin/build-wheels.sh #1> /dev/null
 done
 
 cp ${PYMOR_ROOT}/.ci/docker/deploy_checks/Dockerfile ${BUILDER_WHEELHOUSE}
@@ -34,15 +35,14 @@ for os in debian_stable debian_testing centos_7 ; do
 done
 
 for py in 3.6 3.7 ; do
-    ${REPODIR}/add_wheels.py ${TRAVIS_BRANCH} ${BUILDER_WHEELHOUSE}/pymor*manylinux*.whl
+    ${REPODIR}/add_wheels.py ${CI_COMMIT_REF_NAME} ${BUILDER_WHEELHOUSE}/pymor*manylinux*.whl
 done
 
 set +u
-pip install jinja2
 
 cd ${REPODIR}
 git config user.name "pyMOR Bot"
 git config user.email "travis@pymor.org"
-git commit -am "[deploy] wheels for ${TRAVIS_COMMIT}"
+git commit -am "[deploy] wheels for ${CI_COMMIT_SHA}"
 git pull --rebase
 git push
