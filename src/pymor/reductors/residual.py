[reductors.residual] fix for riesz calculation

src/pymor/reductors/residual.py
@@ -177,7 +177,10 @@ class NonProjectedResidualOperator(ResidualOperator):
                 inversel2 = np.nan_to_num(inversel2)
                 return R_riesz * (np.sqrt(R_riesz.dot(R)) * inversel2)[0]
             else:
-                return R * (np.sqrt(self.product.pairwise_apply2(R, R)) / R.l2_norm())[0]
+                # divide by norm, except when norm is zero:
+                inversel2 = 1./R.l2_norm()
+                inversel2 = np.nan_to_num(inversel2)
+                return R * (np.sqrt(self.product.pairwise_apply2(R, R)) * inversel2)[0]
         else:
             return R
 