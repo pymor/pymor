946c130e097efc3c87b6a4786119ee4ba6573076
René Fritze <rene.fritze@wwu.de>
2019-02-28 13:58:28 +0100

    [caching] update/extend tests

--- src/pymortests/cache.py
+++ src/pymortests/cache.py
@@ -1,24 +1,27 @@
 # This file is part of the pyMOR project (http://www.pymor.org).
 # Copyright 2013-2019 pyMOR developers and contributors. All rights reserved.
 # License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
-
+import tempfile
 import time
 import os
 from uuid import uuid4
-from datetime import datetime
-from tempfile import gettempdir
+from datetime import datetime, timedelta
 
 from pymor.core import cache
 from pymortests.base import TestInterface, runmodule
 
-SLEEP_SECONDS = 0.2
+
+SLEEP_DELTA = timedelta(milliseconds=200)
 
 
 class IamMemoryCached(cache.CacheableInterface):
 
+    def __init__(self):
+        self.cache_region = 'memory'
+
     @cache.cached
     def me_takey_long_time(self, arg):
-        time.sleep(SLEEP_SECONDS)
+        time.sleep(SLEEP_DELTA.total_seconds())
         return arg
 
 
@@ -29,14 +32,14 @@ class IamDiskCached(cache.CacheableInterface):
 
     @cache.cached
     def me_takey_long_time(self, arg):
-        time.sleep(SLEEP_SECONDS)
+        time.sleep(SLEEP_DELTA.total_seconds())
         return arg
 
 
 class IamLimitedCached(cache.CacheableInterface):
 
     def __init__(self, cache_region='disk'):
-        self.cache_region = region
+        self.cache_region = cache_region
 
     @cache.cached
     def me_takey_no_time(self, arg):
@@ -58,11 +61,19 @@ class TestCache(TestInterface):
     def test_runtime(self):
         for Class in [IamMemoryCached, IamDiskCached]:
             r = Class()
-            for val in ['koko', 'koko', 'other']:
-                int0 = datetime.now()
-                r.me_takey_long_time(val)
-                int1 = datetime.now()
-                self.logger.info(int1 - int0)
+            val = 'koko'
+            int0 = datetime.now()
+            r.me_takey_long_time(val)
+            int1 = datetime.now()
+            r.me_takey_long_time(val)
+            int2 = datetime.now()
+            assert int0 < int1 <= int2
+            delta1 = int1 - int0
+            delta2 = int2 - int1
+            assert delta1>= SLEEP_DELTA, r
+            assert delta2 < delta1, r
+            assert delta2 < 0.5 * SLEEP_DELTA, r
+
 
     # def test_limit(self):
     #     for c in [IamLimitedCached('memory'),
@@ -82,13 +93,16 @@ class TestCache(TestInterface):
     #     self.assertNotEqual(x_id, y_id)
 
     def test_region_api(self):
-        tempdir = gettempdir()
-        backends = [cache.MemoryRegion(100), cache.SQLiteRegion(path=os.path.join(tempdir, str(uuid4())),
-                                                                max_size=1024 ** 2, persistent=False)]
-        for backend in backends:
-            assert not backend.get('mykey')[0]
-            backend.set('mykey', 1)
-            assert backend.get('mykey') == (True, 1)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            backends = [cache.MemoryRegion(100), cache.DiskRegion(path=os.path.join(tmpdir, str(uuid4())),
+                                                                    max_size=1024 ** 2, persistent=False)]
+            for backend in backends:
+                assert backend.get('mykey') == (False, None)
+                backend.set('mykey', 1)
+                assert backend.get('mykey') == (True, 1)
+                # second set is ignored
+                backend.set('mykey', 2)
+                assert backend.get('mykey') == (True, 1)
 
 
 if __name__ == "__main__":
