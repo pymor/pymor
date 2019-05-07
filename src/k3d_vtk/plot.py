52539cc7d8c5bd8507306d8e5e7075bd7dc3f89b
René Fritze <rene.fritze@wwu.de>
2019-05-07 10:59:01 +0200

    [vis|k3d] default to static camera + built-in colorbar

--- src/k3d_vtk/plot.py
+++ src/k3d_vtk/plot.py
@@ -39,33 +39,6 @@ def _transform_to_k3d(timestep, poly_data, color_attribute_name):
     return timestep, np.array(attribute, np.float32), color_range[0], color_range[1], \
         np.array(vertices, np.float32), np.array(indices, np.uint32)
 
-def _add_colorbar(vtkplot, bounds, v_minmax, color_map, align='right'):
-    z = (bounds[2] + bounds[5]) / 2
-    gap = 0.1
-    if align == 'left':
-        width = np.array([gap * np.abs(bounds[0]-bounds[3]), 0, 0])
-        grid_top_left = np.array([bounds[0] - gap, bounds[4], z])
-        grid_bottom_left = np.array([bounds[0] - gap, bounds[1], z])
-        top = grid_top_left + np.array([0, -0.2 * gap, 0]) - 1.1 * width
-        bot = grid_bottom_left + np.array([0, 0.2 * gap, 0]) - 1.1 * width
-        rfp = 'rc'
-    else:
-        width = -1 * np.array([gap * np.abs(bounds[0] - bounds[3]), 0, 0])
-        grid_top_left = np.array([bounds[3] + gap, bounds[4], z])
-        grid_bottom_left = np.array([bounds[3] + gap, bounds[1], z])
-        top = grid_top_left + np.array([0, -0.2 * gap, 0]) - 1.1 * width
-        bot = grid_bottom_left + np.array([0, 0.2 * gap, 0]) - 1.1 * width
-        rfp = 'lc'
-
-    vertices = [grid_top_left, grid_bottom_left, grid_bottom_left - width, grid_top_left - width]
-    indices = [[0, 3, 2], [1, 2, 0]]
-    vertex_attribute = [v_minmax[1], v_minmax[0], v_minmax[0], v_minmax[1]]
-    vtkplot += k3d.mesh(vertices, indices, attribute=vertex_attribute,
-                        color_map=color_map, color_range=v_minmax)
-
-    vtkplot += k3d.text(text=f'Max: {np.round(v_minmax[1])}', position=top, color=0, reference_point=rfp)
-    vtkplot += k3d.text(text=f'Min: {np.round(v_minmax[0])}', position=bot, color=0, reference_point=rfp)
-
 
 class VTKPlot(k3dPlot):
     def __init__(self, vtk_data, color_attribute_name='Data',
@@ -84,6 +57,9 @@ class VTKPlot(k3dPlot):
                                       color_map=color_map)
         self.timestep = vtk_data[0][0]
         self += self.mesh
+        self.camera_no_pan = True
+        self.camera_no_rotate = True
+        self.camera_no_zoom = True
 
     def _goto_idx(self, idx):
         if idx > len(self.vtk_data) or idx < 0:
@@ -145,8 +121,6 @@ def plot(vtkfile_path, color_attribute_name, color_map=get_cmap('viridis')):
     vtkplot.camera = (xhalf, yhalf, zhalf + c_dist,
                       xhalf, yhalf, zhalf,
                       0, 1, 0)
-
-    _add_colorbar(vtkplot, combined_bounds, vtkplot.value_minmax, color_map)
     
     if size > 1:
         play = Play(min=0, max=size - 1, step=1, value=0, description='Timestep:')
