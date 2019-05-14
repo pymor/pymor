10aff4fc7453bffb3b0c139ae438821c453014b4
René Fritze <rene.fritze@wwu.de>
2019-05-14 14:50:39 +0200

    [test] extend basic statemachine example

--- src/pymortests/stateful.py
+++ src/pymortests/stateful.py
@@ -4,18 +4,23 @@
 
 import numpy as np
 from hypothesis import assume
-from hypothesis._strategies import just
+from hypothesis._strategies import just, one_of, sampled_from
 from hypothesis.stateful import rule, RuleBasedStateMachine, Bundle
 
 from pymor.algorithms import gram_schmidt
 from pymor.algorithms.projection import project
+from pymor.core.exceptions import ExtensionError
 from pymor.operators.constructions import VectorArrayOperator
+from pymor.reductors.basic import extend_basis
 from pymor.tools.floatcmp import float_cmp
 from pymor.vectorarrays.numpy import NumpyVectorSpace
 from pymortests.strategies import hy_dims, hy_float_array_elements, numpy_vector_array, vector_arrays
 
 
 class BasisMachine(RuleBasedStateMachine):
+    """
+    rules are meant to generate a new basis if they have `target` argument
+    """
     Bases = Bundle('bases')
 
     def __init__(self):
@@ -40,13 +45,25 @@ class BasisMachine(RuleBasedStateMachine):
     def gram_schmidt(self, basis):
         return gram_schmidt.gram_schmidt(basis)
 
-    @rule(basis=Bases, vector=numpy_vector_array(count=1, length=just([1]), dtype=np.float_))
-    def coefficients(self, basis, vector):
-        assume(basis.dim == vector[0].dim)
+    @rule(basis=Bases, vector_tuple=numpy_vector_array(count=1, length=just([1]), dtype=np.float_))
+    def coefficients(self, basis, vector_tuple):
+        assume(basis.dim == vector_tuple[0].dim)
         assume(basis.dim > 0)
-        x = vector[0].data[0,:]
+        x = vector_tuple[0].data[0, :]
         A = basis.data
         coeff = np.linalg.solve(A, x)
+        assert len(basis) == basis.space.dim == len(coeff)
+
+    @rule(basis=Bases, vector_tuple=numpy_vector_array(count=1, length=just([1]), dtype=np.float_),
+          method=sampled_from(('trivial', 'gram_schmidt', 'pod')))
+    def extend(self, basis, vector_tuple, method):
+        vector = vector_tuple[0]
+        assume(basis.dim == vector.dim)
+        try:
+            ret = extend_basis(vector, basis, method=method)
+        except ExtensionError:
+            ret = vector
+        return ret
 
 
 TestBasis = BasisMachine.TestCase
