0b6275d3fa7e69c5dfdab0f866d614f4342457b5
René Fritze <rene.fritze@wwu.de>
2019-05-22 12:02:52 +0200

    [ci] try disabling autodetect provider for coverage

--- .ci/gitlab/submit.bash
+++ .ci/gitlab/submit.bash
@@ -13,4 +13,8 @@ codecov -v --required \
   --token "${PYMOR_CODECOV_TOKEN}" \
   --file .coverage \
   --flags "${PYMOR_PYTEST_MARKER}" \
-  --root "${PYMOR_ROOT}"
+  --root "${PYMOR_ROOT}" \
+  -X detect \
+  --slug pymor/pymor \
+  --commit ${CI_COMMIT_SHA} \
+  --branch ${CI_COMMIT_REF_NAME}
