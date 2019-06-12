5b35a55e737ee1f7ee475d6c9f9f919ed91addc8
Stephan Rave <stephanrave@uni-muenster.de>
2019-06-12 15:46:17 +0200

    [tests] improve test_with_newtype

--- src/pymortests/core/interface.py
+++ src/pymortests/core/interface.py
@@ -87,6 +87,7 @@ def test_with_newtype():
     g = RectGrid(num_intervals=(99, 99))
     g2 = g.with_(new_type=TriaGrid, domain=([0, 0], [2, 2]))
 
+    assert isinstance(g2, TriaGrid)
     assert g2.num_intervals == (99, 99)
     assert np.all(g2.domain == ([0, 0], [2, 2]))
 
