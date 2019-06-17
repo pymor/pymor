4f79c1abd3ec24307bfc5f68f5aaf2f19bf0b422
René Fritze <rene.fritze@wwu.de>
2019-06-17 10:06:49 +0200

    [vis|k3d] refactor plotting to directly consume vectorarrays

--- src/k3d_vtk/plot.py
+++ src/k3d_vtk/plot.py
@@ -2,72 +2,51 @@ import time
 import warnings
 import IPython
 from ipywidgets import IntSlider, interact, widgets, Play
-from k3d.helpers import minmax
-from k3d_vtk.reader import read_vtkfile
+from k3d.objects import Mesh
 from k3d.plot import Plot as k3dPlot
 import k3d
-from vtk.util import numpy_support
 import numpy as np
-import vtk
+from k3d.transform import process_transform_arguments
 from matplotlib.cm import get_cmap
 from matplotlib.colors import Colormap
 
-
-def _transform_to_k3d(timestep, poly_data, color_attribute_name):
-    '''
-    this function mirrors the prepartion in k3d.vtk_poly_data
-    :param timestep: attribute from vtk collection file
-    :param poly_data: vtk reader Output for one single vtk file
-    :param color_attribute_name: Determines mesh colorization, 3-Tuple of Vtk Dataset name, min value, max value
-    :return: 5-Tuple to match necessary updates to mesh when advancing the timestep
-    '''
-    if poly_data.GetPolys().GetMaxCellSize() > 3:
-        cut_triangles = vtk.vtkTriangleFilter()
-        cut_triangles.SetInputData(poly_data)
-        cut_triangles.Update()
-        poly_data = cut_triangles.GetOutput()
-
-    if color_attribute_name is not None:
-        attribute = numpy_support.vtk_to_numpy(poly_data.GetPointData().GetArray(color_attribute_name))
-        color_range = minmax(attribute)
-    else:
-        attribute = []
-        color_range = [0,-1,]#[-np.inf, np.inf]
-    vertices = numpy_support.vtk_to_numpy(poly_data.GetPoints().GetData())
-    indices = numpy_support.vtk_to_numpy(poly_data.GetPolys().GetData()).reshape(-1, 4)[:, 1:4]
-
-    return timestep, np.array(attribute, np.float32), color_range[0], color_range[1], \
-        np.array(vertices, np.float32), np.array(indices, np.uint32)
+from pymor.grids.constructions import flatten_grid
 
 
-class VTKPlot(k3dPlot):
-    def __init__(self, vtk_data, color_attribute_name='Data',
-                 color_map=k3d.basic_color_maps.CoolWarm,
-                 *args, **kwargs):
+class VectorArrayPlot(k3dPlot):
+    def __init__(self, U, grid, codim, color_map, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        if 'transform' in kwargs.keys():
+            raise RuntimeError('supplying transforms is currently not supported for time series Data')
+
+        self.subentities, self.coordinates, entity_map = flatten_grid(grid)
+        self.data = U.to_numpy() if codim == 0 else U.to_numpy()[:, entity_map].copy()
+        if grid.dim == 2:
+            # pad 0 in z dimension
+            self.vertices = np.zeros((len(self.coordinates), 3))
+            self.vertices[:,:-1] = self.coordinates
 
         self.idx = 0
-
-        if 'transform' in kwargs.keys() and len(vtk_data) > 1:
-            raise RuntimeError('supplying transforms is currently not supported for teim series VTK Data')
-
-        self.vtk_data = np.stack([_transform_to_k3d(v[0], v[1], color_attribute_name) for v in vtk_data])
-        self.value_minmax = (np.nanmin(self.vtk_data[:, 2]), np.nanmax(self.vtk_data[:, 3]))
-        self.mesh = k3d.vtk_poly_data(vtk_data[0][1], color_attribute=(color_attribute_name, *self.value_minmax),
-                                      color_map=color_map)
-        self.timestep = vtk_data[0][0]
+        self.mesh = process_transform_arguments(Mesh(vertices=np.array(self.vertices, np.float32),
+             indices=np.array(self.subentities, np.uint32),
+             color=k3d.k3d._default_color,
+             opacity=1.0,
+             attribute=np.array(self.data[self.idx], np.float32),
+             color_range=(np.nanmin(self.data), np.nanmax(self.data)),
+             color_map=np.array(color_map, np.float32),
+             wireframe=False,
+             compression_level=0))
         self += self.mesh
         self.camera_no_pan = True
         self.camera_no_rotate = True
         self.camera_no_zoom = True
 
     def _goto_idx(self, idx):
-        if idx > len(self.vtk_data) or idx < 0:
-            warnings.warn(f'Index {idx} outside data range for VTKPlot', RuntimeWarning)
+        if idx > len(self.data) or idx < 0:
+            warnings.warn(f'Index {idx} outside data range for VectorArrayPlot', RuntimeWarning)
             return
         self.idx = idx
-        self.timestep, self.mesh.attribute, _, _, \
-            self.mesh.vertices, self.mesh.indices = self.vtk_data[self.idx]
+        self.mesh.attribute = np.array(self.data[self.idx], np.float32)
 
     def dec(self):
         self._goto_idx(self.idx-1)
@@ -76,40 +55,62 @@ class VTKPlot(k3dPlot):
         self._goto_idx(self.idx+1)
 
 
-def plot(vtkfile_path, color_attribute_name, color_map=get_cmap('viridis')):
-    ''' Generate a k3d Plot and associated controls for VTK data from file
+def plot(grid, U, bounding_box=([0, 0], [1, 1]), codim=2, title=None, legend=None,
+                    separate_colorbars=False, rescale_colorbars=False, columns=2,
+         color_map=get_cmap('viridis')):
+    """Generate a k3d Plot and associated controls for  scalar data associated to a two-dimensional |Grid|.
 
-    :param vtkfile_path: the path to load vtk data from. Can be a single .vtu or a collection
-    :param color_attribute_name: which data array from vtk to use for plot coloring
-    :param color_map: a Matplotlib Colormap object or a K3D array((step, r, g, b))
-    :return: the generated Plot object
-    '''
+    The grid's |ReferenceElement| must be the triangle or square. The data can either
+    be attached to the faces or vertices of the grid.
+
+    Parameters
+    ----------
+    grid
+        The underlying |Grid|.
+    U
+        |VectorArray| of the data to visualize. If `len(U) 1`, the data is visualized
+        as a time series of plots. Alternatively, a tuple of |VectorArrays| can be
+        provided, in which case a subplot is created for each entry of the tuple. The
+        lengths of all arrays have to agree.
+    bounding_box
+        A bounding box in which the grid is contained.
+    codim
+        The codimension of the entities the data in `U` is attached to (either 0 or 2).
+    title
+        Title of the plot.
+    legend
+        Description of the data that is plotted. Most useful if `U` is a tuple in which
+        case `legend` has to be a tuple of strings of the same length.
+    separate_colorbars
+        If `True`, use separate colorbars for each subplot.
+    rescale_colorbars
+        If `True`, rescale colorbars to data in each frame.
+    columns
+        The number of columns in the visualizer GUI in case multiple plots are displayed
+        at the same time.
+    color_map
+        a Matplotlib Colormap object or a K3D array((step, r, g, b))
+    """
+    assert len(bounding_box) == 2
+    assert all(1 < len(bounding_box[i]) < 4 for i in range(2))
+    assert len(bounding_box[0]) == len(bounding_box[1])
     if isinstance(color_map, Colormap):
         color_map = [(x, *color_map(x)[:3]) for x in np.linspace(0, 1, 256)]
 
-    data = read_vtkfile(vtkfile_path)
-    size = len(data)
+    if len(bounding_box[0]) == 2:
+        lower = np.array([bounding_box[0][0], bounding_box[0][1], 0])
+        upper = np.array([bounding_box[1][0], bounding_box[1][1], 0])
+        bounding_box = (lower, upper)
+    combined_bounds = np.hstack(bounding_box)
 
-    # getbounds: (xmin, xmax, ymin, ymax, zmin, zmax)
-    all_bounds = np.stack([p[1].GetBounds() for p in data])
-    combined_bounds = np.array([np.min(all_bounds[:, 0]),
-                                np.min(all_bounds[:, 2]),
-                                np.min(all_bounds[:, 4]),
-                                np.max(all_bounds[:, 1]),
-                                np.max(all_bounds[:, 3]),
-                                np.max(all_bounds[:, 5])])
-
-    vtkplot = VTKPlot(data, color_attribute_name=color_attribute_name,  grid_auto_fit=False,
-                      camera_auto_fit=False, color_map=color_map, grid=combined_bounds)
+    vtkplot = VectorArrayPlot(U=U, grid=grid, codim=codim, color_attribute_name='None', grid_auto_fit=False,
+                              camera_auto_fit=False, color_map=color_map)
     # display needs to have been called before changing camera/grid_visible
     vtkplot.display()
     # could be replaced with testing if the widget is'ready'
     time.sleep(0.5)
     vtkplot.grid_visible = False
-    try:
-        vtkplot.menu_visibility = False
-    except AttributeError:
-        pass # k3d < 2.5.6
+    vtkplot.menu_visibility = False
     # guesstimate
     fov_angle = 30
     absx = np.abs(combined_bounds[0] - combined_bounds[3])
@@ -121,7 +122,7 @@ def plot(vtkfile_path, color_attribute_name, color_map=get_cmap('viridis')):
     vtkplot.camera = (xhalf, yhalf, zhalf + c_dist,
                       xhalf, yhalf, zhalf,
                       0, 1, 0)
-    
+    size = len(U)
     if size > 1:
         play = Play(min=0, max=size - 1, step=1, value=0, description='Timestep:')
         interact(idx=play).widget(vtkplot._goto_idx)
