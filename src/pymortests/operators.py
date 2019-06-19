f0a3c194163cdda6ae1288b135018234e33b5763
Stephan Rave <stephanrave@uni-muenster.de>
2019-06-19 10:59:50 +0200

    [tests] add test for complex conjugation of LincombOperator

--- src/pymortests/operators.py
+++ src/pymortests/operators.py
@@ -9,10 +9,11 @@ from pymor.algorithms.basic import almost_equal
 from pymor.algorithms.projection import project
 from pymor.algorithms.to_matrix import to_matrix
 from pymor.core.exceptions import InversionError, LinAlgError
-from pymor.operators.constructions import SelectionOperator, InverseOperator, InverseAdjointOperator, IdentityOperator
+from pymor.operators.constructions import (SelectionOperator, InverseOperator, InverseAdjointOperator, IdentityOperator,
+                                           LincombOperator)
 from pymor.operators.numpy import NumpyMatrixOperator
 from pymor.parameters.base import ParameterType
-from pymor.parameters.functionals import GenericParameterFunctional
+from pymor.parameters.functionals import GenericParameterFunctional, ExpressionParameterFunctional
 from pymor.vectorarrays.numpy import NumpyVectorArray, NumpyVectorSpace
 from pymortests.algorithms.stuff import MonomOperator
 from pymortests.fixtures.operator import (operator, operator_with_arrays, operator_with_arrays_and_products,
@@ -75,6 +76,18 @@ def test_lincomb_op():
         projected = project(p, basis, basis)
         pa = projected.apply(vx)
         assert almost_equal(pa, p.apply(vx)).all()
+
+
+def test_lincomb_adjoint():
+    op = LincombOperator([NumpyMatrixOperator(np.eye(10)), NumpyMatrixOperator(np.eye(10))],
+                         [1+3j, ExpressionParameterFunctional('c + 3', {'c': ()})])
+    mu = op.parse_parameter(1j)
+    U = op.range.random()
+    V = op.apply_adjoint(U, mu=mu)
+    VV = op.H.apply(U, mu=mu)
+    assert np.all(almost_equal(V, VV))
+    VVV = op.apply(U, mu=mu).conj()
+    assert np.all(almost_equal(V, VVV))
 
 
 def test_identity_lincomb():
