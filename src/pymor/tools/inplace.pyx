777daa5799569f0d05dd87b6a551e71ac59b0389
Felix Schindler <felix.schindler@wwu.de>
2019-08-26 10:39:24 +0200

    [inplce] add clear_rows_cols() for CSR matrices

--- src/pymor/tools/inplace.pyx
+++ src/pymor/tools/inplace.pyx
@@ -94,6 +94,44 @@ def set_unit_rows_cols(mat, np.ndarray[np.int32_t] indices):
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
+def clear_rows_cols(mat, np.ndarray[np.int32_t] row_indices, np.ndarray[np.int32_t] col_indices):
+
+    if not isinstance(mat, csr_matrix):
+        raise NotImplementedError('Only csr_matrices are supported!')
+
+    M, N = mat.shape
+    cdef np.int32_t i, index, len_indices, min_index, max_index
+    min_index = 1
+    max_index = -1
+    len_indices = row_indices.shape[0]
+    for i in range(len_indices):
+        index = row_indices[i]
+        min_index = min(min_index, index)
+        max_index = max(max_index, index)
+    if min_index < 0:
+        raise ValueError('row_indices have to be positive')
+    if max_index > M:
+        raise ValueError('row_indices to large')
+    min_index = 1
+    max_index = -1
+    len_indices = col_indices.shape[0]
+    for i in range(len_indices):
+        index = col_indices[i]
+        min_index = min(min_index, index)
+        max_index = max(max_index, index)
+    if min_index < 0:
+        raise ValueError('col_indices have to be positive')
+    if max_index > M:
+        raise ValueError('col_indices to large')
+
+    result = _clear_rows_cols_csr(mat.data, mat.indices, mat.indptr, M, N, row_indices, col_indices)
+
+    if result != 0:
+        raise RuntimeError('unknown problem occurred')
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
 cdef int _set_unit_rows_cols_csr(
         np.ndarray[np.double_t] data, np.ndarray[np.int32_t] indcs, np.ndarray[np.int32_t] indptr,
         np.int32_t M, np.int32_t N,
@@ -138,4 +176,45 @@ cdef int _set_unit_rows_cols_csr(
                         data[j] = 1.
                     else:
                         data[j] = 0.
+    return 0
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef int _clear_rows_cols_csr(
+        np.ndarray[np.double_t] data, np.ndarray[np.int32_t] indices, np.ndarray[np.int32_t] indptr,
+        np.int32_t M, np.int32_t N,
+        np.ndarray[np.int32_t] row_indices,
+        np.ndarray[np.int32_t] col_indices):
+
+    cdef np.int32_t i, j, m, n, index, len_row_indices, len_col_indices
+    cdef bint clear_row, clear_col
+    len_row_indices = row_indices.shape[0]
+    len_col_indices = col_indices.shape[0]
+
+    if len_row_indices == 0 and len_row_indices == 0:
+        return 0
+
+    for m in range(M):
+        clear_row = False
+        for i in range(len_row_indices): # not optimal, order n log(n), increment m and i simultaneously
+            index = row_indices[i]
+            if m == index:
+                clear_row = True
+                break
+        if clear_row:
+            for j in range(indptr[m], indptr[m + 1]):
+                n = indices[j]
+                data[j] = 0.
+        else: # we still need to clear all column entries in this row which are in col_indices
+            for j in range(indptr[m], indptr[m + 1]):
+                n = indices[j]
+                clear_col = False
+                for i in range(len_col_indices): # not optimal, see above
+                    index = row_indices[i]
+                    if n == index:
+                        clear_col = True
+                        break
+                if clear_col:
+                    data[j] = 0.
     return 0
