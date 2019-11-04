912aba9a07ac5cac597e1eeac3fd1a200478f2c5
Petar Mlinarić <mlinaric@mpi-magdeburg.mpg.de>
2019-11-04 10:56:09 +0100

    [algorithms.krylov] improve rational_arnoldi

--- src/pymor/algorithms/krylov.py
+++ src/pymor/algorithms/krylov.py
@@ -12,27 +12,40 @@ def rational_arnoldi(A, E, b, sigma, trans=False):
     orthonormal basis for the rational Krylov subspace
 
     .. math::
-        \mathrm{span}\{(\sigma_1 E - A)^{-1} b, (\sigma_2 E - A)^{-1} b, \ldots,
-        (\sigma_r E - A)^{-1} b\},
+        \mathrm{span}\{
+            (\sigma_1 E - A)^{-1} b,
+            (\sigma_2 E - A)^{-1} b,
+            \ldots,
+            (\sigma_r E - A)^{-1} b
+        \},
 
     otherwise, computes the same for
 
     .. math::
-        \mathrm{span}\{(\sigma_1 E - A)^{-T} b^T, (\sigma_2 E - A)^{-T} b^T,
-        \ldots, (\sigma_r E - A)^{-T} b^T\}.
+        \mathrm{span}\{
+            (\sigma_1 E - A)^{-T} b^T,
+            (\sigma_2 E - A)^{-T} b^T,
+            \ldots,
+            (\sigma_r E - A)^{-T} b^T
+        \}.
 
     Interpolation points in `sigma` are allowed to repeat (in any
     order). Then, in the above expression,
 
     .. math::
-        \underbrace{(\sigma_i E - A)^{-1} b, \ldots,
-        (\sigma_i E - A)^{-1} b}_{m \text{ times}}
+        \underbrace{
+            (\sigma_i E - A)^{-1} b,
+            \ldots,
+            (\sigma_i E - A)^{-1} b
+        }_{m \text{ times}}
 
     is replaced by
 
     .. math::
-        (\sigma_i E - A)^{-1} b, (\sigma_i E - A)^{-2} b, \ldots,
-        (\sigma_i E - A)^{-m} b.
+        (\sigma_i E - A)^{-1} b,
+        (\sigma_i E - A)^{-1} E (\sigma_i E - A)^{-1} b,
+        \ldots,
+        \left((\sigma_i E - A)^{-1} E\right)^{m - 1} (\sigma_i E - A)^{-1} b.
 
     Analogously for the `trans == True` case.
 
@@ -46,15 +59,19 @@ def rational_arnoldi(A, E, b, sigma, trans=False):
         Real vector-like operator (if trans is False) or functional (if
         trans is True).
     sigma
-        Interpolation points (closed under conjugation).
+        Sequence of interpolation points (closed under conjugation).
     trans
         Boolean, see above.
 
     Returns
     -------
     V
-        Projection matrix.
+        Orthonormal basis for the Krylov subspace |VectorArray|.
     """
+    assert A.source == A.range
+    assert E.source == A.source
+    assert E.range == A.source
+    assert (b.range if not trans else b.source) == A.source
     assert not trans and b.source.dim == 1 or trans and b.range.dim == 1
 
     r = len(sigma)
@@ -64,28 +81,23 @@ def rational_arnoldi(A, E, b, sigma, trans=False):
     v.scal(1 / v.l2_norm()[0])
 
     for i in range(r):
+        if sigma[i].imag < 0:
+            continue
         if sigma[i].imag == 0:
             sEmA = sigma[i].real * E - A
-
-            if not trans:
-                v = sEmA.apply_inverse(v if len(V) == 0 else E.apply(v))
-            else:
-                v = sEmA.apply_inverse_adjoint(v if len(V) == 0 else E.apply_adjoint(v))
-
-            V.append(v)
-            V = gram_schmidt(V, atol=0, rtol=0, offset=len(V) - 1, copy=False)
-            v = V[-1]
-        elif sigma[i].imag > 0:
+        else:
             sEmA = sigma[i] * E - A
-
-            if not trans:
-                v = sEmA.apply_inverse(v if len(V) == 0 else E.apply(v))
-            else:
-                v = sEmA.apply_inverse_adjoint(v if len(V) == 0 else E.apply_adjoint(v))
-
+        if not trans:
+            v = sEmA.apply_inverse(v if len(V) == 0 else E.apply(v))
+        else:
+            v = sEmA.apply_inverse_adjoint(v if len(V) == 0 else E.apply_adjoint(v))
+        if sigma[i].imag == 0:
+            V.append(v)
+            gram_schmidt(V, atol=0, rtol=0, offset=len(V) - 1, copy=False)
+        else:
             V.append(v.real)
             V.append(v.imag)
-            V = gram_schmidt(V, atol=0, rtol=0, offset=len(V) - 2, copy=False)
-            v = V[-1]
+            gram_schmidt(V, atol=0, rtol=0, offset=len(V) - 2, copy=False)
+        v = V[-1]
 
     return V
