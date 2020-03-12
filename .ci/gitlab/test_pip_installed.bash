ae1539ce3c0d845da88f49941cce2b722f112636
René Fritze <rene.fritze@wwu.de>
2020-03-12 10:32:10 +0100

    [ci] stop using a venv in the pip_installed job

--- .ci/gitlab/test_pip_installed.bash
+++ .ci/gitlab/test_pip_installed.bash
@@ -6,16 +6,16 @@ source ${THIS_DIR}/common_test_setup.bash
 export SDIST_DIR=/tmp/pymor_sdist/
 PIP_CLONE_URL="git+${CI_PROJECT_URL}@${CI_COMMIT_SHA}"
 
-pip install virtualenv
-virtualenv /tmp/venv
-source /tmp/venv/bin/activate
+# Disabled, see https://github.com/pymor/pymor/issues/897
+# pip install virtualenv
+# virtualenv /tmp/venv
+# source /tmp/venv/bin/activate
 pip install ${PIP_CLONE_URL}
 pip uninstall -y pymor
 pip install ${PIP_CLONE_URL}#egg=pymor[full]
 pip uninstall -y pymor
-pip install -r requirements.txt
+# other requirements are installed from pymor[full]
 pip install -r requirements-ci.txt
-pip install -r requirements-optional.txt
 
 python setup.py sdist -d ${SDIST_DIR}/ --format=gztar
 twine check ${SDIST_DIR}/*
