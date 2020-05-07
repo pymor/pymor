54255324b7eb2ba3a093294163c1d6a4f03eee33
René Fritze <rene.fritze@wwu.de>
2020-05-07 13:15:28 +0200

    [ci] fix python extensions not recompiled after updating numpy

--- .ci/gitlab/test_numpy_git.bash
+++ .ci/gitlab/test_numpy_git.bash
@@ -4,6 +4,8 @@ THIS_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ; pwd -P )"
 source ${THIS_DIR}/common_test_setup.bash
 
 pip install git+https://github.com/numpy/numpy@master
+# numpy version changed, recompile extensions
+python setup.py build_ext -i
 # there seems to be no way of really overwriting -p no:warnings from setup.cfg
 sed -i -e 's/\-p\ no\:warnings//g' setup.cfg
 xvfb-run -a py.test ${COMMON_PYTEST_OPTS} -W once::DeprecationWarning -W once::PendingDeprecationWarning
