5cdbda50b3c8070a47e5bb0f114856da8cba7f33
René Fritze <rene.fritze@wwu.de>
2020-05-28 13:51:59 +0200

    [setup.py] add 3.8 to supported pythons in trove classifiers
    Closes #928

--- setup.py
+++ setup.py
@@ -172,6 +172,7 @@ def setup_package():
                      'License :: OSI Approved :: BSD License',
                      'Programming Language :: Python :: 3.6',
                      'Programming Language :: Python :: 3.7',
+                     'Programming Language :: Python :: 3.8',
                      'Intended Audience :: Science/Research',
                      'Topic :: Scientific/Engineering :: Mathematics'],
         license='LICENSE.txt',
