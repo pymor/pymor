c34c50c1f23e006c215985161d3fdf01730ede7d
René Fritze <rene.fritze@wwu.de>
2020-05-28 16:49:14 +0200

    [ci] use larger hypothesis profile for PR builds
    reduce example cout in other PR builds too

--- src/pymortests/conftest.py
+++ src/pymortests/conftest.py
@@ -10,8 +10,9 @@ from hypothesis import settings, Verbosity, HealthCheck
 
 _common_settings = {"print_blob": True, "suppress_health_check": (HealthCheck.too_slow, HealthCheck.data_too_large,),
                     "deadline": 1000, "verbosity": Verbosity.verbose}
-settings.register_profile("ci_large", max_examples=5000, **_common_settings)
-settings.register_profile("ci", max_examples=100, **_common_settings)
+settings.register_profile("ci_large", max_examples=1000, **_common_settings)
+settings.register_profile("ci_pr", max_examples=100, **_common_settings)
+settings.register_profile("ci", max_examples=25, **_common_settings)
 settings.register_profile("dev", max_examples=10, **_common_settings)
 settings.register_profile("debug", max_examples=10, **_common_settings)
 settings.load_profile(os.getenv(u'PYMOR_HYPOTHESIS_PROFILE', 'dev'))
