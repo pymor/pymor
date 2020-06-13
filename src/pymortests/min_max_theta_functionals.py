1b388600701a6c019cee76508e8b5c7dae957c10
Tim Keil <tim.keil@wwu.de>
2020-06-13 20:30:15 +0200

    [pymortest] add a small test where BaseMaxThetaParameterFunctional is used

--- src/pymortests/min_max_theta_functionals.py
+++ src/pymortests/min_max_theta_functionals.py
@@ -4,6 +4,7 @@ from pymor.parameters.functionals import (
         ConstantParameterFunctional,
         ExpressionParameterFunctional,
         MinThetaParameterFunctional,
+        BaseMaxThetaParameterFunctional,
         MaxThetaParameterFunctional,
         ParameterFunctional)
 from pymortests.base import runmodule
@@ -47,6 +48,28 @@ def test_max_theta_parameter_functional():
     mu = theta.parameters.parse(1)
     mu_bar = theta.parameters.parse(mu_bar)
     expected_value = gamma_mu_bar * np.abs(np.max(np.array([t(mu) for t in thetas])/np.array([t(mu_bar) for t in
+        thetas])))
+    actual_value = theta.evaluate(mu)
+    assert expected_value == actual_value
+
+
+def test_base_max_theta_parameter_functional():
+    thetas = (ExpressionParameterFunctional('2*mu[0]', {'mu': 1}, derivative_expressions={'mu': ['2']}),
+              ConstantParameterFunctional(1))
+
+    # theta prime can for example be the derivatives of all theta
+    thetas_prime = tuple([theta.d_mu('mu', 0) for theta in thetas])
+
+    mu_bar = -3
+    gamma_mu_bar = 10
+    theta = BaseMaxThetaParameterFunctional(thetas_prime, thetas, mu_bar, gamma_mu_bar)
+    thetas = [ConstantParameterFunctional(t) if not isinstance(t, ParameterFunctional) else t
+              for t in thetas]
+    thetas_prime = [ConstantParameterFunctional(t) if not isinstance(t, ParameterFunctional) else t
+              for t in thetas_prime]
+    mu = theta.parameters.parse(1)
+    mu_bar = theta.parameters.parse(mu_bar)
+    expected_value = gamma_mu_bar * np.abs(np.max(np.array([t(mu) for t in thetas_prime])/np.array([np.abs(t(mu_bar)) for t in
         thetas])))
     actual_value = theta.evaluate(mu)
     assert expected_value == actual_value
