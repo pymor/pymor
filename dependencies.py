984fa930529dbf7b21138eb75b3077170705562f
René Fritze <rene.fritze@wwu.de>
2020-06-18 13:13:31 +0200

    add jupyter_sphinx as a dependency

--- dependencies.py
+++ dependencies.py
@@ -43,7 +43,7 @@ install_suggests = {'ipython>=5.0': 'an enhanced interactive python shell',
                     'nbresuse': 'resource usage indicator for notebooks',
                     'jupyter_contrib_nbextensions': 'modular collection of jupyter extensions',
                     'pillow': 'image library used for bitmap data functions'}
-doc_requires = ['sphinx>=1.7', 'pymor-nb2plots>=0.7', 'matplotlib', 'PySide2', 'ipyparallel>=6.2.5',
+doc_requires = ['sphinx>=1.7', 'jupyter_sphinx', 'matplotlib', 'PySide2', 'ipyparallel>=6.2.5',
                 'ipywidgets', 'sphinx-qt-documentation'] + install_requires
 ci_requires = [_PYTEST, 'pytest-cov', 'pytest-xdist', 'check-manifest', 'nbconvert', 'pytest-parallel', 'virtualenv',
                'readme_renderer[md]', 'rstcheck', 'codecov', 'twine', 'pytest-memprof', 'pytest-timeout',
