cf85754b40b0dd000d4d3bd7169d3deb84c19055
René Fritze <rene.fritze@wwu.de>
2020-06-22 16:59:39 +0200

    [ci] update ssh init script for dual debian/alpine use

--- .ci/gitlab/init_sshkey.bash
+++ .ci/gitlab/init_sshkey.bash
@@ -5,11 +5,11 @@ set -u
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function init_ssh {
-    which ssh-agent || ( apt-get update -y && apt-get install openssh-client git rsync -y )
+    which ssh-agent || ( apt-get update -y && apt-get install openssh-client git rsync -y ) || \
+      apk --update add openssh-client git rsync
 
     eval $(ssh-agent -s)
-    echo "$WHEELS_DEPLOY_KEY" | tr -d '\r' | ssh-add - > /dev/null
-    echo "$TESTLOG_DEPLOY_KEY" | tr -d '\r' | ssh-add - > /dev/null
+    echo "$DOCS_DEPLOY_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
     [[ -d ~/.ssh ]] || mkdir -p  ~/.ssh
     chmod 700 ~/.ssh
