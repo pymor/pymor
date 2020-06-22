e67d262ad07bcdb28587df65562446d81ce1002e
René Fritze <rene.fritze@wwu.de>
2020-06-22 15:40:50 +0200

    [docs] move docs deploy to git repo

--- .ci/gitlab/deploy_docs.bash
+++ .ci/gitlab/deploy_docs.bash
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-if [ "x${CI_MERGE_REQUEST_ID}" == "x" ] ; then
-    export PULL_REQUEST=false
-else
-    export PULL_REQUEST=${CI_MERGE_REQUEST_ID}
-fi
-
-export PYTHONPATH=${CI_PROJECT_DIR}/src:${PYTHONPATH}
+source ${CI_PROJECT_DIR}/.ci/gitlab/init_sshkey.bash
+init_ssh
 
 PYMOR_ROOT="$(cd "$(dirname ${BASH_SOURCE[0]})" ; cd ../../ ; pwd -P )"
 cd "${PYMOR_ROOT}"
@@ -14,23 +9,30 @@ cd "${PYMOR_ROOT}"
 # any failure here should fail the whole test
 set -eux
 
-docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
-docker pull ${IMAGE}
-container=$(docker create --entrypoint / ${IMAGE})
+REPO=git@github.com:pymor/docs.git
+REPO_DIR=${CI_PROJECT_DIR}/repo
+TARGET_DIR=${REPO_DIR}/${CI_COMMIT_REF_SLUG}/
 
-PUBLIC_DIR=/tmp/public
-mkdir -p ${PUBLIC_DIR}/${CI_COMMIT_REF_SLUG}/
-docker cp ${container}:/public/ ${PUBLIC_DIR}/ || echo "No previous docs builds in storage"
-du -sch ${PUBLIC_DIR}/*
-rm -rf ${PUBLIC_DIR}/${CI_COMMIT_REF_SLUG}/
+git clone --depth 2 ${REPO} ${REPO_DIR}
+
+rm -rf ${TARGET_DIR}
+mkdir -p ${TARGET_DIR}
 
 # we get the already built html documentation as an artefact from an earlier build stage
-rsync -a docs/_build/html/ ${PUBLIC_DIR}/${CI_COMMIT_REF_SLUG}/
-cp -r docs/public_root/* ${PUBLIC_DIR}
-${PYMOR_ROOT}/.ci/gitlab/docs_makeindex.py ${PUBLIC_DIR}
-du -sch ${PUBLIC_DIR}/*
-docker build -t ${IMAGE} -f .ci/docker/docs/Dockerfile ${PUBLIC_DIR}
-docker push ${IMAGE}
-# for automatic deploy gitlab uses ${PROJECT_DIR}/public
-mv ${PUBLIC_DIR} ${PYMOR_ROOT}/
-ls ${PYMOR_ROOT}/public/
+rsync -a ${PYMOR_ROOT}/docs/_build/html/ ${TARGET_DIR}
+cd ${REPO_DIR}
+git config user.name "pyMOR Bot"
+git config user.email "gitlab-ci@pymor.org"
+git add ${TARGET_DIR}
+git commit -m "Updated docs for ${CI_COMMIT_REF_SLUG}"
+
+${PYMOR_ROOT}/.ci/gitlab/docs_makeindex.py ${REPO_DIR}
+git add index.html
+git commit -m "Updated index for ${CI_COMMIT_REF_SLUG}" || echo "nothing to add"
+
+
+git push || (git pull --rebase && git push )
+
+
+du -sch ${REPO_DIR}/*
+du -sch ${TARGET_DIR}/*
