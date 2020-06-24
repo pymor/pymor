4f121f8d877478c1c97513bdb7dc593aa86acb40
Felix Schindler <felix.schindler@wwu.de>
2020-06-24 16:24:19 +0200

    [bindings.dunext] bring up to date

--- src/pymor/bindings/dunext.py
+++ src/pymor/bindings/dunext.py
@@ -8,21 +8,23 @@ from pymor.core.config import config
 
 if config.HAVE_DUNEXT:
     import numpy as np
+    from functools import partial
 
-    from pymor.operators.basic import OperatorBase
+    from pymor.operators.list import ListVectorArrayOperatorBase
     from pymor.operators.constructions import ZeroOperator
-    from pymor.vectorarrays.list import VectorInterface, ListVectorSpace
+    from pymor.vectorarrays.list import Vector, ComplexifiedVector, ComplexifiedListVectorSpace
+    from pymor.vectorarrays.interface import _create_random_values
 
 
-    class DuneXTVector(VectorInterface):
-        """Wraps a vector from dune-xt-la to make it usable with ListVectorArray."""
+    class DuneXTVector(Vector):
+        """Wraps a vector from dune-xt to make it usable with ListVectorArray."""
 
         def __init__(self, impl):
             self.impl = impl
 
-        @property
-        def data(self):
-            return np.frombuffer(self.impl)
+        # @property
+        # def data(self):
+            # return np.frombuffer(self.impl)
 
         def copy(self, deep=False):
             return DuneXTVector(self.impl.copy(deep))
@@ -83,12 +85,10 @@ if config.HAVE_DUNEXT:
             return self * (-1)
 
 
-    class DuneXTVectorSpace(ListVectorSpace):
+    class DuneXTVectorSpace(ComplexifiedListVectorSpace):
 
-        def __init__(self, vector_type, dim, id_='STATE'):
-            self.vector_type = vector_type
-            self.dim = dim
-            self.id = id_
+        def __init__(self, vector_type, dim, id='STATE'):
+            self.__auto_init(locals())
 
         def __eq__(self, other):
             return type(other) is DuneXTVectorSpace and self.vector_type == other.vector_type and self.dim == other.dim
@@ -97,77 +97,77 @@ if config.HAVE_DUNEXT:
         def __hash__(self):
             return id(self.vector_type) + hash(self.dim)
 
-        def zero_vector(self):
+        def real_zero_vector(self):
             return DuneXTVector(self.vector_type(self.dim, 0.))
 
-        def make_vector(self, obj):
+        def real_full_vector(self, value):
+            return DuneXTVector(self.vector_type(self.dim, value))
+
+        def real_random_vector(self, distribution, random_state, **kwargs):
+            values = _create_random_values(self.dim, distribution, random_state, **kwargs)
+            return self.vector_from_numpy(values)
+
+        def real_make_vector(self, obj):
             return DuneXTVector(obj)
 
-        def vector_from_data(self, data):
+        def real_vector_from_numpy(self, data, ensure_copy=False):
             v = self.zero_vector()
-            v.data[:] = data
+            np_view = np.array(v, copy=False)
+            np_view[:] = data
             return v
 
-        @classmethod
-        def space_from_vector_obj(cls, vec, id_):
-            return cls(type(vec), len(vec), id_)
 
-
-    class DuneXTMatrixOperator(OperatorBase):
-        """Wraps a dune-xt-la matrix as an |Operator|."""
+    class DuneXTMatrixOperator(ListVectorArrayOperatorBase):
+        """Wraps a dune-xt matrix as an |Operator|."""
 
         linear = True
 
         def __init__(self, matrix, source_id='STATE', range_id='STATE', solver_options=None, name=None):
             self.source = DuneXTVectorSpace(matrix.vector_type(), matrix.cols, source_id)
             self.range = DuneXTVectorSpace(matrix.vector_type(), matrix.rows, range_id)
-            self.matrix = matrix
-            self.solver_options = solver_options
-            self.name = name
-            self.source_id = source_id  # for with_ support
-            self.range_id = range_id
+            self.__auto_init(locals())
 
-        def apply(self, U, mu=None):
-            assert U in self.source
-            R = self.range.zeros(len(U))
-            for u, r in zip(U._list, R._list):
-                self.matrix.mv(u.impl, r.impl)
-            return R
+        def _apply_one_vector(self, u, mu=None, prepare_data=None):
+            r = self.range.real_zero_vector()
+            self.matrix.mv(u.impl, r.impl)
+            return r
 
-        def apply_transpose(self, V, mu=None):
-            raise NotImplementedError
+        def _apply_adjoint_one_vector(self, v, mu=None, prepare_data=None):
+            r = self.source.real_zero_vector()
+            self.matrix.mtv(v.impl, r.impl)
+            return r
 
-        def apply_inverse(self, V, mu=None, least_squares=False):
-            assert V in self.range
+        def _apply_inverse_one_vector(self, v, mu=None, initial_guess=None,
+                                      least_squares=False, prepare_data=None):
             if least_squares:
                 raise NotImplementedError
+            r = (self.source.real_zero_vector() if initial_guess is None else
+                 initial_guess.copy(deep=True))
+            options = self.solver_options.get('inverse') if self.solver_options else None
 
             from dune.xt.la import make_solver
             solver = make_solver(self.matrix)
-            R = self.source.zeros(len(V))
-            options = self.solver_options.get('inverse') if self.solver_options else None
-            for v, r in zip(V._list, R._list):
-                if options:
-                    solver.apply(v.impl, r.impl, options)
-                else:
-                    solver.apply(v.impl, r.impl)
-            return R
+            if options:
+                solver.apply(v.impl, r.impl, options)
+            else:
+                solver.apply(v.impl, r.impl)
+            return r
 
-        def assemble_lincomb(self, operators, coefficients, solver_options=None, name=None):
-            if not all(isinstance(op, (DuneXTMatrixOperator, ZeroOperator)) for op in operators):
+        def _assemble_lincomb(self, operators, coefficients, identity_shift=0., solver_options=None, name=None):
+            if not all(isinstance(op, DuneXTMatrixOperator) for op in operators):
                 return None
-
-            if isinstance(operators[0], ZeroOperator):
-                return operators[1].assemble_lincomb(operators[1:], coefficients[1:],
-                                                     solver_options=solver_options, name=name)
+            if identity_shift != 0:
+                return None
+            if np.iscomplexobj(coefficients):
+                return None
 
             if coefficients[0] == 1:
                 matrix = operators[0].matrix.copy()
             else:
                 matrix = operators[0].matrix * coefficients[0]
             for op, c in zip(operators[1:], coefficients[1:]):
-                if isinstance(op, ZeroOperator):
-                    continue
-                matrix.axpy(c, op.matrix)
+                matrix.axpy(c, op.matrix) # does not work for all backends for different sparsity patterns
+                # one would have to extract the patterns from the pruned matrices, merge them and create a new matrix
 
             return DuneXTMatrixOperator(matrix, self.source.id, self.range.id, solver_options=solver_options, name=name)
+
