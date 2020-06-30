d3f643151ba79c9f471380dc1029f95e9bd60a03
René Fritze <rene.fritze@wwu.de>
2020-06-30 22:09:00 +0200

    [ci] force hypothesis profile for docs building

--- .ci/gitlab/test_docs.bash
+++ .ci/gitlab/test_docs.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 THIS_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ; pwd -P )"
+export PYMOR_HYPOTHESIS_PROFILE=dev
 source ${THIS_DIR}/common_test_setup.bash
 
 # pymor checks if this file's owner uid matches with the interpreter executor's
