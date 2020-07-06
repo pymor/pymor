c2a1abdf501f7aee602c0f49ed4a6b41e9ed8f2f
René Fritze <rene.fritze@wwu.de>
2020-07-06 16:38:31 +0200

    [tests] reduce hypothesis for all proflies but "debug"

--- src/pymortests/conftest.py
+++ src/pymortests/conftest.py
@@ -6,11 +6,12 @@ import os
 from hypothesis import settings, Verbosity, HealthCheck
 
 _common_settings = {"print_blob": True, "suppress_health_check": (HealthCheck.too_slow, HealthCheck.data_too_large,),
-                    "deadline": 1000, "verbosity": Verbosity.verbose}
+                    "deadline": 1000, "verbosity": Verbosity.normal}
 settings.register_profile("ci_large", max_examples=400, **_common_settings)
 settings.register_profile("ci_pr", max_examples=100, **_common_settings)
 settings.register_profile("ci", max_examples=25, **_common_settings)
 settings.register_profile("dev", max_examples=10, **_common_settings)
+_common_settings["verbosity"] = Verbosity.verbose
 settings.register_profile("debug", max_examples=10, **_common_settings)
 settings.load_profile(os.getenv(u'PYMOR_HYPOTHESIS_PROFILE', 'dev'))
 
