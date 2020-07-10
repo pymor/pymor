d400ebc6da9d05325fffaab59d51ce872831e6dd
Felix Schindler <felix.schindler@wwu.de>
2020-07-10 23:29:31 +0200

    [...dunegdt.ipdg] add NIPDG, IIPDG, SIPDG and SWIPDG discretizer

---
+++ src/pymor/discretizers/dunegdt/__init__.py
@@ -0,0 +1,6 @@
+# This file is part of the pyMOR project (http://www.pymor.org).
+# Copyright 2013-2020 pyMOR developers and contributors. All rights reserved.
+# License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+from pymor.discretizers.dunegdt.cg import discretize_stationary_cg
+from pymor.discretizers.dunegdt.ipdg import discretize_stationary_ipdg
