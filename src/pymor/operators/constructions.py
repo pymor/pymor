92975ede26b4c8897287b70995bfd7371aef906d
Petar Mlinarić <mlinaric@mpi-magdeburg.mpg.de>
2020-08-17 16:23:37 +0200

    [operators.constructions] add deprecation warnings

--- src/pymor/operators/constructions.py
+++ src/pymor/operators/constructions.py
@@ -15,6 +15,7 @@ from pymor.core.exceptions import InversionError
 from pymor.operators.interface import Operator
 from pymor.parameters.base import ParametricObject
 from pymor.parameters.functionals import ParameterFunctional, ConjugateParameterFunctional
+from pymor.tools.deprecated import Deprecated
 from pymor.vectorarrays.interface import VectorArray, VectorSpace
 from pymor.vectorarrays.numpy import NumpyVectorSpace
 
@@ -359,6 +360,11 @@ class ConcatenationOperator(Operator):
             operators = (other,) + self.operators
 
         return ConcatenationOperator(operators, solver_options=other.solver_options)
+
+
+@Deprecated(ConcatenationOperator)
+def Concatenation(*args, **kwargs):
+    return ConcatenationOperator(*args, **kwargs)
 
 
 class ProjectedOperator(Operator):
@@ -753,6 +759,11 @@ class ConstantOperator(Operator):
         if not least_squares:
             raise InversionError('ConstantOperator is not invertible.')
         return self.source.zeros(len(V))
+
+
+@Deprecated(ComponentProjectionOperator)
+def ComponentProjection(*args, **kwargs):
+    return ComponentProjectionOperator(*args, **kwargs)
 
 
 class ZeroOperator(Operator):
