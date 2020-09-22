88568bb8439661c15d968ec86ff45754db7b0032
Stephan Rave <stephanrave@uni-muenster.de>
2020-09-22 16:44:48 +0200

    [algorithms] add 'expand' algorithm

--- src/pymor/basic.py
+++ src/pymor/basic.py
@@ -20,6 +20,7 @@ from pymor.algorithms.newton import newton
 from pymor.algorithms.pod import pod
 from pymor.algorithms.preassemble import preassemble
 from pymor.algorithms.projection import project, project_to_subbasis
+from pymor.algorithms.simplify import expand
 
 from pymor.analyticalproblems.burgers import burgers_problem, burgers_problem_2d
 from pymor.analyticalproblems.domaindescriptions import (RectDomain, CylindricalDomain, TorusDomain, LineDomain,
