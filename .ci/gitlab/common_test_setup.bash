eaf4656ac09bb0d7b39c7394fe415c6553be298a
René Fritze <rene.fritze@wwu.de>
2020-10-13 18:08:18 +0200

    [ci] move coverage config to setup.cfg

--- .ci/gitlab/common_test_setup.bash
+++ .ci/gitlab/common_test_setup.bash
@@ -30,6 +30,6 @@ export PYTHONHASHSEED=0
 python setup.py build_ext -i
 
 PYMOR_VERSION=$(python -c 'import pymor;print(pymor.__version__)')
-COMMON_PYTEST_OPTS="--junitxml=test_results_${PYMOR_VERSION}.xml --cov=src/pymor --cov-report=xml  \
+COMMON_PYTEST_OPTS="--junitxml=test_results_${PYMOR_VERSION}.xml --cov=src/pymor --cov-report=xml --cov-config=setup.cfg \
   --memprof-top-n 50 --memprof-csv-file=memory_usage.txt\
   --hypothesis-profile ${PYMOR_HYPOTHESIS_PROFILE}"
