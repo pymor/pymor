0a046def3ef6be6fef97e0753beb6b20bd258d86
René Fritze <rene.fritze@wwu.de>
2020-10-13 18:50:08 +0200

    [ci] enable coverage run with mpi tests

--- .ci/gitlab/test_mpi.bash
+++ .ci/gitlab/test_mpi.bash
@@ -3,4 +3,6 @@
 THIS_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ; pwd -P )"
 source ${THIS_DIR}/common_test_setup.bash
 
-xvfb-run -a mpirun -n 2 python src/pymortests/mpi_run_demo_tests.py
+xvfb-run -a mpirun -n 2 coverage run --rcfile=setup.cfg --parallel-mode src/pymortests/mpi_run_demo_tests.py
+coverage combine
+coverage xml -i
