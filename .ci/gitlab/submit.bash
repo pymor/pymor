2f7955ac6df214fbe1cb5d7b130c5d00e6d9bd9a
René Fritze <rene.fritze@wwu.de>
2020-11-11 16:31:20 +0100

    fixup! [ci] workaround coverage reportin in xml in mpi jobs broken

--- .ci/gitlab/submit.bash
+++ .ci/gitlab/submit.bash
@@ -9,11 +9,12 @@ cd "${PYMOR_ROOT}"
 python3 -m pip install -U pip codecov coverage
 coverage combine coverage*
 # the mpi test_thermalblock_ipython results in '(builtin)' missing which we "--ignore-errors"
+rm -f coverage.xml
 coverage xml --ignore-errors
 
 codecov  --required \
   --token ${PYMOR_CODECOV_TOKEN} \
-  --file ${PYMOR_ROOT}/.coverage \
+  --file ${PYMOR_ROOT}/coverage.xml \
   --root ${PYMOR_ROOT} \
   -X detect \
   --slug pymor/pymor \
