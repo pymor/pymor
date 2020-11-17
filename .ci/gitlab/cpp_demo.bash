af35263b870717bbaa8a5499974d0e13f2fcdc3a
René Fritze <rene.fritze@wwu.de>
2020-11-17 10:23:54 +0100

    set correct interpreter for cpp demos
    This fixes #1040

--- .ci/gitlab/cpp_demo.bash
+++ .ci/gitlab/cpp_demo.bash
@@ -4,6 +4,6 @@ THIS_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ; pwd -P )"
 source ${THIS_DIR}/common_test_setup.bash
 
 cd src/pymordemos/minimal_cpp_demo
-cmake .
+cmake . -DPYTHON_EXECUTABLE=$(which python)
 make
 xvfb-run -a python -c 'import runpy; runpy.run_module("demo")'
