290e31ccd062b2c68da2ecba434d87a065249f92
René Fritze <rene.fritze@wwu.de>
2020-11-20 15:31:07 +0100

    [ci] fix service name/connection for d-in-d

--- .ci/gitlab/template.ci.py
+++ .ci/gitlab/template.ci.py
@@ -101,7 +101,8 @@ stages:
         - mkdir -p ${SHARED_PATH}
         - docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
     services:
-        - {{registry}}/docker:dind
+        - name: {{registry}}/docker:dind
+          alias: docker
     environment:
         name: unsafe
 
