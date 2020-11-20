33e13df643dc9f3b2aac940b28b2eacfb1aa9c5d
René Fritze <rene.fritze@wwu.de>
2020-11-20 15:01:56 +0100

    [ci] make sure registry login happens in dind

--- .ci/gitlab/template.ci.py
+++ .ci/gitlab/template.ci.py
@@ -99,6 +99,7 @@ stages:
     before_script:
         - 'export SHARED_PATH="${CI_PROJECT_DIR}/shared"'
         - mkdir -p ${SHARED_PATH}
+        - docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
     services:
         - {{registry}}/docker:dind
     environment:
