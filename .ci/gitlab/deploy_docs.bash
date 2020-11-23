c392799cd90bdc429afdcb567445ade5b30c5d75
René Fritze <rene.fritze@wwu.de>
2020-11-23 17:07:51 +0100

    [ci] additionally deploy binder ready sphinx generated NBs

--- .ci/gitlab/deploy_docs.bash
+++ .ci/gitlab/deploy_docs.bash
@@ -11,7 +11,6 @@ set -eux
 
 REPO=git@github.com:pymor/docs.git
 REPO_DIR=${CI_PROJECT_DIR}/repo
-TARGET_DIR=${REPO_DIR}/${CI_COMMIT_REF_SLUG/github\/PUSH_/from_fork__}
 # this must match PYMOR_ROOT/docs/source/conf.py:try_on_binder_branch
 SLUG=${CI_COMMIT_REF_SLUG/github\/PUSH_/from_fork__}
 TARGET_DIR=${REPO_DIR}/${SLUG}
@@ -33,8 +32,27 @@ ${PYMOR_ROOT}/.ci/gitlab/docs_makeindex.py ${REPO_DIR}
 git add list.html
 git commit -m "Updated index for ${CI_COMMIT_REF_NAME}" || echo "nothing to add"
 
-
 git push || (git pull --rebase && git push )
+
+[[ "${SLUG}" != "master" ]] && git checkout -b ${SLUG}
+rm -rf ${REPO_DIR}/.binder
+mkdir ${REPO_DIR}/.binder
+
+# this needs to go into the repo root, not the subdir!
+cp ${PYMOR_ROOT}/.binder/Dockerfile ${REPO_DIR}/.binder/
+
+# for binder the notebooks need to exist alongside their .rst version
+cd ${TARGET_DIR}
+for nb in $(find ./_downloads/ -name "*.ipynb") ; do
+  ln -s ${nb}
+done
+
+git add ${TARGET_DIR}/*ipynb
+git add ${REPO_DIR}/.binder/
+
+git commit -am "Binder setup for ${CI_COMMIT_REF_NAME}"
+git push origin ${SLUG} -f
+
 
 
 du -sch ${REPO_DIR}/*
