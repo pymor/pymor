d7acc0b2748027dd610bc9cc21b8b325e37413f3
Tim Keil <tim.keil@wwu.de>
2020-11-25 17:50:44 +0100

    [docs] also add :private-members:

--- docs/source/gen_apidoc.py
+++ docs/source/gen_apidoc.py
@@ -8,7 +8,7 @@ import pkgutil
 
 BUILD_DIR = 'generated'
 
-CLASS_OPTIONS = [':show-inheritance:', ':members:', ':special-members:', ':exclude-members: __init__, __weakref__']
+CLASS_OPTIONS = [':show-inheritance:', ':members:', ':special-members:', ':private-members:', ':exclude-members: __init__, __weakref__']
 FUNCTION_OPTIONS = []
 MODULE_OPTIONS = [':show-inheritance:']
 
