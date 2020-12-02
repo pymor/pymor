cc8a1d539cd94bc934a4462d1292e87653205bca
René Fritze <rene.fritze@wwu.de>
2020-12-02 15:58:37 +0100

    [ci] fix oldest job using correct packages

--- .ci/gitlab/test_oldest.bash
+++ .ci/gitlab/test_oldest.bash
@@ -3,6 +3,14 @@
 THIS_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ; pwd -P )"
 source ${THIS_DIR}/common_test_setup.bash
 
+# _downgrade_ packages to what's avail in the oldest mirror
+sudo pip uninstall -y -r requirements-optional.txt
+sudo pip uninstall -y -r requirements-ci.txt
+sudo pip uninstall -y -r requirements.txt
+sudo pip install -U --use-feature=2020-resolver -r requirements.txt
+sudo pip install -U --use-feature=2020-resolver -r requirements-ci.txt
+sudo pip install -U --use-feature=2020-resolver -r requirements-optional.txt
+
 # we've changed numpy versions, recompile cyx
 find src/pymor/ -name _*.c | xargs rm -f
 find src/pymor/ -name _*.so | xargs rm -f
