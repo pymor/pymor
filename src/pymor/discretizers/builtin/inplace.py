4186b312db73386a1d37bb0bf17a4afa11701fee
René Fritze <rene.fritze@wwu.de>
2021-02-26 17:11:48 +0100

    [docs] provide documentation dummies for cython functions

--- src/pymor/discretizers/builtin/inplace.py
+++ src/pymor/discretizers/builtin/inplace.py
@@ -2,6 +2,14 @@
 # Copyright 2013-2020 pyMOR developers and contributors. All rights reserved.
 # License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
+def iadd_masked(U, V, U_ind):
+    """This cython function is defined in pymor/discretizers/builtin/inplace.pyx."""
+
+
+def isub_masked(U, V, U_ind):
+    """This cython function is defined in pymor/discretizers/builtin/inplace.pyx."""
+
+
 raise ImportError('''
 Cython extension module 'pymor.tools.inplace' has not been built.
 Please run 'python setup.py build_ext --inplace' in the root
