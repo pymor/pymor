5df54c8ea25bc5b74e13500f2207cc8f31af0a1d
René Fritze <rene.fritze@wwu.de>
2021-02-26 17:36:55 +0100

    [discretizer] delay import error on cython functions
    module level importerror confused astroid/autoapi

--- src/pymor/discretizers/builtin/grids/_unstructured.py
+++ src/pymor/discretizers/builtin/grids/_unstructured.py
@@ -2,11 +2,9 @@
 # Copyright 2013-2021 pyMOR developers and contributors. All rights reserved.
 # License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
 
+from pymor.core.exceptions import CythonExtensionNotBuiltError
+
+
 def compute_edges(faces, num_vertices):
     """This cython function is defined in pymor/discretizers/builtin/grids/_unstructured.pyx."""
-
-
-raise ImportError('''
-Cython extension module 'pymor.discretizers.builtin.grids._unstructured' has not been built.
-Please run 'python setup.py build_ext --inplace' in the root
-directory of the pyMOR repository.''')
+    raise CythonExtensionNotBuiltError()
--- src/pymor/discretizers/builtin/inplace.py
+++ src/pymor/discretizers/builtin/inplace.py
@@ -1,16 +1,14 @@
 # This file is part of the pyMOR project (https://www.pymor.org).
 # Copyright 2013-2021 pyMOR developers and contributors. All rights reserved.
 # License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
+from pymor.core.exceptions import CythonExtensionNotBuiltError
+
 
 def iadd_masked(U, V, U_ind):
     """This cython function is defined in pymor/discretizers/builtin/inplace.pyx."""
+    raise CythonExtensionNotBuiltError()
 
 
 def isub_masked(U, V, U_ind):
     """This cython function is defined in pymor/discretizers/builtin/inplace.pyx."""
-
-
-raise ImportError('''
-Cython extension module 'pymor.tools.inplace' has not been built.
-Please run 'python setup.py build_ext --inplace' in the root
-directory of the pyMOR repository.''')
+    raise CythonExtensionNotBuiltError()
