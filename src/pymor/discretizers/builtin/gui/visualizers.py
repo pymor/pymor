4a46ad8ce65508153991b077e4bed36474ce3806
Stephan Rave <stephanrave@uni-muenster.de>
2021-05-12 12:43:24 +0200

    [gui] improve visualizer backend selection

--- src/pymor/discretizers/builtin/gui/visualizers.py
+++ src/pymor/discretizers/builtin/gui/visualizers.py
@@ -4,6 +4,7 @@
 
 
 from pymor.core.base import ImmutableObject
+from pymor.core.defaults import defaults
 from pymor.core.config import is_jupyter
 from pymor.discretizers.builtin.grids.oned import OnedGrid
 from pymor.discretizers.builtin.grids.referenceelements import triangle, square
@@ -26,16 +27,19 @@ class PatchVisualizer(ImmutableObject):
     codim
         The codimension of the entities the data in `U` is attached to (either 0 or 2).
     backend
-        Plot backend to use ('gl', 'matplotlib', 'jupyter').
+        Plot backend to use ('jupyter_or_gl', 'jupyter', 'gl', 'matplotlib').
     block
         If `True`, block execution until the plot window is closed.
     """
 
-    def __init__(self, grid, bounding_box=([0, 0], [1, 1]), codim=2, backend=None, block=False):
+    @defaults('backend')
+    def __init__(self, grid, bounding_box=([0, 0], [1, 1]), codim=2, backend='jupyter_or_gl', block=False):
         assert grid.reference_element in (triangle, square)
         assert grid.dim == 2
         assert codim in (0, 2)
-        backend = backend or ('jupyter' if is_jupyter() else None)
+        assert backend in {'jupyter_or_gl', 'jupyter', 'gl', 'matplotlib'}
+        if backend == 'jupyter_or_gl':
+            backend = 'jupyter' if is_jupyter() else 'gl'
         self.__auto_init(locals())
 
     def visualize(self, U, m, title=None, legend=None, separate_colorbars=False,
@@ -109,12 +113,17 @@ class OnedVisualizer(ImmutableObject):
         The codimension of the entities the data in `U` is attached to (either 0 or 1).
     block
         If `True`, block execution until the plot window is closed.
+    backend
+        Plot backend to use ('jupyter_or_matplotlib', 'jupyter', 'matplotlib').
     """
 
-    def __init__(self, grid, codim=1, block=False, backend=None):
+    @defaults('backend')
+    def __init__(self, grid, codim=1, block=False, backend='jupyter_or_matplotlib'):
         assert isinstance(grid, OnedGrid)
         assert codim in (0, 1)
-        backend = backend or ('jupyter' if is_jupyter() else None)
+        assert backend in {'jupyter_or_matplotlib', 'jupyter', 'matplotlib'}
+        if backend == 'jupyter_or_matplotlib':
+            backend = 'jupyter' if is_jupyter() else 'matplotlib'
         self.__auto_init(locals())
 
     def visualize(self, U, m, title=None, legend=None, separate_plots=True,
