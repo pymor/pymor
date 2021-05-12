a95ca32f4b338332b2cccf4817c1e44c09a8d3bc
René Fritze <rene.fritze@wwu.de>
2021-05-12 10:34:11 +0200

    fix flake8 issues

--- src/pymor/scripts/check_reqs.py
+++ src/pymor/scripts/check_reqs.py
@@ -3,15 +3,13 @@
 # Copyright 2013-2021 pyMOR developers and contributors. All rights reserved.
 # License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
 
-import os
 import typer
-from pathlib import Path
 import pkg_resources
 
 
 def _output_rich(filename, status):
     from rich.console import Console
-    from rich.table import Table, Column
+    from rich.table import Table
     tbl = Table("Requirement", "Status", "Reason", title=f"Requirements checked for {filename}")
     for req, st in status.items():
         if st is None:
@@ -26,12 +24,12 @@ def _output_plain(filename, status):
     print(f"Requirements checked for {filename}")
     for req, st in status.items():
         if st is None:
-            print(f"OK {req}") 
+            print(f"OK {req}")
         else:
-            print(f"Failed {req}: {st}") 
-    
+            print(f"Failed {req}: {st}")
 
-def _process_file(filename): 
+
+def _process_file(filename):
     req_file = open(filename).read().splitlines()
     status = {}
     for line in req_file:
@@ -43,13 +41,13 @@ def _process_file(filename):
             status[requirement] = e
         except pkg_resources.VersionConflict as d:
             status[requirement] = d
-        # Requirement.parse cannot raises on include and comment directives 
+        # Requirement.parse cannot raises on include and comment directives
         except (ValueError,) as f:
             if line.startswith('-r'):
                 status.update(_process_file(line[3:]))
             elif line.startswith('http'):
                 status[line] = 'http link skipped'
-            elif line.startswith('#'): 
+            elif line.startswith('#'):
                 continue
             else:
                 raise RuntimeError(f'Error parsing {filename}:\n{f}')
@@ -57,7 +55,7 @@ def _process_file(filename):
     return status
 
 
-def main(filename: str): 
+def main(filename: str):
     status = _process_file(filename)
     try:
         _output_rich(filename, status)
