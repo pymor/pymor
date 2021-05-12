1e2012576e889c5d919ceb118c9ce418c9db4f28
Petar Mlinarić <mlinaric@mpi-magdeburg.mpg.de>
2021-05-12 14:49:48 +0200

    [demos] remove warning suppressions

--- src/pymordemos/delay.py
+++ src/pymordemos/delay.py
@@ -3,8 +3,6 @@
 # Copyright 2013-2021 pyMOR developers and contributors. All rights reserved.
 # License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
 
-import warnings
-
 import numpy as np
 from typer import Argument, run
 
@@ -33,9 +31,7 @@ def main(
     fom_properties(tf, w)
 
     # Transfer function IRKA (TF-IRKA)
-    with warnings.catch_warnings():
-        warnings.simplefilter('ignore')  # suppress IntegrationWarning from H2 error computation
-        run_mor_method(tf, w, TFIRKAReductor(tf), 'TF-IRKA', r, maxit=1000)
+    run_mor_method(tf, w, TFIRKAReductor(tf), 'TF-IRKA', r, maxit=1000)
 
 
 if __name__ == '__main__':
