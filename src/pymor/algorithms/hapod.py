22d780deddecd2f97319164c6edc6a7b025b5113
Stephan Rave <stephanrave@uni-muenster.de>
2021-05-21 17:59:19 +0200

    [hapod] remove excutor argument from inc_hapod

--- src/pymor/algorithms/hapod.py
+++ src/pymor/algorithms/hapod.py
@@ -188,7 +188,7 @@ def hapod(tree, snapshots, local_eps, product=None, pod_method=default_pod_metho
     return result
 
 
-def inc_hapod(steps, snapshots, eps, omega, product=None, executor=None, eval_snapshots_in_executor=False):
+def inc_hapod(steps, snapshots, eps, omega, product=None):
     """Incremental Hierarchical Approximate POD.
 
     This computes the incremental HAPOD from :cite:`HLR18`.
@@ -208,11 +208,6 @@ def inc_hapod(steps, snapshots, eps, omega, product=None, executor=None, eval_sn
         approximation quality.
     product
         Inner product |Operator| w.r.t. which to compute the POD.
-    executor
-        If not `None`, a :class:`concurrent.futures.Executor` object to use
-        for parallelization.
-    eval_snapshots_in_executor
-        If `True` also parallelize the evaluation of the snapshot map.
 
     Returns
     -------
@@ -237,9 +232,7 @@ def inc_hapod(steps, snapshots, eps, omega, product=None, executor=None, eval_sn
     result = hapod(tree,
                    get_snapshots,
                    std_local_eps(tree, eps, omega, False),
-                   product=product,
-                   executor=executor,
-                   eval_snapshots_in_executor=eval_snapshots_in_executor)
+                   product=product)
     assert last_step == steps - 1
     return result
 
@@ -286,7 +279,7 @@ def dist_hapod(num_slices, snapshots, eps, omega, product=None, executor=None, e
                  eval_snapshots_in_executor=eval_snapshots_in_executor)
 
 
-def inc_vectorarray_hapod(steps, U, eps, omega, product=None, executor=None):
+def inc_vectorarray_hapod(steps, U, eps, omega, product=None):
     """Incremental Hierarchical Approximate POD.
 
     This computes the incremental HAPOD from :cite:`HLR18` for a given |VectorArray|.
@@ -304,9 +297,6 @@ def inc_vectorarray_hapod(steps, U, eps, omega, product=None, executor=None):
         approximation quality.
     product
         Inner product |Operator| w.r.t. which to compute the POD.
-    executor
-        If not `None`, a :class:`concurrent.futures.Executor` object to use
-        for parallelization.
 
     Returns
     -------
@@ -325,7 +315,7 @@ def inc_vectorarray_hapod(steps, U, eps, omega, product=None, executor=None):
             yield U[slice: slice+chunk_size]
 
     return inc_hapod(len(slices), snapshots(),
-                     eps, omega, product=product, executor=executor)
+                     eps, omega, product=product)
 
 
 def dist_vectorarray_hapod(num_slices, U, eps, omega, product=None, executor=None):
