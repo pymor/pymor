1b2d9226a90fc1a2f80b2536121ca344226c9222
Hendrik Kleikamp <hendrik.kleikamp@uni-muenster.de>
2021-05-27 17:38:27 +0200

    [neural-networks] added outputs and reductor to instationary demo

--- src/pymordemos/neural_networks_instationary.py
+++ src/pymordemos/neural_networks_instationary.py
@@ -3,12 +3,14 @@
 # Copyright 2013-2021 pyMOR developers and contributors. All rights reserved.
 # License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
 
+import time
 import numpy as np
 from typer import Argument, run
 
 from pymor.basic import *
 from pymor.core.config import config
 from pymor.core.exceptions import TorchMissing
+from pymor.reductors.neural_network import NeuralNetworkInstationaryReductor, NeuralNetworkInstationaryOutputReductor
 
 
 def main(
@@ -28,8 +30,6 @@ def main(
 
     parameter_space = fom.parameters.space(1., 2.)
 
-    from pymor.reductors.neural_network import NeuralNetworkInstationaryReductor
-
     training_set = parameter_space.sample_uniformly(training_samples)
     validation_set = parameter_space.sample_randomly(validation_samples)
 
@@ -39,8 +39,6 @@ def main(
     test_set = parameter_space.sample_randomly(10)
 
     speedups = []
-
-    import time
 
     print(f'Performing test on set of size {len(test_set)} ...')
 
@@ -61,17 +59,54 @@ def main(
     absolute_errors = (U - U_red).norm2()
     relative_errors = (U - U_red).norm2() / U.norm2()
 
+    output_reductor = NeuralNetworkInstationaryOutputReductor(fom, time_steps+1, training_set,
+                                                              validation_set, validation_loss=1e-5)
+    output_rom = output_reductor.reduce(restarts=100)
+
+    outputs = []
+    outputs_red = []
+    outputs_speedups = []
+
+    print(f'Performing test on set of size {len(test_set)} ...')
+
+    for mu in test_set:
+        tic = time.perf_counter()
+        outputs.append(fom.compute(output=True, mu=mu)['output'])
+        time_fom = time.perf_counter() - tic
+
+        tic = time.perf_counter()
+        outputs_red.append(output_rom.compute(output=True, mu=mu)['output'])
+        time_red = time.perf_counter() - tic
+
+        outputs_speedups.append(time_fom / time_red)
+
+    outputs = np.squeeze(np.array(outputs))
+    outputs_red = np.squeeze(np.array(outputs_red))
+
+    outputs_absolute_errors = np.abs(outputs - outputs_red)
+    outputs_relative_errors = np.abs(outputs - outputs_red) / np.abs(outputs)
+
+    print('Results for state approximation:')
     print(f'Average absolute error: {np.average(absolute_errors)}')
     print(f'Average relative error: {np.average(relative_errors)}')
     print(f'Median of speedup: {np.median(speedups)}')
 
+    print()
+    print('Results for output approximation:')
+    print(f'Average absolute error: {np.average(outputs_absolute_errors)}')
+    print(f'Average relative error: {np.average(outputs_relative_errors)}')
+    print(f'Median of speedup: {np.median(outputs_speedups)}')
+
 
 def create_fom(grid_intervals, time_steps):
     problem = burgers_problem()
+    f = LincombFunction(
+        [ExpressionFunction('ones(x.shape[:-1])', 1, ()), ConstantFunction(1., 1)],
+        [ProjectionParameterFunctional('exponent'), 0.1])
+    problem = problem.with_stationary_part(outputs=[('l2', f)])
 
     print('Discretize ...')
-    discretizer = discretize_instationary_fv
-    fom, _ = discretizer(problem, diameter=1. / grid_intervals, nt=time_steps)
+    fom, _ = discretize_instationary_fv(problem, diameter=1. / grid_intervals, nt=time_steps)
 
     return fom
 
