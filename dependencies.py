900370e6d7085d2f4071cc2e2688caeb6fd592b8
René Fritze <rene.fritze@wwu.de>
2021-06-30 12:04:18 +0200

    [deps] we no longer directly depend on jupyter-sphinx

--- dependencies.py
+++ dependencies.py
@@ -58,7 +58,7 @@ install_suggests = {
     'jupyter_contrib_nbextensions': 'modular collection of jupyter extensions',
     'pillow': 'image library used for bitmap data functions',
 }
-doc_requires = ['sphinx>=3.4', 'jupyter_sphinx', 'matplotlib', _PYSIDE, 'ipyparallel>=6.2.5', 'python-slugify',
+doc_requires = ['sphinx>=3.4', 'matplotlib', _PYSIDE, 'ipyparallel>=6.2.5', 'python-slugify',
                 'ipywidgets', 'sphinx-qt-documentation', 'bash_kernel', 'sphinx-material',
                 'sphinxcontrib-bibtex', 'sphinx-autoapi>=1.8', 'myst-nb'] + install_requires
 ci_requires = [_PYTEST, 'pytest-cov', 'pytest-xdist', 'check-manifest', 'nbconvert', 'pytest-parallel',
