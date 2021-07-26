aab33f18abb0a1003c96adda6200457afd584cf7
René Fritze <rene.fritze@wwu.de>
2021-07-26 09:20:25 +0200

    [gui] start pyvista setup from 3js template

--- src/pymor/discretizers/builtin/gui/jupyter/__init__.py
+++ src/pymor/discretizers/builtin/gui/jupyter/__init__.py
@@ -20,13 +20,17 @@ from pymor.core.config import config
 # and we have to make sure we do not setup two redirects
 _extension_loaded = False
 
+
 @defaults('backend')
 def get_visualizer(backend='py3js'):
-    if backend not in ('py3js', 'MPL'):
+    if backend not in ('py3js', 'pyvista', 'MPL'):
         raise ValueError
     if backend == 'py3js' and config.HAVE_PYTHREEJS:
         from pymor.discretizers.builtin.gui.jupyter.threejs import visualize_py3js
         return visualize_py3js
+    elif backend == 'pyvista' and config.HAVE_PYVISTA:
+        from pymor.discretizers.builtin.gui.jupyter.vista import visualize_vista_vectorarray
+        return visualize_vista_vectorarray
     else:
         return visualize_patch
 
