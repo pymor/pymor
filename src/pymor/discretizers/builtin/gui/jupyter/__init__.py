b5a9ed137c3f38082a94a67586e5f64dea09559d
René Fritze <rene.fritze@wwu.de>
2021-07-29 14:18:38 +0200

    [gui] make pyvista the default jupyter backend
    Also warn if selected backend not avilable

--- src/pymor/discretizers/builtin/gui/jupyter/__init__.py
+++ src/pymor/discretizers/builtin/gui/jupyter/__init__.py
@@ -13,6 +13,7 @@ inside the given notebook.
 import IPython
 
 from pymor.core.defaults import defaults
+from pymor.core.logger import getLogger
 from pymor.discretizers.builtin.gui.jupyter.matplotlib import visualize_patch
 from pymor.core.config import config
 
@@ -22,7 +23,7 @@ _extension_loaded = False
 
 
 @defaults('backend')
-def get_visualizer(backend='py3js'):
+def get_visualizer(backend='pyvista'):
     if backend not in ('py3js', 'pyvista', 'MPL'):
         raise ValueError
     if backend == 'py3js' and config.HAVE_PYTHREEJS:
@@ -31,8 +32,10 @@ def get_visualizer(backend='py3js'):
     elif backend == 'pyvista' and config.HAVE_PYVISTA:
         from pymor.discretizers.builtin.gui.jupyter.vista import visualize_vista_vectorarray
         return visualize_vista_vectorarray
-    else:
-        return visualize_patch
+    if backend != 'MPL':
+        msg = f'Selected {backend} as visualizer not available. Falling back to matplotlib'
+        getLogger('pymor.discretizers.builtin.gui.jupyter').warning(msg=msg)
+    return visualize_patch
 
 
 def progress_bar(sequence, every=None, size=None, name='Parameters'):
