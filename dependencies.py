e5c3923a0ec8ff2be0cd22939d0a68e963a2ba1d
René Fritze <rene.fritze@wwu.de>
2021-09-21 10:55:34 +0200

    bump dune dependencies and CI images

--- dependencies.py
+++ dependencies.py
@@ -56,8 +56,8 @@ install_suggests = {
     'torch': 'PyTorch open source machine learning framework',
     'jupyter_contrib_nbextensions': 'modular collection of jupyter extensions',
     'pillow': 'image library used for bitmap data functions',
-    'dune-gdt~=2021.1,>=2021.1.2': 'generic discretization toolbox',
-    'dune-xt~=2021.1,>=2021.1.2': 'DUNE extensions for dune-gdt',
+    'dune-gdt~=2021.1,>=2021.1.3': 'generic discretization toolbox',
+    'dune-xt~=2021.1,>=2021.1.3': 'DUNE extensions for dune-gdt',
 }
 io_requires = ['pyevtk', 'xmljson', 'meshio>=4.4', 'lxml', 'gmsh']
 install_suggests.update({p: 'optional File I/O support libraries' for p in io_requires})
