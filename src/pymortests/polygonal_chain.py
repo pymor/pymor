949f0f50ceb00632c5673e0b9ca660a5b26a62d3
Peter Oehme <oehme.pb@gmail.com>
2021-10-08 13:23:49 +0200

    adjust for PEP8

--- src/pymortests/polygonal_chain.py
+++ src/pymortests/polygonal_chain.py
@@ -3,6 +3,7 @@ import numpy as np
 from pymor.analyticalproblems.domaindescriptions import PolygonalDomain
 from pymortests.base import runmodule
 
+
 def determine_boundary_type(point):
     x = point[0]
     y = point[1]
@@ -12,14 +13,27 @@ def determine_boundary_type(point):
 
     return "dirichlet"
 
+
 def test_polygonal_chain_boundary_function():
-    chain = [[2, 0], [4, 0], [3, 2]]
-    reference_boundary_types = {"neumann": [0, 1], "dirichlet": [2]}
-    domain = PolygonalDomain(points=chain, boundary_types=determine_boundary_type)
+    chain = [
+        [2, 0], [4, 0], [3, 2]
+    ]
+
+    reference_boundary_types = {
+        "neumann":
+            [0, 1],
+        "dirichlet":
+            [2]
+    }
+
+    domain = PolygonalDomain(
+         points=chain, boundary_types=determine_boundary_type
+    )
 
     print(domain.boundary_types)
 
     assert domain.boundary_types == reference_boundary_types
+
 
 if __name__ == "__main__":
     runmodule(filename=__file__)
