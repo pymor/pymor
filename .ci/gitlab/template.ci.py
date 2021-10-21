759226d2d3393e11f81e96ee79196c4cb10afdfb
René Fritze <rene.fritze@wwu.de>
2021-10-21 17:37:42 +0200

    [ci] also move d-in-d image with ci_tags

--- .ci/gitlab/template.ci.py
+++ .ci/gitlab/template.ci.py
@@ -107,8 +107,7 @@ rules:
             - api_failure
             - unknown_failure
             - job_execution_timeout
-    {# this is intentionally NOT moving with CI_IMAGE_TAG #}
-    image: {{registry}}/pymor/docker-in-docker:d1b5ebb4dc42a77cae82411da2e503a88bb8fb3a
+    image: {{registry}}/pymor/docker-in-docker:{{ci_image_tag}}
     variables:
         DOCKER_HOST: tcp://docker:2375/
         DOCKER_DRIVER: overlay2
