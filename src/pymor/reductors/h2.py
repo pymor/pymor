a099d10aa1d537dbd9e29aefb84cb34b096da48a
Petar Mlinarić <mlinaric@vt.edu>
2021-12-01 12:50:48 -0500

    [reductors.h2] remove InputOutputModel import

--- src/pymor/reductors/h2.py
+++ src/pymor/reductors/h2.py
@@ -15,7 +15,7 @@ from pymor.algorithms.riccati import solve_ricc_dense
 from pymor.algorithms.sylvester import solve_sylv_schur
 from pymor.algorithms.to_matrix import to_matrix
 from pymor.core.base import BasicObject
-from pymor.models.iosys import InputOutputModel, LTIModel, _lti_to_poles_b_c, _poles_b_c_to_lti
+from pymor.models.iosys import LTIModel, _lti_to_poles_b_c, _poles_b_c_to_lti
 from pymor.operators.constructions import IdentityOperator
 from pymor.parameters.base import Mu
 from pymor.reductors.basic import LTIPGReductor
@@ -561,7 +561,7 @@ class TFIRKAReductor(GenericIRKAReductor):
     """
 
     def __init__(self, fom, mu=None):
-        assert isinstance(fom, InputOutputModel)
+        assert hasattr(fom, 'eval_tf') and hasattr(fom, 'eval_dtf')
         super().__init__(fom, mu=mu)
 
     def reduce(self, rom0_params, tol=1e-4, maxit=100, num_prev=1,
