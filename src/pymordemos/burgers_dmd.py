5d4887dceabfb488d3111f7aaac9a06e68773efb
Stephan Rave <stephanrave@uni-muenster.de>
2021-12-02 18:52:39 +0100

    [demos] polish burgers_dmd demo

--- src/pymordemos/burgers_dmd.py
+++ src/pymordemos/burgers_dmd.py
@@ -5,14 +5,15 @@
 
 """Burgers demo with different applications of Dynamic Mode Decomposition."""
 
-import math
 import sys
 import time
 
 import matplotlib.pyplot as plt
+import numpy as np
+
 from pymor.algorithms.dmd import dmd
-from pymor.analyticalproblems.burgers import burgers_problem_2d
-from pymor.discretizers.builtin import discretize_instationary_fv, RectGrid, TriaGrid
+from pymor.analyticalproblems.burgers import burgers_problem
+from pymor.discretizers.builtin import discretize_instationary_fv
 from pymor.tools.typer import Choices
 from typer import Argument, Option, run
 
@@ -20,42 +21,32 @@ from typer import Argument, Option, run
 def main(
         exp: float = Argument(..., help='Exponent'),
 
-        grid: int = Option(60, help='Use grid with (2*NI)*NI elements.'),
-        grid_type: Choices('rect tria') = Option('rect', help='Type of grid to use.'),
+        atol: float = Option(None, help='Absolute tolerance'),
+        continuous_time: bool = Option(False, help='Show continous time system eigenvalues.'),
+        grid: int = Option(100, help='Use grid with this number of elements.'),
         initial_data: Choices('sin bump') = Option('sin', help='Select the initial data.'),
-        lxf_lambda: float = Option(1., help='Parameter lambda in Lax-Friedrichs flux.'),
+        modes: int = Option(None, help='Number of DMD modes'),
+        nt: int = Option(1000, help='Number of time steps.'),
         periodic: bool = Option(True,
                                 help='If not, solve with dirichlet boundary conditions on left and bottom boundary.'),
-        nt: int = Option(100, help='Number of time steps.'),
-        num_flux: Choices('lax_friedrichs engquist_osher simplified_engquist_osher') = Option(
-            'engquist_osher',
-            help='Numerical flux to use.'
-        ),
-        vx: float = Option(1., help='Speed in x-direction.'),
-        vy: float = Option(1., help='Speed in y-direction.'),
+        rtol: float = Option(None, help='Relative tolerance'),
+        v: float = Option(10., help='Speed in x-direction.'),
 ):
-    """Solves a two-dimensional Burgers-type equation.
+    """Solves a one-dimensional Burgers-type equation.
 
     See pymor.analyticalproblems.burgers for more details.
     """
     print('Setup Problem ...')
-    problem = burgers_problem_2d(vx=vx, vy=vy, initial_data_type=initial_data.value,
-                                 parameter_range=(0, 1e42), torus=periodic)
+    problem = burgers_problem(v=v, initial_data_type=initial_data.value, circle=periodic)
 
     print('Discretize ...')
-    if grid_type == 'rect':
-        grid *= 1. / math.sqrt(2)
     m, data = discretize_instationary_fv(
         problem,
-        diameter=1. / grid,
-        grid_type=RectGrid if grid_type == 'rect' else TriaGrid,
-        num_flux=num_flux.value,
-        lxf_lambda=lxf_lambda,
+        diameter=1 / grid,
+        num_flux='engquist_osher',
         nt=nt
     )
     print(m.operator.grid)
-
-    print(f'The parameters are {m.parameters}')
 
     mu = exp
     print(f'Solving for exponent = {mu} ... ')
@@ -65,21 +56,31 @@ def main(
     print(f'Solving took {time.perf_counter() - tic}s')
     m.visualize(U)
 
-    # ----- Testing DMD -----
-    W1, E1 = dmd(X=U, modes='standard')
-    W2, E2 = dmd(X=U, svd_rank=5, modes='exact')
+    print('Computing DMD ...')
+    dt = problem.T / nt if continuous_time else None
+    W1, E1 = dmd(X=U, modes=modes, atol=atol, rtol=rtol, type='standard', cont_time_dt=dt, order='frequency')
+    W2, E2 = dmd(X=U, modes=modes, atol=atol, rtol=rtol, type='exact', cont_time_dt=dt, order='frequency')
+
+    print(E1)
 
     print('Visualize ...')
-    m.visualize(W1, title='DMD Modes (Standard)')
-    plt.plot(E1.real, E1.imag, '.')
-    plt.title('DMD Eigenvalues (Standard)')
-    plt.xlabel('Real')
-    plt.ylabel('Imaginary')
-    plt.show()
+    # plot DMD modes
+    m.visualize((W1.real, W1.imag, W2.real, W2.imag),
+                legend=('standard - real part', 'standard - imaginary part',
+                        'exact - real part', 'exact - imaginary part'),
+                title='DMD Modes')
 
-    m.visualize(W2, title='Five DMD Modes (Exact)')
-    plt.plot(E2.real, E2.imag, '.')
-    plt.title('DMD Eigenvalues (Exact)')
+    # plot DMD eigenvalues (same for standard and exact)
+    plt.figure()
+    if not continuous_time:
+        c = plt.Circle((0., 0.), 1., linestyle='--', color='k', fill=False)
+        xl = max(np.max(np.abs(E1.real)), 1.) * 1.1
+        yl = max(np.max(np.abs(E1.imag)), 1.) * 1.1
+        plt.gca().add_artist(c)
+        plt.xlim([-xl, xl])
+        plt.ylim([-yl, yl])
+    plt.plot(E1.real, E1.imag, '.')
+    plt.title(f'{"Continuous" if continuous_time else "Discrete"}-time DMD Eigenvalues')
     plt.xlabel('Real')
     plt.ylabel('Imaginary')
     plt.show()
