7067baa283317b3f65b27ca8296fda6bae609be6
Stephan Rave <stephanrave@uni-muenster.de>
2022-01-18 17:41:38 +0100

    [algorithms] add contract operator transformation

--- src/pymor/algorithms/simplify.py
+++ src/pymor/algorithms/simplify.py
@@ -4,8 +4,11 @@
 
 from pymor.algorithms.rules import RuleTable, match_class
 from pymor.models.interface import Model
-from pymor.operators.constructions import LincombOperator, ConcatenationOperator
-from pymor.operators.interface import Operator
+from pymor.operators.constructions import LincombOperator, ConcatenationOperator, VectorArrayOperator
+from pymor.operators.interface import Operator, as_array_max_length
+from pymor.operators.numpy import NumpyMatrixOperator
+from pymor.parameters.functionals import ParameterFunctional
+from pymor.vectorarrays.numpy import NumpyVectorSpace
 
 
 def expand(obj):
@@ -41,6 +44,10 @@ def expand(obj):
     The transformed object.
     """
     return ExpandRules().apply(obj)
+
+
+def contract(obj):
+    return ContractRules().apply(obj)
 
 
 class ExpandRules(RuleTable):
@@ -89,6 +96,84 @@ class ExpandRules(RuleTable):
 
             # there can still be LincombOperators within the summands so we recurse ..
             op = self.apply(op)
+
+        return op
+
+    @match_class(Model, Operator)
+    def action_recurse(self, op):
+        return self.replace_children(op)
+
+
+class ContractRules(RuleTable):
+
+    def __init__(self):
+        super().__init__(use_caching=True)
+
+    @match_class(LincombOperator)
+    def action_LincombOperator(self, op):
+        # recursively contract all children
+        op = self.replace_children(op)
+
+        # merge non-parametric part
+        param_ops, param_coeffs = [], []
+        non_param_ops, non_param_coeffs = [], []
+        for o, c in zip(op.operators, op.coefficients):
+            if isinstance(c, ParameterFunctional):
+                param_ops.append(o)
+                param_coeffs.append(c)
+            else:
+                non_param_ops.append(o)
+                non_param_coeffs.append(c)
+
+        if not non_param_ops:  # there is not non-parametric part at all
+            return op
+
+        non_parametric_part = LincombOperator(non_param_ops, non_param_coeffs).assemble()
+
+        if not param_ops:  # there is no parametric part at all
+            return non_parametric_part
+
+        if isinstance(non_parametric_part, LincombOperator):
+            if len(non_parametric_part.operators) == len(non_param_ops):  # nothing could be contracted
+                return op
+            else:
+                param_ops.extend(non_parametric_part.operators)
+                param_coeffs.extend(non_parametric_part.param_coeffs)
+        else:
+            param_ops.append(non_parametric_part)
+            param_coeffs.append(1.)
+
+        return op.with_(operators=param_ops, coefficients=param_coeffs)
+
+    @match_class(ConcatenationOperator)
+    def action_ConcatenationOperator(self, op):
+        op = self.replace_children(op)
+
+        ops_rev = list(op.operators[::-1])
+        i = 0
+        while i + 1 < len(ops_rev):
+            if (ops_rev[i+1].linear and not ops_rev[i+1].parametric):
+                if isinstance(ops_rev[i], NumpyMatrixOperator):
+                    if not ops_rev[i].sparse:  # do not touch sparse matrices
+                        U = ops_rev[i+1].source.from_numpy(ops_rev[i].matrix.T)
+                        ops_rev[i+1] = VectorArrayOperator(ops_rev[i+1].apply(U))
+                        del ops_rev[i]
+                    else:
+                        i += 1
+                elif (ops_rev[i].linear and not ops_rev[i].parametric
+                      and isinstance(ops_rev[i].source, NumpyVectorSpace)
+                      and ops_rev[i].source.dim <= as_array_max_length()):
+                    # the following might in fact convert small sparse matrices in external solvers
+                    # to dense ...
+                    U = ops_rev[i].as_range_array()
+                    ops_rev[i+1] = VectorArrayOperator(ops_rev[i+1].apply(U))
+                    del ops_rev[i]
+                else:
+                    i += 1
+            else:
+                i += 1
+
+        op = op.with_(operators=ops_rev[::-1])
 
         return op
 
