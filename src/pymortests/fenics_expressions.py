6236703f7e98e0b4c19eea27a3715fc3babc1719
Peter Oehme <oehme.pb@gmail.com>
2022-02-19 12:24:47 +0100

    avoid runnning tests without fenics

--- src/pymortests/fenics_expressions.py
+++ src/pymortests/fenics_expressions.py
@@ -2,10 +2,17 @@ from pymortests.base import runmodule
 import numpy as np
 from pymor.core.config import config
 from pymor.analyticalproblems.expressions import parse_expression
+import os
+import pytest
 
 
 def test_fenics_expression_vectorized():
-    from dolfin import UnitSquareMesh
+    try:
+        from dolfin import UnitSquareMesh
+    except ImportError as ie:
+        if not os.environ.get('DOCKER_PYMOR', False):
+            pytest.skip('skipped test due to missing fenics')
+        raise ie
     e = parse_expression('[1., 2.] + [x[0], x[1]]', {'x': 2})
     mesh = UnitSquareMesh(10,10)
     f_fenics, f_fenics_params = e.to_fenics(mesh)
@@ -16,7 +23,12 @@ def test_fenics_expression_vectorized():
 
 
 def test_fenics_expression_vectorized():
-    from dolfin import UnitSquareMesh
+    try:
+        from dolfin import UnitSquareMesh
+    except ImportError as ie:
+        if not os.environ.get('DOCKER_PYMOR', False):
+            pytest.skip('skipped test due to missing fenics')
+        raise ie
     e = parse_expression('sin([x[0], x[1]])', {'x': 2})
     mesh = UnitSquareMesh(10,10)
     f_fenics, f_fenics_params = e.to_fenics(mesh)
@@ -27,7 +39,12 @@ def test_fenics_expression_vectorized():
 
 
 def test_fenics_expression_scalar():
-    from dolfin import UnitSquareMesh
+    try:
+        from dolfin import UnitSquareMesh
+    except ImportError as ie:
+        if not os.environ.get('DOCKER_PYMOR', False):
+            pytest.skip('skipped test due to missing fenics')
+        raise ie
     e = parse_expression('x[0] + sin(x[1])', {'x': 2})
     mesh = UnitSquareMesh(10,10)
     f_fenics, f_fenics_params = e.to_fenics(mesh)
@@ -38,5 +55,4 @@ def test_fenics_expression_scalar():
 
 
 if __name__ == "__main__":
-    if config.HAVE_FENICS: # this just does not need to run without fenics
-        runmodule(filename=__file__)
+    runmodule(filename=__file__)
