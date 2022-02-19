c77cd8a1e8b7000afd043158f3b1916132601d07
Peter Oehme <oehme.pb@gmail.com>
2022-02-19 10:07:49 +0100

    add case for literals in expressions

--- src/pymortests/fenics_expressions.py
+++ src/pymortests/fenics_expressions.py
@@ -6,6 +6,17 @@ from pymor.analyticalproblems.expressions import parse_expression
 
 def test_fenics_expression_vectorized():
     from dolfin import UnitSquareMesh
+    e = parse_expression('[1., 2.] + [x[0], x[1]]', {'x': 2})
+    mesh = UnitSquareMesh(10,10)
+    f_fenics, f_fenics_params = e.to_fenics(mesh)
+    eval = f_fenics.item()([5., 2.])
+    val = [1. + 5., 2. + 2.]
+
+    assert np.allclose(eval, val)
+
+
+def test_fenics_expression_vectorized():
+    from dolfin import UnitSquareMesh
     e = parse_expression('sin([x[0], x[1]])', {'x': 2})
     mesh = UnitSquareMesh(10,10)
     f_fenics, f_fenics_params = e.to_fenics(mesh)
