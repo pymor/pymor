22ed7739eaafbec9cfe61755b6fe7b5d68537c3b
René Fritze <rene.fritze@wwu.de>
2022-02-24 13:50:08 +0100

    fixup! fixup! fixup! mpi log to file

--- .ci/gitlab/test_mpi.bash
+++ .ci/gitlab/test_mpi.bash
@@ -3,6 +3,7 @@
 THIS_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ; pwd -P )"
 source ${THIS_DIR}/common_test_setup.bash
 
+sudo apt update && sudo apt install -y mmv
 # as a workaround intermittent MPI finalize errors which we
 # cannot seem to directly affect, we save the intermediate
 # pytest exit code in a file and check that afterwards
@@ -12,6 +13,8 @@ xvfb-run -a mpirun --output-filename mpi_logs --report-state-on-timeout \
   --get-stack-traces --timestamp-output --tag-output --timeout 1200 \
   --mca btl self,vader -n ${RANKS} python -u -m coverage run --rcfile=setup.cfg \
   --parallel-mode src/pymortests/mpi_run_demo_tests.py || true
+
+mmv mpi_logs/\*/rank.\*/\* mpi_log.rank_#2.#3.txt
 
 for fn in ./.mpirun_*/pytest.mpirun.success ; do
   [[ "$(cat ${fn})" == "True" ]] || exit 127
