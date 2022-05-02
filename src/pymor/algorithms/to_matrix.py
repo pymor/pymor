bb98fa15b8e39fdeb62266bdc5e1d7bb6510b49d
Art Pelling <a.pelling@tu-berlin.de>
2022-05-02 19:15:27 +0200

    [algorithms.to_matrix] add matching rule for MoebiusTransform

--- src/pymor/algorithms/to_matrix.py
+++ src/pymor/algorithms/to_matrix.py
@@ -9,6 +9,7 @@ import scipy.sparse.linalg as spsla
 
 from pymor.algorithms.rules import RuleTable, match_class
 from pymor.core.config import config
+from pymor.models.transforms import MoebiusTransform
 from pymor.operators.block import BlockOperatorBase
 from pymor.operators.constructions import (AdjointOperator, ComponentProjectionOperator, ConcatenationOperator,
                                            IdentityOperator, LincombOperator, LowRankOperator, LowRankUpdatedOperator,
@@ -46,6 +47,10 @@ class ToMatrixRules(RuleTable):
     def __init__(self, format, mu):
         super().__init__()
         self.__auto_init(locals())
+
+    @match_class(MoebiusTransform)
+    def action_MoebiusTransform(self, op):
+        return op.coefficients.reshape(2, 2)
 
     @match_class(NumpyHankelOperator)
     def action_NumpyHankelOperator(self, op):
