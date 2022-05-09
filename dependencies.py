feb76055d0734b8d124525b9e53186b50835d22e
Felix Schindler <felix.schindler@wwu.de>
2022-05-09 12:21:58 +0200

    require dune-xt[visualisation]>=2022.1.4 and dune-gdt>=2022.1.1

--- dependencies.py
+++ dependencies.py
@@ -55,8 +55,8 @@ install_suggests = {
     'torch': 'PyTorch open source machine learning framework',
     'jupyter_contrib_nbextensions': 'modular collection of jupyter extensions',
     'pillow': 'image library used for bitmap data functions',
-    'dune-gdt>=2021.1.3': 'generic discretization toolbox',
-    'dune-xt[visualisation]>=2021.1.3': 'DUNE extensions for dune-gdt',
+    'dune-gdt>=2022.1.1': 'generic discretization toolbox',
+    'dune-xt[visualisation]>=2022.1.4': 'DUNE extensions for dune-gdt',
 }
 io_requires = ['pyevtk', 'xmljson', 'meshio>=4.4', 'lxml', 'gmsh']
 install_suggests.update({p: 'optional File I/O support libraries' for p in io_requires})
