11b6eaecd1bb8da79401866a329d8c87027a2f0e
René Fritze <rene.fritze@wwu.de>
2022-05-16 12:08:40 +0200

    [deps] force newest pyside2

--- dependencies.py
+++ dependencies.py
@@ -9,7 +9,7 @@ _PYTEST = 'pytest>=6.0'
 # 5.12.* blocked due to https://bugreports.qt.io/browse/PYSIDE-1004
 # however the problem is not actually fixed in 5.12.3 as advertised,
 # but only starting from 5.13.1
-_PYSIDE = 'PySide2!=5.15.2,!=5.15.2.*,!=5.11.*,!=5.12.*,!=5.13.0'
+_PYSIDE = 'PySide2>=5.15.2.1'
 
 
 def _numpy_scipy():
