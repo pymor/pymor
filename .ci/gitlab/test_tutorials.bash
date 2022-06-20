0699985e65e9ca1df7adc912ef03f76dce389aee
René Fritze <rene.fritze@wwu.de>
2022-06-20 16:28:47 +0200

    [ci] simplify tutorial testing
    and fix coverage generation

--- .ci/gitlab/test_tutorials.bash
+++ .ci/gitlab/test_tutorials.bash
@@ -3,6 +3,11 @@
 THIS_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ; pwd -P )"
 source ${THIS_DIR}/common_test_setup.bash
 
-xvfb-run -a py.test ${COMMON_PYTEST_OPTS} docs/test_tutorials.py
+for fn in ${PYMOR_ROOT}/docs/source/tutorial*md ; do
+  mystnb-to-jupyter -o "${fn}" "${fn/tutorial/..\/converted_tutorial}".ipynb
+done
+
+xvfb-run -a py.test ${COMMON_PYTEST_OPTS} -s --cov= -p no:pycharm \
+  docs/test_tutorials.py
 coverage xml
 
