ef7bd15028f2b88c34972cba79a7661d7ea0df95
Stephan Rave <stephanrave@uni-muenster.de>
2022-07-13 17:21:46 +0200

    [tests] use skip_if_missing in fenics_expressions.py

--- src/pymortests/fenics_expressions.py
+++ src/pymortests/fenics_expressions.py
@@ -1,17 +1,14 @@
 from pymortests.base import runmodule
 import numpy as np
 from pymor.analyticalproblems.expressions import parse_expression
+from pymortests.base import skip_if_missing
 import os
 import pytest
 
 
+@skip_if_missing('FENICS')
 def test_fenics_expression_vectorized():
-    try:
-        from dolfin import UnitSquareMesh
-    except ImportError as ie:
-        if not os.environ.get('DOCKER_PYMOR', False):
-            pytest.skip('skipped test due to missing fenics')
-        raise ie
+    from dolfin import UnitSquareMesh
     e = parse_expression('[1., 2.] + [x[0], x[1]]', {'x': 2})
     mesh = UnitSquareMesh(10, 10)
     f_fenics, f_fenics_params = e.to_fenics(mesh)
@@ -21,13 +18,9 @@ def test_fenics_expression_vectorized():
     assert np.allclose(eval, val)
 
 
+@skip_if_missing('FENICS')
 def test_fenics_expression_vectorized2():
-    try:
-        from dolfin import UnitSquareMesh
-    except ImportError as ie:
-        if not os.environ.get('DOCKER_PYMOR', False):
-            pytest.skip('skipped test due to missing fenics')
-        raise ie
+    from dolfin import UnitSquareMesh
     e = parse_expression('sin([x[0], x[1]])', {'x': 2})
     mesh = UnitSquareMesh(10, 10)
     f_fenics, f_fenics_params = e.to_fenics(mesh)
@@ -37,13 +30,9 @@ def test_fenics_expression_vectorized2():
     assert np.allclose(eval, val)
 
 
+@skip_if_missing('FENICS')
 def test_fenics_expression_scalar():
-    try:
-        from dolfin import UnitSquareMesh
-    except ImportError as ie:
-        if not os.environ.get('DOCKER_PYMOR', False):
-            pytest.skip('skipped test due to missing fenics')
-        raise ie
+    from dolfin import UnitSquareMesh
     e = parse_expression('x[0] + sin(x[1])', {'x': 2})
     mesh = UnitSquareMesh(10, 10)
     f_fenics, f_fenics_params = e.to_fenics(mesh)
