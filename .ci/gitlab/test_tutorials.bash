603458654def71a7901cdf1a2783c205913f640d
René Fritze <rene.fritze@wwu.de>
2022-08-03 08:56:47 +0200

    [ci|tutorials] fix notebook plugin missing

--- .ci/gitlab/test_tutorials.bash
+++ .ci/gitlab/test_tutorials.bash
@@ -8,7 +8,8 @@ for fn in ${PYMOR_ROOT}/docs/source/tutorial*md ; do
   mystnb-to-jupyter -o "${fn}" "${fn/tutorial/..\/converted_tutorial}".ipynb
 done
 
+# manually add plugins to load that are excluded for other runs
 xvfb-run -a py.test ${COMMON_PYTEST_OPTS} -s --cov= -p no:pycharm \
-  docs/test_tutorials.py
+  -p nb_regression -p notebook docs/test_tutorials.py
 coverage xml
 
