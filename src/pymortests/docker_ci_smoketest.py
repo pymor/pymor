31146f92fbe7ed1f183a3477685e91e208b7cb8c
René Fritze <rene.fritze@wwu.de>
2022-09-01 16:09:15 +0200

    fixup! xfail dune

--- src/pymortests/docker_ci_smoketest.py
+++ src/pymortests/docker_ci_smoketest.py
@@ -13,17 +13,21 @@ from pymor.core.config import config, _PACKAGES
 @pytest.mark.skipif(condition=not os.environ.get('DOCKER_PYMOR', False),
                     reason='Guarantee only valid in the docker container')
 def test_config(pkg):
+    if 'DUNE' in pkg:
+        pytest.xfail('DUNE currently not installed due to broken dependencies')
     assert getattr(config, f'HAVE_{pkg}')
 
 
 @pytest.mark.skipif(condition=not os.environ.get('DOCKER_PYMOR', False),
                     reason='Guarantee only valid in the docker container')
+@pytest.mark.xfail(True, reason='DUNE currently not installed due to broken dependencies')
 def test_no_dune_warnings():
     _test_dune_import_warn()
 
 
 @pytest.mark.skipif(condition=not os.environ.get('DOCKER_PYMOR', False),
                     reason='Guarantee only valid in the docker container')
+@pytest.mark.xfail(True, reason='DUNE currently not installed due to broken dependencies')
 def test_dune_warnings(monkeypatch):
     from dune import xt, gdt
     monkeypatch.setattr(gdt, "__version__", "2020.0.0")
