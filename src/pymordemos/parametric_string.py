3fe1a7baf48fafec7331161d4e643de7914ecfba
Petar Mlinarić <mlinaric@vt.edu>
2022-10-20 11:50:04 -0400

    [demos] reduce the number of figures, refactoring

--- src/pymordemos/parametric_string.py
+++ src/pymordemos/parametric_string.py
@@ -8,7 +8,6 @@ import scipy.sparse as sps
 import matplotlib.pyplot as plt
 from typer import Argument, run
 
-from pymor.core.config import config
 from pymor.core.logger import set_log_levels
 from pymor.models.iosys import SecondOrderModel
 from pymor.operators.numpy import NumpyMatrixOperator
@@ -19,7 +18,7 @@ from pymor.reductors.mt import MTReductor
 from pymor.reductors.sobt import (SOBTpReductor, SOBTvReductor, SOBTpvReductor, SOBTvpReductor,
                                   SOBTfvReductor, SOBTReductor)
 from pymor.reductors.sor_irka import SORIRKAReductor
-from pymordemos.parametric_heat import run_mor_method_param
+from pymordemos.parametric_heat import fom_properties_param, run_mor_method_param
 
 
 def main(
@@ -27,7 +26,12 @@ def main(
         r: int = Argument(5, help='Order of the ROMs.'),
 ):
     """Parametric string example."""
-    set_log_levels({'pymor.algorithms.gram_schmidt.gram_schmidt': 'WARNING'})
+    set_log_levels({
+        'pymor.algorithms.gram_schmidt.gram_schmidt': 'WARNING',
+        'pymor.algorithms.lradi.solve_lyap_lrcf': 'WARNING',
+        'pymor.reductors.basic.LTIPGReductor': 'WARNING',
+    })
+    plt.rcParams['axes.grid'] = True
 
     # Assemble M, D, K, B, C_p
     assert n % 2 == 1, 'The order has to be an odd integer.'
@@ -57,67 +61,21 @@ def main(
 
     so_sys = SecondOrderModel(Mop, Eop, Kop, Bop, Cpop)
 
-    print(f'order of the model = {so_sys.order}')
-    print(f'number of inputs   = {so_sys.dim_input}')
-    print(f'number of outputs  = {so_sys.dim_output}')
-
-    mu_list = [1, 5, 10]
+    mus = [1, 5, 10]
     w = np.logspace(-3, 2, 200)
-
-    # System poles
-    fig, ax = plt.subplots()
-    for mu in mu_list:
-        poles = so_sys.poles(mu=mu)
-        ax.plot(poles.real, poles.imag, '.', label=fr'$\mu = {mu}$')
-    ax.set_title('System poles')
-    ax.legend()
-    plt.show()
-
-    # Magnitude plots
-    fig, ax = plt.subplots()
-    for mu in mu_list:
-        so_sys.transfer_function.mag_plot(w, ax=ax, mu=mu, label=fr'$\mu = {mu}$')
-    ax.set_title('Magnitude plot of the full model')
-    ax.legend()
-    plt.show()
-
-    # "Hankel" singular values
-    fig, ax = plt.subplots(2, 2, figsize=(12, 8), sharey=True, constrained_layout=True)
-    for mu in mu_list:
-        psv = so_sys.psv(mu=mu)
-        vsv = so_sys.vsv(mu=mu)
-        pvsv = so_sys.pvsv(mu=mu)
-        vpsv = so_sys.vpsv(mu=mu)
-        ax[0, 0].semilogy(range(1, len(psv) + 1), psv, '.-', label=fr'$\mu = {mu}$')
-        ax[0, 1].semilogy(range(1, len(vsv) + 1), vsv, '.-')
-        ax[1, 0].semilogy(range(1, len(pvsv) + 1), pvsv, '.-')
-        ax[1, 1].semilogy(range(1, len(vpsv) + 1), vpsv, '.-')
-    ax[0, 0].set_title('Position singular values')
-    ax[0, 1].set_title('Velocity singular values')
-    ax[1, 0].set_title('Position-velocity singular values')
-    ax[1, 1].set_title('Velocity-position singular values')
-    fig.legend(loc='upper center', ncol=len(mu_list))
-    plt.show()
-
-    # System norms
-    for mu in mu_list:
-        print(f'mu = {mu}:')
-        print(f'    H_2-norm of the full model:    {so_sys.h2_norm(mu=mu):e}')
-        if config.HAVE_SLYCOT:
-            print(f'    H_inf-norm of the full model:  {so_sys.hinf_norm(mu=mu):e}')
-        print(f'    Hankel-norm of the full model: {so_sys.hankel_norm(mu=mu):e}')
+    fom_properties_param(so_sys, w, mus)
 
     # Model order reduction
-    run_mor_method_param(so_sys, r, w, mu_list, SOBTpReductor, 'SOBTp')
-    run_mor_method_param(so_sys, r, w, mu_list, SOBTvReductor, 'SOBTv')
-    run_mor_method_param(so_sys, r, w, mu_list, SOBTpvReductor, 'SOBTpv')
-    run_mor_method_param(so_sys, r, w, mu_list, SOBTvpReductor, 'SOBTvp')
-    run_mor_method_param(so_sys, r, w, mu_list, SOBTfvReductor, 'SOBTfv')
-    run_mor_method_param(so_sys, r, w, mu_list, SOBTReductor, 'SOBT')
-    run_mor_method_param(so_sys, r, w, mu_list, SORIRKAReductor, 'SOR-IRKA')
-    run_mor_method_param(so_sys.to_lti(), r, w, mu_list, BTReductor, 'BT')
-    run_mor_method_param(so_sys.to_lti(), r, w, mu_list, IRKAReductor, 'IRKA')
-    run_mor_method_param(so_sys.to_lti(), r, w, mu_list, MTReductor, 'MT')
+    run_mor_method_param(so_sys, r, w, mus, SOBTpReductor, 'SOBTp')
+    run_mor_method_param(so_sys, r, w, mus, SOBTvReductor, 'SOBTv')
+    run_mor_method_param(so_sys, r, w, mus, SOBTpvReductor, 'SOBTpv')
+    run_mor_method_param(so_sys, r, w, mus, SOBTvpReductor, 'SOBTvp')
+    run_mor_method_param(so_sys, r, w, mus, SOBTfvReductor, 'SOBTfv')
+    run_mor_method_param(so_sys, r, w, mus, SOBTReductor, 'SOBT')
+    run_mor_method_param(so_sys, r, w, mus, SORIRKAReductor, 'SOR-IRKA')
+    run_mor_method_param(so_sys.to_lti(), r, w, mus, BTReductor, 'BT')
+    run_mor_method_param(so_sys.to_lti(), r, w, mus, IRKAReductor, 'IRKA')
+    run_mor_method_param(so_sys.to_lti(), r, w, mus, MTReductor, 'MT')
 
 
 if __name__ == "__main__":
