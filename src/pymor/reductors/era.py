19a1abcbffd6581c103c6d5c1d7d4fdfb4ceeb1c
Art Pelling <a.pelling@tu-berlin.de>
2022-11-09 17:16:18 +0100

    [era] use pymor caching and rename to _sv_U_V

--- src/pymor/reductors/era.py
+++ src/pymor/reductors/era.py
@@ -6,60 +6,57 @@ import scipy.linalg as spla
 
 from pymor.algorithms.projection import project
 from pymor.algorithms.to_matrix import to_matrix
-from pymor.core.base import BasicObject
+from pymor.core.cache import cached, CacheableObject
 from pymor.models.iosys import LTIModel
 from pymor.operators.numpy import NumpyHankelOperator, NumpyMatrixOperator
 
 
-class ERAReductor(BasicObject):
+class ERAReductor(CacheableObject):
+    cache_region = 'memory'
+
     def __init__(self, data, sampling_time):
         assert sampling_time > 0
-        self.sampling_time = sampling_time
         self.H = NumpyHankelOperator(data)
-        self._input_projector_cache = None
-        self._output_projector_cache = None
-        self._SVD_cache = {}
+        self.__auto_init(locals())
 
-    def _output_projector(self, l1):
+    @cached
+    def _s1_W1(self):
+        self.logger.info('Computing output SVD ...')
+        W1, s1, _ = spla.svd(np.hstack(self.H.markov_parameters), full_matrices=False)
+        return s1, W1
+
+    @cached
+    def _s2_W2(self):
+        self.logger.info('Computing input SVD ...')
+        _, s2, W2 = spla.svd(np.vstack(self.H.markov_parameters), full_matrices=False)
+        return s2, W2.conj().T
+
+    @cached
+    def _sv_U_V(self, l1, l2):
+        H = self.H
+        if l1:
+            W1 = self.output_projector(l1)
+            self.logger.info('Projecting Markov parameters ...')
+            H = NumpyHankelOperator(W1.conj().T @ H.markov_parameters)
+        if l2:
+            W2 = self.input_projector(l2)
+            self.logger.info('Projecting Markov parameters ...')
+            H = NumpyHankelOperator(H.markov_parameters @ W2)
+
+        self.logger.info(f'Computing SVD of the {"projected " if l1 or l2 else ""}Hankel matrix ...')
+        U, sv, V = spla.svd(to_matrix(H), full_matrices=False)
+
+        return sv, U.T, V
+
+    def output_projector(self, l1):
         self.logger.info(f'Constructing output projector ({l1} tangential directions) ...')
-        p = self.H.markov_parameters.shape[1]
-        assert isinstance(l1, int) and l1 <= p
-        if self._output_projector_cache is None:
-            W1, s1, _ = spla.svd(np.hstack(self.H.markov_parameters), full_matrices=False)
-            self._output_projector_cache = (W1, s1)
-        W1, s1 = self._output_projector_cache
-        return W1[:, :l1]
+        assert isinstance(l1, int) and l1 <= self.H.markov_parameters.shape[1]
+        return self._s1_W1()[1][:, :l1]
 
-    def _input_projector(self, l2):
+    def input_projector(self, l2):
         self.logger.info(f'Constructing input projector ({l2} tangential directions) ...')
-        m = self.H.markov_parameters.shape[2]
-        assert isinstance(l2, int) and l2 <= m
-        if self._input_projector_cache is None:
-            _, s2, W2 = spla.svd(np.vstack(self.H.markov_parameters), full_matrices=False)
-            self._input_projector_cache = (W2.conj().T, s2)
-        W2, s2 = self._input_projector_cache
-        return W2[:, :l2]
-
-    def _SVD(self, r, l1=None, l2=None):
-        key = f'{(l1,l2)}'
-        if key in self._SVD_cache.keys():
-            U, sv, Vh = self._SVD_cache[key]
-        else:
-            H = self.H
-            if l1:
-                W1 = self._output_projector(l1)
-                self.logger.info('Projecting Markov parameters ...')
-                H = NumpyHankelOperator(W1.conj().T @ H.markov_parameters)
-            if l2:
-                W2 = self._input_projector(l2)
-                self.logger.info('Projecting Markov parameters ...')
-                H = NumpyHankelOperator(H.markov_parameters @ W2)
-
-            self.logger.info(f'Computing SVD of the {"projected " if l1 or l2 else ""}Hankel matrix ...')
-            U, sv, Vh = spla.svd(to_matrix(H), full_matrices=False)
-            self._SVD_cache[key] = (U, sv, Vh)
-
-        return U[:, :r], sv[:r], Vh[:r]
+        assert isinstance(l2, int) and l2 <= self.H.markov_parameters.shape[2]
+        return self._s2_W2()[1][:, :l2]
 
     def reduce(self, r=None, l1=None, l2=None, tol=None):
         assert r is not None or tol is not None
@@ -68,22 +65,23 @@ class ERAReductor(BasicObject):
         assert l2 is None or isinstance(l2, int) and l2 <= m
         assert r is None or 0 < r <= min(self.H.range.dim * (l1 or p) / p, self.H.source.dim * (l2 or m) / m)
 
-        U, sv, Vh = self._SVD(r, l1=l1, l2=l2)
+        sv, U, V = self._sv_U_V(l1, l2)
+        sv, U, V = sv[:r], U[:r], V[:r]
 
         self.logger.info(f'Constructing reduced realization of order {r} ...')
         sqS = np.diag(np.sqrt(sv))
-        Zo = U @ sqS
+        Zo = U.T @ sqS
         A = NumpyMatrixOperator(spla.pinv(Zo[: -(l1 or p)]) @ Zo[(l1 or p):])
-        B = NumpyMatrixOperator(sqS @ Vh[:, :(l2 or m)])
+        B = NumpyMatrixOperator(sqS @ V[:, :(l2 or m)])
         C = NumpyMatrixOperator(Zo[:(l1 or p)])
 
         if l1:
             self.logger.info('Backprojecting tangential output directions ...')
-            W1 = self._output_projector(l1)
+            W1 = self.output_projector(l1)
             C = project(C, source_basis=None, range_basis=C.range.from_numpy(W1))
         if l2:
             self.logger.info('Backprojecting tangential input directions ...')
-            W2 = self._input_projector(l2)
+            W2 = self.input_projector(l2)
             B = project(B, source_basis=B.source.from_numpy(W2), range_basis=None)
 
         return LTIModel(A, B, C, sampling_time=self.sampling_time,
