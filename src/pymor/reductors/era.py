a072fb5e9b8ec83758dbe6ce04a214086e1f24bb
Art Pelling <a.pelling@tu-berlin.de>
2022-11-15 17:59:53 +0100

    [era] more docstrings

--- src/pymor/reductors/era.py
+++ src/pymor/reductors/era.py
@@ -14,11 +14,38 @@ from pymor.operators.numpy import NumpyHankelOperator, NumpyMatrixOperator
 class ERAReductor(CacheableObject):
     r"""Eigensystem Realization Algorithm reductor.
 
-    Constructs a (reduced) realization that matches the Markov parameters optimally for given order
-    by orthogonal factorization of the Hankel matrix of Markov parameters (see :cite:`K78`).
+    Constructs a (reduced) realization from a sequence of Markov parameters :math:`h_i`,
+    for :math:`i\in\{1,\,\dots,\,2s-1\}`, :math:`s\in\mathbb{N}`, by a (reduced) orthogonal
+    factorization of the Hankel matrix of Markov parameters
+
+    .. math::
+        H =
+        \begin{bmatrix}
+            h_1 & h_2 & \dots & h_s \\
+            h_2 & h_3 & \dots & h_{s+1}\\
+            \vdots & \vdots && \vdots\\
+            h_s & h_{s+1} & \dots & h_{2s-1}
+        \end{bmatrix}=U\Sigma V^T\in\mathbb{R}^{ms\times ps},
+
+    where :math:`r\leq\min\{ms,ps\}` is the reduced order. See :cite:`K78`.
+
+    In order for the identified model to be stable, the Markov parameters must satisfy
+
+    .. math::
+        h_i\rightarrow0~\text{for}~i>s.
+
+    Stability is enforced automatically and can be deactivated by setting `force_stability=False`.
 
     For a large number if inputs and/or outputs, the factorization of the Hankel matrix can be
-    accelerated by tangential projections of the Markov parameters first (see :cite:`KG16`).
+    accelerated by tangentially projecting of the Markov parameters to reduce the dimension of
+    the Hankel matrix, i.e.
+
+    .. math::
+        \hat{h}_i=W_1^Th_iW_2,
+
+    where :math:`l_1\leq p` and :math:`l_2\leq m` are the number of left and right tangential
+    directions :math:`W_1\in\mathbb{R}^{p\times l_1}` and :math:`W_2\in\mathbb{R}^{m\times l_2}`
+    are the left and right projectors, respectively. See :cite:`KG16`.
 
     Attributes
     ----------
@@ -38,7 +65,7 @@ class ERAReductor(CacheableObject):
         A positive number that denotes the sampling time of the system (in seconds).
     force_stability
         Whether the Markov parameters are zero-padded to double the length in order to enforce
-        Kung's stability assumption :cite:`K78`. Defaults to `True`.
+        Kung's stability assumption. See :cite:`K78`. Defaults to `True`.
     """
 
     cache_region = 'memory'
@@ -82,16 +109,41 @@ class ERAReductor(CacheableObject):
         return sv, U.T, V
 
     def output_projector(self, l1):
+        """Construct the left/output projector :math:`W_1`."""
         assert isinstance(l1, int) and l1 <= self.data.shape[1]
         self.logger.info(f'Constructing output projector ({l1} tangential directions) ...')
         return self._s1_W1()[1][:, :l1]
 
     def input_projector(self, l2):
+        """Construct the right/input projector :math:`W_2`."""
         assert isinstance(l2, int) and l2 <= self.data.shape[2]
         self.logger.info(f'Constructing input projector ({l2} tangential directions) ...')
         return self._s2_W2()[1][:, :l2]
 
     def error_bounds(self, l1=None, l2=None):
+        r"""Compute the error bounds for all possible reduction orders.
+
+        Without tangential projection of the Markov parameters, the error bounds are defined as
+
+        .. math::
+            \sum_{i=1}^{2s-1}\lVert C_rA_r^{i-1}B_r-h_i\rVert_F^2\leq\sigma_{r+1}(\mathcal{H})
+            \sqrt{r+p+m},
+
+        where :math:`(A_r,B_r,C_r)` is the reduced realization of order :math:`r`,
+        :math:`h_i\in\mathbb{R}^{p\times m}` is the :math:`i`-th Markov parameter
+        and :math:`\sigma_{r+1}(\mathcal{H})` is the first neglected singular value of the
+        Hankel matrix of Markov parameters.
+
+        With tangential projection, the bound is given by
+
+        .. math::
+            \sum_{i=1}^{2s-1}\lVert C_rA_r^{i-1}B_r-h_i\rVert_F^2\leq
+            4\left(\sum_{i=l_1+1}^p\sigma_i^2(\Theta_L)+\sum_{i=l_2+1}^m\sigma_i^2(\Theta_R)\right)
+            +2\sigma_{r+1}(\mathcal{H})\sqrt{r+l_1+l_2},
+
+        where :math:`\Theta_L,\Theta_R` is the matrix of horizontally or vertically stacked Markov
+        parameters, respectively. See :cite:`KG16` (Thm. 3.4) for details.
+        """
         sv = self._sv_U_V(l1, l2)[0]
         _, p, m = self.data.shape
         err = (np.sqrt(np.arange(len(sv)) + (l2 or m) + (l1 or p)) * sv)[1:]
