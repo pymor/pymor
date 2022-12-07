8e3d9efb3346d9f353627c8f06f9d6a10b3d6c19
Tim Keil <tim.keil@wwu.de>
2022-12-07 14:41:43 +0100

    [pymortest] clean up tests

--- src/pymortests/operators/block.py
+++ src/pymortests/operators/block.py
@@ -5,8 +5,10 @@
 import numpy as np
 import scipy.linalg as spla
 
+from pymor.algorithms.projection import project
 from pymor.operators.numpy import NumpyMatrixOperator
 from pymor.operators.block import BlockOperator, BlockDiagonalOperator
+from pymor.parameters.functionals import ProjectionParameterFunctional, ExpressionParameterFunctional
 from pymor.vectorarrays.block import BlockVectorSpace
 from pymor.vectorarrays.numpy import NumpyVectorSpace
 
@@ -110,47 +112,68 @@ def test_blk_diag_apply_inverse_adjoint():
 
 
 def test_sparse():
-    # TODO: organize this test better
-    op = np.empty((4, 4), dtype=object)
+    np.random.seed(0)
+
+    # construct an operator with parametric parts outside the diagonal
+    ops = np.zeros((4, 4), dtype=object)
     for I in range(3):
-        op[I, I] = NumpyMatrixOperator(np.eye(20))
-    op[3, 3] = NumpyMatrixOperator(np.eye(10))
-    from pymor.parameters.functionals import ProjectionParameterFunctional, ExpressionParameterFunctional
-    op[0, 2] = ProjectionParameterFunctional('mu', 1, 0) * NumpyMatrixOperator(4 * np.eye(20))
-    block_op = BlockOperator(op)
-    block_op_half = BlockOperator([[op[0, 0], None], [None, op[1, 1]]])
-    assert not block_op_half.parametric
+        ops[I, I] = NumpyMatrixOperator(np.random.randn(3, 3))
+    ops[3, 3] = NumpyMatrixOperator(np.random.randn(2, 2))
+    ops[0, 2] = ProjectionParameterFunctional('mu', 1, 0) * NumpyMatrixOperator(np.random.randn(3, 3))
+
+    # call operator
+    block_op = BlockOperator(ops)
     assert block_op.parametric
+
+    # assemble with a parameter
     mu = block_op.parameters.parse([1])
     assembled_op = block_op.assemble(mu=mu)
     assert assembled_op != block_op
-    op2 = op.copy()
-    op2[1, 2] = ExpressionParameterFunctional('0*mu[0]', {'mu': 1}) * NumpyMatrixOperator(4 * np.eye(20))
-    block_op_2 = BlockOperator(op2)
-    (0.5 * block_op + 0.5 * block_op_2).assemble(mu)
-    block_op_2_dense = BlockOperator(op2)
-    (0.5 * block_op + 0.5 * block_op_2_dense).assemble(mu)
+
+    # construct another operator
+    ops2 = ops.copy()
+    ops2[1, 2] = ExpressionParameterFunctional('0*mu[0]', {'mu': 1}) * NumpyMatrixOperator(np.random.randn(3, 3))
+    block_op_2 = BlockOperator(ops2)
+
+    # densify
+    block_op_2_dense = block_op_2.to_dense()
+    dense_block_op = block_op.to_dense()
+    sparse_block_op = dense_block_op.to_sparse()
     ones = block_op.range.ones()
+    a = dense_block_op.apply2(ones, ones, mu)
+    b = block_op.apply2(ones, ones, mu)
+    c = sparse_block_op.apply2(ones, ones, mu)
+    assert a == b == c
+
+    # assembly
+    assembled_sum = (0.5 * block_op + 0.5 * block_op_2).assemble(mu)
+    assembled_sum_2 = (0.5 * block_op + 0.5 * block_op_2_dense).assemble(mu)
+    a = block_op.apply2(ones, ones, mu)
+    b = block_op_2.apply2(ones, ones, mu)
+    c = assembled_sum.apply2(ones, ones, mu)
+    d = assembled_sum_2.apply2(ones, ones, mu)
+    # all have the same result
+    assert a == b == c == d
+
+    # other methods
     block_op.apply(ones, mu=mu)
     block_op.apply_adjoint(ones, mu=mu)
     block_op.d_mu('something')
     block_op.H
-    dense_block_op = block_op.to_dense()
-    sparse_block_op = dense_block_op.to_sparse()
-    dense_block_op.assemble(mu=mu)
-    a = dense_block_op.as_source_array(mu=mu).to_numpy()
-    b = block_op.as_source_array(mu=mu).to_numpy()
-    c = sparse_block_op.as_source_array(mu=mu).to_numpy()
-    assert np.allclose(a, b, c)
-    a = dense_block_op.as_range_array(mu=mu).to_numpy()
-    b = block_op.as_range_array(mu=mu).to_numpy()
-    c = sparse_block_op.as_range_array(mu=mu).to_numpy()
-    assert np.allclose(a, b, c)
-    assert block_op.apply2(ones, ones, mu=mu) == dense_block_op.apply2(ones, ones, mu=mu)
-    block_diag_1 = BlockDiagonalOperator([op[0, 0], op[0, 2]])
-    block_diag_2 = BlockDiagonalOperator([op2[0, 0], op2[1, 2]])
+
+    # call as a list
+    block_op_half = BlockOperator([[ops[0, 0], None], [None, ops[1, 1]]])
+    assert not block_op_half.parametric
+    # call with (data, coords) functionality
+    block_op_ = BlockOperator((block_op.blocks.data, block_op.blocks.coords))
+    assert block_op.apply2(ones, ones, mu=mu) == block_op_.apply2(ones, ones, mu=mu)
+
+    # BlockDiagOperator
+    block_diag_1 = BlockDiagonalOperator([ops[0, 0], ops[0, 2]])
+    block_diag_2 = BlockDiagonalOperator([ops2[0, 0], ops2[1, 2]])
     (block_diag_1 + block_diag_2).assemble(mu)
-    from pymor.algorithms.projection import project
+
+    # projection
     projected_block = project(block_op, block_op.source.ones(), block_op.source.ones())
     projected_block_dense = project(dense_block_op, block_op.source.ones(), block_op.source.ones())
     projected_block.assemble(mu).matrix == projected_block_dense.assemble(mu).matrix
