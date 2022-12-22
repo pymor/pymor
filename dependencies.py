141e4b3630898018823d36bbb76fc47e1f033658
Hendrik Kleikamp <hendrik.kleikamp@uni-muenster.de>
2022-12-22 16:26:32 +0100

    [setup] set lower bound for scipy version

--- dependencies.py
+++ dependencies.py
@@ -20,7 +20,7 @@ def _numpy_scipy():
         'numpy>=1.19.4;python_version >= "3.9"',
     ]
     scipys = [
-        'scipy>=1.3;python_version < "3.8"',
+        'scipy>=1.5.0;python_version < "3.8"',
         'scipy>=1.3.3;python_version == "3.8"',
         'scipy>=1.5.4;python_version >= "3.9"',
     ]
