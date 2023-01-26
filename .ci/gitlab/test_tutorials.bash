fd253e8c522787b483cedf41359d574faa98e455
René Fritze <rene.fritze@wwu.de>
2023-01-26 10:48:31 +0100

    [ci] workaround for missing coverage data

--- .ci/gitlab/test_tutorials.bash
+++ .ci/gitlab/test_tutorials.bash
@@ -10,4 +10,7 @@ source ${THIS_DIR}/common_test_setup.bash
 xvfb-run -a pytest ${COMMON_PYTEST_OPTS} --nb-coverage -s -p no:pycharm \
   -p nb_regression --cov=pymor -p notebook docs/test_tutorials.py
 
+# for unknown reasons, coverage is missing `pymor/__init__.py` in the report
+# this is a workaround
+coverage run --append .ci/gitlab/import_pymor_for_coverage.py
 _coverage_xml
