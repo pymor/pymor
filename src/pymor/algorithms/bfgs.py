3ce720912446b80f53be7c7af1a5ace81f3cf405
Peter Oehme <oehme.pb@gmail.com>
2023-02-08 11:49:34 +0100

    [tr] refactor and remove stages

--- src/pymor/algorithms/bfgs.py
+++ src/pymor/algorithms/bfgs.py
@@ -48,8 +48,8 @@ def get_active_and_inactive_sets(parameter_space, mu, epsilon=1e-8):
     return active, inactive
 
 
-def project_hessian(hessian, direction, active, inactive):
-    """Project the Hessian matrix using an active and an inactive set.
+def compute_hessian_action_matrix(hessian, direction, active, inactive):
+    """Project the Hessian matrix applied to the direction using an active and an inactive set.
 
     Parameters
     ----------
@@ -61,10 +61,16 @@ def project_hessian(hessian, direction, active, inactive):
         The active set computed from the |ParameterSpace|.
     inactive
         The inactive set computed from the |ParameterSpace|.
+
+    Returns
+    -------
+    hessian
+        The projection of the Hessian applied to the direction.
     """
     direction_A = np.multiply(active, direction)
     direction_I = np.multiply(inactive, direction)
-    return direction_A + np.multiply(inactive, hessian.dot(direction_I))
+    hessian = direction_A + np.multiply(inactive, hessian.dot(direction_I))
+    return hessian
 
 
 def update_hessian(hessian, mu, old_mu, gradient, old_gradient):
@@ -82,6 +88,11 @@ def update_hessian(hessian, mu, old_mu, gradient, old_gradient):
         The current gradient with respect to the parameter.
     old_gradient
         The previous gradient with respect to the parameter.
+
+    Returns
+    -------
+    hessian
+        The next BFGS Hessian matrix iterate.
     """
     gradient_difference = gradient - old_gradient
     mu_difference = mu - old_mu
@@ -104,8 +115,8 @@ def update_hessian(hessian, mu, old_mu, gradient, old_gradient):
     return hessian
 
 
-@defaults('miniter', 'maxiter', 'atol', 'tol_sub', 'stagnation_window', 'stagnation_threshold')
-def bfgs(model, parameter_space, initial_guess=None, miniter=0, maxiter=100, atol=1e-16,
+@defaults('miniter', 'maxiter', 'rtol', 'tol_sub', 'stagnation_window', 'stagnation_threshold')
+def bfgs(model, parameter_space, initial_guess=None, miniter=0, maxiter=100, rtol=1e-16,
          tol_sub=1e-8, line_search_params=None, stagnation_window=3, stagnation_threshold=np.inf,
          error_aware=False, error_criterion=None, beta=None, radius=None, return_stages=False):
     """BFGS algorithm.
@@ -124,13 +135,14 @@ def bfgs(model, parameter_space, initial_guess=None, miniter=0, maxiter=100, ato
         The |ParameterSpace| for enforcing the box constraints on the parameter `mu`.
     initial_guess
         If not `None`, a |Mu| instance of length 1 containing an initial guess for the
-        solution `mu`.
+        solution `mu`. Otherwise, a random parameter from the parameter space is chosen
+        as the initial value.
     miniter
         Minimum amount of iterations to perform.
     maxiter
         Fail if the iteration count reaches this value without converging.
-    atol
-        Finish when the absolute error measure is below this threshold.
+    rtol
+        Finish when the relative error measure is below this threshold.
     tol_sub
         Finish when the clipped parameter error measure is below this threshold.
     line_search_params
@@ -144,15 +156,14 @@ def bfgs(model, parameter_space, initial_guess=None, miniter=0, maxiter=100, ato
         If `True`, perform an additional error aware check during the line search phase.
         Intended for use with the trust region algorithm. Requires the parameters `beta`
         and `radius` to be set.
+    error_criterion
+        The additional error criterion used to check model confidence in the line search.
     beta
         Intended for use with the trust region algorithm. Indicates the factor for checking
         if the current value is close to the trust region boundary. See `error_aware`.
     radius
         Intended for use with the trust region algorithm. Indicates the radius of the
         current trust region. See `error_aware`.
-    return_stages
-        If `True`, return a `list` of the intermediate parameter values of `mu` after
-        each iteration.
 
     Returns
     -------
@@ -161,10 +172,14 @@ def bfgs(model, parameter_space, initial_guess=None, miniter=0, maxiter=100, ato
     data
         Dict containing the following fields:
 
-            :mus:             `list` of parameters after each iteration.
-            :mu_norms:        |NumPy array| of the parameter norms after each iteration.
-            :update_norms:    |NumPy array| of the norms of the update vectors for each iteration.
-            :stages:          See `return_stages`.
+            :mus:                       `list` of parameters after each iteration.
+            :foc_norms:                 |NumPy array| of the first order criticity norms
+                                        after each iteration.
+            :update_norms:              |NumPy array| of the norms of the update vectors
+                                        after each iteration.
+            :iterations:                Number of total BFGS iterations.
+            :line_search_iterations:    |NumPy array| of the number of line search
+                                        iterations per BFGS iteration.
 
     Raises
     ------
@@ -188,9 +203,6 @@ def bfgs(model, parameter_space, initial_guess=None, miniter=0, maxiter=100, ato
         assert error_criterion is not None and beta is not None and radius is not None
         assert callable(error_criterion)
 
-    if return_stages:
-        stages = []
-
     gradient = model.parameters.parse(model.output_d_mu(mu)).to_numpy()
     current_output = output(mu)
     eps = np.linalg.norm(gradient)
@@ -208,11 +220,11 @@ def bfgs(model, parameter_space, initial_guess=None, miniter=0, maxiter=100, ato
     iteration = 0
     while True:
         if iteration >= miniter:
-            if output_diff < atol:
-                logger.info(f'Absolute tolerance of {atol} for output error reached. Converged.')
+            if output_diff < rtol:
+                logger.info(f'Relative tolerance of {rtol} for output difference reached. Converged.')
                 break
-            if mu_diff < atol:
-                logger.info(f'Absolute tolerance of {atol} for parameter difference reached. Converged.')
+            if mu_diff < rtol:
+                logger.info(f'Relative tolerance of {rtol} for parameter difference reached. Converged.')
                 break
             if first_order_criticity < tol_sub:
                 logger.info(f'Absolute tolerance of {tol_sub} for first order criticity reached. Converged.')
@@ -234,17 +246,13 @@ def bfgs(model, parameter_space, initial_guess=None, miniter=0, maxiter=100, ato
 
         iteration += 1
 
-        # store convergence history
-        if iteration > 0 and return_stages:
-            stages.append(model.parameters.parse(mu))
-
         active, inactive = get_active_and_inactive_sets(parameter_space, mu, eps)
 
         # compute update to mu
         if sum(inactive) == 0.:
             direction = -gradient
         else:
-            direction = project_hessian(hessian, -gradient, active, inactive)
+            direction = compute_hessian_action_matrix(hessian, -gradient, active, inactive)
 
         if error_aware:
             step_size, line_search_iteration = armijo(
@@ -253,13 +261,12 @@ def bfgs(model, parameter_space, initial_guess=None, miniter=0, maxiter=100, ato
         else:
             step_size, line_search_iteration = armijo(output, mu, direction, grad=gradient,
                                initial_value=current_output, **(line_search_params or {}))
-        line_search_iterations.append(line_search_iteration + 1)
+        line_search_iterations.append(line_search_iteration)
 
         # update mu
         old_mu = mu.copy()
         old_output = current_output.copy()
-        mu_update = step_size * direction
-        mu += mu_update
+        mu += step_size * direction
         mu = parameter_space.clip(mu).to_numpy()
         current_output = output(mu)
         data['mus'].append(mu.copy())
@@ -276,8 +283,7 @@ def bfgs(model, parameter_space, initial_guess=None, miniter=0, maxiter=100, ato
         foc_norms.append(first_order_criticity)
 
         # set new active inactive threshhold
-        eps_update_mu = mu - gradient
-        eps = np.linalg.norm(eps_update_mu - mu)
+        eps = np.linalg.norm(gradient)
 
         # update relative errors
         output_diff = abs(old_output - current_output) / abs(old_output)
@@ -301,7 +307,5 @@ def bfgs(model, parameter_space, initial_guess=None, miniter=0, maxiter=100, ato
     data['foc_norms'] = np.array(foc_norms)
     data['iterations'] = iteration
     data['line_search_iterations'] = np.array(line_search_iterations)
-    if return_stages:
-        data['stages'] = np.array(stages)
 
     return mu, data
