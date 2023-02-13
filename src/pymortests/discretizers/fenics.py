2113490c88b90cd32c0a0d9d98d211399a02eca5
Stephan Rave <stephanrave@uni-muenster.de>
2023-02-13 12:04:44 +0100

    [tests] ensure the fenics discretizer test does not fail
    for analyticalproblems which contain functions that cannot be converted
    to an UFL expression.

--- src/pymortests/discretizers/fenics.py
+++ src/pymortests/discretizers/fenics.py
@@ -9,7 +9,12 @@ def test_discretize(analytical_problem):
     from pymor.discretizers.fenics.cg import discretize_stationary_cg
     if not isinstance(analytical_problem, StationaryProblem):
         pytest.skip('fenics does not provide InstationaryProblem discretization')
-    discretize_stationary_cg(analytical_problem)
+    try:
+        discretize_stationary_cg(analytical_problem)
+    except NotImplementedError as e:
+        if 'Conversion to UFL' in str(e):
+            pytest.skip('Problem contains functions that cannot be converted to UFL expression.')
+        raise
 
 
 if __name__ == '__main__':
