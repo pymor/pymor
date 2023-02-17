08520154d62e3e00e804438ecf3812faf1536fd3
Stephan Rave <stephanrave@uni-muenster.de>
2023-02-17 21:16:36 +0100

    [ci] fix pytest args

--- .ci/gitlab/test_vanilla.bash
+++ .ci/gitlab/test_vanilla.bash
@@ -4,5 +4,7 @@ THIS_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ; pwd -P )"
 source ${THIS_DIR}/common_test_setup.bash
 # this runs in pytest in a fake, auto numbered, X Server
 #xvfb-run -a pytest ${COMMON_PYTEST_OPTS} --hypothesis-show-statistics
-xvfb-run -a pytest ${COMMON_PYTEST_OPTS}
+echo ${PYMOR_PYTEST_EXTRA}
+echo ${COMMON_PYTEST_OPTS}
+eval xvfb-run -a pytest ${COMMON_PYTEST_OPTS}
 _coverage_xml
