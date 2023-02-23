38a6bd9e37803a7b0a8f473cac4ed5cb43e0eb36
Stephan Rave <stephanrave@uni-muenster.de>
2023-02-23 10:08:26 +0100

    [gui] remove progress_bar

--- src/pymor/discretizers/builtin/gui/jupyter/__init__.py
+++ src/pymor/discretizers/builtin/gui/jupyter/__init__.py
@@ -10,8 +10,6 @@ To use these routines you first have to execute ::
 
 inside the given notebook.
 """
-import IPython
-
 from pymor.core.config import config
 from pymor.core.defaults import defaults
 
@@ -34,60 +32,6 @@ def get_visualizer(backend='py3js'):
     else:
         from pymor.discretizers.builtin.gui.jupyter.matplotlib import visualize_patch
         return visualize_patch
-
-
-def progress_bar(sequence, every=None, size=None, name='Parameters'):
-    from ipywidgets import HTML, IntProgress, VBox
-
-    # c&p from https://github.com/kuk/log-progress
-    is_iterator = False
-    if size is None:
-        try:
-            size = len(sequence)
-        except TypeError:
-            is_iterator = True
-    if size is not None:
-        if every is None:
-            if size <= 200:
-                every = 1
-            else:
-                every = int(size / 200)     # every 0.5%
-    else:
-        assert every is not None, 'sequence is iterator, set every'
-
-    if is_iterator:
-        progress = IntProgress(min=0, max=1, value=1)
-        progress.bar_style = 'info'
-    else:
-        progress = IntProgress(min=0, max=size, value=0)
-    label = HTML()
-    box = VBox(children=[label, progress])
-    IPython.display.display(box)
-
-    index = 0
-    try:
-        for index, record in enumerate(sequence, 1):
-            if index == 1 or index % every == 0:
-                if is_iterator:
-                    label.value = '{name}: {index} / ?'.format(
-                        name=name,
-                        index=index
-                    )
-                else:
-                    progress.value = index
-                    label.value = u'{name}: {index} / {size}'.format(
-                        name=name,
-                        index=index,
-                        size=size
-                    )
-            yield record
-    except:
-        progress.bar_style = 'danger'
-        raise
-    else:
-        progress.bar_style = 'success'
-        progress.value = index
-        label.value = f"{name}: {str(index or '?')}"
 
 
 def load_ipython_extension(ipython):
