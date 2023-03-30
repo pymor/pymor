3bd39bfacb9c9c3de1deccfbb0ef56bf3e38d21c
Jonas Nicodemus <44996273+Jonas-Nicodemus@users.noreply.github.com>
2023-03-30 14:36:01 -0400

    Update src/pymor/reductors/ph/ph_irka.py
    Co-authored-by: Petar Mlinarić <mlinaric@vt.edu>

--- src/pymor/reductors/ph/ph_irka.py
+++ src/pymor/reductors/ph/ph_irka.py
@@ -81,7 +81,7 @@ class PHIRKAReductor(GenericIRKAReductor):
             Reduced-order |PHLTIModel|.
         """
         one_sided_irka_reductor = OneSidedIRKAReductor(self.fom.to_lti(), 'V')
-        _ = one_sided_irka_reductor.reduce(rom0_params=rom0_params, tol=tol, maxit=maxit, num_prev=num_prev,
+        _ = one_sided_irka_reductor.reduce(rom0_params, tol=tol, maxit=maxit, num_prev=num_prev,
                                            projection=projection, conv_crit=conv_crit, compute_errors=compute_errors)
 
         self._pg_reductor = PHLTIPGReductor(self.fom, one_sided_irka_reductor.V)
