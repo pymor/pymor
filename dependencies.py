5d70d43a10b0ec8ba27af39e2a784017c52fc8f7
Stephan Rave <stephanrave@uni-muenster.de>
2023-04-14 17:26:42 +0200

    [packaging] fix linter errors

--- dependencies.py
+++ dependencies.py
@@ -3,12 +3,13 @@
 # Copyright pyMOR developers and contributors. All rights reserved.
 # License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
 
+import os
 import platform
+
 if not platform.python_version_tuple()[:2] == ('3', '10'):
     raise RuntimeError('Call this script with Python 3.10 (newest version tested in CI)')
 
 # update requirements files using pip-compile
-import os
 os.system('pip-compile --resolver backtracking -o requirements.txt')
 os.system('pip-compile --resolver backtracking --extra ci -o requirements-ci.txt')
 os.system('pip-compile --resolver backtracking --extra all --extra ci -o requirements-optional.txt')
