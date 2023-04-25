64b42b2b10b0230703db03ce67e73b472498d237
Stephan Rave <stephanrave@uni-muenster.de>
2023-04-25 17:44:14 +0200

    [ci] add torch back to ci image

--- .ci/update_requirements_ci.bash
+++ .ci/update_requirements_ci.bash
@@ -3,6 +3,6 @@
 echo "Updating requirements-ci.txt ..."
 
 pip-compile --resolver backtracking --extra ci --extra docs --extra io \
-	--extra ipyparallel --extra optional --extra compiled \
+	--extra ipyparallel --extra ann --extra optional --extra compiled \
 	--extra-index-url https://download.pytorch.org/whl/cpu \
 	-o requirements-ci.txt
