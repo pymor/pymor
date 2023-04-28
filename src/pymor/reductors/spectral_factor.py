3a2de93296a28307854d228a1f9ca84e8f888007
Steffen Müller <arxisos@gmail.com>
2023-04-28 17:32:34 +0200

    [reductors.spectral_factor] Add more debugging statements.
    It seems like the problem is that IRKA does not return a stable system
    for the spectral factor?

--- src/pymor/reductors/spectral_factor.py
+++ src/pymor/reductors/spectral_factor.py
@@ -2,7 +2,6 @@
 # Copyright pyMOR developers and contributors. All rights reserved.
 # License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
 
-from scipy.linalg import solve
 import numpy as np
 
 from pymor.core.base import BasicObject
@@ -13,6 +12,7 @@ from pymor.algorithms.to_matrix import to_matrix
 from pymor.algorithms.lyapunov import _chol
 from pymor.operators.numpy import NumpyMatrixOperator
 from pymor.reductors.h2 import IRKAReductor
+from pymor.reductors.bt import BTReductor
 
 class SpectralFactorReductor(BasicObject):
     r"""
@@ -33,7 +33,7 @@ class SpectralFactorReductor(BasicObject):
         A = to_matrix(self.fom.A, format='dense')
         B = to_matrix(self.fom.B, format='dense')
         C = to_matrix(self.fom.C, format='dense')
-        R = to_matrix(self.fom.D + self.fom.D.H, 'dense')
+        D = to_matrix(self.fom.D, format='dense')
         print('[spectralFactor] A')
         print(A)
 
@@ -47,7 +47,7 @@ class SpectralFactorReductor(BasicObject):
         print(X)
         
         # Compute Cholesky-like factorization of W(X)
-        M = _chol(R).T
+        M = _chol(D+D.T).T
         # TODO Alternatives to taking matrix inverse?
         L = np.linalg.solve(M.T, C-B.T@X)
         print('[spectralFactor] L')
@@ -58,6 +58,9 @@ class SpectralFactorReductor(BasicObject):
         print(np.linalg.norm(L.T@L-LTL)/np.linalg.norm(LTL))
         print('[spectralFactor] M')
         print(M)
+        print('[spectralFactor] Relative LTM error')
+        LTM = C.T - X@B
+        print(np.linalg.norm(L.T@M-LTM)/np.linalg.norm(LTM))
 
         spectral_factor = LTIModel(self.fom.A, self.fom.B,
             NumpyMatrixOperator(L, source_id=self.fom.A.range.id),
@@ -66,13 +69,26 @@ class SpectralFactorReductor(BasicObject):
         # TODO Allow to set other reductor or reductor options from outside
         irka = IRKAReductor(spectral_factor, self.mu)
         spectral_factor_reduced = irka.reduce(r)
+        # bt = BTReductor(spectral_factor, self.mu)
+        # spectral_factor_reduced = bt.reduce(r, projection='sr')
+
+        spectralH2err = spectral_factor_reduced - spectral_factor
+        print('[spectralFactor] Relative H2 error of spectral factor: '
+            f'{spectralH2err.h2_norm() / spectral_factor.h2_norm():.3e}')
+
         Ar = to_matrix(spectral_factor_reduced.A, format='dense')
         Br = to_matrix(spectral_factor_reduced.B, format='dense')
         Lr = to_matrix(spectral_factor_reduced.C, format='dense')
         Mr = to_matrix(spectral_factor_reduced.D, format='dense')
 
-        # TODO Add skew-symmetric part of `Mr`
-        Dr = 0.5*(Mr.T @ Mr)
+        ev = np.max(np.real(np.linalg.eig(Ar)[0]))
+        if ev > 0:
+            print(f'[spectralFactor] Warning: reduced spectral factor not stable. {ev}')
+
+        print('[spectralFactor] Mr')
+        print(Mr)
+
+        Dr = 0.5*(Mr.T @ Mr) + 0.5*(D-D.T)
 
         Xr = solve_cont_lyap_dense(A=Ar, E=None, B=Lr, trans=True)
         Cr = Br.T @ Xr + Mr.T @ Lr
