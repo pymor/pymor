5f3461fee6792aa773a2b257a0e95ba900c48029
Peter Oehme <oehme.pb@gmail.com>
2023-04-29 12:37:10 +0200

    [ph dmd] include licence statements

--- src/pymor/algorithms/phdmd.py
+++ src/pymor/algorithms/phdmd.py
@@ -1,3 +1,7 @@
+# This file is part of the pyMOR project (https://www.pymor.org).
+# Copyright pyMOR developers and contributors. All rights reserved.
+# License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
+
 import numpy as np
 from scipy.linalg import eigh, lstsq, svd
 from scipy.sparse import diags
