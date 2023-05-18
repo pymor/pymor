00ed7848f036f94f2f3e066f61bf64b856b34b78
Petar Mlinarić <mlinaric@vt.edu>
2023-05-18 14:42:15 -0400

    [parameters.base] some improvements

--- src/pymor/parameters/base.py
+++ src/pymor/parameters/base.py
@@ -540,14 +540,13 @@ class ParameterSpace(ParametricObject):
         -------
         List of |parameter value| dicts.
         """
-        if isinstance(counts, dict):
-            pass
-        else:
+        if not isinstance(counts, dict):
             counts = {k: counts for k in self.parameters}
 
-        linspaces = tuple(np.linspace(self.ranges[k][0], self.ranges[k][1], num=counts[k]) for k in self.parameters)
-        iters = tuple(product(ls, repeat=max(0, np.zeros(sps).size))
-                      for ls, sps in zip(linspaces, self.parameters.values()))
+        linspaces = tuple(np.linspace(self.ranges[k][0], self.ranges[k][1], num=counts[k])
+                          for k in self.parameters)
+        iters = tuple(product(linspace, repeat=size)
+                      for linspace, size in zip(linspaces, self.parameters.values()))
         return [Mu((k, np.array(v)) for k, v in zip(self.parameters, i))
                 for i in product(*iters)]
 
@@ -565,8 +564,8 @@ class ParameterSpace(ParametricObject):
         -------
         The sampled |parameter values|.
         """
-        get_param = lambda: Mu(((k, get_rng().uniform(self.ranges[k][0], self.ranges[k][1], size))
-                               for k, size in self.parameters.items()))
+        get_param = lambda: Mu((k, get_rng().uniform(self.ranges[k][0], self.ranges[k][1], size))
+                               for k, size in self.parameters.items())
         if count is None:
             return get_param()
         else:
@@ -589,16 +588,14 @@ class ParameterSpace(ParametricObject):
         if not isinstance(counts, dict):
             counts = {k: counts for k in self.parameters}
 
-        logspaces = tuple(np.geomspace(self.ranges[k][0],
-                                       self.ranges[k][1],
-                                       num=counts[k])
+        logspaces = tuple(np.geomspace(self.ranges[k][0], self.ranges[k][1], num=counts[k])
                           for k in self.parameters)
-        iters = tuple(product(ls, repeat=max(0, np.zeros(sps).size))
-                      for ls, sps in zip(logspaces, self.parameters.values()))
+        iters = tuple(product(logspace, repeat=size)
+                      for logspace, size in zip(logspaces, self.parameters.values()))
         return [Mu((k, np.array(v)) for k, v in zip(self.parameters, i))
                 for i in product(*iters)]
 
-    def sample_logarithmic_randomly(self, count=None, pool_multiplier=100):
+    def sample_logarithmic_randomly(self, count=None):
         """Logarithmically scaled random sample |parameter values| from the space.
 
         Parameters
@@ -607,19 +604,15 @@ class ParameterSpace(ParametricObject):
             If `None`, a single dict `mu` of |parameter values| is returned.
             Otherwise, the number of logarithmically random samples to generate and return as
             a list of |parameter values| dicts.
-        pool_multiplier
-            Determines how big of a pool the samples are selected.
 
         Returns
         -------
         The sampled |parameter values|.
         """
-        def get_param():
-            return Mu(((k, np.random.choice(np.geomspace(self.ranges[k][0],
-                                                         self.ranges[k][1],
-                                                         pool_multiplier*size),
-                                            size=size,replace=False))
-                      for k, size in self.parameters.items()))
+        get_param = lambda: Mu((k, np.exp(get_rng().uniform(np.log(self.ranges[k][0]),
+                                                            np.log( self.ranges[k][1]),
+                                                            size)))
+                               for k, size in self.parameters.items())
         if count is None:
             return get_param()
         else:
