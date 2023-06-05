42649b761cab8bca823017718d0500546ad39468
Hendrik Kleikamp <hendrik.kleikamp@uni-muenster.de>
2023-06-05 14:56:30 +0200

    [docs] load font awesome css file during sphinx build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -187,6 +187,8 @@ html_sidebars = {
 #    'index': 'indexcontent.html',
 # }
 
+html_css_files = ['https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0/css/all.min.css']
+
 # If false, no module index is generated.
 html_use_modindex = True
 
