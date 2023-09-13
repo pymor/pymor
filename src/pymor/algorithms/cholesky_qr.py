d3ce675b8aaab89e2c3c1ff22e8c100639fc1ca1
Art Pelling <a.pelling@tu-berlin.de>
2023-09-13 18:24:51 +0200

    refactor

--- src/pymor/algorithms/cholesky_qr.py
+++ src/pymor/algorithms/cholesky_qr.py
@@ -9,8 +9,8 @@ from pymor.core.logger import getLogger
 logger = getLogger('pymor.algorithms.cholesky_qr.cholesky_qr')
 
 
-def _chol_qr(V, product=None, gramian=None, check_finite=True):
-    X = V.gramian(product=product) if gramian is None else gramian
+def _chol_qr(A, product=None, gramian=None, check_finite=True):
+    X = A.gramian(product=product) if gramian is None else gramian
 
     while True:
         try:
@@ -18,42 +18,42 @@ def _chol_qr(V, product=None, gramian=None, check_finite=True):
             break
         except spla.LinAlgError:
             logger.warning('Cholesky factorization broke down! Matrix is ill-conditioned.')
-            m, n = V.dim, len(V)
+            m, n = A.dim, len(A)
             eps = np.finfo(X.dtype).eps
             s = m*n+n*(n+1) if product is None else 2*m*np.sqrt(m*n)+n*(n+1)*np.sqrt(np.abs(eigs(product, k=1)[0][0]))
             s *= 11*eps*spla.norm(X, ord=2, check_finite=check_finite)
             logger.info(f'Applying shift: {s}')
             np.fill_diagonal(X, np.diag(X)+s)
 
-    return V.lincomb(spla.lapack.dtrtri(R)[0].T), R
+    return A.lincomb(spla.lapack.dtrtri(R)[0].T), R
 
 
-def cholesky_qr(V, product=None, maxiter=3, tol=None, check_finite=True, return_R=True, copy=True):
+def cholesky_qr(A, product=None, maxiter=3, tol=None, check_finite=True, return_R=True, copy=True):
     if copy:
-        V = V.copy()
+        A = A.copy()
 
     iter = 1
     with logger.block(f'Iteration {iter}'):
-        Q, R = _chol_qr(V, product=product, gramian=None, check_finite=check_finite)
+        A, R = _chol_qr(A, product=product, gramian=None, check_finite=check_finite)
 
-    gramian = None
     while iter < maxiter:
         iter += 1
-
-        if tol is not None:
-            gramian = Q.gramian(product=product)
-            res = spla.norm(gramian - np.eye(len(V)), ord='fro', check_finite=check_finite)
+        if tol is None:
+            X = None
+        else:
+            X = A.gramian(product=product)
+            res = spla.norm(X - np.eye(len(A)), ord='fro', check_finite=check_finite)
             logger.info(f'Residual = {res}')
-            if res <= tol*np.sqrt(len(V)):
+            if res <= tol*np.sqrt(len(A)):
                 break
 
         with logger.block(f'Iteration {iter}'):
-            Q, S = _chol_qr(Q, product=product, gramian=gramian, check_finite=check_finite)
+            A, S = _chol_qr(A, product=product, gramian=X, check_finite=check_finite)
 
             if return_R:
                 spla.blas.dtrmm(1, S, R, overwrite_b=True)
 
     if return_R:
-        return Q, R
+        return A, R
     else:
-        return Q
+        return A
