8a3b8af8edf064dcb0efeb2bb3417f665f9bb98b
niklasreich <niklas.reich@gmx.net>
2023-09-21 14:56:57 +0200

    deleted unnessecary code

--- src/pymordemos/nonlinear_reaction.py
+++ src/pymordemos/nonlinear_reaction.py
@@ -12,6 +12,7 @@ parameters = Parameters({'reaction': 2})
 diffusion = ConstantFunction(1,2)
 
 diameter = 1/40
+this_mu = [10, 10]
 
 nonlinear_reaction_coefficient = ConstantFunction(1,2)
 test_nonlinearreaction = ExpressionFunction('reaction[0] * (exp(reaction[1] * u[0]) - 1) / reaction[1]', dim_domain = 1, parameters = parameters, variable = 'u')
@@ -22,11 +23,12 @@ grid, boundary_info = discretize_domain_default(problem.domain, diameter=diamete
 print('Anzahl Element', grid.size(0))
 print('Anzahl DoFs', grid.size(2))
 fom, data = discretizer(problem, diameter = diameter)
-u= newton(fom.operator, fom.rhs.as_range_array(), mu = problem.parameters.parse([0.01, 0.01]))[0]
+u = fom.solve(this_mu)
+# u= newton(fom.operator, fom.rhs.as_range_array(), mu = problem.parameters.parse([0.01, 0.01]))[0]
 fom.visualize(u, title = 'cg')
 # u= newton(fom.operator, fom.rhs.as_range_array(), mu = problem.parameters.parse([10, 10]))[0]
 # fom.visualize(u, title = 'cg')
 problem_fv = StationaryProblem(domain = domain, rhs = l, diffusion = diffusion, nonlinear_reaction = test_nonlinearreaction, nonlinear_reaction_derivative = test_nonlinearreaction_derivative)
 fom_fv, data_fv = discretize_stationary_fv(problem_fv, diameter = diameter)
-u_fv = fom_fv.solve([0.01, 0.01])
+u_fv = fom_fv.solve(this_mu)
 fom_fv.visualize(u_fv, title = 'fv')
