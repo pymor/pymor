eb7e8b49e77a63a434040e56a3fcb12abdf039a8
Petar Mlinarić <mlinaric@vt.edu>
2023-10-07 18:10:38 -0400

    [test.algorithms.riccati] test matrix change in lrcf

--- src/pymortests/algorithms/riccati.py
+++ src/pymortests/algorithms/riccati.py
@@ -7,6 +7,7 @@ from itertools import chain, product
 import numpy as np
 import pytest
 import scipy.linalg as spla
+import scipy.sparse as sps
 
 from pymor.algorithms.lyapunov import _chol
 from pymor.algorithms.riccati import solve_pos_ricc_lrcf, solve_ricc_dense, solve_ricc_lrcf
@@ -138,13 +139,23 @@ def test_ricc_lrcf(n, m, p, with_E, with_R, with_S, trans, solver):
     if with_S and (solver.startswith('pymess') or solver == 'lrradi'):
         pytest.xfail('solver not implemented')
 
+    mat_old = []
+    mat_new = []
     if not with_E:
         A = conv_diff_1d_fd(n, 1, 1)
         E = None
     else:
         A, E = conv_diff_1d_fem(n, 1, 1)
+        mat_old.append(E.copy())
+        mat_new.append(E)
+    mat_old.append(A.copy())
+    mat_new.append(A)
     B = np.random.randn(n, m)
+    mat_old.append(B.copy())
+    mat_new.append(B)
     C = np.random.randn(p, n)
+    mat_old.append(C.copy())
+    mat_new.append(C)
     D = np.random.randn(p, m)
     if not trans:
         R0 = np.random.randn(p, p)
@@ -154,6 +165,12 @@ def test_ricc_lrcf(n, m, p, with_E, with_R, with_S, trans, solver):
         R0 = np.random.randn(m, m)
         R = D.T.dot(D) + R0.dot(R0.T) if with_R else None
         S = 1e-1 * C.T @ D if with_S else None
+    if with_R:
+        mat_old.append(R.copy())
+        mat_new.append(R)
+    if with_S:
+        mat_old.append(S.copy())
+        mat_new.append(S)
 
     Aop = NumpyMatrixOperator(A)
     Eop = NumpyMatrixOperator(E) if with_E else None
@@ -167,6 +184,13 @@ def test_ricc_lrcf(n, m, p, with_E, with_R, with_S, trans, solver):
 
     Z = Zva.to_numpy().T
     assert relative_residual(A, E, B, C, R, S, Z, trans) < 1e-8
+
+    for mat1, mat2 in zip(mat_old, mat_new):
+        assert type(mat1) == type(mat2)
+        if sps.issparse(mat1):
+            mat1 = mat1.toarray()
+            mat2 = mat2.toarray()
+        assert np.all(mat1 == mat2)
 
 
 @pytest.mark.parametrize('n', n_list_small)
@@ -182,13 +206,23 @@ def test_pos_ricc_lrcf(n, m, p, with_E, with_R, with_S, trans, solver):
     if with_S and solver.startswith('pymess'):
         pytest.xfail('solver not implemented')
 
+    mat_old = []
+    mat_new = []
     if not with_E:
         A = conv_diff_1d_fd(n, 1, 1)
         E = None
     else:
         A, E = conv_diff_1d_fem(n, 1, 1)
+        mat_old.append(E.copy())
+        mat_new.append(E)
+    mat_old.append(A.copy())
+    mat_new.append(A)
     B = np.random.randn(n, m)
+    mat_old.append(B.copy())
+    mat_new.append(B)
     C = np.random.randn(p, n)
+    mat_old.append(C.copy())
+    mat_new.append(C)
     D = np.random.randn(p, m)
     if not trans:
         R0 = np.random.randn(p, p)
@@ -198,6 +232,12 @@ def test_pos_ricc_lrcf(n, m, p, with_E, with_R, with_S, trans, solver):
         R0 = np.random.randn(m, m)
         R = D.T.dot(D) + 10 * R0.dot(R0.T) if with_R else None
         S = np.random.randn(n,m) if with_S else None
+    if with_R:
+        mat_old.append(R.copy())
+        mat_new.append(R)
+    if with_S:
+        mat_old.append(S.copy())
+        mat_new.append(S)
 
     Aop = NumpyMatrixOperator(A)
     Eop = NumpyMatrixOperator(E) if with_E else None
@@ -213,3 +253,10 @@ def test_pos_ricc_lrcf(n, m, p, with_E, with_R, with_S, trans, solver):
     if not with_R:
         R = np.eye(p if not trans else m)
     assert relative_residual(A, E, B, C, -R, S, Z, trans) < 1e-8
+
+    for mat1, mat2 in zip(mat_old, mat_new):
+        assert type(mat1) == type(mat2)
+        if sps.issparse(mat1):
+            mat1 = mat1.toarray()
+            mat2 = mat2.toarray()
+        assert np.all(mat1 == mat2)
