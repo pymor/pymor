c2d206161fbe50fd5c8cad054425da8e4be1e365
Art Pelling <a.pelling@tu-berlin.de>
2023-10-31 19:02:37 +0100

    compute correct s for non-trivial product

--- src/pymor/algorithms/cholesky_qr.py
+++ src/pymor/algorithms/cholesky_qr.py
@@ -3,7 +3,6 @@
 import numpy as np
 import scipy.linalg as spla
 
-from pymor.algorithms.eigs import eigs
 from pymor.core.logger import getLogger
 
 logger = getLogger('pymor.algorithms.cholesky_qr.cholesky_qr')
@@ -20,10 +19,16 @@ def _chol_qr(A, product=None, gramian=None, check_finite=True):
             logger.warning('Cholesky factorization broke down! Matrix is ill-conditioned.')
             m, n = A.dim, len(A)
             eps = np.finfo(X.dtype).eps
-            s = m*n+n*(n+1) if product is None else 2*m*np.sqrt(m*n)+n*(n+1)*np.sqrt(np.abs(eigs(product, k=1)[0][0]))
-            s *= 11*eps*spla.norm(X, ord=2, check_finite=check_finite)
+            if product is None:
+                s = m*n+n*(n+1)
+                s *= 11*eps*spla.norm(X, ord=2, check_finite=check_finite)
+            else:
+                from pymor.algorithms.eigs import eigs
+                s = 2*m*np.sqrt(m*n)+n*(n+1)
+                s *= 11*eps*spla.norm(A, ord=2, check_finite=check_finite)**2
+                s *= np.sqrt(np.abs(eigs(product, k=1)[0][0]))
             logger.info(f'Applying shift: {s}')
-            np.fill_diagonal(X, np.diag(X)+s)
+            np.fill_diagonal(X, np.diag(X) + s)
 
     return A.lincomb(spla.lapack.dtrtri(R)[0].T), R
 
