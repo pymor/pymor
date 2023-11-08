81442a94199a1046c8cf3cd252efed85d73bec77
Petar Mlinarić <mlinaric@vt.edu>
2023-11-08 15:30:34 -0500

    [algorithms.qr] edit docs, change var name

--- src/pymor/algorithms/qr.py
+++ src/pymor/algorithms/qr.py
@@ -11,15 +11,25 @@ from pymor.core.exceptions import AccuracyError
 
 
 @defaults('check', 'check_orth_tol', 'check_recon_tol', 'method')
-def qr(A, product=None, offset=0,
+def qr(V, product=None, offset=0,
        check=True, check_orth_tol=1e-3, check_recon_tol=1e-3,
        copy=True, method='gram_schmidt', **kwargs):
-    r"""Compute a QR decomposition.
+    r"""Compute a thin QR decomposition.
+
+    For a |VectorArray| `V` and inner product |Operator| `product`,
+    the thin QR decomposition of `V` is given by
+    orthonormal |VectorArray| `Q` and
+    upper-triangular matrix `R` such that
+    `V == Q.lincomb(R.T)` and `len(V) == len(Q)`.
+
+    .. note::
+        If the set of vectors in :math:`V` is linearly dependent,
+        the implementation may raise an exception (see below).
 
     Parameters
     ----------
-    A
-        The |VectorArray| for which to compute the QR decomposition.
+    V
+        The |VectorArray| for which to compute the thin QR decomposition.
     product
         The inner product |Operator| w.r.t. which to orthonormalize.
         If `None`, the Euclidean product is used.
@@ -27,15 +37,16 @@ def qr(A, product=None, offset=0,
         Assume that the first `offset` vectors are already orthonormal.
         Those vectors will not be changed even when `copy` is `False`.
     check
-        If `True`, check if the resulting |VectorArray| is really orthonormal.
+        If `True`, check if the resulting `Q` is orthonormal and
+        if `Q.lincomb(R.T)` approximates `V`.
     check_orth_tol
         Tolerance for the orthogonality check:
-        :math:`\lVert Q^H P Q - I \rVert_F \leqslant \mathtt{orth_tol}`.
+        :math:`\lVert Q^H P Q - I \rVert_F \leqslant \mathtt{orth\_tol}`.
     check_recon_tol
         Tolerance for the reconstruction check:
-        :math:`\lVert A - Q R \rVert_F \leqslant \mathtt{recon_tol} \lVert A \rVert_F`.
+        :math:`\lVert V - Q R \rVert_F \leqslant \mathtt{recon\_tol} \lVert A \rVert_F`.
     copy
-        If `True`, create a copy of `A` instead of modifying `A` in-place.
+        If `True`, create a copy of `V` instead of modifying `V` in-place.
     method
         QR decomposition method (currently only `'gram_schmidt'`).
     kwargs
@@ -44,42 +55,65 @@ def qr(A, product=None, offset=0,
     Returns
     -------
     Q
-        The orthonormal |VectorArray| of the same length as `A`.
+        The orthonormal |VectorArray| of the same length as `V`.
     R
         The upper-triangular/trapezoidal matrix.
+
+    Raises
+    ------
+    RuntimeError
+        If the vectors in `V` are detected to be linearly dependent.
     """
     assert method == 'gram_schmidt'
+    len_V = len(V)
     if check:
-        A_orig = A if copy else A.copy()
-    Q, R = gram_schmidt(A, product=product, return_R=True, atol=0, rtol=0, offset=offset,
+        V_orig = V if copy else V.copy()
+    Q, R = gram_schmidt(V, product=product, return_R=True, atol=0, rtol=0, offset=offset,
                         check=False, copy=copy, **kwargs)
-    if len(Q) < len(A):
+    if len(Q) < len_V:
         raise RuntimeError('Could not construct an orthonormal basis of full length.')
     if check:
-        _check_qr(A_orig, Q, R, product, check_orth_tol, check_recon_tol)
+        _check_qr(V_orig, Q, R, product, offset, check_orth_tol, check_recon_tol)
     return Q, R
 
 
-@defaults('check', 'check_orth_tol', 'check_recon_tol', 'method')
-def rrqr(A, product=None, offset=0,
+@defaults('atol', 'rtol', 'check', 'check_orth_tol', 'check_recon_tol', 'method')
+def rrqr(V, product=None, atol=1e-13, rtol=1e-13, offset=0,
          check=True, check_orth_tol=1e-3, check_recon_tol=1e-3,
          copy=True, method='gram_schmidt', **kwargs):
-    """Compute a rank-revealing QR (RRQR) decomposition.
+    r"""Compute a rank-revealing QR (RRQR) decomposition.
+
+    For a |VectorArray| `V` and inner product |Operator| `product`,
+    a rank-revealing QR decomposition of `V` is given by any
+    orthonormal |VectorArray| `Q` and
+    upper-triangular/trapezoidal matrix `R` such that
+    the norm of `V - Q.lincomb(R.T)` is "small" and
+    `len(Q) <= len(V)`.
 
     Parameters
     ----------
-    A
-        The |VectorArray| for which to compute the QR decomposition.
+    V
+        The |VectorArray| for which to compute the rank-revealing QR decomposition.
     product
         The inner product |Operator| w.r.t. which to orthonormalize.
         If `None`, the Euclidean product is used.
+    atol
+        Absolute tolerance used to detect linearly dependent vectors
+        (exact meaning depends on the `method`).
+    rtol
+        Relative tolerance used to detect linearly dependent vectors
+        (exact meaning depends on the `method`).
     offset
         Assume that the first `offset` vectors are already orthonormal.
         Those vectors will not be changed even when `copy` is `False`.
     check
-        If `True`, check if the resulting |VectorArray| is really orthonormal.
-    check_tol
-        Tolerance for the check.
+        If `True`, check if the resulting `Q` is orthonormal and if `Q*R` approximates `A`.
+    check_orth_tol
+        Tolerance for the orthogonality check:
+        :math:`\lVert Q^H P Q - I \rVert_F \leqslant \mathtt{orth\_tol}`.
+    check_recon_tol
+        Tolerance for the reconstruction check:
+        :math:`\lVert A - Q R \rVert_F \leqslant \mathtt{recon\_tol} \lVert A \rVert_F`.
     copy
         If `True`, create a copy of `A` instead of modifying `A` in-place.
     method
@@ -90,28 +124,29 @@ def rrqr(A, product=None, offset=0,
     Returns
     -------
     Q
-        The orthonormal |VectorArray| of the length less or equal to the length of `A`.
+        The orthonormal |VectorArray| such that `len(Q) <= len(V)`.
     R
         The upper-triangular/trapezoidal matrix.
     """
     assert method == 'gram_schmidt'
     if check:
-        A_orig = A if copy else A.copy()
-    Q, R = gram_schmidt(A, product=product, return_R=True, offset=offset,
+        V_orig = V if copy else V.copy()
+    Q, R = gram_schmidt(V, product=product, return_R=True, atol=atol, rtol=rtol, offset=offset,
                         check=False, copy=copy, **kwargs)
     if check:
-        _check_qr(A_orig, Q, R, product, check_orth_tol, check_recon_tol)
+        _check_qr(V_orig, Q, R, product, offset, check_orth_tol, check_recon_tol)
     return Q, R
 
 
-def _check_qr(A, Q, R, product, check_orth_tol, check_recon_tol):
-    orth_error_matrix = Q.gramian(product) - np.eye(len(Q))
+def _check_qr(V, Q, R, product, offset, check_orth_tol, check_recon_tol):
+    orth_error_matrix = Q[offset:].inner(Q, product)
+    orth_error_matrix[:, offset:] -= np.eye(len(Q) - offset)
     if orth_error_matrix.size > 0:
-        err = spla.norm(orth_error_matrix)
-        if err > check_orth_tol:
-            raise AccuracyError(f'Q not orthonormal (orth err={err})')
-    A_norm = spla.norm(A.norm())
-    recon_err = spla.norm((A - Q.lincomb(R.T)).norm())
-    recon_err_rel = recon_err / A_norm if A_norm > 0 else 0
-    if recon_err > check_recon_tol * A_norm:
+        orth_err = spla.norm(orth_error_matrix)
+        if orth_err > check_orth_tol:
+            raise AccuracyError(f'Q not orthonormal (orth err={orth_err})')
+    V_norm = spla.norm(V.norm())
+    recon_err = spla.norm((V - Q.lincomb(R.T)).norm())
+    recon_err_rel = recon_err / V_norm if V_norm > 0 else 0
+    if recon_err > check_recon_tol * V_norm:
         raise AccuracyError(f'QR not accurate (rel recon err={recon_err_rel})')
