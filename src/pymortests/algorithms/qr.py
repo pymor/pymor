a252c937d1fa4251d0e417ea422bef5e13f5e2f8
Petar Mlinarić <mlinaric@vt.edu>
2023-11-08 15:30:56 -0500

    [tests.algorithms.qr] add more tests

--- src/pymortests/algorithms/qr.py
+++ src/pymortests/algorithms/qr.py
@@ -7,26 +7,84 @@ import pytest
 from pymor.algorithms.qr import qr, rrqr
 from pymor.vectorarrays.numpy import NumpyVectorSpace
 
-
-def test_qr_random():
-    A = NumpyVectorSpace(5).random(3)
-    Q, R = qr(A)
-    assert len(A) == len(Q)
+pytestmark = pytest.mark.builtin
 
 
-def test_qr_zeros():
-    A = NumpyVectorSpace(5).zeros(2)
+@pytest.mark.parametrize('copy', [False, True])
+def test_qr_random(copy):
+    V = NumpyVectorSpace(5).random(3)
+    Q, R = qr(V, copy=copy)
+    assert len(V) == len(Q) == 3
+
+
+@pytest.mark.parametrize('copy', [False, True])
+def test_qr_random_offset(copy):
+    V = NumpyVectorSpace(5).random(3)
+    V[0].scal(1 / V[0].norm())
+    Q, R = qr(V, offset=1, copy=copy)
+    assert len(V) == len(Q) == 3
+
+
+@pytest.mark.parametrize('copy', [False, True])
+def test_qr_ones(copy):
+    V = NumpyVectorSpace(5).ones(2)
     with pytest.raises(RuntimeError):
-        Q, R = qr(A)
+        Q, R = qr(V, copy=copy)
 
 
-def test_rrqr_random():
-    A = NumpyVectorSpace(5).random(3)
-    Q, R = rrqr(A)
-    assert len(A) == len(Q)
+@pytest.mark.parametrize('copy', [False, True])
+def test_qr_zeros(copy):
+    V = NumpyVectorSpace(5).zeros(2)
+    with pytest.raises(RuntimeError):
+        Q, R = qr(V, copy=copy)
 
 
-def test_rrqr_zeros():
-    A = NumpyVectorSpace(5).zeros(2)
-    Q, R = rrqr(A)
+@pytest.mark.parametrize('copy', [False, True])
+def test_qr_empty(copy):
+    V = NumpyVectorSpace(5).empty(0)
+    Q, R = qr(V, copy=copy)
+    assert len(V) == len(Q) == 0
+
+
+@pytest.mark.parametrize('copy', [False, True])
+def test_rrqr_random(copy):
+    V = NumpyVectorSpace(5).random(3)
+    Q, R = rrqr(V, copy=copy)
+    assert len(V) == len(Q) == 3
+
+
+@pytest.mark.parametrize('copy', [False, True])
+def test_rrqr_random_offset(copy):
+    V = NumpyVectorSpace(5).random(3)
+    V[0].scal(1 / V[0].norm())
+    Q, R = rrqr(V, offset=1, copy=copy)
+    assert len(V) == len(Q) == 3
+
+
+@pytest.mark.parametrize('copy', [False, True])
+def test_rrqr_ones(copy):
+    V = NumpyVectorSpace(5).ones(2)
+    Q, R = rrqr(V, copy=copy)
+    if copy:
+        assert len(V) == 2
+    else:
+        assert len(V) == 1
+    assert len(Q) == 1
+
+
+@pytest.mark.parametrize('copy', [False, True])
+def test_rrqr_zeros(copy):
+    V = NumpyVectorSpace(5).zeros(2)
+    Q, R = rrqr(V, copy=copy)
+    if copy:
+        assert len(V) == 2
+    else:
+        assert len(V) == 0
     assert len(Q) == 0
+
+
+@pytest.mark.parametrize('copy', [False, True])
+def test_rrqr_empty(copy):
+    V = NumpyVectorSpace(5).empty(0)
+    Q, R = rrqr(V, copy=copy)
+    assert len(V) == len(Q) == 0
