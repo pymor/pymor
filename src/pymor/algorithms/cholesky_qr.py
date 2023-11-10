f1fe3761e6fe6efc1b7a694008217a24910292e7
Art Pelling <a.pelling@tu-berlin.de>
2023-11-10 17:47:51 +0100

    refactor for readability

--- src/pymor/algorithms/cholesky_qr.py
+++ src/pymor/algorithms/cholesky_qr.py
@@ -3,57 +3,9 @@
 import numpy as np
 import scipy.linalg as spla
 
-from pymor.core.base import BasicObject
-from pymor.core.cache import CacheableObject, cached
 from pymor.core.logger import getLogger
 
 logger = getLogger('pymor.algorithms.cholesky_qr.shifted_cholqr')
-
-
-class _ProductNorm(CacheableObject):
-    cache_region = 'memory'
-
-    def __init__(self, product):
-        self.__auto_init(locals())
-
-    @cached
-    def norm(self):
-        if self.product is None:
-            return 1
-        else:
-            from pymor.algorithms.eigs import eigs
-            return np.sqrt(np.abs(eigs(self.product, k=1)[0][0]))
-
-
-class _CholeskyShifter(BasicObject):
-    def __init__(self, A, product, product_norm):
-        self.__auto_init(locals())
-
-    def compute_shift(self, X, check_finite=True):
-        m, n = self.A.dim, len(self.A)
-        if self.product is None:
-            s = m*n+n*(n+1)
-        else:
-            s = 2*m*np.sqrt(m*n)+n*(n+1)*self.product_norm.norm()
-            X = self.A.gramian(product=self.product)
-        eps = np.finfo(X.dtype).eps
-        s *= 11*eps*spla.norm(X, ord=2, check_finite=check_finite)
-        return s
-
-
-def _shifted_cholesky(X, shifter, check_finite=True):
-    """This will return the Cholesky factor and keep applying shifts if it breaks down."""
-    while True:
-        try:
-            R = spla.cholesky(X, overwrite_a=True, check_finite=check_finite)
-            break
-        except spla.LinAlgError:
-            logger.warning('Cholesky factorization broke down! Matrix is ill-conditioned.')
-            shift = shifter.compute_shift(X, check_finite=check_finite)
-            logger.info(f'Applying shift: {shift}')
-            np.fill_diagonal(X, np.diag(X) + shift)
-
-    return R
 
 
 def shifted_cholqr(A, product=None, return_R=True, maxiter=3, offset=0, check_tol=None, check_finite=True, copy=True):
@@ -124,16 +76,35 @@ def shifted_cholqr(A, product=None, return_R=True, maxiter=3, offset=0, check_to
         A = A.copy()
 
     iter = 1
-    product_norm = _ProductNorm(product)
+    shift = None
     while iter <= maxiter:
         with logger.block(f'Iteration {iter}'):
             if check_tol is None or iter == 1:
                 # compute only the necessary parts of the Gramian
                 B, X = np.split(A[offset:].inner(A, product=product), [offset], axis=1)
 
-            # compute Cholesky factor of lower right block
-            shifter = _CholeskyShifter(A[offset:], product, product_norm)
-            Rx = _shifted_cholesky(X, shifter, check_finite=check_finite)
+            # This will compute the Cholesky factor of the lower right block
+            # and keep applying shifts if it breaks down.
+            while True:
+                try:
+                    Rx = spla.cholesky(X, overwrite_a=True, check_finite=check_finite)
+                    break
+                except spla.LinAlgError:
+                    logger.warning('Cholesky factorization broke down! Matrix is ill-conditioned.')
+                    if shift is None:
+                        m, n = A.dim, len(A[offset:])
+                        if product is None:
+                            shift = m*n+n*(n+1)
+                            XX = X
+                        else:
+                            from pymor.algorithms.eigs import eigs
+                            shift = 2*m*np.sqrt(m*n)+n*(n+1)*np.sqrt(np.abs(eigs(product, k=1)[0][0]))
+                            XX = A[offset:].gramian(product=product)
+                        eps = np.finfo(XX.dtype).eps
+                        shift *= 11*eps*spla.norm(XX, ord=2, check_finite=check_finite)
+
+                    logger.info(f'Applying shift: {shift}')
+                    np.fill_diagonal(X, np.diag(X) + shift)
 
             # orthogonalize
             Rinv = spla.lapack.dtrtri(Rx)[0].T
@@ -150,7 +121,7 @@ def shifted_cholqr(A, product=None, return_R=True, maxiter=3, offset=0, check_to
                     Bi += B.T @ Rx
                     spla.blas.dtrmm(1, Rx, Ri, overwrite_b=True)
 
-            # check orthogonality
+            # check orthogonality (for an iterative algorithm)
             if check_tol is not None:
                 B, X = np.split(A[offset:].inner(A, product=product), [offset], axis=1)
                 res = spla.norm(X - np.eye(len(A) - offset), ord='fro', check_finite=check_finite)
