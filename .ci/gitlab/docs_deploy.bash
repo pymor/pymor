7977ffa9c13f5d59f0f9b9ca965dd5fdd09907c0
Stephan Rave <stephanrave@uni-muenster.de>
2024-01-11 18:00:44 +0100

    [ci] directly rsync docs to docs.pymor.org

--- .ci/gitlab/docs_deploy.bash
+++ .ci/gitlab/docs_deploy.bash
@@ -9,9 +9,6 @@ function init_ssh {
 
     eval $(ssh-agent -s)
 
-    chmod 600 "$DOCS_DEPLOY_KEY"
-    ssh-add "$DOCS_DEPLOY_KEY"
-
     chmod 600 "$DOCS_DEPLOY_KEY_ZIV"
     ssh-add "$DOCS_DEPLOY_KEY_ZIV"
 
@@ -28,38 +25,15 @@ PYMOR_ROOT="$(cd "$(dirname ${BASH_SOURCE[0]})" ; cd ../../ ; pwd -P )"
 cd "${PYMOR_ROOT}"
 
 
-REPO=git@github.com:pymor/docs.git
-REPO_DIR=${CI_PROJECT_DIR}/repo
 if [[ "x${CI_COMMIT_REF_SLUG}" == "x" ]] ; then
   CI_COMMIT_REF_SLUG=$(slugify ${CI_COMMIT_REF_NAME})
 fi
 # this must match PYMOR_ROOT/docs/source/conf.py:try_on_binder_branch
 SLUG=${CI_COMMIT_REF_SLUG/github\/PUSH_/from_fork__}
-TARGET_DIR=${REPO_DIR}/${SLUG}
-
-git clone --depth 2 ${REPO} ${REPO_DIR}
-
-rm -rf ${TARGET_DIR}
-mkdir -p ${TARGET_DIR}
 
 # we get the already built html documentation as an artefact from an earlier build stage
-rsync -a ${PYMOR_ROOT}/docs/_build/html/ ${TARGET_DIR}
-cd ${REPO_DIR}
-git config user.name "pyMOR Bot"
-git config user.email "gitlab-ci@pymor.org"
-git add ${TARGET_DIR}
-(git diff --quiet && git diff --staged --quiet) || \
-  git commit -m "Updated docs for ${CI_COMMIT_REF_NAME}"
-
-${PYMOR_ROOT}/.ci/gitlab/docs_makeindex.py ${REPO_DIR}
-git add list.html
-(git diff --quiet && git diff --staged --quiet) || \
-  git commit -m "Updated index for ${CI_COMMIT_REF_NAME}"
-
-git push || (git pull --rebase && git push )
-
-ssh docs@docs-ng.pymor.org
-
+touch ${PYMOR_ROOT}/docs/_build/html/
+rsync -a --delete ${PYMOR_ROOT}/docs/_build/html/ docs@docs-ng.pymor.org:${SLUG}
 
 ## binder
 cd "${PYMOR_ROOT}"
