401ed2f7c48b70017f8f861b6fa76838f730cfbe
Stephan Rave <stephanrave@uni-muenster.de>
2024-03-07 13:20:03 +0100

    [discretizers.builtin] make indices_from_parent_indices more readable

--- src/pymor/discretizers/builtin/grids/subgrid.py
+++ src/pymor/discretizers/builtin/grids/subgrid.py
@@ -87,10 +87,11 @@ class SubGrid(Grid):
         """
         assert 0 <= codim <= self.dim, 'Invalid codimension'
         ind = ind.ravel()
-        # TODO: Find better implementation of the following
-        R = np.argmax(ind[:, np.newaxis] - self.__parent_indices[codim][np.newaxis, :] == 0, axis=1)
-        if not np.all(self.__parent_indices[codim][R] == ind):
+        PI = self.__parent_indices[codim]
+        X, R = np.nonzero(PI[np.newaxis, :] == ind[:, np.newaxis])
+        if not len(X) == len(ind):
             raise ValueError('Not all parent indices found')
+        assert np.all(PI[R] == ind)
         return np.array(R, dtype=np.int32)
 
     def size(self, codim):
