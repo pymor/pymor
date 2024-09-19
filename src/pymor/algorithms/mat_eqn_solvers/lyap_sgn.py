2c70bf4084ed3e87f26d767f32cc9a8be64d408c
Petar Mlinarić <mlinaric@vt.edu>
2024-09-19 14:31:21 -0400

    Add dlyap_smith

--- src/pymor/algorithms/mat_eqn_solvers/lyap_sgn.py
+++ src/pymor/algorithms/mat_eqn_solvers/lyap_sgn.py
@@ -154,36 +154,7 @@ def lyap_sgn(A, G, E, maxiter=100, atol=0, rtol=None):
     lyap_sgn_ldl
     """
     # check inputs
-    assert isinstance(A, np.ndarray)
-    assert A.ndim == 2
-    assert A.shape[0] == A.shape[1]
-
-    assert isinstance(G, np.ndarray)
-    assert G.ndim == 2
-    assert G.shape[0] == G.shape[1]
-    assert G.shape[0] == A.shape[0]
-
-    assert E is None or isinstance(E, np.ndarray)
-    if E is None:
-        E = np.eye(A.shape[0])
-        hasE = False
-    else:
-        assert E.ndim == 2
-        assert E.shape[0] == E.shape[1]
-        assert E.shape[0] == A.shape[0]
-        hasE = True
-
-    n = A.shape[0]
-
-    # check and assign optional parameters
-    assert isinstance(maxiter, Integral)
-    assert maxiter >= 1
-
-    assert atol >= 0
-
-    if rtol is None:
-        rtol = 10 * n * np.finfo(np.float64).eps
-    assert rtol >= 0
+    n, E, hasE, rtol = _check_a_g_e(A, G, E, maxiter, atol, rtol)
 
     # case of empty data
     if n == 0:
@@ -415,6 +386,158 @@ def lyap_sgn_fac(A, B, E, maxiter=100, atol=0, rtol=None, ctol=None):
     }
 
     return Z, info
+
+
+def dlyap_smith(A, G, E, maxiter=100, atol=0, rtol=None):
+    """Solve continuous-time Lyapunov equation.
+
+    Computes the solution matrix of the standard discrete-time Lyapunov
+    equation
+
+        A*X*A' - X + G = 0,                                             (1)
+
+    or of the generalized Lyapunov equation
+
+        A*X*A' - E*X*E' + G = 0,                                        (2)
+
+    using the Smith iteration. It is assumed that the eigenvalues
+    of A (or s*E - A) lie inside the open unit-circle.
+    See :cite:`S16`.
+
+    Parameters
+    ----------
+    A
+        |NumPy array| with dimensions n x n in (1) or (2).
+    G
+        |NumPy array| with dimensions n x n in (1) or (2).
+    E
+        |NumPy array| with dimensions n x n in (2).
+        If `None`, the standard equation (1) is solved.
+    maxiter
+        Positive integer, maximum number of iteration steps.
+    atol
+        Nonnegative scalar, tolerance for the absolute error in the last
+        iteration step.
+    rtol
+        Nonnegative scalar, tolerance for the relative error in the last
+        iteration step.
+        If `None`, the value is `10*n*eps`.
+
+    Returns
+    -------
+    X
+        Solution matrix of (1) or (2) as a |NumPy array| with dimensions n x n.
+    info
+        Dict with the following fields:
+
+        :abs_err:
+            Vector, containing the absolute error of the iteration matrix in
+            each iteration step.
+        :rel_err:
+            Vector, containing the relative error of the iteration matrix in
+            each iteration step.
+        :num_iter:
+            Number of performed iteration steps.
+
+    See Also
+    --------
+    dlyap_smith_fac
+    dlyap_smith_ldl
+    """
+    # check inputs
+    n, E, hasE, rtol = _check_a_g_e(A, G, E, maxiter, atol, rtol)
+
+    # case of empty data
+    if n == 0:
+        X = np.empty(0)
+        info = {}
+        return X, info
+
+    # initialization
+    logger = getLogger('pymor.algorithms.mat_eqn_solver.lyap_sgn.dlyap_smith')
+    if hasE:
+        A = spla.solve(E, A)
+        X = spla.solve(E, spla.solve(E, G).T).T
+    else:
+        X = G
+    niter = 1
+    converged = False
+
+    abs_err = []
+    rel_err = []
+
+    # squared Smith iteration
+    while niter <= maxiter and not converged:
+        # construction of next solution matrix
+        AXA = A @ (X @ A.T)
+        X = X + AXA
+
+        # update of iteration matrix
+        A = A @ A
+
+        # information about current iteration step
+        abs_err.append(spla.norm(AXA))
+        rel_err.append(abs_err[-1] / spla.norm(X))
+
+        logger.info(f'step {niter:4d}, absolute error {abs_err[-1]:e}, relative error {rel_err[-1]:e}')
+
+        # method is converged if absolute or relative errors are small enough
+        converged = abs_err[-1] <= atol or rel_err[-1] <= rtol
+        niter += 1
+
+    niter -= 1
+
+    # warning if iteration not converged
+    if niter == maxiter and not converged:
+        logger.warning(
+            f'No convergence in {niter:d} iteration steps.\n'
+            f'Abs. tolerance: {atol:e}, Abs. error: {abs_err[-1]:e}\n'
+            f'Rel. tolerance: {rtol:e}, Rel. error: {rel_err[-1]:e}\n'
+            f'Try to increase the tolerances or number of iteration steps.'
+        )
+
+    # assign information about iteration
+    info = {
+        'abs_err': np.array(abs_err),
+        'rel_err': np.array(rel_err),
+        'num_iter': niter,
+    }
+
+    return X, info
+
+
+def _check_a_g_e(A, G, E, maxiter, atol, rtol):
+    assert isinstance(A, np.ndarray)
+    assert A.ndim == 2
+    assert A.shape[0] == A.shape[1]
+
+    assert isinstance(G, np.ndarray)
+    assert G.ndim == 2
+    assert G.shape[0] == G.shape[1]
+    assert G.shape[0] == A.shape[0]
+
+    assert E is None or isinstance(E, np.ndarray)
+    if E is None:
+        E = np.eye(A.shape[0])
+        hasE = False
+    else:
+        assert E.ndim == 2
+        assert E.shape[0] == E.shape[1]
+        assert E.shape[0] == A.shape[0]
+        hasE = True
+
+    n = A.shape[0]
+
+    assert isinstance(maxiter, Integral)
+    assert maxiter >= 1
+
+    assert atol >= 0
+
+    if rtol is None:
+        rtol = 10 * n * np.finfo(np.float64).eps
+    assert rtol >= 0
+
+    return n, E, hasE, rtol
 
 
 def compress_fac(Z, tol, column_compression=True):
