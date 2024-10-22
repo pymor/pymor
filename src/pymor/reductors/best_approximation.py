6b2e3ebfc79030ba8911194e3a4a926d6bddd00e
Felix Schindler <107669806+ftschindler@users.noreply.github.com>
2024-10-22 17:15:31 +0200

    examples| rebase BestApproximationReductor onto ProjectionBasedReductor

--- src/pymor/reductors/best_approximation.py
+++ src/pymor/reductors/best_approximation.py
@@ -1,57 +1,59 @@
 import numpy as np
 
-from pymor.core.base import BasicObject
+from pymor.core.defaults import defaults
 from pymor.models.interface import Model
+from pymor.reductors.basic import ProjectionBasedReductor
 from pymor.vectorarrays.interface import VectorArray
 
 from models.black_box import BlackBoxModel
 
 
-class BestApproximationReductor(BasicObject):
+class BestApproximationReductor(ProjectionBasedReductor):
     """Generic reductor using best-approximation onto a reduced basis.
 
     _Note_ that the resulting model does does not bring any computational benefits.
+
+    We achieve restriction to sub-basis by using the projected operators as a dimension tag.
     """
 
-    def __init__(self, fom: Model, basis: VectorArray = None):
+    @defaults("check_orthonormality", "check_tol")
+    def __init__(
+        self,
+        fom: Model,
+        basis: VectorArray = None,
+        check_orthonormality=True,
+        check_tol=1e-3,
+    ):
         basis = basis or fom.solution_space.empty()
         assert basis in fom.solution_space
+        super().__init__(
+            fom,
+            {"RB": basis},
+            check_orthonormality=check_orthonormality,
+            check_tol=check_tol,
+        )
         self.__auto_init(locals())
 
-    def reduce(self):
+    def project_operators(self):
+        return len(self.basis)
+
+    def project_operators_to_subbasis(self, dims):
+        dim = dims["RB"]
+        assert dim <= len(self.basis)
+        return dim
+
+    def build_rom(self, projected_operators, error_estimator):
+        dim = projected_operators
+        assert dim <= len(self.basis)
+
         def project_onto_basis(U):
             # See https://docs.pymor.org/2024-1-2/tutorial_basis_generation.html#a-trivial-reduced-basis
-            G = self.basis.gramian()
-            R = self.basis.inner(U)
+            G = self.basis[:dim].gramian()
+            R = self.basis[:dim].inner(U)
             return np.linalg.solve(G, R).T
 
         return BlackBoxModel(
-            len(self.basis),
+            dim,
             self.fom.parameters,
             lambda mu: project_onto_basis(self.fom.solve(mu)),
-            name=f"{self.fom.name}_BestApproxReduced",
         )
-
-    def extend_basis(
-        self,
-        U: VectorArray,
-        method="gram_schmidt",
-        pod_modes=1,
-        pod_orthonormalize=True,
-        copy_U=True,
-    ):
-        from pymor.reductors.basic import extend_basis as _extend_basis
-
-        assert U in self.fom.solution_space
-        _extend_basis(
-            U,
-            self.basis,
-            product=None,
-            method=method,
-            pod_modes=pod_modes,
-            pod_orthonormalize=pod_orthonormalize,
-            copy_U=copy_U,
-        )
-
-    def reconstruct(self, u: VectorArray):
-        return self.basis.lincomb(u.to_numpy())
