220a7c13096ef4f20ba763b2a31322cfb749a062
Stephan Rave <stephanrave@uni-muenster.de>
2024-12-11 09:49:43 +0100

    Revert "trigger ci"
    This reverts commit eea63d1f556888debb3adc2c8a70ad0249a8dd16.

--- src/pymortests/demos.py
+++ src/pymortests/demos.py
@@ -26,6 +26,7 @@ runner = CliRunner()
 
 
 DISCRETIZATION_ARGS = (
+    ('elliptic', [0, 0, 0, 0]),
     ('elliptic', [1, 2, 0, 3]),
     ('elliptic', ['--rect', 1, 2, 0, 3]),
     ('elliptic', [0, 0, 2, 1]),
