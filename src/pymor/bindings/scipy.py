16fdbe936e651a3cfa94e7cc1c5697d6faf510d4
Stephan Rave <stephanrave@uni-muenster.de>
2024-12-18 14:52:21 +0100

    warn if using gesdd instead of gesvd

--- src/pymor/bindings/scipy.py
+++ src/pymor/bindings/scipy.py
@@ -27,6 +27,13 @@ def svd_lapack_driver(driver='gesvd_unless_win_mkl'):
     assert driver in {'gesvd', 'gesdd', 'gesvd_unless_win_mkl'}
     if driver == 'gesvd_unless_win_mkl':
         if is_windows_platform() and is_scipy_mkl():
+            from warnings import warn
+            warn('Your SciPy installation seems to be using MKL as LAPACK library.\n'
+                 'To avoid stability issues on Windows, we use gesdd instead of gesvd\n'
+                 'for SVD computation. This may lead to reduced numerical accuracy.\n'
+                 'See https://github.com/pymor/pymor/issues/2391 for further discuson.\n'
+                 'To silence this warning, set the pymor.bindings.scipy.svd_lapack_driver.driver\n'
+                 'default to either "gesvd" or "gesdd".')
             return 'gesdd'
         else:
             return 'gesvd'
