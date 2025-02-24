ad0b5bd879c2a9caef9c969ccca6b9449c43e5ab
Stephan Rave <stephanrave@uni-muenster.de>
2025-02-24 11:05:09 +0100

    make benchmarks compatible with great_transposition branch

--- benchmarks/algorithms.py
+++ benchmarks/algorithms.py
@@ -2,15 +2,14 @@
 # Copyright pyMOR developers and contributors. All rights reserved.
 # License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
 
-import numpy as np
-
 from pymor.algorithms.gram_schmidt import gram_schmidt
 from pymor.algorithms.svd_va import method_of_snapshots
+from pymor.tools.random import new_rng
 
 
 class VectorArrayAlgorithm:
     version = 1
-    timeout = 600
+    timeout = 3600
 
     param_names = ('va_impl', 'dim', 'count')
     params = (['numpy', 'list'], [10**k for k in range(1, 7)], [10, 100])
@@ -19,10 +18,9 @@ class VectorArrayAlgorithm:
         from pymor.vectorarrays.list import NumpyListVectorSpace
         from pymor.vectorarrays.numpy import NumpyVectorSpace
 
-        rng = np.random.default_rng(42)
-        data = rng.random((count, dim))
-        space = NumpyVectorSpace if va_impl == 'numpy' else NumpyListVectorSpace
-        self.array = space.from_numpy(data)
+        Space = NumpyVectorSpace if va_impl == 'numpy' else NumpyListVectorSpace
+        with new_rng(42):
+            self.array = Space(dim).random(count)
 
     def time_gram_schmidt(self, va_impl, dim, count):
         gram_schmidt(self.array, copy=False)
