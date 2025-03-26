57310235235f3a040299ac9a8521db3625652e06
Hendrik Kleikamp <hendrik.kleikamp@uni-muenster.de>
2025-03-26 15:11:27 +0100

    [timestepping] allow VectorArrays as rhs in time stepping methods

--- src/pymortests/algorithms/timestepping.py
+++ src/pymortests/algorithms/timestepping.py
@@ -1,22 +1,73 @@
 import numpy as np
 
 from pymor.algorithms.timestepping import DiscreteTimeStepper, ImplicitEulerTimeStepper, ImplicitMidpointTimeStepper
-from pymor.models.examples import heat_equation_non_parametric_example
+from pymor.models.examples import heat_equation_example, heat_equation_non_parametric_example
 
-nt = 10
-initial_time = 0.
-end_time = 1.
-fom = heat_equation_non_parametric_example(diameter=1., nt=nt)
-initial_data = fom.initial_data.as_range_array()
-operator = fom.operator
-rhs = fom.rhs
-mass = fom.mass
-
-tol = 1e-12
-num_values = 19
+tol = 1e-10
 
 
-def test_implicit_euler():
+def setup_non_parametric_example():
+    initial_time = 0.
+    end_time = 1.
+    nt = 10
+    fom = heat_equation_non_parametric_example(diameter=1., nt=nt)
+    num_values = 19
+    initial_data = fom.initial_data.as_range_array()
+    operator = fom.operator
+    rhs = fom.rhs
+    mass = fom.mass
+    return fom, initial_time, end_time, nt, num_values, initial_data, operator, rhs, mass
+
+
+def setup_parametric_example():
+    nt = 10
+    num_values = 19
+    initial_time = 0.
+    end_time = 1.
+    grid_intervals = 20
+    fom = heat_equation_example(grid_intervals=grid_intervals, nt=nt)
+    initial_data = fom.initial_data.as_range_array()
+    operator = fom.operator
+    rhs = fom.rhs
+    mass = fom.mass
+    mu = fom.parameters.parse({'top': 1.})
+    return fom, initial_time, end_time, nt, num_values, initial_data, operator, rhs, mass, mu
+
+
+def test_va_as_rhs_implicit_euler():
+    fom, initial_time, end_time, nt, num_values, initial_data, operator, rhs, mass, mu = setup_parametric_example()
+    dt = (end_time - initial_time) / nt
+
+    rhs_time_dep = fom.operator.range.empty(reserve=nt)
+    for n in range(nt):
+        mu_t = mu.at_time(initial_time + (n + 1) * dt)
+        rhs_time_dep.append(2. * rhs.as_vector(mu=mu_t))
+
+    time_stepper = ImplicitEulerTimeStepper(nt)
+    U_va_rhs = time_stepper.solve(initial_time, end_time, initial_data, operator, rhs=rhs_time_dep,
+                                  mass=mass, mu=mu)
+    U = time_stepper.solve(initial_time, end_time, initial_data, operator, rhs=2. * rhs, mass=mass, mu=mu)
+    assert np.all((U - U_va_rhs).norm() <= tol)
+
+
+def test_va_as_rhs_implicit_midpoint():
+    fom, initial_time, end_time, nt, num_values, initial_data, operator, rhs, mass, mu = setup_parametric_example()
+    dt = (end_time - initial_time) / nt
+
+    rhs_time_dep = fom.operator.range.empty(reserve=nt)
+    for n in range(nt):
+        mu_t = mu.at_time(initial_time + (n + 0.5) * dt)
+        rhs_time_dep.append(2. * rhs.as_vector(mu=mu_t))
+
+    time_stepper = ImplicitMidpointTimeStepper(nt)
+    U_va_rhs = time_stepper.solve(initial_time, end_time, initial_data, operator, rhs=rhs_time_dep,
+                                  mass=mass, mu=mu)
+    U = time_stepper.solve(initial_time, end_time, initial_data, operator, rhs=2. * rhs, mass=mass, mu=mu)
+    assert np.all((U - U_va_rhs).norm() <= tol)
+
+
+def test_backward_stepping_implicit_euler():
+    fom, initial_time, end_time, nt, num_values, initial_data, operator, rhs, mass = setup_non_parametric_example()
     time_stepper = ImplicitEulerTimeStepper(nt)
 
     U = time_stepper.solve(initial_time, end_time, initial_data, operator, rhs=rhs, mass=mass)
@@ -29,7 +80,8 @@ def test_implicit_euler():
     assert np.all((U - U_backwards).norm() <= tol)
 
 
-def test_implicit_midpoint():
+def test_backward_stepping_implicit_midpoint():
+    fom, initial_time, end_time, nt, num_values, initial_data, operator, rhs, mass = setup_non_parametric_example()
     time_stepper = ImplicitMidpointTimeStepper(nt)
 
     U = time_stepper.solve(initial_time, end_time, initial_data, operator, rhs=rhs, mass=mass)
@@ -42,7 +94,8 @@ def test_implicit_midpoint():
     assert np.all((U - U_backwards).norm() <= tol)
 
 
-def test_discrete():
+def test_backward_stepping_discrete():
+    fom, initial_time, end_time, nt, num_values, initial_data, operator, rhs, mass = setup_non_parametric_example()
     time_stepper = DiscreteTimeStepper()
 
     initial_time = 0
