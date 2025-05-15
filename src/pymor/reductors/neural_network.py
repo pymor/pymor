dd38406c2483be87ff786355810ee24579df0f4a
Hendrik Kleikamp <hendrik.kleikamp@uni-muenster.de>
2025-05-15 14:08:25 +0200

    [neural-networks] project output functional only if available

--- src/pymor/reductors/neural_network.py
+++ src/pymor/reductors/neural_network.py
@@ -475,12 +475,13 @@ class NeuralNetworkReductor(BasicObject):
 
     def _build_rom(self):
         """Construct the reduced order model."""
+        projected_output_functional = None
         if self.fom:
-            projected_output_functional = project(self.fom.output_functional, None, self.reduced_basis)
+            if hasattr(self.fom, 'output_functional'):
+                projected_output_functional = project(self.fom.output_functional, None, self.reduced_basis)
             parameters = self.fom.parameters
             name = self.fom.name
         else:
-            projected_output_functional = None
             parameters = self.training_parameters[0].parameters()
             name = 'data_driven'
 
