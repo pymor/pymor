8bbaa78db6b5517ff86fb3a526f0c08a31a26307
Felix Schindler <107669806+ftschindler@users.noreply.github.com>
2025-06-04 08:21:53 +0200

    refactor BlackBoxModel to allow arbitrary computers

--- src/pymor/models/black_box.py
+++ src/pymor/models/black_box.py
@@ -1,4 +1,5 @@
 from collections.abc import Callable
+from numbers import Number
 from typing import Optional
 
 import numpy as np
@@ -7,56 +8,62 @@ import numpy.typing as npt
 from pymor.models.interface import Model
 from pymor.parameters.base import Mu, Parameters
 from pymor.vectorarrays.interface import VectorArray, VectorSpace
-from pymor.vectorarrays.numpy import NumpyVectorSpace
 
 
-# TODO: replace solve_lambda by compute_lambdas = {id: lambda}
 class BlackBoxModel(Model):
+
     def __init__(
         self,
-        solution_space: VectorSpace,
-        parameters: Parameters,
-        solve_lambda: Callable[[Mu], VectorArray],
         *,
+        parameters: Parameters,
+        computers: dict[str, tuple[VectorSpace | Number, Callable[[Mu], VectorArray | npt.ArrayLike]]],
+        # compute_lambdas: dict[str, Callable[[Mu], VectorArray | npt.ArrayLike]],
+        output_functional=None,
         visualizer=None,
         name: Optional[str] = None,
-    ):
+        ):
+        assert isinstance(parameters, Parameters)
+
+        assert isinstance(computers, dict)
+        assert all(isinstance(k, str) for k in computers)
+        assert all(isinstance(v, tuple) and len(v) == 2 for v in computers.values())
+        assert all(isinstance(s, (VectorSpace, Number)) for s, _ in computers.values())
+        assert all(callable(c) for _, c in computers.values())
+
         super().__init__(visualizer=visualizer, name=name or 'BlackBoxModel')
+
+        if 'solution' in computers:
+            self.solution_space = computers['solution'][0]
+        if 'output' in computers:
+            assert not isinstance(computers['output'][0], VectorSpace)
+            self.dim_output = computers['output'][0]
+            assert not output_functional, 'TODO: comes later'
+
+        self.computable_quantities = set(computers.keys())
+
         self.__auto_init(locals())
 
     def _compute(self, quantities, data, mu=None):
-        if 'solution' in quantities:
-            if self.solution_space.dim == 0:
-                data['solution'] = self.solution_space.zeros(1)
-            else:
-                u = self.solve_lambda(mu)
-                assert u in self.solution_space
-                data['solution'] = u
-            quantities.remove('solution')
-
+        assert isinstance(quantities, (tuple, list, set))
+        # we first compute all the data we're responsible for ...
+        mu = self.parameters.parse(mu)
+        for quantity in set(quantities):  # we modify quantities in place, so we need to iterate over a copy
+            if quantity in self.computers:
+                target_shape, computer = self.computers[quantity]
+                if isinstance(target_shape, VectorSpace):
+                    if target_shape.dim == 0:
+                        data[quantity] = target_shape.zeros(1)
+                    else:
+                        computed_data = computer(mu)
+                        assert computed_data in target_shape
+                        data[quantity] = computed_data
+                else:  # this needs to be the numpy case
+                    if target_shape == 0:
+                        data[quantity] = np.zeros((target_shape, 1))
+                    else:
+                        computed_data = computer(mu)
+                        computed_data = computed_data.reshape(target_shape, -1)
+                        data[quantity] = computed_data
+                quantities.remove(quantity)
+        # ... and delegate the rest to the parent class
         super()._compute(quantities, data, mu=mu)
-
-
-class NumpyBlackBoxModel(BlackBoxModel):
-    def __init__(
-        self,
-        solution_dim: int,
-        parameters: Parameters,
-        numpy_solve_lambda: Callable[[Mu], npt.ArrayLike],
-        *,
-        visualizer=None,
-        name: Optional[str] = None,
-    ):
-        solution_space = NumpyVectorSpace(solution_dim)
-
-        def solve_lambda(mu):
-            u = np.array(numpy_solve_lambda(mu)).reshape(solution_space.dim, -1)
-            return solution_space.from_numpy(u)
-
-        super().__init__(
-            solution_space=solution_space,
-            parameters=parameters,
-            solve_lambda=solve_lambda,
-            visualizer=visualizer,
-            name=name or 'NumpyBlackBoxModel')
-        self.__auto_init(locals())
