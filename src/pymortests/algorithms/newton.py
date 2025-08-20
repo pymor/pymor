ba8eec1257fdd51bbaf991ab3ad766408ccdf057
Petar Mlinarić <petar.mlinaric@gmail.com>
2025-08-20 17:29:25 +0200

    Revert "Fix PT028"
    This reverts commit 7584dd43ab602ad4f16fdd18de8387c61cf75f40.

--- src/pymortests/algorithms/newton.py
+++ src/pymortests/algorithms/newton.py
@@ -55,7 +55,7 @@ def test_newton_unknown_line_search():
         _ = _newton(mop, relax='armo')
 
 
-def test_newton_residual_is_zero(order):
+def test_newton_residual_is_zero(order=5):
     mop = MonomOperator(order)
     U, _ = _newton(mop, initial_value=0.0)
     assert float_cmp(mop.apply(U).to_numpy(), 0.0)
