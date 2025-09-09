8b29fae36049f3a99dd0d35a9b53266f14867f75
Stephan Rave <stephanrave@uni-muenster.de>
2025-09-09 11:22:48 +0200

    add tests for jupyter visualizers

---
+++ .ci/gitlab/test_jupyter.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+THIS_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ; pwd -P )"
+source ${THIS_DIR}/common_test_setup.bash
+echo ${PYMOR_PYTEST_EXTRA}
+echo ${COMMON_PYTEST_OPTS}
+eval pytest --nbmake ${PYMOR_ROOT}/src/pymortests/jupyter/*.ipynb
+_coverage_xml
