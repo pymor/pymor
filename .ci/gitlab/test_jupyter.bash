7dffa5616edc0aaf29388fecbcb4fae789e3e4fc
Stephan Rave <stephanrave@uni-muenster.de>
2025-09-09 16:37:16 +0200

    fix coverage issues

--- .ci/gitlab/test_jupyter.bash
+++ .ci/gitlab/test_jupyter.bash
@@ -4,5 +4,5 @@ THIS_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ; pwd -P )"
 source ${THIS_DIR}/common_test_setup.bash
 echo ${PYMOR_PYTEST_EXTRA}
 echo ${COMMON_PYTEST_OPTS}
-eval pytest --nbmake ${PYMOR_ROOT}/src/pymortests/jupyter/*.ipynb
+eval pytest --nbmake ${COMMON_PYTEST_OPTS} ${PYMOR_ROOT}/src/pymortests/jupyter/*.ipynb
 _coverage_xml
