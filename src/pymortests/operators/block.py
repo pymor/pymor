74951ce3d1a6fa80b8479a7373a7045cd6787cae
ullmannsven <ullmannsven@gmx.de>
2025-10-02 15:55:18 +0200

    add test for BlockOperators with only Nones in rows and columns

--- src/pymortests/operators/block.py
+++ src/pymortests/operators/block.py
@@ -7,6 +7,7 @@ import pytest
 import scipy.linalg as spla
 
 from pymor.operators.block import BlockDiagonalOperator, BlockOperator
+from pymor.operators.constructions import ZeroOperator
 from pymor.operators.numpy import NumpyMatrixOperator
 from pymor.vectorarrays.block import BlockVectorSpace
 from pymor.vectorarrays.numpy import NumpyVectorSpace
@@ -142,3 +143,79 @@ def test_block_jacobian(rng):
     assert np.all(jac.blocks[0, 0].vector.to_numpy()[:, 0] == np.dot(A.T, v1) + np.dot(A, v1))
     assert np.all(jac.blocks[1, 1].vector.to_numpy()[:, 0] == np.dot(B.T, v2) + np.dot(B, v2))
     assert np.all(jac.blocks[2, 2].matrix == C)
+
+
+def test_blockOperator_none_everywhere():
+    S0 = NumpyVectorSpace(4)
+    S1 = NumpyVectorSpace(2)
+    R0 = NumpyVectorSpace(5)
+    R1 = NumpyVectorSpace(3)
+
+    blocks = np.array([[None, None], [None, None]], dtype=object)
+
+    bop = BlockOperator(blocks, range_spaces=[R0, R1], source_spaces=[S0, S1])
+
+    for (i, j), o in np.ndenumerate(bop.blocks):
+        assert isinstance(o, ZeroOperator)
+        assert o.range == (R0 if i == 0 else R1)
+        assert o.source == (S0 if j == 0 else S1)
+
+    assert bop.source.dim == S0.dim + S1.dim
+    assert bop.range.dim == R0.dim + R1.dim
+
+
+def test_raises_if_row_or_column_empty_without_spaces():
+    """If a row/column is entirely None and spaces are NOT provided, we expect an AssertionError."""
+    blocks = np.array([[None, None], [None, None]], dtype=object)
+    with pytest.raises(AssertionError):
+        BlockOperator(blocks)
+
+
+def test_allow_single_empty_row_or_column_with_explicit_spaces():
+
+    A = NumpyMatrixOperator(np.eye(2))
+    blocks = np.array([[A, None], [None, None]], dtype=object)
+
+    R0 = A.range
+    R1 = NumpyVectorSpace(2)
+    S0 = A.source
+    S1 = NumpyVectorSpace(2)
+
+    bop = BlockOperator(blocks, range_spaces=[R0, R1], source_spaces=[S0, S1])
+
+    assert isinstance(bop.blocks[0, 1], ZeroOperator)
+    assert isinstance(bop.blocks[1, 0], ZeroOperator)
+    assert isinstance(bop.blocks[1, 1], ZeroOperator)
+
+    assert bop.blocks[0, 1].range == R0
+    assert bop.blocks[0, 1].source == S1
+    assert bop.blocks[1, 0].range == R1
+    assert bop.blocks[1, 0].source == S0
+    assert bop.blocks[1, 1].range == R1
+    assert bop.blocks[1, 1].source == S1
+
+    assert bop.blocks[0, 0] is A
+
+
+def test_raises_if_operator_spaces_mismatch_provided_spaces():
+    """Non-None operator must match the provided source/range spaces."""
+    A = NumpyMatrixOperator(np.eye(2))
+
+    # Provide spaces that do not match A
+    wrong_R0 = NumpyVectorSpace(3)
+    correct_R0 = NumpyVectorSpace(2)
+    correct_R1 = A.range
+    wrong_S0 = NumpyVectorSpace(3)
+    correct_S0 = NumpyVectorSpace(2)
+    correct_S1 = A.source
+
+    blocks = np.array([[A, None], [None, None]], dtype=object)
+
+    with pytest.raises(AssertionError):
+        BlockOperator(blocks, range_spaces=[wrong_R0, correct_R1], source_spaces=[wrong_S0, correct_S1])
+
+    with pytest.raises(AssertionError):
+        BlockOperator(blocks, range_spaces=[correct_R0, correct_R1], source_spaces=[wrong_S0, correct_S1])
+
+    with pytest.raises(AssertionError):
+        BlockOperator(blocks, range_spaces=[wrong_R0, correct_R1], source_spaces=[correct_S0, correct_S1])
