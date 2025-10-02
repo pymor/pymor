0b5993462037df26d5ba9024568faae6e6a88da5
Stephan Rave <stephanrave@uni-muenster.de>
2025-10-02 17:27:29 +0200

    extend tests

--- src/pymortests/solvers/solvers.py
+++ src/pymortests/solvers/solvers.py
@@ -6,6 +6,7 @@ import numpy as np
 import pytest
 from scipy.sparse import diags
 
+from pymor.algorithms.basic import almost_equal
 from pymor.bindings.scipy import (
     ScipyBicgStabSolver,
     ScipyBicgStabSpILUSolver,
@@ -14,28 +15,15 @@ from pymor.bindings.scipy import (
     ScipyLSQRSolver,
     ScipySpSolveSolver,
 )
-from pymor.operators.interface import Operator
 from pymor.operators.numpy import NumpyMatrixOperator
 from pymor.solvers.generic import LGMRESSolver, LSMRSolver, LSQRSolver
-from pymor.vectorarrays.numpy import NumpyVectorSpace
 
 pytestmark = pytest.mark.builtin
 
 
-class GenericOperator(Operator):
-
-    source = range = NumpyVectorSpace(10)
-    op = NumpyMatrixOperator(np.eye(10) * np.arange(1, 11))
-    linear = True
-
-    def __init__(self, solver=None):
-        self.solver = solver
-
-    def apply(self, U, mu=None):
-        return self.op.apply(U, mu=mu)
-
-    def apply_adjoint(self, V, mu=None):
-        return self.op.apply_adjoint(V, mu=mu)
+mat = np.eye(10) * np.arange(1, 11)
+mat[-1,0] = 11
+mat_op = NumpyMatrixOperator(mat)
 
 
 all_generic_solvers = [LGMRESSolver, LSMRSolver, LSQRSolver]
@@ -53,17 +41,39 @@ def numpy_sparse_solver(request):
 
 
 def test_generic_solvers(generic_solver):
-    op = GenericOperator(generic_solver)
+    op = mat_op.with_(solver=generic_solver)
+    op2 = mat_op
     rhs = op.range.make_array(np.ones(10))
-    solution = op.apply_inverse(rhs)
-    assert ((op.apply(solution) - rhs).norm() / rhs.norm())[0] < 1e-8
+    solution = generic_solver.solve(op, rhs)
+    solution2 = op.apply_inverse(rhs)
+    solution3 = op2.apply_inverse(rhs, solver=generic_solver)
+    assert np.all(almost_equal(solution, solution2))
+    assert np.all(almost_equal(solution, solution3))
+    assert ((op.apply(solution) - rhs).norm() / rhs.norm())[0] < 1e-6
+
+
+def test_generic_adjoint_solvers(generic_solver):
+    op = mat_op.with_(solver=generic_solver)
+    op2 = mat_op
+    rhs = op.source.make_array(np.ones(10))
+    solution = generic_solver.solve_adjoint(op, rhs)
+    solution2 = op.apply_inverse_adjoint(rhs)
+    solution3 = op2.apply_inverse_adjoint(rhs, solver=generic_solver)
+    assert np.all(almost_equal(solution, solution2))
+    assert np.all(almost_equal(solution, solution3))
+    assert ((op.apply_adjoint(solution) - rhs).norm() / rhs.norm())[0] < 1e-6
 
 
 def test_numpy_dense_solvers():
-    op = NumpyMatrixOperator(np.eye(10) * np.arange(1, 11))
-    rhs = op.range.make_array(np.ones(10))
-    solution = op.apply_inverse(rhs)
-    assert ((op.apply(solution) - rhs).norm() / rhs.norm())[0] < 1e-8
+    rhs = mat_op.range.make_array(np.ones(10))
+    solution = mat_op.apply_inverse(rhs)
+    assert ((mat_op.apply(solution) - rhs).norm() / rhs.norm())[0] < 1e-8
+
+
+def test_numpy_dense_adjoint_solvers():
+    rhs = mat_op.source.make_array(np.ones(10))
+    solution = mat_op.apply_inverse_adjoint(rhs)
+    assert ((mat_op.apply_adjoint(solution) - rhs).norm() / rhs.norm())[0] < 1e-8
 
 
 def test_numpy_sparse_solvers(numpy_sparse_solver):
@@ -71,3 +81,10 @@ def test_numpy_sparse_solvers(numpy_sparse_solver):
     rhs = op.range.make_array(np.ones(10))
     solution = op.apply_inverse(rhs)
     assert ((op.apply(solution) - rhs).norm() / rhs.norm())[0] < 1e-8
+
+
+def test_numpy_sparse_adjoint_solvers(numpy_sparse_solver):
+    op = NumpyMatrixOperator(diags([np.arange(1., 11.)], [0], format='csc'), solver=numpy_sparse_solver)
+    rhs = op.source.make_array(np.ones(10))
+    solution = op.apply_inverse_adjoint(rhs)
+    assert ((op.apply_adjoint(solution) - rhs).norm() / rhs.norm())[0] < 1e-8
