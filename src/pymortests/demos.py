7e0ae06087aeb03fef3dc6cd3e70d9d61f37d92c
Hendrik Kleikamp <hendrik.kleikamp@uni-graz.at>
2025-10-29 17:56:00 +0100

    [vkoga] first version of VKOGA implementation

--- src/pymortests/demos.py
+++ src/pymortests/demos.py
@@ -163,6 +163,15 @@ SYMPLECTIC_WAVE_ARGS = (
     ('symplectic_wave_equation', [1., 10]),
 )
 
+VKOGA_ARGS = (
+    ('vkoga', ['--greedy_criterion=fp', '--kernel=non-diagonal']),
+    ('vkoga', ['--greedy_criterion=fp', '--kernel=diagonal']),
+    ('vkoga', ['--greedy_criterion=f', '--kernel=non-diagonal']),
+    ('vkoga', ['--greedy_criterion=f', '--kernel=diagonal']),
+    ('vkoga', ['--greedy_criterion=p', '--kernel=non-diagonal']),
+    ('vkoga', ['--greedy_criterion=p', '--kernel=diagonal']),
+)
+
 DEMO_ARGS = (
     DISCRETIZATION_ARGS
     + SUCCESSIVE_CONSTRAINTS_ARGS
@@ -181,6 +190,7 @@ DEMO_ARGS = (
     + DMD_ARGS
     + PHLTI_ARGS
     + SYMPLECTIC_WAVE_ARGS
+    + VKOGA_ARGS
 )
 
 DEMO_ARGS = [(f'pymordemos.{a}', b) for (a, b) in DEMO_ARGS]
