10e8e6759c7af2cb6642866b4b8e017ecbc717fa
Felix Schindler <107669806+ftschindler@users.noreply.github.com>
2025-11-18 11:20:48 +0100

    reductors| add custom model + sorting to SkLearn reductor

--- src/pymor/reductors/sklearn.py
+++ src/pymor/reductors/sklearn.py
@@ -12,8 +12,9 @@ from vkoga.kernels import Gaussian
 from vkoga.vkoga import VKOGA
 
 from pymor.core.base import BasicObject
-from pymor.models.black_box import BlackBoxModel
+from pymor.models.compute import ComputeModel
 from pymor.parameters.base import Mu, ParameterSpace, Parameters
+from pymor.tools.random import get_rng
 from pymor.vectorarrays.interface import VectorSpace, VectorArray
 
 
@@ -53,6 +54,50 @@ class VKOGAEstimator(VKOGA, BasicObject):
         return hasattr(self, "ctrs_") and self.ctrs_ is not None
 
 
+class ScikitLearnComputeModel(ComputeModel):
+    def __init__(
+        self,
+        parameters,
+        parameter_scaling,
+        compute_shapes,
+        estimators,
+        name: Optional[str] = "ScikitLearnComputeModel",
+    ):
+        self.__auto_init(locals())
+        computers = {}
+        for compute_id, estimator in self.estimators.items():
+            data_dim, data_length = self.compute_shapes[compute_id]
+            if not skutils.validation._is_fitted(estimator):
+                if isinstance(data_dim, VectorSpace):
+
+                    def trivial_solution(mu):
+                        return data_dim.zeros(data_length)
+                else:
+
+                    def trivial_solution(mu):
+                        return np.zeros((data_dim, data_length))
+
+                computers[compute_id] = (data_dim, trivial_solution)
+            else:
+                if isinstance(data_dim, VectorSpace):
+
+                    def predict(mu):
+                        mu = self.parameter_scaling(mu)
+                        X = mu.to_numpy().reshape(1, -1)
+                        Y = estimator.predict(X)
+                        return data_dim.from_numpy(Y.reshape(data_length, data_dim.dim))
+                else:
+
+                    def predict(mu):
+                        mu = self.parameter_scaling(mu)
+                        X = mu.to_numpy().reshape(1, -1)
+                        Y = estimator.predict(X)
+                        return Y.reshape((data_length, data_dim))
+
+                computers[compute_id] = (data_dim, predict)
+        super().__init__(parameters=parameters, computers=computers, name=name)
+
+
 class ScikitLearnComputeReductor(BasicObject):
     def __init__(
         self,
@@ -66,11 +111,13 @@ class ScikitLearnComputeReductor(BasicObject):
         training_data: Optional[
             tuple[Iterable[Mu], dict[str, Iterable[VectorSpace | npt.ArrayLike]]]
         ] = None,
+        shuffle_mode: str = "sort+shuffle",  # highly recommended, possible values are None, 'sort', 'shuffle', 'sort+shuffle'
         rom_name: str = "SkLearnReducedModel",
     ):
         assert isinstance(parameters, Parameters)
         assert isinstance(compute_shapes, dict)
         assert all(isinstance(k, str) for k in compute_shapes)
+        self.reducable_computes = set(compute_shapes.keys())
         assert all(
             isinstance(v, tuple) and len(v) == 2 for v in compute_shapes.values()
         )
@@ -81,28 +128,24 @@ class ScikitLearnComputeReductor(BasicObject):
         # We use skbase.clone to ensure no one tempers with the estimators while we hold them.
         if isinstance(estimators, skbase.BaseEstimator):
             estimators = {
-                compute_id: skbase.clone(estimators) for compute_id in compute_shapes
+                compute_id: skbase.clone(estimators)
+                for compute_id in self.reducable_computes
             }
         assert isinstance(estimators, dict)
         assert all(isinstance(k, str) for k in estimators)
-        assert all(k in compute_shapes for k in estimators)
+        assert all(k in self.reducable_computes for k in estimators)
         assert all(isinstance(v, skbase.BaseEstimator) for v in estimators.values())
         parameter_scaling = parameter_scaling or (lambda mu: mu)
         assert callable(parameter_scaling)
         # training_data will be checked in _parse_data
+        # shuffle_mode will be checked in _parse_data
         assert isinstance(rom_name, str)
         rom_name = rom_name.strip()
         assert rom_name
 
         self.__auto_init(locals())
         self._data = self._parse_data(training_data)
-
-        # # Even though it does not fit perfectly, we implement bases to mimic other reductors.
-        # self.bases = {
-        #     # TODO: we need something with a len() here, do
-        #     #       (mu, state) for mu, state in zip(self._data)?
-        #     'DATA': self._data[0],
-        # }
+        self._shuffle_data()
 
         # Keeping the fitted estimators is not required, but facilitates debugging and post-processing
         self._fitted_estimators = {}
@@ -114,10 +157,41 @@ class ScikitLearnComputeReductor(BasicObject):
         )
         if len(additional_training_inputs) > 0:
             self._data[0] += additional_training_inputs
-            for compute_id in self.compute_shapes:
+            for compute_id in self.reducable_computes:
                 self._data[1][compute_id] += additional_training_data[compute_id]
+        self._shuffle_data()
 
-    def reduce(self, data_size: Optional[Number] = None) -> BlackBoxModel:
+    def _shuffle_data(self):
+        assert self.shuffle_mode in (None, "sort", "shuffle", "sort+shuffle")
+        if self.shuffle_mode is None:
+            return
+        if len(self._data[0]) == 0:
+            return
+        data_tuples = list(
+            zip(self._data[0], *(self._data[1][k] for k in self._data[1]))
+        )
+        if self.shuffle_mode in ("sort", "sort+shuffle"):
+            if self.parameters.dim == 1:
+
+                def reduce(pair):
+                    return pair[0].to_numpy().ravel()[0]
+            else:
+
+                def reduce(pair):
+                    return np.linalg.norm(pair[0].to_numpy())
+
+            data_tuples.sort(key=reduce)
+        if self.shuffle_mode in ("shuffle", "sort+shuffle"):
+            get_rng().shuffle(data_tuples)
+        reordered_inputs_and_outputs = list(zip(*data_tuples))
+        reordered_inputs = reordered_inputs_and_outputs[0]
+        reordered_outputs = {
+            k: list(reordered_inputs_and_outputs[i + 1])
+            for i, k in enumerate(self._data[1])
+        }
+        self._data = [reordered_inputs, reordered_outputs]
+
+    def reduce(self, data_size: Optional[Number] = None) -> ComputeModel:
         """Fits the estimator to the amount of data specified by data_size."""
         if data_size is None:
             data_size = len(self._data[0])
@@ -139,8 +213,37 @@ class ScikitLearnComputeReductor(BasicObject):
                         f'Fitting estimator for {compute_id} to {"all" if data_size == len(self._data[0]) else "first"} {data_size} samples ...'
                     ):
                         estimator.fit(X, Y)
-            self._roms[data_size] = self._build_rom(self._fitted_estimators[data_size])
+            self._roms[data_size] = ScikitLearnComputeModel(
+                parameters=self.parameters,
+                parameter_scaling=self.parameter_scaling,
+                compute_shapes=self.compute_shapes,
+                estimators=self._fitted_estimators[data_size],
+                name=self.rom_name,
+            )
         return self._roms[data_size]
+
+    def data_as_X_y(
+        self, quantity=None, data_size: Number = None
+    ) -> tuple[npt.NDArray, dict[str, npt.NDArray]]:
+        if quantity is None:
+            assert len(self.reducable_computes) == 1
+            quantity = next(iter(self.reducable_computes))
+        assert quantity in self.reducable_computes
+        if data_size is None:
+            data_size = len(self._data[0])
+        assert isinstance(data_size, Number)
+        assert data_size >= 0
+        assert data_size <= len(self._data[0])
+        if data_size == 0:
+            return np.empty((0, self.parameters.dim)), {
+                compute_id: np.empty((0, data_length))
+                for compute_id, (_, data_length) in self.compute_shapes.items()
+            }
+        X = [mu.to_numpy() for mu in self._data[0][:data_size]]
+        assert np.all(mu.shape == (self.parameters.dim,) for mu in X)
+        X = np.stack(X)
+        y = np.stack(self._data[1][quantity][:data_size])
+        return X, y
 
     # def reconstruct(self, u, basis='DATA'):
     #     """Returns an identical copy of the MLM prediction within the solution_space."""
@@ -181,7 +284,7 @@ class ScikitLearnComputeReductor(BasicObject):
                         return Y.reshape((data_dim, data_length))
 
                 computers[compute_id] = (data_dim, predict)
-        rom = BlackBoxModel(
+        rom = ComputeModel(
             parameters=self.parameters,
             computers=computers,
             name=self.rom_name,
