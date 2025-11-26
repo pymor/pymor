ca2b99489d51a678652b4f3b22aa553190f10128
ullmannsven <ullmannsven@gmx.de>
2025-11-26 10:26:19 +0100

    use newton basis to update coefficents

--- src/pymor/algorithms/vkoga/vkoga.py
+++ src/pymor/algorithms/vkoga/vkoga.py
@@ -28,8 +28,18 @@ class VKOGASurrogate(WeakGreedySurrogate):
         self._centers = None
         self._centers_idx = None
         self._coefficients = None
-        self._L = None  # block-Cholesky factor of the kernel matrix
+        self._z = None
+        self._L = None
+        self._K_stack = None
+        self._V = None
+        self._C = None
         self._init_power_function_evals()
+
+    def _init_power_function_evals(self):
+        """Initialize power2 and V when no centers were selected yet."""
+        # power2 = trace(k(x,x)) for each training point
+        diag = self.kernel.diag(self.X_train)
+        self._power2 = np.add.reduceat(diag, range(0, len(diag), len(diag) // len(self.X_train)))
 
     def predict(self, X):
         X = np.asarray(X)
@@ -90,11 +100,14 @@ class VKOGASurrogate(WeakGreedySurrogate):
         * the coefficients of the interpolant (`self._coefficients`)
         * the power function evaluations on the training set (`self._power2`)
         * the Newton basis evaluations on the training set (`self._V`)
+        * the residual :math:`f - s_f` (`self.res`)
+        * the inverse of the Cholesky matrix (`self._C)
 
         using a numerically stable Newton basis expansion.
 
         In the following derivation we leave out the regularization term for simplicity.
-        If :math:`K_n` denotes the full block kernel matrix, we can write :math:`K_{n+1}` as
+        If :math:`K_n` denotes the full block kernel  for :math:`X_n`, we can write :math:`K_{n+1}`
+        as
 
         .. math::
             K_{n+1} = \begin{bmatrix}
@@ -110,14 +123,17 @@ class VKOGASurrogate(WeakGreedySurrogate):
         .. math::
             L_{n+1} = \begin{bmatrix}
                 L_n & 0 \\
-                W^\top & L_{22}
+                W^\top & l_{n+1, n+1}
             \end{bmatrix},
 
-        where :math:`W=L_n^{-1}B` and :math:`L_{22}` is the Cholesky decomposition
+        where :math:`W=L_n^{-1}B` and :math:`l_{n+1, n+1}` is the Cholesky decomposition
         of :math:`k_{n+1,n+1}-W^\top W`.
 
         It is further possible to update the coefficient vector in a suitable way by reusing
-        the old coefficients and without solving the whole system again:
+        the old coefficients and without solving the whole system again. To do so, the inverse of
+        the Cholesky factor :math:`L_{n}^{-1} =: C_{n}` is required.  We remark that :math:`C_n`
+        can also be updated incrementally in a similar fashion as the Cholesky factor.
+
         Let :math:`c_n\in\mathbb{R}^n` denote the coefficients associated to the interpolant
         for the first :math:`n` selected centers, i.e.
 
@@ -131,50 +147,45 @@ class VKOGASurrogate(WeakGreedySurrogate):
             \sum\limits_{i=1}^{n} (c_n)_i k(\,\cdot\,,x_i),
 
         where :math:`x_1,\dots,x_n\in\mathbb{R}^d` are the :math:`n` selected centers.
-        When adding :math:`x_{n+1}\in\mathbb{R}^d` as new center, we can compute
-        :math:`c_{n+1}\in\mathbb{R}^{n+1}` in the following way: Let us write
+        When adding :math:`x_{n+1}\in\mathbb{R}^d` as new center, the new coefficient
+        vector :math:`c_{n+1}\in \mathbb{R}^{n+1}` is given as
 
         .. math::
-            c_{n+1} = \begin{bmatrix}
-                a_1 \\
-                a_2
+            c_{n+1} = C_{n+1}^\top \begin{bmatrix}
+                c_n \\
+                z_{n+1}
             \end{bmatrix}
 
-        for unknown coefficients :math:`a_1\in\mathbb{R}^n` and :math:`a_2\in\mathbb{R}`.
-        We then have (by splitting the right-hand side :math:`f_{n+1}` accordingly)
+        for unknown coefficient :math:`z_{n+1}\in\mathbb{R}`, which is computed via the
+        residual :math:`r_n` and a scalar version of the power function :math:`p_n`:
 
         .. math::
             \begin{align*}
-                K_na_1+Ba_2 &= f_1, \\
-                B^\top a_1+k_{n+1,n+1}a_2 &= f_2.
+               z_{n+1} = {\frac{r_n(x_{n+1})}{p_n(x_{n+1})}
             \end{align*}
 
-        One can now solve for :math:`a_1` and :math:`a_2` in the following way:
+        where the residual :math:`r_n` is given by
 
         .. math::
             \begin{align*}
-                y_1 &= L_n^{-1} f_1,\\
-                y_2 &= L_{22}^{-1}(f_2-W^\top y_1),\\
-                a_1 &= L_n^{-\top}(y_1-Wy_2) = c_n - L_n^{-\top}Wy_2,\\
-                a_2 &= L_{22}^{-\top}y_2,
+                r_n(x) := r_{n-1}(x) - z_n V_n(x).
             \end{align*}
 
-        where we observe that :math:`y_1` is given by the last iteration and therefore
-        the update of :math:`a_1` and the computation of :math:`a_2` are
-        relatively cheap.
-
-        Regarding the power function (measuring for :math:`x\in\mathbb{R}^d` how well
-        the current subspace can represent :math:`k(\,\cdot\,,x)`, i.e. the projection
+        with initial residual :math:`r_0 := f`. Here :math:`V_n` denotes the Newton basis,
+        which we define below.  Regarding the power function (measuring for :math:`x\in\mathbb{R}^d`
+        how well the current subspace can represent :math:`k(\,\cdot\,,x)`, i.e. the projection
         error in the reproducing kernel Hilbert space):
-        The power function in matrix form is defined as
+        The power function :math:`P_n` (for the centers :math:`X_n`) in matrix form is
+        defined as
 
         .. math::
             \begin{align*}
                 P_n^2(x) = k(x,x) - k(x,X_n)k(X_n,X_n)^{-1}k(X_n,x).
             \end{align*}
 
-        Since we would like to use the power function as selection criterion for centers
-        within the greedy iteration, we consider a scalar version by taking the trace:
+        Since we use the power function to compute the coefficients of the kernel interpolant and
+        want to use it as selection criterion for centers within the greedy iteration,
+        we consider a scalar version by taking the trace:
 
         .. math::
             \begin{align*}
@@ -187,7 +198,7 @@ class VKOGASurrogate(WeakGreedySurrogate):
 
         .. math::
             \begin{align*}
-                V_{n,i} = L_n^{-1}k(X_n,y_i)
+                V_{n,i} = C_n k(X_n,y_i)
             \end{align*}
 
         for all training points :math:`y_i`.
@@ -197,23 +208,19 @@ class VKOGASurrogate(WeakGreedySurrogate):
 
         .. math::
             \begin{align*}
-                p_n^2(y_i) = \operatorname{trace}(k(y_i,y_i)-V_{1,i}^\top V_{1,i}),
+                p_n^2(y_i) = \operatorname{trace}(k(y_i,y_i)-V_{1,i}^\top V_{1,i}).
             \end{align*}
 
-        where :math:`V_{1,i} = L_{22}^{-1}k(x_1,y_i)`.
-
         The incremental updates of :math:`V_{n,i}` to :math:`V_{n+1,i}` and the power
-        function values is then performed in the following way:
-        Define :math:`\Delta_i=k(x_{n+1},y_i)-W^\top V_{n,i}`
-        and :math:`\Xi=L_{22}^{-1}\Delta_i`.
-        Then, we have
+        function values is then performed in the following way: Compute the latest
+        Newton basis via :math:`\Xi_i = C_{n+1}_{-m:, :} \cdot k(X_{n+1}, y_i)`
 
         .. math::
             \begin{align*}
                 p_{n+1}^2(y_i) = p_n^2(y_i) - \lVert \Xi_i\rVert_F^2
             \end{align*}
 
-        and
+        and update the Newton basis:
 
         .. math::
             \begin{align*}
@@ -239,125 +246,118 @@ class VKOGASurrogate(WeakGreedySurrogate):
         else:
             idx_in_X = int(np.argmin(np.linalg.norm(self.X_train - mu, axis=1)))
 
-        f_new = self.F_train[idx_in_X].reshape(self.m,)
+        # update the residual
+        if self._V is None and self._z is None:
+            self.res = self.F_train
+        else:
+            self.res = self.res - np.einsum('nij, i->nj', self._V[:, -self.m:, :], self._z[-self.m:])
 
-        # --- Case 1: first center ---
         if self._L is None or self._centers is None:
-            K_nn = self.kernel(mu, mu) + self.reg * np.eye(self.m)
-            L22 = np.linalg.cholesky(K_nn)
-            y_new = solve_triangular(L22, f_new, lower=True)
-            a2 = solve_triangular(L22.T, y_new, lower=False)
-            self._L = L22
-            self._centers = np.array([mu])
-            self._centers_idx = np.array([idx_in_X], dtype=int)
-            self._coefficients = a2.reshape(1, self.m)
-            self.y = y_new  # store y for reuse
-            self._init_power_function_evals_after_first_center(mu, L22)
-            return
+            self._extend_first_center(mu, idx_in_X)
+        else:
+            self._extend_incremental(mu, idx_in_X)
 
-        # --- Case 2: incremental update ---
+    def _extend_first_center(self, mu, idx_in_X):
+        """Add the first center to the interpolant."""
+        k_nn = self.kernel(mu, mu) + self.reg * np.eye(self.m)
+        l_nn = np.linalg.cholesky(k_nn)
+        self._L = l_nn
+
+        self._z = self.res[idx_in_X] * np.sqrt(self.m) / np.sqrt(self._power2[idx_in_X])
+        self._init_power_function_evals_after_first_center(mu, l_nn)
+
+        self._coefficients = (self._C.T @ self._z).reshape(1, self.m)
+        self._centers = np.array([mu])
+        self._centers_idx = np.array([idx_in_X], dtype=int)
+
+    def _init_power_function_evals_after_first_center(self, mu, l_nn):
+        K_mu_X = np.array([self.kernel(mu, x) for x in self.X_train])
+        self._K_stack = K_mu_X
+        self._C = solve_triangular(l_nn, np.eye(l_nn.shape[0]), lower=True)
+        self._V = np.einsum('ij, Njk->Nik', self._C, self._K_stack)
+
+        # update power2: p_{n+1}^2(x) = p_n^2(x) - V_i^T @ V_i
+        norms = np.sum(self._V * self._V, axis=(1, 2))
+        self._power2 = np.maximum(self._power2 - norms, 0.0)
+
+    def _extend_incremental(self, mu, idx_in_X):
+        """Incrementally add a new center to the existing interpolant."""
         n = len(self._centers)
-        p = n * self.m
-        L_old = self._L
 
-        # build B_col
+        # Compute kernel matrix blocks and Cholesky update
         B_col = np.zeros((n * self.m, self.m))
         for j, cj in enumerate(self._centers):
-            B_col[j*self.m:(j+1)*self.m, :] = self.kernel(cj, mu)
+           B_col[j*self.m:(j+1)*self.m, :] = self.kernel(cj, mu)
 
-        K_nn = self.kernel(mu, mu)
-        W = solve_triangular(L_old, B_col, lower=True)
+        # Compute Cholesky update
+        k_nn = self.kernel(mu, mu)
+        W = solve_triangular(self._L, B_col, lower=True)
+        S = k_nn - W.T @ W + self.reg * np.eye(self.m)
+        l_nn = np.linalg.cholesky(S)
+        self._update_cholesky_factor(W, l_nn)
 
-        # Schur complement and its Cholesky decomposition (small, only for the new block)
-        S = K_nn - W.T @ W + self.reg * np.eye(self.m)
-        L22 = np.linalg.cholesky(S)
+        #update coefficient:
+        z_new = (self.res[idx_in_X] * np.sqrt(self.m) / np.sqrt(self._power2[idx_in_X])).reshape(self.m, )
+        self._z = np.hstack([self._z, z_new])
 
-        # extend L
-        new_p = p + self.m
-        L_new = np.zeros((new_p, new_p))
-        L_new[:p, :p] = L_old
-        L_new[p:new_p, :p] = W.T
-        L_new[p:new_p, p:new_p] = L22
-        self._L = L_new
+        # Update power function, Newton basis and the transformation matrix C
+        self._update_cholesky_inverse(W, l_nn)
+        self._update_newton_basis(mu)
+        self._update_power_function_evals()
 
-        # incremental update of y
-        rhs_proj = f_new - W.T @ self.y
-        y_new = solve_triangular(L22, rhs_proj, lower=True)
-        self.y = np.concatenate([self.y, y_new])  # store for next iteration
-
-        # incremental update of coefficients
-        a2 = solve_triangular(L22.T, y_new, lower=False)
-        delta_vec = solve_triangular(L_old.T, W @ a2, lower=False)
-
-        a1_flat_old = self._coefficients.reshape(p,)
-        a1_flat_new = a1_flat_old - delta_vec
-        a_flat_new = np.concatenate([a1_flat_new, a2.reshape(self.m,)])
-        self._coefficients = a_flat_new.reshape(n + 1, self.m)
-
-        # update power function evaluations on training set
-        self._update_power_function_evals(mu, W, L22)
-
-        # update centers and indices
+        # Update centers and final coefficients
+        self._coefficients = (self._C.T @ self._z).reshape(n + 1, self.m)
         self._centers = np.vstack([self._centers, mu])
         self._centers_idx = np.concatenate([self._centers_idx, np.array([idx_in_X], dtype=int)])
 
-    def _init_power_function_evals(self):
-        """Initialize power2 and V when no centers were selected yet."""
-        # power2 = trace(k(x,x)) for each training point
-        diag = self.kernel.diag(self.X_train)
-        self._power2 = np.add.reduceat(diag, range(0, len(diag), len(diag) // len(self.X_train)))
-        # no V yet, we will create V after the first center is added
-        self._V = None
+    def _update_cholesky_factor(self, W, l_nn):
+        """Extend the Cholesky factor with the new block."""
+        p = self._L.shape[0]
+        new_p = p + self.m
+        L_new = np.zeros((new_p, new_p))
+        L_new[:p, :p] = self._L
+        L_new[p:new_p, :p] = W.T
+        L_new[p:new_p, p:new_p] = l_nn
+        self._L = L_new
 
-    def _init_power_function_evals_after_first_center(self, mu, L22):
-        """After adding the first center, compute V and power2."""
-        N = len(self.X_train)
-        m = self.m
-        K_mu_X = np.array([self.kernel(mu, x) for x in self.X_train])
-        # V_i = L22^{-1} k(mu, y_i) for each i.
-        rhs = K_mu_X.transpose(1, 0, 2).reshape(m, N * m)
-        sol = solve_triangular(L22, rhs, lower=True)
-        V_new = sol.reshape(m, N, m).transpose(1, 0, 2)
+    def _update_cholesky_inverse(self, W, l_nn):
+        """Extend the inverse of the Choleksky matrix with the new block."""
+        c_nn = solve_triangular(l_nn, np.eye(l_nn.shape[0]), lower=True, check_finite=False)
+        C_old = self._C
+        p_old = C_old.shape[0]
+        new_p = p_old + self.m
+        C_new = np.zeros((new_p, new_p))
+        C_new[:p_old, :p_old] = C_old
+        C_new[p_old:new_p, :p_old] = - c_nn.T @ W.T @ C_old
+        C_new[p_old:new_p, p_old:new_p] = c_nn
+        self._C = C_new
 
-        # compute power2[i]: trace(K_xx - V_i.T @ V_i)
-        power2 = np.empty(N)
-        for i, x in enumerate(self.X_train):
-            S = self.kernel(x, x) - V_new[i].T @ V_new[i]
-            power2[i] = np.trace(S) + self.reg * m
-
-        self._V = V_new.reshape(N, m, m)
-        self._power2 = np.maximum(power2, 0.0)
-
-    def _update_power_function_evals(self, mu, W, L22):
-        """Incrementally update self._power2 and self._V when adding center mu."""
+    def _update_newton_basis(self, mu):
+        """Append Xi as the last block to the Newton basis V."""
         N = len(self.X_train)
         m = self.m
         V_old = self._V
         p_old = V_old.shape[1]
+
         # compute k(mu, y_i) for all training points y_i
-        K_mu_X = np.array([self.kernel(mu, x) for x in self.X_train])
+        K_stack_new = np.zeros((N, p_old + m, m))
+        K_stack_new[:, :-m, :] = self._K_stack
+        K_stack_new[:, -m:, :] = np.array([self.kernel(x, mu) for x in self.X_train])
+        self._K_stack = K_stack_new
 
-        # Delta_i = k(mu,y_i) - M_i
-        Delta = K_mu_X - np.einsum('ap,npj->naj', W.T, V_old)
-
-        # for each i, compute Xi_i = solve(L22, Delta_i)
-        rhs = Delta.transpose(1, 0, 2).reshape(m, N * m)
-        sol = solve_triangular(L22, rhs, lower=True, check_finite=False)
-        Xi = sol.reshape(m, N, m).transpose(1, 0, 2)
-
-        # reduction: norms = ||Xi||_F^2 = sum over entries^2
-        norms = np.sum(Xi * Xi, axis=(1, 2))
-
-        # update power2: p_{n+1}^2(x) = p_n^2(x) - norms
-        self._power2 = np.maximum(self._power2 - norms, 0.0)
-
-        # update V: append Xi as the last block of rows
-        p_new = p_old + m
-        V_new = np.zeros((N, p_new, m))
+        # update Newton basis
+        Xi = np.einsum('ij,Njk->Nik', self._C[-m:, :], self._K_stack)
+        V_new = np.zeros((N, p_old + m, m))
         V_new[:, :p_old, :] = V_old
         V_new[:, p_old:p_old + m, :] = Xi
         self._V = V_new
 
+    def _update_power_function_evals(self):
+        """Incrementally update self._power2 and self._V when adding center mu."""
+        # update power2: p_{n+1}^2(x) = p_n^2(x) - norms
+        Xi = self._V[:, -self.m:, :]
+        norms = np.sum(Xi * Xi, axis=(1, 2))
+        self._power2 = np.maximum(self._power2 - norms, 0.0)
 
 class VKOGAEstimator(BasicObject):
     """Scikit-learn-style estimator using the :class:`VKOGASurrogate`.
