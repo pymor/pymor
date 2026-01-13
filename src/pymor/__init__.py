6206db257de973f88caf65cb728159c9a29b5e6e
Stephan Rave <stephanrave@uni-muenster.de>
2026-01-13 14:23:16 +0100

    bump version to 2026.1.0.dev0

--- src/pymor/__init__.py
+++ src/pymor/__init__.py
@@ -2,7 +2,7 @@
 # Copyright pyMOR developers and contributors. All rights reserved.
 # License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
 
-__version__ = '2025.2.0.dev0'
+__version__ = '2026.1.0.dev0'
 __asv_api_gen__ = 2  # used by asv benchmarks to determine to adapt test code in case of api breakage
 
 import os
