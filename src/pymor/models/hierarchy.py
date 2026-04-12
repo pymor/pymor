b537a2ce74bdee4499e8fab4d0d72ebe9e7ecbd2
Hendrik Kleikamp <hendrik.kleikamp@uni-graz.at>
2026-04-12 18:50:13 +0200

    [hierarchy] extended logging and documentation

--- src/pymor/models/hierarchy.py
+++ src/pymor/models/hierarchy.py
@@ -10,6 +10,45 @@ from pymor.reductors.data_driven import DataDrivenReductor
 
 
 class DDRBModelHierarchy(Model):
+    """Adaptive model hierarchy combining data-driven and reduced basis models.
+
+    The adaptive model hierarchy consists of three models: A full-order model,
+    a reduced basis ROM and a data-driven model. For a new parameter, the hierarchy
+    evaluates the data-driven model first as it is (usually) the fastest model.
+    The estimated error of the data-driven model is compared to the prescribed
+    tolerance. If the error is below the tolerance, the data-driven solution is
+    returned without calling the other two models. If the error is larger than the
+    tolerance, the hierarchy falls back to the reduced basis model that is more
+    accurate compared to the data-driven model but also more costly to evaluate.
+    If the error of the reduced basis model is below the tolerance, its solution
+    is returned and used to improve the data-driven model. If the error is too
+    large, the full-order model is called and its solution used to extend the
+    reduced basis of the reduced basis model.
+
+    The implementation is based on the strategies described in :cite:`HKOSW23`.
+
+    Parameters
+    ----------
+    fom
+        Full-order model used as last fallback option in the hierarchy.
+    rb_reductor
+        Reductor used to generate the reduced basis model.
+    dd_reductor_parameters
+        Attributes used to generate the
+        :class:`~pymor.reductors.data_driven.DataDrivenReductor`.
+    tol
+        Tolerance to which the estimated errors are compared to decide which
+        solution to return.
+    compression
+        Either `None` or a callable used to compress solutions before extending
+        the reduced basis. Particularly useful when dealing with instationary
+        problems where the solution trajectory should first be compressed using
+        for instance the :func:`~pymor.algorithms.pod.pod` algorithm.
+    retrain_interval
+        Integer used to determine how many new training data points should be
+        collected before training the data-driven model again.
+    """
+
     def __init__(self, fom, rb_reductor, dd_reductor_parameters, tol, compression=None, retrain_interval=1):
         self.__auto_init(locals())
 
@@ -29,10 +68,17 @@ class DDRBModelHierarchy(Model):
         super().__init__(products=fom.products, visualizer=fom.visualizer)
 
     def _update_dd_models(self, mu, reduced_coefficients):
-        """Extend DD reductors with new training data and retrain if needed.
+        """Extend data-driven reductors with new training data and retrain if needed.
 
         Training data is always accumulated in the reductor. When `retrain_interval`
         new samples have been added, the regressor is retrained from scratch.
+
+        Parameters
+        ----------
+        mu
+            |Parameter value| for which reduced coefficients have been computed.
+        reduced_coefficients
+            Reduced solution associated to `mu` that is obtained from the reduced basis model.
         """
         sum_dims = 0
         for i, red in enumerate(self.dd_reductors):
@@ -45,19 +91,25 @@ class DDRBModelHierarchy(Model):
             sum_dims += red.dim_solution_space
 
     def _solve_reduced(self, data, mu, update=True):
-        """Solve the hierarchy and return the reduced solution.
+        """Query the hierarchy and add solution to data dictionary.
 
         The reduced solution, used model type, and estimated error are stored in `data`.
         If the FOM fallback is used, `data['solution']` is set to the FOM solution directly.
 
-        If `update` is `True`, bases and training data are extended as needed.
-        If `update` is `False`, only the error estimation is performed without
-        modifying the hierarchy.
+        Parameters
+        ----------
+        data
+            Dictionary to store the results in.
+        mu
+            |Parameter value| to query the hierarchy for.
+        update
+            If `True`, bases and training data are extended as needed and surrogates are trained.
+            If `False`, only the error estimation is performed without modifying the models.
         """
         if '_estimated_error' in data:
             return
 
-        # Compute ML solution and estimate error
+        # compute ML solution and estimate error
         if len(self.dd_models) == 0:
             nt = getattr(getattr(self._rb_model, 'time_stepper', None), 'nt', 0)
             dd_solution = self._rb_model.solution_space.zeros(nt + 1)
@@ -65,40 +117,43 @@ class DDRBModelHierarchy(Model):
             dd_solution_np = np.vstack([dd_model.solve(mu).to_numpy() for dd_model in self.dd_models])
             dd_solution = self._rb_model.solution_space.make_array(dd_solution_np)
         dd_estimated_error = self._rb_model.error_estimator.estimate_error(dd_solution, mu, self._rb_model)
-        self.logger.info(f'Estimated error of ML: {np.max(dd_estimated_error)}')
 
         if np.max(dd_estimated_error) <= self.tol:
+            self.logger.info(f'Returning ML solution, estimated error of ML: {np.max(dd_estimated_error)}')
             data['_reduced_solution'] = dd_solution
             data['_used_model'] = 'ML'
             data['_estimated_error'] = dd_estimated_error
             return
 
-        # Compute RB solution and estimate error
+        self.logger.info(f'Falling back to RB, estimated error of ML: {np.max(dd_estimated_error)}')
+
+        # compute RB solution and estimate error
         rb_solution = self._rb_model.solve(mu)
         rb_estimated_error = self._rb_model.error_estimator.estimate_error(rb_solution, mu, self._rb_model)
-        self.logger.info(f'Estimated error of RB: {np.max(rb_estimated_error)}')
 
         if np.max(rb_estimated_error) <= self.tol:
+            self.logger.info(f'Returning RB solution, estimated error of RB: {np.max(rb_estimated_error)}')
             data['_reduced_solution'] = rb_solution
             data['_used_model'] = 'RB'
             data['_estimated_error'] = rb_estimated_error
 
             if update:
-                self._update_dd_models(mu, rb_solution.to_numpy())
+                with self.logger.block('Updating data-driven models ...'):
+                    self._update_dd_models(mu, rb_solution.to_numpy())
 
             return
 
+        data['_estimated_error'] = np.array([0.])
         if not update:
-            data['_estimated_error'] = rb_estimated_error
             return
 
-        # Compute FOM solution
+        # compute FOM solution
+        self.logger.info('Falling back to FOM')
         fom_solution = self.fom.solve(mu)
         data['solution'] = fom_solution
         data['_used_model'] = 'FOM'
-        data['_estimated_error'] = np.array([0.])
 
-        # Extend reduced basis of RB reductor and reduce again
+        # extend reduced basis of RB reductor and reduce again
         old_rb_size = self._rb_model.order
         extension_data = fom_solution
         if self.compression is not None:  # Perform additional compression if desired
@@ -106,14 +161,15 @@ class DDRBModelHierarchy(Model):
         self.rb_reductor.extend_basis(extension_data)
         self._rb_model = self.rb_reductor.reduce()
 
-        # Project FOM solution onto reduced basis
+        # project FOM solution onto reduced basis
         projected_fom_solution = self.rb_reductor.bases['RB'].inner(fom_solution,
                                                                     product=self.rb_reductor.products.get('RB'))
 
-        # Extend training data of existing data driven reductors
-        self._update_dd_models(mu, projected_fom_solution[:old_rb_size])
+        # extend training data of existing data driven reductors
+        with self.logger.block('Updating data-driven models ...'):
+            self._update_dd_models(mu, projected_fom_solution[:old_rb_size])
 
-        # Add new data driven reductor and model to account for new basis components
+        # add new data driven reductor and model to account for new basis components
         T = getattr(self.fom, 'T', None)
         self.dd_reductors.append(DataDrivenReductor([mu], projected_fom_solution[old_rb_size:].T,
                                                     T=T, **self.dd_reductor_parameters))
