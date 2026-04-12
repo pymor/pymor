07be910456a3ada212fb7eb7945741bd6c429f09
Hendrik Kleikamp <hendrik.kleikamp@uni-graz.at>
2026-04-12 17:47:37 +0200

    [tests] add instationary example to hierarchy demo tests

--- src/pymortests/demos.py
+++ src/pymortests/demos.py
@@ -73,7 +73,8 @@ DATA_DRIVEN_ARGS = (
 )
 
 ADAPTIVE_MODEL_HIERARCHY_ARGS = (
-    ('hierarchy', ['vkoga', 20, 50])
+    ('hierarchy', [0, 'vkoga', 20, 50])
+    ('hierarchy', [1, 'vkoga', 20, 50, '--time-steps=10'])
 )
 
 THERMALBLOCK_ARGS = (
