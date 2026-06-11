cd0021e3a9484f9fc2ce3c0b96873c9c5a1461ba
ullmannsven <ullmannsven@gmx.de>
2026-06-11 11:42:14 +0200

    get rid of high-dim np.array by using ProjectedEIOperator directly

--- src/pymor/reductors/ph/ph_deim.py
+++ src/pymor/reductors/ph/ph_deim.py
@@ -4,12 +4,12 @@
 
 import numpy as np
 
-from pymor.algorithms.ei import deim
 from pymor.algorithms.projection import project
 from pymor.models.nonlinear_ph import NonlinearPHModel
 from pymor.operators.constructions import IdentityOperator
-from pymor.operators.ei import EmpiricalInterpolatedOperator
+from pymor.operators.ei import EmpiricalInterpolatedOperator, ProjectedEmpiricalInterpolatedOperator
 from pymor.reductors.basic import ProjectionBasedReductor
+from pymor.vectorarrays.numpy import NumpyVectorSpace
 
 
 class PHdeimReductor(ProjectionBasedReductor):
@@ -19,6 +19,9 @@ class PHdeimReductor(ProjectionBasedReductor):
     structure-preserving Petrov--Galerkin projection combined with the port-Hamiltonian
     discrete empirical interpolation method (pH-DEIM), see Algorithm 5 in :cite:`CBG16`.
 
+    The interpolation DOFs and the collateral basis can be generated using
+    the algorithms provided in the :mod:`pymor.algorithms.ei` module.
+
 
     Parameters
     ----------
@@ -26,49 +29,66 @@ class PHdeimReductor(ProjectionBasedReductor):
         The full order |NonlinearPHModel| to reduce.
     V
         The basis of the trail space.
-    G
+    U
         The collateral basis as a |VectorArray| contained in
         ``fom.dh.range``. Its vectors are used to construct
-        the DEIM approximation of the nonlinear operator ``fom.dh``.
+        the |EmpiricalInterpolatedOperator| approximation of the
+        nonlinear operator ``fom.dh``.
+    Z
+        List or 1D |NumPy array| of the interpolation DOFs.
     QTE_orthonormal
         If `True`, no `E` matrix will be assembled for the reduced |Model|.
         Set to `True` if `V` is orthonormal w.r.t. `fom.Q.H @ fom.E`.
     """
 
-    def __init__(self, fom, V, G, QTE_orthonormal):
+    def __init__(self, fom, V, U, Z, QTE_orthonormal):
         assert isinstance(fom, NonlinearPHModel)
 
         W = fom.Q.apply(V)
 
-        super().__init__(fom, {'V': V, 'W': W, 'G': G})
+        super().__init__(fom, {'V': V, 'W': W, 'U': U})
 
         self.QTE_orthonormal = QTE_orthonormal
+        self.U = U
+        self.Z = Z
 
     def project_operators(self):
         fom = self.fom
         V = self.bases['V']
-        G = self.bases['G']
+        U = self.bases['U']
         W = self.bases['W']
-        J = project(fom.J, W, W)
+        Z = self.Z
 
-        interpolation_dofs, U, _ = deim(G, pod=False)
+        ei_dh = EmpiricalInterpolatedOperator(fom.dh, Z, U, triangular=False)
 
-        emp_grad_h = EmpiricalInterpolatedOperator(fom.dh, interpolation_dofs, U, triangular=False)
-
-        ET_U = U.dofs(interpolation_dofs)
+        ET_U = U.dofs(Z)
         UT_V = U.inner(V)
         C = np.linalg.solve(ET_U.T, UT_V)
 
-        PT_V_numpy = np.zeros((fom.solution_space.dim, len(V)))
-        PT_V_numpy[interpolation_dofs, :] = C
-        PT_V = fom.solution_space.from_numpy(PT_V_numpy)
+        # Calling project(ei_dh, ...) would require instantiating a high-dimensional array for
+        # the projected source basis P^T V, where P = E (E^T U)^{-1} E^T is the DEIM projector
+        # and E is the selection matrix for the interpolation DOFs Z. Instead, we construct
+        # ProjectedEmpiricalInterpolatedOperator directly: since P^T V = E C is row-sparse
+        # (nonzero only at the DEIM DOFs Z), its restriction to source_dofs reduces to selecting
+        # the rows of C whose DOF indices appear in source_dofs, via np.intersect1d, staying
+        # entirely in reduced dimensions.
+        source_dofs = ei_dh.source_dofs
+        source_basis_dofs = np.zeros((len(source_dofs), len(V)))
+        _, z_idx, s_idx = np.intersect1d(Z, source_dofs, return_indices=True)
+        source_basis_dofs[s_idx] = C[z_idx]
+
+        pei_dh = ProjectedEmpiricalInterpolatedOperator(ei_dh.restricted_operator,
+                                                        ei_dh.interpolation_matrix,
+                                                        NumpyVectorSpace.make_array(source_basis_dofs),
+                                                        NumpyVectorSpace.make_array(V.inner(U)),
+                                                        triangular=False)
 
         projected_operators = {'E': None if self.QTE_orthonormal else project(fom.E, W, V),
-                               'J': J,
+                               'J': project(fom.J, W, W),
                                'R': project(fom.R, W, W),
                                'G': project(fom.G, W, None),
-                               'dh': project(emp_grad_h, V, PT_V),
-                               'Q': IdentityOperator(J.source),
+                               'dh': pei_dh,
+                               'Q': IdentityOperator(pei_dh.source),
                                'P': project(fom.P, W, None),
                                'S': fom.S,
                                'N': fom.N}
