c4a9888fcccaf444f231f829dcf8c88562277da6
maxbindhak <bindhak@mpi-magdeburg.mpg.de>
2026-07-13 17:43:54 +0200

    Add QR test helper function to evaluate the result of a QR decomposition algorithm for varying parameters and use it for existing tests of this kind.
    Currently the QR tests for `gram_schmidt` and `shifted_chol_qr` are basically duplicates.
    Furthermore, there are multiple tests for with/without `return_R`, `product` and `recompute_shift` (for `shifted_chol_qr`).
    The helper function is a unification of these tests and provides a single function to perform the test verification.
    The helper function is intended to be used for parameterized tests.
    
    Given a QR decomposition algorithm and input parameters (`copy`, `return_R`, `product` and method specific arguments `qr_kwargs`) the helper function computes the decomposition
    and evaluates the output based on the parameters (correct copy, `R` was returned, correct output datatypes, errors below a threshhold, result deterministic, ...).

--- src/pymortests/algorithms/qr_test_util.py
+++ src/pymortests/algorithms/qr_test_util.py
@@ -1,6 +1,19 @@
+from warnings import warn
+
+import numpy as np
+import pytest
 from scipy.linalg import hilbert
 
+from pymor.algorithms.basic import almost_equal
+from pymor.core.config import is_scipy_mkl
+from pymor.operators.interface import Operator
 from pymor.vectorarrays.interface import VectorArray, VectorSpace
+
+# use lower tolerance for MKL
+if is_scipy_mkl():
+    TOL = {'atol': 1e-12, 'rtol': 1e-13}
+else:
+    TOL = {'atol': 1e-13, 'rtol': 1e-13}
 
 
 def generate_hilbert_va(vector_space_type: VectorSpace, n: int) -> VectorArray:
@@ -12,3 +25,63 @@ def generate_hilbert_va(vector_space_type: VectorSpace, n: int) -> VectorArray:
     but are never rank-deficient in exact arithmetic
     """
     return vector_space_type(n).from_numpy(hilbert(n))
+
+
+def evaluate_qr(qr_method, A: VectorArray, product: Operator, return_R: bool, copy: bool, qr_kwargs: dict={}):
+    r"""Tests a `qr_method` for given parameters.
+
+    This test is to ensure that the arguments `product`, `return_R`, `copy`
+    and potential method specific arguments in `qr_kwargs` are working as intended.
+    """
+    # create copy of input matrix and work with it
+    CPY = A.copy()
+
+    tup = qr_method(A=CPY, product=product, return_R=return_R, copy=copy, **qr_kwargs)
+
+    # check number outputs, their types and dimensions
+    if return_R:
+        assert isinstance(tup, tuple)
+        Q, R = tup
+        assert isinstance(R, np.ndarray)
+        assert len(R.shape) == 2
+        assert R.shape == (len(Q), len(A))
+    else:
+        Q = tup
+    assert isinstance(Q, VectorArray)
+    assert Q.space == A.space
+
+    # check if copies work properly, i.e., if we have a copy A should not change and, vise-versa,
+    # if we do not have a copy A should be overwritten by Q
+    if copy:
+        assert np.all(almost_equal(A, CPY, **TOL)), 'Reference copied from might be overwritten.'
+    else:
+        assert np.all(almost_equal(Q, CPY, **TOL)), 'In-place QR decomposition did not modifiy its input completly.'
+
+    # check if solution has low errors
+    # check loss of orthogonality
+    assert np.allclose(Q.inner(Q, product=product), np.eye(len(Q)), **TOL), 'Too high Loss of Orthogonality'
+    if return_R:
+        # check reconstruction error
+        assert np.all(almost_equal(A, Q.lincomb(R), **TOL)), 'Too high Reconstruction error'
+        # check Cholesky error
+        assert np.allclose(A.inner(A, product=product), (R.conj().T) @ R, **TOL),\
+            'Too high Cholesky error ($A^H A - R^H R$)'
+
+    # check if solution is deterministic
+    Q2, R2 = qr_method(A=A, product=product, return_R=True, copy=False, **qr_kwargs)
+    try:
+        assert np.all(almost_equal(Q, Q2, **TOL))
+        if return_R:
+            assert np.allclose(R, R2, **TOL)
+    except AssertionError:
+        precision = np.get_printoptions()['precision']
+        np.set_printoptions(precision=2)
+
+        L = [f'Q\n{Q.to_numpy()}', f'Q2\n{Q2.to_numpy()}']
+        if return_R:
+            L += [f'R\n{R}', f'R2\n{R2}']
+        warn('\n'.join(L))
+
+        np.set_printoptions(precision=precision)
+
+        pytest.xfail('QR method not deterministic.')
