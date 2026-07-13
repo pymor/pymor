8681f10498a9ae3d7fc0535d8c4f788e21b67b72
maxbindhak <bindhak@mpi-magdeburg.mpg.de>
2026-07-13 20:17:15 +0200

    Add missing license statement to `qr_test_util.py`

--- src/pymortests/algorithms/chol_qr.py
+++ src/pymortests/algorithms/chol_qr.py
@@ -2,7 +2,6 @@
 # Copyright pyMOR developers and contributors. All rights reserved.
 # License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
 
-
 import pytest
 from hypothesis import assume
 from hypothesis.errors import UnsatisfiedAssumption
