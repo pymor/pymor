6701998606f9127182e3226fde6cbb443b89125c
Stephan Rave <stephanrave@uni-muenster.de>
2026-07-23 10:44:48 +0200

    Update src/pymortests/algorithms/qr_test_util.py

--- src/pymortests/algorithms/qr_test_util.py
+++ src/pymortests/algorithms/qr_test_util.py
@@ -25,7 +25,7 @@ else:
 def generate_hilbert_va(vector_space_type: VectorSpace, n: int) -> VectorArray:
     """Generates a Hilbert matrix as a VectorArray of the specified VectorSpace type.
 
-    Creates a in exact arithmetic full-rank, potentially ill-conditioned,
+    Creates an in exact arithmetic full-rank, potentially ill-conditioned,
     square Hilbert matrix of the given dimension `n`.
     Larger hilbert matrices are more ill-conditioned,
     but are never rank-deficient in exact arithmetic
