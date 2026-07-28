9cafe684a3a914fdb853e053c755ef937ab57117
ullmannsven <ullmannsven@gmx.de>
2026-07-28 08:47:13 +0200

    fixe import issues in demo logging

--- src/pymordemos/heat.py
+++ src/pymordemos/heat.py
@@ -307,7 +307,7 @@ def main(
     """
     set_log_levels({
         'pymor.algorithms.gram_schmidt.gram_schmidt': 'WARNING',
-        'pymor.solvers.matrix_equations.lradi.solve_lyap_lrcf': 'WARNING',
+        'pymor.solvers.matrix_equations.lradi.LradiLyapunovSolverLRCF': 'WARNING',
         'pymor.reductors.basic.LTIPGReductor': 'WARNING',
     })
     plt.rcParams['axes.grid'] = True
--- src/pymordemos/parametric_synthetic.py
+++ src/pymordemos/parametric_synthetic.py
@@ -32,7 +32,7 @@ def main(n: int = 100, r: int = 10):
     """
     set_log_levels({
         'pymor.algorithms.gram_schmidt.gram_schmidt': 'WARNING',
-        'pymor.solvers.matrix_equations.lradi.solve_lyap_lrcf': 'WARNING',
+        'pymor.solvers.matrix_equations.lradi.LradiLyapunovSolverLRCF': 'WARNING',
         'pymor.reductors.basic.LTIPGReductor': 'WARNING',
     })
     plt.rcParams['axes.grid'] = True
