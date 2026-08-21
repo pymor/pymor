431199072f0d0a07d66632df9afbd05d0e77bdbd
Stephan Rave <stephanrave@uni-muenster.de>
2026-08-21 18:32:37 +0200

    pca: shorten docstring

--- src/pymor/algorithms/pca.py
+++ src/pymor/algorithms/pca.py
@@ -11,22 +11,11 @@ from pymor.vectorarrays.interface import VectorArray
 
 
 def pca(A, product=None, modes=None, rtol=None, atol=None, l2_err=None,
-        method=None, orth_tol=None,
-        return_reduced_coefficients=False, copy=True):
-    """Principal component analysis (PCA) of `A` using :func:`~pymor.algorithms.pod.pod`.
+        method=None, orth_tol=None, return_reduced_coefficients=False, copy=True):
+    """Principal component analysis (PCA) using :func:`~pymor.algorithms.pod.pod`.
 
-    Viewing the |VectorArray| `A` as an `A.dim` x `len(A)` matrix, the
-    return values of this method are the |VectorArray| of left singular
-    vectors and a |NumPy array| of singular values of the singular value
-    decomposition of `A` centered around the `mean`, where the inner
-    product on R^(`dim(A)`) is given by `product` and the inner product
-    on R^(`len(A)`) is the Euclidean inner product. If desired, also
-    the right singular vectors, which correspond to the reduced
-    coefficients of `A` w.r.t. the left singular vectors and singular
-    values, are returned. To approximately reconstruct the original `A`,
-    add the mean back, e.g.:
-
-        reconstructed = principal_components.lincomb(coeffs) + mean
+    The principal components of `A` are the :func:`~pymor.algorithms.pod.pod`
+    modes of `A` centered around the `mean`.
 
     Parameters
     ----------
@@ -49,11 +38,13 @@ def pca(A, product=None, modes=None, rtol=None, atol=None, l2_err=None,
     return_reduced_coefficients
         See :func:`~pymor.algorithms.pod.pod`.
     copy
-        If `True` (default) create a centered copy of `A`. If `False` subtract the mean
+        If `True` (default) do not modify `A`. If `False` subtract the mean
         from `A` in-place.
 
     Returns
     -------
+    mean
+        |VectorArray| containing the empirical mean of the input `A`.
     principal_components
         |VectorArray| of principal components.
     svals
@@ -61,8 +52,6 @@ def pca(A, product=None, modes=None, rtol=None, atol=None, l2_err=None,
     coeffs
         If `return_reduced_coefficients` is `True`, a |NumPy array|
         of right singular vectors as conjugated rows.
-    mean
-        |VectorArray| containing the empirical mean of the input `A`.
     """
     assert isinstance(A, VectorArray)
     assert product is None or isinstance(product, Operator)
