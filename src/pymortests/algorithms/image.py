f4b188c81e688e5d4dd17fd34bc6d8f33d83c859
Hendrik Kleikamp <hendrik.kleikamp@uni-graz.at>
2026-08-25 16:24:53 +0200

    [tests] added test for image estimation

---
+++ src/pymortests/algorithms/image.py
@@ -0,0 +1,54 @@
+# This file is part of the pyMOR project (https://www.pymor.org).
+# Copyright pyMOR developers and contributors. All rights reserved.
+# License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
+
+import numpy as np
+import pytest
+
+from pymor.algorithms.image import estimate_image
+from pymor.operators.block import BlockColumnOperator, BlockDiagonalOperator, BlockOperator, BlockRowOperator
+from pymor.operators.constructions import LincombOperator
+from pymor.operators.numpy import NumpyMatrixOperator
+from pymor.parameters.functionals import ProjectionParameterFunctional
+
+tol = 1e-12
+
+
+def affine_operator(rng, num_range, num_source):
+    ops = [NumpyMatrixOperator(rng.random((num_range, num_source))) for _ in range(2)]
+    return LincombOperator(ops, [1., ProjectionParameterFunctional('param')])
+
+
+def max_residual(op, image, domain, num_params=5):
+    basis = image.to_numpy()
+    residual = 0.
+    parameters = op.parameters.space(0, 1).sample_randomly(num_params)
+    for mu in parameters:
+        target = op.apply(domain, mu=mu).to_numpy()
+        coeffs = np.linalg.lstsq(basis, target, rcond=None)[0]
+        residual = max(residual, np.linalg.norm(basis @ coeffs - target))
+    return residual
+
+
+BLOCK_OPERATORS = {
+    'block_row': lambda rng: BlockRowOperator([affine_operator(rng, 5, 2), affine_operator(rng, 5, 3)]),
+    'block_column': lambda rng: BlockColumnOperator([affine_operator(rng, 4, 2), affine_operator(rng, 6, 2)]),
+    'block': lambda rng: BlockOperator([[affine_operator(rng, 4, 2), affine_operator(rng, 4, 3)],
+                                        [affine_operator(rng, 6, 2), affine_operator(rng, 6, 3)]]),
+    'block_diagonal': lambda rng: BlockDiagonalOperator([affine_operator(rng, 4, 2), affine_operator(rng, 6, 3)]),
+}
+
+
+def test_estimate_image_affine(rng):
+    op = affine_operator(rng, 6, 3)
+    domain = op.source.random(6)
+    image = estimate_image([op], domain=domain)
+    assert max_residual(op, image, domain) < tol
+
+
+@pytest.mark.parametrize('block_type', list(BLOCK_OPERATORS))
+def test_estimate_image_block_operators(block_type, rng):
+    op = BLOCK_OPERATORS[block_type](rng)
+    domain = op.source.random(6)
+    image = estimate_image([op], domain=domain)
+    assert max_residual(op, image, domain) < tol
