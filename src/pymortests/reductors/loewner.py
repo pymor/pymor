3ebf09e43325ea12790ca1e1364f907bb87e6e9b
Art Pelling <account@artpelling.de>
2026-09-14 17:51:40 +0200

    add tests for realification

--- src/pymortests/reductors/loewner.py
+++ src/pymortests/reductors/loewner.py
@@ -82,6 +82,20 @@ def test_loewner_data(reduce_kwargs, loewner_kwargs, model_args):
     assert rom.order <= model_args[0]
 
 
+def test_loewner_unitary_realification():
+    s = np.array([0, 2, 1j, -1j, 2 + 2j, 2 - 2j])
+    Hs = np.array([sum((i + 1) / (ss + i + 1) for i in range(5)) for ss in s])
+    partitioning = (np.array([0, 2, 3]), np.array([1, 4, 5]))
+
+    complex_quadruple = LoewnerReductor(s, Hs, partitioning=partitioning,
+                                        conjugate=False).loewner_quadruple()
+    real_quadruple = LoewnerReductor(s, Hs, conjugate=True).loewner_quadruple()
+
+    assert all(not np.iscomplexobj(matrix) for matrix in real_quadruple)
+    for complex_matrix, real_matrix in zip(complex_quadruple, real_quadruple, strict=True):
+        assert np.allclose(np.linalg.norm(complex_matrix), np.linalg.norm(real_matrix))
+
+
 def test_loewner_magnitude_ordering_without_conjugates():
     s = 1j * np.arange(1, 5)
     Hs = np.array([4., 1., 3., 2.])
